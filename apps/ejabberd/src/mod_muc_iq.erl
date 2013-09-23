47ed9e251fedf443136038fee199bdd69e1b36d5
Uvarov Michael <freeakk@gmail.com>
2013-09-23 15:53:49 +0400

    Inlined mod_muc_iq:srv_name/0.

--- apps/ejabberd/src/mod_muc_iq.erl
+++ apps/ejabberd/src/mod_muc_iq.erl
@@ -10,6 +10,7 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
+-compile({inline, [srv_name/0]}).
 
 -include("ejabberd.hrl").
 -include("jlib.hrl").
