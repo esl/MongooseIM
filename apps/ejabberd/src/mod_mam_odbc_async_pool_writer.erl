a0bb5b320f676c7f38603a7368597e9269076faa
Michael Uvarov <michael.uvarov@erlang-solutions.com>
2013-12-03 18:05:40 +0100

    Added mod_mam_odbc_async_pool_writer.

---
+++ apps/ejabberd/src/mod_mam_odbc_async_pool_writer.erl
@@ -0,0 +1,207 @@
+%%%-------------------------------------------------------------------
+%%% @author Uvarov Michael <arcusfelis@gmail.com>
+%%% @copyright (C) 2013, Uvarov Michael
+%%% @doc Collect messages and flush them into the database.
+%%% @end
+%%%-------------------------------------------------------------------
+-module(mod_mam_odbc_async_pool_writer).
+%% Backend's callbacks
+-export([start/2,
+         stop/2,
+         start_link/2,
+         archive_message/9,
+         wait_flushing/4]).
+
+%% Helpers
+-export([queue_length/1]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+
+-include("ejabberd.hrl").
+-include("jlib.hrl").
+
+-record(state, {
+    flush_interval=500,
+    max_packet_size=30,
+    host,
+    conn,
+    acc=[],
+    subscribers=[],
+    flush_interval_tref}).
+
+worker_prefix() ->
+    "ejabberd_mod_mam_writer".
+
+worker_count(_Host) ->
+    10.
+
+worker_names(Host) ->
+    [worker_name(Host, N) || N <- lists:seq(0, worker_count(Host) - 1)].
+
+worker_name(Host, N) ->
+    list_to_atom(worker_prefix() ++ "_" ++ binary_to_list(Host) ++ "_" ++ integer_to_list(N)).
+
+select_worker(Host, ArcID) ->
+    N = worker_number(Host, ArcID),
+    worker_name(Host, N).
+
+worker_number(Host, ArcID) ->
+    ArcID rem worker_count(Host).
+
+%%====================================================================
+%% API
+%%====================================================================
+
+start(Host, _Mod) ->
+    [start_worker(WriterProc, Host) ||  WriterProc <- worker_names(Host)].
+
+stop(Host, _Mod) ->
+    [stop_worker(WriterProc) ||  WriterProc <- worker_names(Host)].
+
+start_worker(WriterProc, Host) ->
+    WriterChildSpec =
+    {WriterProc,
+     {mod_mam_odbc_async_pool_writer, start_link, [WriterProc, Host]},
+     permanent,
+     5000,
+     worker,
+     [mod_mam_odbc_async_writer]},
+    supervisor:start_child(ejabberd_sup, WriterChildSpec).
+
+stop_worker(Proc) ->
+    supervisor:terminate_child(ejabberd_sup, Proc),
+    supervisor:delete_child(ejabberd_sup, Proc).
+
+
+start_link(ProcName, Host) ->
+    gen_server:start_link({local, ProcName}, ?MODULE, [Host], []).
+
+archive_message(Host, _Mod,
+        MessID, ArcID, LocJID, RemJID, SrcJID, Dir, Packet) ->
+    Row = mod_mam_odbc_arch:prepare_message(Host,
+        MessID, ArcID, LocJID, RemJID, SrcJID, Dir, Packet),
+    gen_server:cast(select_worker(Host, ArcID), {archive_message, Row}).
+
+%% For folsom.
+queue_length(Host) ->
+    Len = lists:sum([worker_queue_length(SrvName)
+                     || SrvName <- worker_names(Host)]),
+    {ok, Len}.
+
+worker_queue_length(SrvName) ->
+    case whereis(SrvName) of
+    undefined ->
+        0;
+    Pid ->
+        {message_queue_len, Len} = erlang:process_info(Pid, message_queue_len),
+        Len
+    end.
+
+wait_flushing(Host, _Mod, ArcID, _ArcJID) ->
+    gen_server:call(select_worker(Host, ArcID), wait_flushing).
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
+
+run_flush(State=#state{acc=[]}) ->
+    State;
+run_flush(State=#state{conn=Conn, flush_interval_tref=TRef, acc=Acc,
+                       subscribers=Subs}) ->
+    TRef =/= undefined andalso erlang:cancel_timer(TRef),
+    ?DEBUG("Flushed ~p entries.", [length(Acc)]),
+    Result = mod_mam_odbc_arch:archive_messages(Conn, Acc),
+    case Result of
+        {updated, _Count} -> ok;
+        {error, Reason} ->
+            ?ERROR_MSG("archive_message query failed with reason ~p", [Reason]),
+            ok
+    end,
+    [gen_server:reply(Sub, ok) || Sub <- Subs],
+    State#state{acc=[], subscribers=[], flush_interval_tref=undefined}.
+
+%%====================================================================
+%% gen_server callbacks
+%%====================================================================
+
+%%--------------------------------------------------------------------
+%% Function: init(Args) -> {ok, State} |
+%%                         {ok, State, Timeout} |
+%%                         ignore               |
+%%                         {stop, Reason}
+%% Description: Initiates the server
+%%--------------------------------------------------------------------
+init([Host]) ->
+    %% Use a private ODBC-connection.
+    {ok, Conn} = ejabberd_odbc:get_dedicated_connection(Host),
+    {ok, #state{host=Host, conn=Conn}}.
+
+%%--------------------------------------------------------------------
+%% Function: %% handle_call(Request, From, State) -> {reply, Reply, State} |
+%%                                      {reply, Reply, State, Timeout} |
+%%                                      {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, Reply, State} |
+%%                                      {stop, Reason, State}
+%% Description: Handling call messages
+%%--------------------------------------------------------------------
+handle_call(wait_flushing, _From, State=#state{acc=[]}) ->
+    {reply, ok, State};
+handle_call(wait_flushing, From, State=#state{subscribers=Subs}) ->
+    {noreply, State#state{subscribers=[From|Subs]}}.
+
+
+%%--------------------------------------------------------------------
+%% Function: handle_cast(Msg, State) -> {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, State}
+%% Description: Handling cast messages
+%%--------------------------------------------------------------------
+
+handle_cast({archive_message, Row},
+            State=#state{acc=Acc, flush_interval_tref=TRef, flush_interval=Int,
+                         max_packet_size=Max}) ->
+    TRef2 = case {Acc, TRef} of
+            {[], undefined} -> erlang:send_after(Int, self(), flush);
+            {_, _} -> TRef
+            end,
+    State2 = State#state{acc=[Row|Acc], flush_interval_tref=TRef2},
+    case length(Acc) + 1 >= Max of
+        true -> {noreply, run_flush(State2)};
+        false -> {noreply, State2}
+    end;
+handle_cast(Msg, State) ->
+    ?WARNING_MSG("Strange message ~p.", [Msg]),
+    {noreply, State}.
+
+
+%%--------------------------------------------------------------------
+%% Function: handle_info(Info, State) -> {noreply, State} |
+%%                                       {noreply, State, Timeout} |
+%%                                       {stop, Reason, State}
+%% Description: Handling all non call/cast messages
+%%--------------------------------------------------------------------
+
+handle_info(flush, State) ->
+    {noreply, run_flush(State)}.
+
+%%--------------------------------------------------------------------
+%% Function: terminate(Reason, State) -> void()
+%% Description: This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any necessary
+%% cleaning up. When it returns, the gen_server terminates with Reason.
+%% The return value is ignored.
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% Func: code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% Description: Convert process state when code is changed
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
