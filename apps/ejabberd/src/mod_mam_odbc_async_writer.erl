6a6fe0c08d5e8d6fe56b1b1bf049d49c68e91c8b
Michael Uvarov <michael.uvarov@erlang-solutions.com>
2014-01-07 23:52:41 +0100

    Converted mod_mam_odbc_async_writer.

--- apps/ejabberd/src/mod_mam_odbc_async_writer.erl
+++ apps/ejabberd/src/mod_mam_odbc_async_writer.erl
@@ -5,16 +5,27 @@
 %%% @end
 %%%-------------------------------------------------------------------
 -module(mod_mam_odbc_async_writer).
-%% Backend's callbacks
--export([start/2,
-         stop/2,
-         start_link/2,
-         srv_name/1,
-         archive_message/9,
-         wait_flushing/4]).
 
-%% Helpers
--export([queue_length/1]).
+%% ----------------------------------------------------------------------
+%% Exports
+
+%% gen_mod handlers
+-export([start/2, stop/1]).
+
+%% MAM hook handlers
+-export([archive_size/4,
+         archive_message/9,
+         lookup_messages/13,
+         remove_archive/3,
+         purge_single_message/6,
+         purge_multiple_messages/9]).
+
+%% Helpers for debugging
+-export([queue_length/1,
+         wait_flushing/1]).
+
+%% Internal exports
+-export([start_link/2]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
@@ -37,34 +48,94 @@
 srv_name() ->
     ejabberd_mod_mam_writer.
 
+
+
+%% ----------------------------------------------------------------------
+%% gen_mod callbacks
+%% Starting and stopping functions for users' archives
+
+start(Host, Opts) ->
+    start_server(Host),
+    case gen_mod:get_module_opt(Host, ?MODULE, pm, false) of
+        true ->
+            start_pm(Host, Opts);
+        false ->
+            ok
+    end,
+    case gen_mod:get_module_opt(Host, ?MODULE, muc, false) of
+        true ->
+            start_muc(Host, Opts);
+        false ->
+            ok
+    end.
+
+stop(Host) ->
+    stop_server(Host),
+    case gen_mod:get_module_opt(Host, ?MODULE, pm, false) of
+        true ->
+            stop_pm(Host);
+        false ->
+            ok
+    end,
+    case gen_mod:get_module_opt(Host, ?MODULE, muc, false) of
+        true ->
+            stop_muc(Host);
+        false ->
+            ok
+    end.
+
+%% ----------------------------------------------------------------------
+%% Add hooks for mod_mam
+
+start_pm(Host, _Opts) ->
+    ejabberd_hooks:add(mam_archive_message, Host, ?MODULE, archive_message, 50),
+    ejabberd_hooks:add(mam_archive_size, Host, ?MODULE, archive_size, 30),
+    ejabberd_hooks:add(mam_lookup_messages, Host, ?MODULE, lookup_messages, 30),
+    ejabberd_hooks:add(mam_remove_archive, Host, ?MODULE, remove_archive, 100),
+    ejabberd_hooks:add(mam_purge_single_message, Host, ?MODULE, purge_single_message, 30),
+    ejabberd_hooks:add(mam_purge_multiple_messages, Host, ?MODULE, purge_multiple_messages, 30),
+    ok.
+
+stop_pm(Host) ->
+    ejabberd_hooks:delete(mam_archive_message, Host, ?MODULE, archive_message, 50),
+    ejabberd_hooks:delete(mam_archive_size, Host, ?MODULE, archive_size, 30),
+    ejabberd_hooks:delete(mam_lookup_messages, Host, ?MODULE, lookup_messages, 30),
+    ejabberd_hooks:delete(mam_remove_archive, Host, ?MODULE, remove_archive, 30),
+    ejabberd_hooks:delete(mam_purge_single_message, Host, ?MODULE, purge_single_message, 30),
+    ejabberd_hooks:delete(mam_purge_multiple_messages, Host, ?MODULE, purge_multiple_messages, 30),
+    ok.
+
+
+%% ----------------------------------------------------------------------
+%% Add hooks for mod_mam_muc
+
+start_muc(Host, _Opts) ->
+    ejabberd_hooks:add(mam_muc_archive_message, Host, ?MODULE, archive_message, 50),
+    ejabberd_hooks:add(mam_muc_archive_size, Host, ?MODULE, archive_size, 30),
+    ejabberd_hooks:add(mam_muc_lookup_messages, Host, ?MODULE, lookup_messages, 30),
+    ejabberd_hooks:add(mam_muc_remove_archive, Host, ?MODULE, remove_archive, 30),
+    ejabberd_hooks:add(mam_muc_purge_single_message, Host, ?MODULE, purge_single_message, 30),
+    ejabberd_hooks:add(mam_muc_purge_multiple_messages, Host, ?MODULE, purge_multiple_messages, 30),
+    ok.
+
+stop_muc(Host) ->
+    ejabberd_hooks:delete(mam_muc_archive_message, Host, ?MODULE, archive_message, 50),
+    ejabberd_hooks:delete(mam_muc_archive_size, Host, ?MODULE, archive_size, 30),
+    ejabberd_hooks:delete(mam_muc_lookup_messages, Host, ?MODULE, lookup_messages, 30),
+    ejabberd_hooks:delete(mam_muc_remove_archive, Host, ?MODULE, remove_archive, 100),
+    ejabberd_hooks:delete(mam_muc_purge_single_message, Host, ?MODULE, purge_single_message, 30),
+    ejabberd_hooks:delete(mam_muc_purge_multiple_messages, Host, ?MODULE, purge_multiple_messages, 30),
+    ok.
+
+
 %%====================================================================
 %% API
 %%====================================================================
 
-start(Host, _Mod) ->
-    WriterProc = srv_name(Host),
-    WriterChildSpec =
-    {WriterProc,
-     {mod_mam_odbc_async_writer, start_link, [WriterProc, Host]},
-     permanent,
-     5000,
-     worker,
-     [mod_mam_odbc_async_writer]},
-    supervisor:start_child(ejabberd_sup, WriterChildSpec).
-
-stop(Host, _Mod) ->
-    Proc = srv_name(Host),
-    supervisor:terminate_child(ejabberd_sup, Proc),
-    supervisor:delete_child(ejabberd_sup, Proc).
-
-
 start_link(ProcName, Host) ->
     gen_server:start_link({local, ProcName}, ?MODULE, [Host], []).
 
-srv_name(Host) ->
-    gen_mod:get_module_proc(Host, srv_name()).
-
-archive_message(Host, _Mod,
+archive_message(_Result, Host,
         MessID, UserID, LocJID, RemJID, SrcJID, Dir, Packet) ->
     Row = mod_mam_odbc_arch:prepare_message(Host,
         MessID, UserID, LocJID, RemJID, SrcJID, Dir, Packet),
@@ -80,12 +151,73 @@ queue_length(Host) ->
         {ok, Len}
     end.
 
-wait_flushing(Host, _Mod, _ArcID, _ArcJID) ->
+archive_size(Size, Host, _UserID, _UserJID) when is_integer(Size) ->
+    wait_flushing(Host),
+    Size.
+
+lookup_messages(Result, Host, _UserID, _UserJID,
+                _RSM, _Borders,
+                _Start, End, Now, _WithJID,
+                _PageSize, _LimitPassed, _MaxResultLimit) ->
+    wait_flushing_before(Host, End, Now),
+    Result.
+
+remove_archive(Host, _UserID, _UserJID) ->
+    wait_flushing(Host),
+    ok.
+
+purge_single_message(Result, Host, MessID, _UserID, _UserJID, Now) ->
+    {Microseconds, _NodeMessID} = mod_mam_utils:decode_compact_uuid(MessID),
+    wait_flushing_before(Host, Microseconds, Now),
+    Result.
+
+purge_multiple_messages(Result, Host, _UserID, _UserJID, _Borders,
+                        _Start, End, Now, _WithJID) ->
+    wait_flushing_before(Host, End, Now),
+    Result.
+
+wait_flushing(Host) ->
     gen_server:call(srv_name(Host), wait_flushing).
+
+wait_flushing_before(Host, End, Now) ->
+    case is_recent_entries_required(End, Now) of
+        true ->
+            wait_flushing(Host);
+        false ->
+            ok
+    end.
+
+%% @doc Returns true, if `End' is too old.
+is_recent_entries_required(End, Now) when is_integer(End) ->
+    %% If `End' is older than 10 seconds?
+    End + 10000000 < Now;
+is_recent_entries_required(_End, _Now) ->
+    true.
+
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+start_server(Host) ->
+    WriterProc = srv_name(Host),
+    supervisor:start_child(ejabberd_sup, writer_child_spec(WriterProc, Host)).
+
+stop_server(Host) ->
+    Proc = srv_name(Host),
+    supervisor:terminate_child(ejabberd_sup, Proc),
+    supervisor:delete_child(ejabberd_sup, Proc).
+
+srv_name(Host) ->
+    gen_mod:get_module_proc(Host, srv_name()).
+
+writer_child_spec(WriterProc, Host) ->
+    {WriterProc,
+     {mod_mam_odbc_async_writer, start_link, [WriterProc, Host]},
+     permanent,
+     5000,
+     worker,
+     [mod_mam_odbc_async_writer]}.
 
 run_flush(State=#state{acc=[]}) ->
     State;
