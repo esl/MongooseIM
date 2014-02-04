c28fbcf462a7bc506778d78176e672309f545bd8
Michael Uvarov <michael.uvarov@erlang-solutions.com>
2014-02-04 15:32:33 +0100

    Moved backend-specific functions from mod_offline into mod_offline_mnesia.

--- apps/ejabberd/src/mod_offline.erl
+++ apps/ejabberd/src/mod_offline.erl
@@ -30,34 +30,43 @@
 -behaviour(gen_mod).
 
 -export([start/2,
-	 loop/1,
 	 stop/1,
+	 loop/1,
 	 inspect_packet/3,
 	 resend_offline_messages/2,
 	 pop_offline_messages/3,
 	 get_sm_features/5,
 	 remove_expired_messages/0,
 	 remove_old_messages/1,
-	 remove_user/2,
-	 get_queue_length/2]).
+	 remove_user/2]).
 
 -include("ejabberd.hrl").
 -include("jlib.hrl").
-
--record(offline_msg, {us, timestamp, expire, from, to, packet}).
+-include("mod_offline.hrl").
 
 -define(PROCNAME, ejabberd_offline).
--define(OFFLINE_TABLE_LOCK_THRESHOLD, 1000).
 
 %% default value for the maximum number of user messages
 -define(MAX_USER_MESSAGES, infinity).
+-define(BACKEND, (mod_offline_backend:backend())).
+
+%% ------------------------------------------------------------------
+%% Backend callbacks
+
+-callback init(Host, Opts) -> ok when
+    Host :: binary(),
+    Opts :: list().
+
+-callback remove_user(LUser, LServer) -> ok when
+    LUser :: binary(),
+    LServer :: binary().
+
+%% gen_mod callbacks
+%% ------------------------------------------------------------------
 
 start(Host, Opts) ->
-    mnesia:create_table(offline_msg,
-			[{disc_only_copies, [node()]},
-			 {type, bag},
-			 {attributes, record_info(fields, offline_msg)}]),
-    update_table(),
+    start_backend_module(Opts),
+    ?BACKEND:init(Host, Opts),
     ejabberd_hooks:add(offline_message_hook, Host,
 		       ?MODULE, inspect_packet, 50),
     ejabberd_hooks:add(resend_offline_messages_hook, Host,
@@ -74,42 +83,62 @@ start(Host, Opts) ->
     register(gen_mod:get_module_proc(Host, ?PROCNAME),
 	     spawn(?MODULE, loop, [AccessMaxOfflineMsgs])).
 
+stop(Host) ->
+    ejabberd_hooks:delete(offline_message_hook, Host,
+			  ?MODULE, inspect_packet, 50),
+    ejabberd_hooks:delete(resend_offline_messages_hook, Host,
+			  ?MODULE, pop_offline_messages, 50),
+    ejabberd_hooks:delete(remove_user, Host,
+			  ?MODULE, remove_user, 50),
+    ejabberd_hooks:delete(anonymous_purge_hook, Host,
+			  ?MODULE, remove_user, 50),
+    ejabberd_hooks:delete(disco_sm_features, Host, ?MODULE, get_sm_features, 50),
+    ejabberd_hooks:delete(disco_local_features, Host, ?MODULE, get_sm_features, 50),
+    Proc = gen_mod:get_module_proc(Host, ?PROCNAME),
+    exit(whereis(Proc), stop),
+    {wait, Proc}.
+
+
+%% Dynamic modules
+%% ------------------------------------------------------------------
+
+start_backend_module(Opts) ->
+    Backend = gen_mod:get_opt(backend, Opts, mnesia),
+    {Mod, Code} = dynamic_compile:from_string(mod_offline_backend(Backend)),
+    code:load_binary(Mod, "mod_offline_backend.erl", Code).
+
+-spec mod_offline_backend(atom()) -> string().
+mod_offline_backend(Backend) when is_atom(Backend) ->
+    lists:flatten(
+      ["-module(mod_offline_backend).
+        -export([backend/0]).
+        -spec backend() -> atom().
+        backend() ->
+        mod_offline_",
+                   atom_to_list(Backend),
+                   ".\n"]).
+
+%% Server process loop
+%% ------------------------------------------------------------------
+
 loop(AccessMaxOfflineMsgs) ->
     receive
-	#offline_msg{us=US} = Msg ->
-	    Msgs = receive_all(US, [Msg]),
-	    Len = length(Msgs),
-	    {User, Host} = US,
-	    MaxOfflineMsgs = get_max_user_messages(AccessMaxOfflineMsgs,
-						   User, Host),
-	    F = fun() ->
-			%% Only count messages if needed:
-			Count = if MaxOfflineMsgs =/= infinity ->
-					Len + p1_mnesia:count_records(
-						offline_msg,
-						#offline_msg{us=US, _='_'});
-				   true ->
-					0
-				end,
-			if
-			    Count > MaxOfflineMsgs ->
-				discard_warn_sender(Msgs);
-			    true ->
-				if
-				    Len >= ?OFFLINE_TABLE_LOCK_THRESHOLD ->
-					mnesia:write_lock_table(offline_msg);
-				    true ->
-					ok
-				end,
-				lists:foreach(fun(M) ->
-						      mnesia:write(M)
-					      end, Msgs)
-			end
-		end,
-	    mnesia:transaction(F),
-	    loop(AccessMaxOfflineMsgs);
-	_ ->
-	    loop(AccessMaxOfflineMsgs)
+        #offline_msg{us=US} = Msg ->
+            {LUser, LServer} = US,
+            Msgs = receive_all(US, [Msg]),
+            MaxOfflineMsgs = get_max_user_messages(
+                AccessMaxOfflineMsgs, LUser, LServer),
+            case ?BACKEND:write_messages(LUser, LServer, Msgs, MaxOfflineMsgs) of
+                ok ->
+                    ok;
+                {discarded, DiscardedMsgs} ->
+                    discard_warn_sender(DiscardedMsgs);
+                {error, _Reason} ->
+                    discard_warn_sender(Msgs)
+            end,
+            loop(AccessMaxOfflineMsgs);
+        _ ->
+            loop(AccessMaxOfflineMsgs)
     end.
 
 %% Function copied from ejabberd_sm.erl:
@@ -128,21 +157,8 @@ receive_all(US, Msgs) ->
 	    Msgs
     end.
 
-
-stop(Host) ->
-    ejabberd_hooks:delete(offline_message_hook, Host,
-			  ?MODULE, inspect_packet, 50),
-    ejabberd_hooks:delete(resend_offline_messages_hook, Host,
-			  ?MODULE, pop_offline_messages, 50),
-    ejabberd_hooks:delete(remove_user, Host,
-			  ?MODULE, remove_user, 50),
-    ejabberd_hooks:delete(anonymous_purge_hook, Host,
-			  ?MODULE, remove_user, 50),
-    ejabberd_hooks:delete(disco_sm_features, Host, ?MODULE, get_sm_features, 50),
-    ejabberd_hooks:delete(disco_local_features, Host, ?MODULE, get_sm_features, 50),
-    Proc = gen_mod:get_module_proc(Host, ?PROCNAME),
-    exit(whereis(Proc), stop),
-    {wait, Proc}.
+%% Handlers
+%% ------------------------------------------------------------------
 
 get_sm_features(Acc, _From, _To, <<"">> = _Node, _Lang) ->
     add_feature(Acc, ?NS_FEATURE_MSGOFFLINE);
@@ -218,23 +234,29 @@ check_event_chatstates(From, To, Packet) ->
 			false ->
 			    true;
 			_ ->
-			    ID = case xml:get_tag_attr_s(<<"id">>, Packet) of
-				     <<"">> ->
-					 #xmlel{name = <<"id">>};
-				     S ->
-					 #xmlel{name = <<"id">>,
-					        children = [#xmlcdata{content = S}]}
-				 end,
-			    ejabberd_router:route(
-			      To, From, Packet#xmlel{children = [#xmlel{name = <<"x">>,
-					                                attrs = [{<<"xmlns">>, ?NS_EVENT}],
-					                                children = [ID, #xmlel{name = <<"offline">>}]}]}),
+                ejabberd_router:route(To, From, patch_offline_message(Packet)),
 			    true
 		    end;
 		_ ->
 		    false
 	    end
     end.
+
+patch_offline_message(Packet) ->
+    ID = case xml:get_tag_attr_s(<<"id">>, Packet) of
+         <<"">> ->
+         #xmlel{name = <<"id">>};
+         S ->
+         #xmlel{name = <<"id">>,
+                children = [#xmlcdata{content = S}]}
+     end,
+    Packet#xmlel{children = [x_elem(ID)]}.
+
+x_elem(ID) ->
+    #xmlel{
+        name = <<"x">>,
+        attrs = [{<<"xmlns">>, ?NS_EVENT}],
+        children = [ID, #xmlel{name = <<"offline">>}]}.
 
 %% Check if the packet has subelements about XEP-0022, XEP-0085 or other
 find_x_event_chatstates([], Res) ->
@@ -275,228 +297,69 @@ find_x_expire(TimeStamp, [El | Els]) ->
 	    find_x_expire(TimeStamp, Els)
     end.
 
+pop_offline_messages(Ls, User, Server) ->
+    Ls ++ pop_offline_messages(User, Server).
+
+pop_offline_messages(User, Server) ->
+    LUser = jlib:nodeprep(User),
+    LServer = jlib:nameprep(Server),
+    case ?BACKEND:pop_messages(LUser, LServer) of
+        {ok, Rs} ->
+            lists:map(fun(R) ->
+                Packet = resend_offline_message_packet(Server, R),
+                compose_offline_message(R, Packet)
+              end, Rs);
+        {error, _Reason} ->
+            []
+    end.
 
 resend_offline_messages(User, Server) ->
     LUser = jlib:nodeprep(User),
     LServer = jlib:nameprep(Server),
-    US = {LUser, LServer},
-    F = fun() ->
-		Rs = mnesia:wread({offline_msg, US}),
-		mnesia:delete({offline_msg, US}),
-		Rs
-	end,
-    case mnesia:transaction(F) of
-	{atomic, Rs} ->
-	    lists:foreach(
-	      fun(R) ->
-		      XE = #xmlel{children = Els} = R#offline_msg.packet,
-		      ejabberd_sm !
-			  {route,
-			   R#offline_msg.from,
-			   R#offline_msg.to,
-			   XE#xmlel{children = Els ++
-			                         [jlib:timestamp_to_xml(calendar:now_to_universal_time(R#offline_msg.timestamp),
-			                                                utc,
-			                                                jlib:make_jid(<<>>,
-				                                                      Server,
-			                                                              <<>>),
-                                                                        <<"Offline Storage">>),%% TODO: Delete the next three lines once XEP-0091 is Obsolete
-			                                                                   jlib:timestamp_to_xml(calendar:now_to_universal_time(R#offline_msg.timestamp))]}}
-	      end,
-	      lists:keysort(#offline_msg.timestamp, Rs));
-	_ ->
-	    ok
+    case ?BACKEND:pop_messages(LUser, LServer) of
+        {ok, Rs} ->
+            lists:foreach(fun(R) ->
+                  Packet = resend_offline_message_packet(Server, R),
+                  route_offline_message(R, Packet)
+              end, Rs);
+        {error, _Reason} ->
+            ok
     end.
 
-pop_offline_messages(Ls, User, Server) ->
-    LUser = jlib:nodeprep(User),
-    LServer = jlib:nameprep(Server),
-    US = {LUser, LServer},
-    F = fun() ->
-		Rs = mnesia:wread({offline_msg, US}),
-		mnesia:delete({offline_msg, US}),
-		Rs
-	end,
-    case mnesia:transaction(F) of
-	{atomic, Rs} ->
-	    TS = now(),
-	    Ls ++ lists:map(
-		    fun(R) ->
-			    XE = #xmlel{children = Els} = R#offline_msg.packet,
-			    {route,
-			     R#offline_msg.from,
-			     R#offline_msg.to,
-			     XE#xmlel{children = Els ++
-			                           [jlib:timestamp_to_xml(calendar:now_to_universal_time(R#offline_msg.timestamp),
-			                                                  utc,
-			                                                  jlib:make_jid(<<>>,
-				                                                        Server,
-				                                                        <<>>),
-				                                          <<"Offline Storage">>),%% TODO: Delete the next three lines once XEP-0091 is Obsolete
-				                                                             jlib:timestamp_to_xml(calendar:now_to_universal_time(R#offline_msg.timestamp))]}}
-		    end,
-		    lists:filter(
-		      fun(R) ->
-			    case R#offline_msg.expire of
-				never ->
-				    true;
-				TimeStamp ->
-				    TS < TimeStamp
-			    end
-		      end,
-		      lists:keysort(#offline_msg.timestamp, Rs)));
-	_ ->
-	    Ls
-    end.
+route_offline_message(#offline_msg{from=From, to=To}, Packet) ->
+    ejabberd_sm:route(From, To, Packet).
 
+compose_offline_message(#offline_msg{from=From, to=To}, Packet) ->
+    {route, From, To, Packet}.
+
+resend_offline_message_packet(Server,
+        #offline_msg{timestamp=TimeStamp, packet = Packet}) ->
+    Time = calendar:now_to_universal_time(TimeStamp),
+    %% TODO: Delete the next element once XEP-0091 is Obsolete
+    TimeStampLegacyXML = timestamp_legacy_xml(Server, Time),
+    TimeStampXML = jlib:timestamp_to_xml(Time),
+    xml:append_subtags(Packet, [TimeStampLegacyXML, TimeStampXML]).
+
+timestamp_legacy_xml(Server, Time) ->
+    FromJID = jlib:make_jid(<<>>, Server, <<>>),
+    jlib:timestamp_to_xml(Time, utc, FromJID, <<"Offline Storage">>).
 
 remove_expired_messages() ->
-    TimeStamp = now(),
-    F = fun() ->
-		mnesia:write_lock_table(offline_msg),
-		mnesia:foldl(
-		  fun(Rec, _Acc) ->
-			  case Rec#offline_msg.expire of
-			      never ->
-				  ok;
-			      TS ->
-				  if
-				      TS < TimeStamp ->
-					  mnesia:delete_object(Rec);
-				      true ->
-					  ok
-				  end
-			  end
-		  end, ok, offline_msg)
-	end,
-    mnesia:transaction(F).
+    ?BACKEND:remove_expired_messages().
 
 remove_old_messages(Days) ->
-    {MegaSecs, Secs, _MicroSecs} = now(),
-    S = MegaSecs * 1000000 + Secs - 60 * 60 * 24 * Days,
-    MegaSecs1 = S div 1000000,
-    Secs1 = S rem 1000000,
-    TimeStamp = {MegaSecs1, Secs1, 0},
-    F = fun() ->
-		mnesia:write_lock_table(offline_msg),
-		mnesia:foldl(
-		  fun(#offline_msg{timestamp = TS} = Rec, _Acc)
-		     when TS < TimeStamp ->
-			  mnesia:delete_object(Rec);
-		     (_Rec, _Acc) -> ok
-		  end, ok, offline_msg)
-	end,
-    mnesia:transaction(F).
+    ?BACKEND:remove_expired_messages(Days).
 
 remove_user(User, Server) ->
-    LUser = jlib:nodeprep(User),
-    LServer = jlib:nameprep(Server),
-    US = {LUser, LServer},
-    F = fun() ->
-		mnesia:delete({offline_msg, US})
-	end,
-    mnesia:transaction(F).
-
-update_table() ->
-    Fields = record_info(fields, offline_msg),
-    case mnesia:table_info(offline_msg, attributes) of
-	Fields ->
-	    ok;
-	[user, timestamp, expire, from, to, packet] ->
-	    ?INFO_MSG("Converting offline_msg table from "
-		      "{user, timestamp, expire, from, to, packet} format", []),
-	    Host = ?MYNAME,
-	    {atomic, ok} = mnesia:create_table(
-			     mod_offline_tmp_table,
-			     [{disc_only_copies, [node()]},
-			      {type, bag},
-			      {local_content, true},
-			      {record_name, offline_msg},
-			      {attributes, record_info(fields, offline_msg)}]),
-	    mnesia:transform_table(offline_msg, ignore, Fields),
-	    F1 = fun() ->
-			 mnesia:write_lock_table(mod_offline_tmp_table),
-			 mnesia:foldl(
-			   fun(#offline_msg{us = U} = R, _) ->
-				   mnesia:dirty_write(
-				     mod_offline_tmp_table,
-				     R#offline_msg{us = {U, Host}})
-			   end, ok, offline_msg)
-		 end,
-	    mnesia:transaction(F1),
-	    mnesia:clear_table(offline_msg),
-	    F2 = fun() ->
-			 mnesia:write_lock_table(offline_msg),
-			 mnesia:foldl(
-			   fun(R, _) ->
-				   mnesia:dirty_write(R)
-			   end, ok, mod_offline_tmp_table)
-		 end,
-	    mnesia:transaction(F2),
-	    mnesia:delete_table(mod_offline_tmp_table);
-	[user, timestamp, from, to, packet] ->
-	    ?INFO_MSG("Converting offline_msg table from "
-		      "{user, timestamp, from, to, packet} format", []),
-	    Host = ?MYNAME,
-	    {atomic, ok} = mnesia:create_table(
-			     mod_offline_tmp_table,
-			     [{disc_only_copies, [node()]},
-			      {type, bag},
-			      {local_content, true},
-			      {record_name, offline_msg},
-			      {attributes, record_info(fields, offline_msg)}]),
-	    mnesia:transform_table(
-	      offline_msg,
-	      fun({_, U, TS, F, T, P}) ->
-		      #xmlel{children = Els} = P,
-		      Expire = find_x_expire(TS, Els),
-		      #offline_msg{us = U,
-				   timestamp = TS,
-				   expire = Expire,
-				   from = F,
-				   to = T,
-				   packet = P}
-	      end, Fields),
-	    F1 = fun() ->
-			 mnesia:write_lock_table(mod_offline_tmp_table),
-			 mnesia:foldl(
-			   fun(#offline_msg{us = U} = R, _) ->
-				   mnesia:dirty_write(
-				     mod_offline_tmp_table,
-				     R#offline_msg{us = {U, Host}})
-			   end, ok, offline_msg)
-		 end,
-	    mnesia:transaction(F1),
-	    mnesia:clear_table(offline_msg),
-	    F2 = fun() ->
-			 mnesia:write_lock_table(offline_msg),
-			 mnesia:foldl(
-			   fun(R, _) ->
-				   mnesia:dirty_write(R)
-			   end, ok, mod_offline_tmp_table)
-		 end,
-	    mnesia:transaction(F2),
-	    mnesia:delete_table(mod_offline_tmp_table);
-	_ ->
-	    ?INFO_MSG("Recreating offline_msg table", []),
-	    mnesia:transform_table(offline_msg, ignore, Fields)
-    end.
-
-
-%% Helper functions:
+    ?BACKEND:remove_user(User, Server).
 
 %% Warn senders that their messages have been discarded:
 discard_warn_sender(Msgs) ->
     lists:foreach(
       fun(#offline_msg{from=From, to=To, packet=Packet}) ->
-	      ErrText = "Your contact offline message queue is full. The message has been discarded.",
-	      Lang = xml:get_tag_attr_s("xml:lang", Packet),
+	      ErrText = <<"Your contact offline message queue is full. The message has been discarded.">>,
+	      Lang = xml:get_tag_attr_s(<<"xml:lang">>, Packet),
 	      Err = jlib:make_error_reply(
 		      Packet, ?ERRT_RESOURCE_CONSTRAINT(Lang, ErrText)),
-	      ejabberd_router:route(
-		To,
-		From, Err)
+	      ejabberd_router:route(To, From, Err)
       end, Msgs).
-
-get_queue_length(User, Server) ->
-    length(mnesia:dirty_read({offline_msg, {User, Server}})).
