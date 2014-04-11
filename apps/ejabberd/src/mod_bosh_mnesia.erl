6dd8cec6eb1fb12cda7f1e4a5c2f4661b935a06f
Radosław Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2014-04-11 11:14:57 +0200

    Describe the usage of transaction/dirty ops in mod_bosh_mnesia

--- apps/ejabberd/src/mod_bosh_mnesia.erl
+++ apps/ejabberd/src/mod_bosh_mnesia.erl
@@ -17,6 +17,22 @@ start(_Opts) ->
                          {attributes, record_info(fields, bosh_session)}]),
     mnesia:add_table_copy(bosh_session, node(), ram_copies).
 
+%% The choice of the operation context here (transaction vs dirty,
+%% see man on mnesia:activity/4 for description of contexts) and the deletion
+%% in delete_session/1 below depends on the availability of a load balancer
+%% capable of doing server/session affiliation.
+%%
+%% With affiliation, it suffices for this write to be synchronous, since the
+%% client can issue no subsequent request without a session ID and the ID is
+%% returned to the client only after the dirty synchronous write returns.
+%% Other nodes in the cluster eventually will have the current view of the
+%% database, possibly (significantly) later than the write returns. However,
+%% the only node serving the client in question always operates on valid data.
+%%
+%% Without affiliation, each BOSH/HTTP request may be handled by a different
+%% node in the cluster. Hence, we must guarantee that once the write
+%% operation returns, all nodes in the cluster will have access to currently
+%% valid data -- that's why a transaction is used instead of a dirty write.
 create_session(#bosh_session{} = Session) ->
     mnesia:sync_transaction(fun mnesia:write/1, [Session]).
 
