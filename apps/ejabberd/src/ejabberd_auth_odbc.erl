2f1f2795f6b2db30444f78338f00b1c8cd900fa0
Pawel Pikula <pawel.pikula@erlang-solutions.com>
2014-06-20 13:10:14 +0200

    add stop function to auth modules

--- apps/ejabberd/src/ejabberd_auth_odbc.erl
+++ apps/ejabberd/src/ejabberd_auth_odbc.erl
@@ -30,23 +30,24 @@
 %% External exports
 -behaviour(ejabberd_gen_auth).
 -export([start/1,
-	 set_password/3,
-	 check_password/3,
-	 check_password/5,
-	 try_register/3,
-	 dirty_get_registered_users/0,
-	 get_vh_registered_users/1,
-	 get_vh_registered_users/2,
-	 get_vh_registered_users_number/1,
-	 get_vh_registered_users_number/2,
-	 get_password/2,
-	 get_password_s/2,
-	 is_user_exists/2,
-	 remove_user/2,
-	 remove_user/3,
-	 store_type/1,
-	 plain_password_required/0
-	]).
+         stop/1,
+         set_password/3,
+         check_password/3,
+         check_password/5,
+         try_register/3,
+         dirty_get_registered_users/0,
+         get_vh_registered_users/1,
+         get_vh_registered_users/2,
+         get_vh_registered_users_number/1,
+         get_vh_registered_users_number/2,
+         get_password/2,
+         get_password_s/2,
+         is_user_exists/2,
+         remove_user/2,
+         remove_user/3,
+         store_type/1,
+         plain_password_required/0
+        ]).
 
 -export([login/2, get_password/3]).
 
@@ -62,6 +63,9 @@
 %%%----------------------------------------------------------------------
 
 start(_Host) ->
+    ok.
+
+stop(_Host) ->
     ok.
 
 plain_password_required() ->
