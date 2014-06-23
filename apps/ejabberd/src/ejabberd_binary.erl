ad7ead7d9fd971d9328d79d80570b4e9cc251bb8
Michal Piotrowski <michal.piotrowski@erlang-solutions.com>
2014-06-23 12:30:24 +0200

    clean mod_register opts before starting the module
    fixes #117

--- apps/ejabberd/src/ejabberd_binary.erl
+++ apps/ejabberd/src/ejabberd_binary.erl
@@ -1,5 +1,7 @@
 -module(ejabberd_binary).
 
+-export([string_to_binary/1]).
+
 -ifdef(no_binary_to_integer).
 
 -export([binary_to_integer/1,
@@ -12,3 +14,9 @@ integer_to_binary(I) ->
     catch list_to_binary(integer_to_list(I)).
 
 -endif.
+
+-spec string_to_binary(binary() | list()) -> binary().
+string_to_binary(S) when is_list(S) ->
+    list_to_binary(S);
+string_to_binary(B) when is_binary(B) ->
+    B.
