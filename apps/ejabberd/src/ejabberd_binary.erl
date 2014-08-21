7ebf7235e9e6b81a1b79aa069457888414a40f92
Piotr Nosek <piotr.nosek@erlang-solutions.com>
2014-08-21 15:13:45 +0200

    Fix Unicode handling in mod_admin_extra_roster, fixes #266

--- apps/ejabberd/src/ejabberd_binary.erl
+++ apps/ejabberd/src/ejabberd_binary.erl
@@ -17,6 +17,14 @@ integer_to_binary(I) ->
 
 -spec string_to_binary(binary() | list()) -> binary().
 string_to_binary(S) when is_list(S) ->
-    list_to_binary(S);
+    % If list is in Erlang representation of Unicode, we must use `unicode` module
+    % If it's not or is already converted, we must use list_to_binary
+    % since input can be from `file:consult/1` and prior to 17.0
+    % this function returned bytes in a list instead of proper unicode string
+    % so it is already like after a call to `unicode`.
+    case lists:any(fun(C) -> C > 255 end, S) of
+        true -> unicode:characters_to_binary(S);
+        false -> list_to_binary(S)
+    end;
 string_to_binary(B) when is_binary(B) ->
     B.
