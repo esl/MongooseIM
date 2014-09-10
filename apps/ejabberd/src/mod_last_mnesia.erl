93c7d48a6392f54285c025a089a56d058f06253a
Michal Piotrowski <michal.piotrowski@erlang-solutions.com>
2014-09-10 15:04:23 +0200

    unify mod_last logic #123
    with mnesia and odbc backends

---
+++ apps/ejabberd/src/mod_last_mnesia.erl
@@ -0,0 +1,59 @@
+%%%----------------------------------------------------------------------
+%%% File    : mod_last.erl
+%%% Author  : Michał Piotrowski <michal.piotrowski@erlang-solutions.com>
+%%% Purpose : mod_last mnesia backend (XEP-0012)
+%%%
+%%%
+%%% ejabberd, Copyright (C) 2002-2014   ProcessOne
+%%% MongooseIM, Copyright (C) 2014      Erlang Solutions Ltd.
+%%%
+%%%----------------------------------------------------------------------
+
+
+-module(mod_last_mnesia).
+
+-behaviour(mod_last).
+
+-include("mod_last.hrl").
+
+%% API
+-export([init/2,
+         get_last/2,
+         set_last_info/4,
+         remove_user/2]).
+
+-spec init(ejabberd:server(), list()) -> no_return().
+init(_Host, _Opts) ->
+    mnesia:create_table(last_activity,
+        [{disc_copies, [node()]},
+            {attributes,
+                record_info(fields, last_activity)}]).
+
+-spec get_last(ejabberd:luser(), ejabberd:lserver()) ->
+    {ok, non_neg_integer(), binary()} | {error, term()} | not_found.
+get_last(LUser, LServer) ->
+    case catch mnesia:dirty_read(last_activity, {LUser, LServer}) of
+        {'EXIT', Reason} -> {error, Reason};
+        [] -> not_found;
+        [#last_activity{timestamp = TimeStamp,
+            status = Status}] ->
+            {ok, TimeStamp, Status}
+    end.
+
+-spec set_last_info(ejabberd:luser(), ejabberd:lserver(),
+                    non_neg_integer(), binary()) ->
+    {atomic, ok} | {aborted, term()}.
+set_last_info(LUser, LServer, TimeStamp, Status) ->
+    US = {LUser, LServer},
+    F = fun() ->
+        mnesia:write(#last_activity{us = US,
+            timestamp = TimeStamp,
+            status = Status})
+    end,
+    mnesia:transaction(F).
+
+-spec remove_user(ejabberd:luse(), ejabberd:lserver()) -> ok.
+remove_user(LUser, LServer) ->
+    US = {LUser, LServer},
+    F = fun() -> mnesia:delete({last_activity, US}) end,
+    mnesia:transaction(F).
