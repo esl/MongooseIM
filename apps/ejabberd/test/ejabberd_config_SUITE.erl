820e14a3f38adf8ff346503e9d71791f386dc0c8
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2014-10-15 10:15:25 +0200

    Test starting up new modules when dynamically reloading config
    (cherry picked from commit 8b5e9f31a82007abfe9259d8ae75596fb54bd0ac)

--- apps/ejabberd/test/ejabberd_config_SUITE.erl
+++ apps/ejabberd/test/ejabberd_config_SUITE.erl
@@ -15,7 +15,8 @@ all() ->
      {group, reload_local}].
 
 groups() ->
-    [{reload_local, [], [coalesce_multiple_local_config_options]}].
+    [{reload_local, [], [coalesce_multiple_local_config_options,
+                         add_a_module]}].
 
 init_per_suite(Config) ->
     Config.
@@ -45,6 +46,19 @@ coalesce_multiple_local_config_options(_Config) ->
     F = fun ejabberd_config:group_host_changes/1,
     ?eq(coalesced_modules_section(), F(multiple_modules_sections())).
 
+add_a_module(Config) ->
+    % given a running server with a specific module off
+    copy_config(Config, "ejabberd.default.cfg", "ejabberd.cfg"),
+    start_ejabberd_with_config(Config, "ejabberd.cfg"),
+    ?eq(false, gen_mod:is_loaded(<<"localhost">>, mod_offline)),
+    % when adding the module to the configuration
+    copy_config(Config, "ejabberd.with_mod_offline.cfg", "ejabberd.cfg"),
+    ejabberd_config:reload_local(),
+    % then the new module gets started
+    ?eq(true, gen_mod:is_loaded(<<"localhost">>, mod_offline)),
+    % cleanup
+    ok = stop_ejabberd().
+
 %%
 %% Helpers
 %%
@@ -60,3 +74,9 @@ multiple_modules_sections() ->
 coalesced_modules_section() ->
     [{{modules,<<"localhost">>}, [{mod_adhoc,[]},
                                   {mod_offline,[]}]}].
+
+copy_config(Config, Src, Dst) ->
+    DataDir = proplists:get_value(data_dir, Config),
+    SrcConfigPath = filename:join([DataDir, Src]),
+    DstConfigPath = filename:join([DataDir, Dst]),
+    {ok, _} = file:copy(SrcConfigPath, DstConfigPath).
