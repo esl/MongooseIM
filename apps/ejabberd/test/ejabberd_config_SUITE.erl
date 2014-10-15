ba0ac0ea42422dd657277c70b212b5b83e0a59a4
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2014-10-15 10:13:39 +0200

    Test group_host_changes/1
    (cherry picked from commit c77910d446a9e09aecab82e56d4d4134f05b00a1)

--- apps/ejabberd/test/ejabberd_config_SUITE.erl
+++ apps/ejabberd/test/ejabberd_config_SUITE.erl
@@ -11,10 +11,11 @@
                           use_config_file/2]).
 
 all() ->
-    [smoke].
+    [smoke,
+     {group, reload_local}].
 
 groups() ->
-    [].
+    [{reload_local, [], [coalesce_multiple_local_config_options]}].
 
 init_per_suite(Config) ->
     Config.
@@ -40,6 +41,10 @@ smoke(Config) ->
     % cleanup
     ok = stop_ejabberd().
 
+coalesce_multiple_local_config_options(_Config) ->
+    F = fun ejabberd_config:group_host_changes/1,
+    ?eq(coalesced_modules_section(), F(multiple_modules_sections())).
+
 %%
 %% Helpers
 %%
@@ -47,3 +52,11 @@ smoke(Config) ->
 start_ejabberd_with_config(Config, ConfigFile) ->
     use_config_file(Config, ConfigFile),
     ok = start_ejabberd(Config).
+
+multiple_modules_sections() ->
+    [{local_config, {modules, <<"localhost">>}, [{mod_offline, []}]},
+     {local_config, {modules, <<"localhost">>}, [{mod_adhoc, []}]}].
+
+coalesced_modules_section() ->
+    [{{modules,<<"localhost">>}, [{mod_adhoc,[]},
+                                  {mod_offline,[]}]}].
