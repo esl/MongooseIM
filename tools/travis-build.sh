1df7a05d4c2c0df963afe17a38ff7e8461ea80d1
Rafal Studnicki <rafal.studnicki@erlang-solutions.com>
2014-10-15 15:41:29 +0200

    Run nodes on Travis after running unit tests

--- tools/travis-build.sh
+++ tools/travis-build.sh
@@ -7,16 +7,7 @@ TRAVIS_DB_PASSWORD=$(cat /tmp/travis_db_password)
 ${TOOLS}/set-odbc-password vars ${TRAVIS_DB_PASSWORD}
 
 make devclean devrel
-echo -n "starting MongooseIM node 1: "
-${EJD1CTL} start && echo ok || echo failed
-echo -n "starting MongooseIM node 2: "
-${EJD2CTL} start && echo ok || echo failed
-sleep 1
-echo -n "pinging MongooseIM node 1: "
-${EJD1CTL} ping
-echo -n "pinging MongooseIM node 2: "
-${EJD2CTL} ping
-
 make test_deps
+
 ${TOOLS}/set-odbc-password test ${TRAVIS_DB_PASSWORD}
 
