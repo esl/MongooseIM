90754c3c9db6bc7b897fe8bdb524c65784110768
Michal Piotrowski <michal.piotrowski@erlang-solutions.com>
2014-11-19 14:15:47 +0100

    makes ejabberd_node_utils more flexible

--- tests/ejabberd_node_utils.erl
+++ tests/ejabberd_node_utils.erl
@@ -16,24 +16,35 @@
 
 -module(ejabberd_node_utils).
 
--export([init/1, restart_application/1, call_fun/3, call_ctl/2, file_exists/1,
-         backup_config_file/1, restore_config_file/1, modify_config_file/2]).
+-export([init/1, init/2,
+         restart_application/1, restart_application/2,
+         call_fun/3, call_fun/4,
+         call_ctl/2, call_ctl/3,
+         file_exists/1,
+         backup_config_file/1, backup_config_file/2,
+         restore_config_file/1, restore_config_file/2,
+         modify_config_file/2, modify_config_file/4]).
 
 -include_lib("common_test/include/ct.hrl").
 
--define(CWD(Config), ?config(ejabberd_node_cwd, Config)).
--define(CURRENT_CFG_PATH(Config),
-        filename:join([?CWD(Config), "etc", "ejabberd.cfg"])).
--define(BACKUP_CFG_PATH(Config),
-        filename:join([?CWD(Config), "etc","ejabberd.cfg.bak"])).
--define(CFG_TEMPLATE_PATH(Config),
-        filename:join([?CWD(Config), "..", "..", "rel", "files",
+-define(CWD(Node, Config), ?config({ejabberd_cwd, Node}, Config)).
+
+-define(CURRENT_CFG_PATH(Node, Config),
+        filename:join([?CWD(Node, Config), "etc", "ejabberd.cfg"])).
+
+-define(BACKUP_CFG_PATH(Node, Config),
+        filename:join([?CWD(Node, Config), "etc","ejabberd.cfg.bak"])).
+
+-define(CFG_TEMPLATE_PATH(Node, Config),
+        filename:join([?CWD(Node, Config), "..", "..", "rel", "files",
                        "ejabberd.cfg"])).
--define(CFG_VARS_PATH(Config),
-        filename:join([?CWD(Config), "..", "..", "rel",
-                       "vars.config"])).
--define(CTL_PATH(Config),
-        filename:join([?CWD(Config), "bin", "mongooseimctl"])).
+
+-define(CFG_VARS_PATH(Node, Config, File),
+        filename:join([?CWD(Node, Config), "..", "..", "rel",
+                       File])).
+
+-define(CTL_PATH(Node, Config),
+        filename:join([?CWD(Node, Config), "bin", "mongooseimctl"])).
 
 -type ct_config() :: list({Key :: term(), Value :: term()}).
 
@@ -43,35 +54,64 @@
 
 -spec init(ct_config()) -> ct_config().
 init(Config) ->
-    set_ejabberd_node_cwd(Config).
+    Node = ct:get_config(ejabberd_node),
+    init(Node, Config).
+
+init(Node, Config) ->
+    set_ejabberd_node_cwd(Node, Config).
 
 -spec restart_application(atom()) -> ok.
 restart_application(ApplicationName) ->
-    ok = ejabberd_node_utils:call_fun(application, stop, [ApplicationName]),
-    ok = ejabberd_node_utils:call_fun(application, start, [ApplicationName]).
+    Node = ct:get_config(ejabberd_node),
+    restart_application(Node, ApplicationName).
+
+-spec restart_application(node(), atom()) -> ok.
+restart_application(Node, ApplicationName) ->
+    ok = ejabberd_node_utils:call_fun(Node, application, stop, [ApplicationName]),
+    ok = ejabberd_node_utils:call_fun(Node, application, start, [ApplicationName]).
+
 
 -spec backup_config_file(ct_config()) -> ct_config().
 backup_config_file(Config) ->
-    {ok, _} = call_fun(file, copy, [?CURRENT_CFG_PATH(Config),
-                                    ?BACKUP_CFG_PATH(Config)]).
+    Node = ct:get_config(ejabberd_node),
+    backup_config_file(Node, Config).
+
+-spec backup_config_file(node(), ct_config()) -> ct_config().
+backup_config_file(Node, Config) ->
+    {ok, _} = call_fun(Node, file, copy, [?CURRENT_CFG_PATH(Node, Config),
+                                          ?BACKUP_CFG_PATH(Node, Config)]).
 
 -spec restore_config_file(ct_config()) -> ct_config().
 restore_config_file(Config) ->
-    ok = call_fun(file, rename, [?BACKUP_CFG_PATH(Config),
-                                 ?CURRENT_CFG_PATH(Config)]).
+    Node = ct:get_config(ejabberd_node),
+    restore_config_file(Node, Config).
 
--spec call_fun(module(), atom(), [term()]) -> term() | {badrpc, term()}.
+-spec restore_config_file(node(), ct_config()) -> ct_config().
+restore_config_file(Node, Config) ->
+    ok = call_fun(Node, file, rename, [?BACKUP_CFG_PATH(Node, Config),
+                                       ?CURRENT_CFG_PATH(Node, Config)]).
+
+-spec call_fun(module(), atom(), []) -> term() | {badrpc, term()}.
 call_fun(M, F, A) ->
     Node = ct:get_config(ejabberd_node),
+    call_fun(Node, M, F, A).
+
+-spec call_fun(node(), module(), atom(), []) -> term() | {badrpc, term()}.
+call_fun(Node, M, F, A) ->
     rpc:call(Node, M, F, A).
 
 %% @doc Calls the mongooseimctl script with given command `Cmd'.
 %%
 %% For example to restart mongooseim call `call_ctl(restart, Config).'.
--spec call_ctl(atom(), ct_config()) -> term() | {badrpc, term()}.
+-spec call_ctl(atom(), ct_config()) -> term() | term().
 call_ctl(Cmd, Config) ->
-    OsCmd = io_lib:format("~p ~p", [?CTL_PATH(Config), atom_to_list(Cmd)]),
-    call_fun(os, cmd, [OsCmd]).
+    Node = ct:get_config(ejabberd_node),
+    call_ctl(Node, Cmd, Config).
+
+-spec call_ctl(node(), atom(), ct_config()) -> term() | term().
+call_ctl(Node, Cmd, Config) ->
+    OsCmd = io_lib:format("~p ~p", [?CTL_PATH(Node, Config), atom_to_list(Cmd)]),
+    os:cmd(OsCmd).
 
 -spec file_exists(file:name_all()) -> term() | {badrpc, term()}.
 file_exists(Filename) ->
@@ -92,25 +132,39 @@ file_exists(Filename) ->
       ConfigVariable :: atom(),
       Value :: string().
 modify_config_file(CfgVarsToChange, Config) ->
-    CurrentCfgPath = ?CURRENT_CFG_PATH(Config),
-    {ok, CfgTemplate} = ejabberd_node_utils:call_fun(
-                          file, read_file, [?CFG_TEMPLATE_PATH(Config)]),
-    {ok, CfgVars} = ejabberd_node_utils:call_fun(file, consult,
-                                                 [?CFG_VARS_PATH(Config)]),
+    Node = ct:get_config(ejabberd_node),
+    modify_config_file(Node, "vars.config", CfgVarsToChange, Config).
+
+-spec modify_config_file(node(), string(), [{ConfigVariable, Value}], ct_config()) -> ok when
+      ConfigVariable :: atom(),
+      Value :: string().
+modify_config_file(Node, VarsFile, CfgVarsToChange, Config) ->
+    CurrentCfgPath = ?CURRENT_CFG_PATH(Node, Config),
+    {ok, CfgTemplate} = ejabberd_node_utils:call_fun(Node,
+                          file, read_file, [?CFG_TEMPLATE_PATH(Node, Config)]),
+    {ok, DefaultVars} = ejabberd_node_utils:call_fun(Node, file, consult,
+                                                 [?CFG_VARS_PATH(Node, Config, "vars.config")]),
+    {ok, NodeVars} = ejabberd_node_utils:call_fun(Node, file, consult,
+                                                  [?CFG_VARS_PATH(Node, Config, VarsFile)]),
+
+    CfgVars = dict:to_list(dict:merge(fun(_, V, _) -> V end,
+                                      dict:from_list(NodeVars),
+                                      dict:from_list(DefaultVars))),
+
     UpdatedCfgVars = update_config_variables(CfgVarsToChange, CfgVars),
     CfgTemplateList = binary_to_list(CfgTemplate),
     UpdatedCfgFile = mustache:render(CfgTemplateList,
                                      dict:from_list(UpdatedCfgVars)),
-    ok = ejabberd_node_utils:call_fun(file, write_file, [CurrentCfgPath,
-                                                         UpdatedCfgFile]).
+    ok = ejabberd_node_utils:call_fun(Node, file, write_file, [CurrentCfgPath,
+                                                               UpdatedCfgFile]).
 
 %%--------------------------------------------------------------------
 %% Internal functions
 %%--------------------------------------------------------------------
 
-set_ejabberd_node_cwd(Config) ->
-    {ok, Cwd} = call_fun(file, get_cwd, []),
-    [{ejabberd_node_cwd, Cwd} | Config].
+set_ejabberd_node_cwd(Node, Config) ->
+    {ok, Cwd} = call_fun(Node, file, get_cwd, []),
+    [{{ejabberd_cwd, Node}, Cwd} | Config].
 
 update_config_variables(CfgVarsToChange, CfgVars) ->
     lists:foldl(fun({Var, Val}, Acc) ->
