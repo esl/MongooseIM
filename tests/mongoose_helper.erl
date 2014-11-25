a859aed4a8462fe6b56b381bf3b0506123b65cb8
Michal Piotrowski <michal.piotrowski@erlang-solutions.com>
2014-11-25 09:18:48 +0100

    covert count result to integer if is not

--- tests/mongoose_helper.erl
+++ tests/mongoose_helper.erl
@@ -112,4 +112,9 @@ count_wildpattern(Table) ->
 count_odbc(Table) ->
     {selected, _, [{N}]} =
         ?RPC(ejabberd_odbc,sql_query, [<<"localhost">>,<<"select count(*) from ", Table/binary, " ;">>]),
-    list_to_integer(binary_to_list(N)).
+    count_to_integer(N).
+
+count_to_integer(N) when is_binary(N) ->
+    list_to_integer(binary_to_list(N));
+count_to_integer(N) when is_integer(N)->
+    N.
