cc8bfe72b66d5d75a0a5b422eb4be43143398b1e
Michael Uvarov <michael.uvarov@erlang-solutions.com>
2014-12-11 11:59:32 +0100

    Allow to fold messages from travis-test.sh

--- tools/travis-test.sh
+++ tools/travis-test.sh
@@ -4,14 +4,25 @@ source tools/travis-common-vars.sh
 
 echo ${BASE}
 
+fold_start() {
+    echo -en "travis_fold:start:$1\\r"
+    echo "$2"
+}
+
+fold_end() {
+    echo -en "travis_fold:end:$1\\r"
+}
+
 SUMMARIES_DIRS=${BASE}'/test/ejabberd_tests/ct_report/ct_run*'
 
+fold_start "embeded" "Running embeded common tests"
 echo "############################"
 echo "Running embeded common tests"
 echo "############################"
 
 make ct
 EMBEDED_CT_STATUS=$?
+fold_end "embeded"
 
 echo "############################"
 echo "Running ejabberd_tests"
@@ -28,6 +39,7 @@ tail -f ${BASE}/dev/mongooseim_node1/log/ejabberd.log &
 tail -f ${BASE}/dev/mongooseim_node2/log/ejabberd.log &
 
 # Actual run
+fold_start "start_nodes" "Starting MongooseIM nodes"
 echo -n "starting MongooseIM node 1: "
 ${EJD1CTL} start && echo ok || echo failed
 echo -n "starting MongooseIM node 2: "
@@ -37,16 +49,20 @@ echo -n "pinging MongooseIM node 1: "
 ${EJD1CTL} ping
 echo -n "pinging MongooseIM node 2: "
 ${EJD2CTL} ping
+fold_end "start_nodes"
 
 make cover_test_preset TESTSPEC=default.spec PRESET=$TEST_CONFIG
 
 RAN_TESTS=`cat /tmp/ct_count`
 
+fold_start "stop_nodes" "Stopping MongooseIM nodes"
 echo -n "stopping MongooseIM node 1: "
 ${EJD1CTL} stop
 echo -n "stopping MongooseIM node 2: "
 ${EJD2CTL} stop
+fold_end "stop_nodes"
 
+fold_start "sum_ct" "Summarise Common Tests results"
 if [ `uname` = "Darwin" ]; then
     SUMMARIES_DIR=`ls -dt ${SUMMARIES_DIRS} | head -n ${RAN_TESTS}`
 else
@@ -55,6 +71,7 @@ fi
 
 ${TOOLS}/summarise-ct-results ${SUMMARIES_DIR}
 CT_STATUS=$?
+fold_end "sum_ct"
 
 echo
 echo "All tests done."
