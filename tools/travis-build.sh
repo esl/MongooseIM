33317b718dc1bd529f0b6ea47d6de11c76ade9f5
Michal Piotrowski <michal.piotrowski@erlang-solutions.com>
2014-12-16 14:28:40 +0100

    create minimal required rels on travis

--- tools/travis-build.sh
+++ tools/travis-build.sh
@@ -6,6 +6,9 @@ TRAVIS_DB_PASSWORD=$(cat /tmp/travis_db_password)
 
 ${TOOLS}/set-odbc-password vars ${TRAVIS_DB_PASSWORD}
 
+${TOOLS}/configure $REL_CONFIG
+cat configure.out
+
 make devclean devrel
 make test_deps
 
