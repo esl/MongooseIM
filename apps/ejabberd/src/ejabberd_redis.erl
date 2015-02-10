7bbf35110ea682ade932800c3e3267cde3b5b671
Michal Piotrowski <michal.piotrowski@erlang-solutions.com>
2015-02-10 14:52:39 +0100

    use cuesport from esl's branch

--- apps/ejabberd/src/ejabberd_redis.erl
+++ apps/ejabberd/src/ejabberd_redis.erl
@@ -18,12 +18,13 @@ start_link(Opts) ->
     PoolSize = proplists:get_value(pool_size, Opts, 10),
     RedoOpts = proplists:get_value(worker_config, Opts, []),
     ChildMods = [redo, redo_redis_proto, redo_uri],
-    ChildMFA = {redo, start_link, [undefined, RedoOpts]},
+    ChildMF = {redo, start_link},
+    ChildArgs = {for_all, [undefined, RedoOpts]},
 
     supervisor:start_child(ejabberd_sm_backend_sup,
                            {ejabberd_redis_sup,
                             {cuesport, start_link,
-                             [?POOL_NAME, PoolSize, ChildMods, ChildMFA]},
+                             [?POOL_NAME, PoolSize, ChildMods, ChildMF, ChildArgs]},
                             transient, 2000, supervisor, [cuesport | ChildMods]}).
 
 -spec cmd(iolist()) -> binary()
