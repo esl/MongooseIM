6658d862fe175688b2faa34d48c90096abdcf31e
Giuseppe Modarelli <giuseppe.modarelli@gmail.com>
2015-03-13 11:57:50 +0100

    Remove date from random password

--- apps/ejabberd/src/mod_admin_extra_accounts.erl
+++ apps/ejabberd/src/mod_admin_extra_accounts.erl
@@ -245,14 +245,8 @@ set_random_password(User, Server, Reason) ->
 
 -spec build_random_password(Reason :: binary()) -> binary().
 build_random_password(Reason) ->
-    {{Year, Month, Day}, {Hour, Minute, Second}} = calendar:now_to_universal_time(now()),
-    Date = list_to_binary(
-             lists:flatten(
-               io_lib:format("~4..0w-~2..0w-~2..0wT~2..0w:~2..0w:~2..0w",
-                             [Year, Month, Day, Hour, Minute, Second]))),
-
     RandomString = list_to_binary(randoms:get_string()),
-    <<"BANNED_ACCOUNT--", Date/binary, "--", RandomString/binary, "--", Reason/binary>>.
+    <<"BANNED_ACCOUNT--", RandomString/binary, "--", Reason/binary>>.
 
 
 -spec set_password_auth(ejabberd:user(), ejabberd:server(), binary()) -> 'ok'.
