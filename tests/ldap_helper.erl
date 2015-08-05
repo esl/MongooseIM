57ce19d4a987e8a25728fb0a1b7c352a765f3aec
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2015-08-05 16:14:23 +0200

    Fix formatting

--- tests/ldap_helper.erl
+++ tests/ldap_helper.erl
@@ -1,31 +1,31 @@
-
 -module(ldap_helper).
 
 -behaviour(escalus_user_db).
 
-%copied from ejabberd_auth_ldap
--record(state,
-{host = <<"">>          :: ejabberd:server(),
-    eldap_id = <<"">>      :: binary(),
-    bind_eldap_id = <<"">> :: binary(),
-    servers = []           :: [ejabberd:server()],
-    backups = []           :: [binary()],
-    port = 389             :: inet:port_number(),
-    tls_options = []       :: list(),
-    dn = <<"">>            :: binary(),
-    password = <<"">>      :: binary(),
-    base = <<"">>          :: binary(),
-    uids = []              :: [{binary()} | {binary(), binary()}],
-    ufilter = <<"">>       :: binary(),
-    sfilter = <<"">>       :: binary(),
-    lfilter                :: {any(), any()},
-    deref_aliases = never  :: never | searching | finding | always,
-    dn_filter              :: binary(),
-    dn_filter_attrs = []   :: [binary()]
-}).
+% copied from ejabberd_auth_ldap
+-record(state, {host = <<"">>          :: ejabberd:server(),
+                eldap_id = <<"">>      :: binary(),
+                bind_eldap_id = <<"">> :: binary(),
+                servers = []           :: [ejabberd:server()],
+                backups = []           :: [binary()],
+                port = 389             :: inet:port_number(),
+                tls_options = []       :: list(),
+                dn = <<"">>            :: binary(),
+                password = <<"">>      :: binary(),
+                base = <<"">>          :: binary(),
+                uids = []              :: [{binary()} | {binary(), binary()}],
+                ufilter = <<"">>       :: binary(),
+                sfilter = <<"">>       :: binary(),
+                lfilter                :: {any(), any()},
+                deref_aliases = never  :: never | searching | finding | always,
+                dn_filter              :: binary(),
+                dn_filter_attrs = []   :: [binary()]}).
 
 %% API
--export([start/1, stop/1, create_users/2, delete_users/2]).
+-export([start/1,
+         stop/1,
+         create_users/2,
+         delete_users/2]).
 
 -spec start(any()) -> ok.
 start(_) ->
@@ -49,16 +49,14 @@ create_user({_User, Spec}) ->
     {User, Server, Password} = get_usp(Spec),
     {ok, State} = escalus_ejabberd:rpc(eldap_utils, get_state, [Server, ejabberd_auth_ldap]),
     UserStr=binary_to_list(User),
-    DN = "cn="++UserStr++","++binary_to_list(State#state.base),
-    Attrs =   [{"objectclass", ["inetOrgPerson"]},
-        {"cn", [UserStr]},
-        {"sn", [UserStr]},
-        {"userPassword",[binary_to_list(Password)]},
-        {"ou", ["shared_group"]},
-        {"uid",[UserStr]}],
-    escalus_ejabberd:rpc(eldap_pool, add, [State#state.eldap_id,DN,Attrs]).
-
-
+    DN = "cn=" ++ UserStr ++ "," ++ binary_to_list(State#state.base),
+    Attrs = [{"objectclass", ["inetOrgPerson"]},
+             {"cn", [UserStr]},
+             {"sn", [UserStr]},
+             {"userPassword", [binary_to_list(Password)]},
+             {"ou", ["shared_group"]},
+             {"uid", [UserStr]}],
+    escalus_ejabberd:rpc(eldap_pool, add, [State#state.eldap_id, DN, Attrs]).
 
 delete_user({_Name, Spec}) ->
     {User, Server, _Password} = get_usp(Spec),
