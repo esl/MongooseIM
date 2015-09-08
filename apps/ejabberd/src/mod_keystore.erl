832457ea4c26b27de99946ad6c0bc4e6e100f714
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2015-09-08 12:32:03 +0200

    Add some specs

--- apps/ejabberd/src/mod_keystore.erl
+++ apps/ejabberd/src/mod_keystore.erl
@@ -9,14 +9,23 @@
 %% Hook handlers
 -export([get_key/2]).
 
+%% Public types
+-export_type([key_id/0]).
+
 -include("ejabberd.hrl").
 
 -define(iol2b(L), iolist_to_binary(L)).
+
+-type key_id() :: atom().
+-type key() :: binary().
+-type key_list() :: [{key_id(), key()}].
+-type key_type() :: ram | {file, file:name_all()}.
 
 %%
 %% gen_mod callbacks
 %%
 
+-spec start(ejabberd:server(), list()) -> ok.
 start(Domain, Opts) ->
     create_keystore_ets(),
     load_keys(Opts),
@@ -24,6 +33,7 @@ start(Domain, Opts) ->
       || {Hook, Handler, Priority} <- hook_handlers() ],
     ok.
 
+-spec stop(ejabberd:server()) -> ok.
 stop(Domain) ->
     [ ejabberd_hooks:delete(Hook, Domain, ?MODULE, Handler, Priority)
       || {Hook, Handler, Priority} <- hook_handlers() ],
@@ -34,6 +44,10 @@ stop(Domain) ->
 %% Hook handlers
 %%
 
+-spec get_key(HandlerAcc, KeyID) -> Result when
+      HandlerAcc :: key_list(),
+      KeyID :: key_id(),
+      Result :: key_list().
 get_key(HandlerAcc, KeyID) ->
     try
         ets:lookup(keystore, KeyID) ++ HandlerAcc
@@ -70,10 +84,12 @@ does_table_exist(NameOrTID) ->
 load_keys(Opts) ->
     [ load_key(K) || K <- proplists:get_value(keys, Opts, []) ].
 
+-spec load_key({key_id(), key_type()}) -> ok.
 load_key({KeyID, {file, Path}}) ->
     {ok, Data} = file:read_file(Path),
     Trimmed = ?iol2b(re:replace(Data, <<"[\n\r]+">>, <<>>,
                                 [global, {newline, any}])),
-    ets:insert(keystore, {KeyID, Trimmed});
+    true = ets:insert(keystore, {KeyID, Trimmed}),
+    ok;
 load_key({KeyID, ram}) ->
     error(not_implemented, [{KeyID, ram}]).
