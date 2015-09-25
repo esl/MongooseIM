6fbe92d15bf2256da0222a307a9e5ed27d668a5d
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2015-09-25 15:40:38 +0200

    Move a type definition where it belongs

--- apps/ejabberd/src/mod_auth_token.hrl
+++ apps/ejabberd/src/mod_auth_token.hrl
@@ -1,6 +1,4 @@
--type token_type() :: access | refresh | provision.
-
--record (auth_token, { type :: token_type(),
+-record (auth_token, { type :: mod_auth_token:token_type(),
                        expiry_datetime :: calendar:datetime(),
                        user_jid = <<>> :: binary(),
                        sequence_no = <<>> :: integer(),
