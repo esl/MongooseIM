92e305fdac74a03723882e3a2ecfa91dbf627ff2
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2015-09-28 12:57:48 +0200

    Rename: #auth_token{} -> #token{}

--- apps/ejabberd/src/mod_auth_token.hrl
+++ apps/ejabberd/src/mod_auth_token.hrl
@@ -1,6 +1,6 @@
--record (auth_token, { type :: mod_auth_token:token_type(),
-                       expiry_datetime :: calendar:datetime(),
-                       user_jid = <<>> :: binary(),
-                       sequence_no = <<>> :: integer(),
-                       mac_signature = <<>> :: binary(),
-                       token_body = <<>> :: binary() }).
+-record (token, { type :: mod_auth_token:token_type(),
+                  expiry_datetime :: calendar:datetime(),
+                  user_jid = <<>> :: binary(),
+                  sequence_no = <<>> :: integer(),
+                  mac_signature = <<>> :: binary(),
+                  token_body = <<>> :: binary() }).
