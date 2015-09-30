8f0ebd6d9f5b76e8a5b3e8cff5230db9fb1fc8c8
Krzysztof Wende <iraasta@gmail.com>
2015-09-30 16:05:18 +0200

    removed mochijson2

--- apps/ejabberd/test/json_SUITE.erl
+++ apps/ejabberd/test/json_SUITE.erl
@@ -13,7 +13,7 @@ all() ->
 
 groups() ->
     [
-     {json, [], [mochijson_test, jsx_test, j2m_test]} 
+     {json, [], [jsx_test, j2m_test]} 
     ].
 
 init_per_suite(Config) ->
@@ -36,35 +36,28 @@ end_per_testcase(_CaseName, _Config) ->
 
 % Tests
 
-mochijson_test(_Config) ->
-    A = "{\"a\":[{\"b\": 10},2]}",
-    {struct,[{<<"a">>,[{struct,[{<<"b">>,10}]},2]}]} = mochijson2:decode(A).
-
 jsx_test(_Config) ->
     A = "{\"a\":[{\"b\": 10},2]}",
     [{<<"a">>,[[{<<"b">>,10}],2]}] = jsx:decode(list_to_binary(A)).
  
 j2m_test(_Config) ->
-    check_j2m("{}"),
-    check_j2m("{\"a\": 1}"),
-    check_j2m("{\"a\": [1,2,3]}"),
-    check_j2m("{\"a\" : [1,2, {\"b\" : 10}]}"),
-    check_j2m("{\"a\" : [1,2, {\"b\" : 10}], \"b\" : \"string\", \"c\" : 1.22}"),
-    check_j2m("{\"user\" : { \"password\" : \"password\"}}"). 
+    j2m("{}",
+        {struct,[]}),
+    j2m("{\"a\": 1}",
+        {struct,[{<<"a">>,1}]}),
+    j2m("{\"a\": [1,2,3]}",
+        {struct,[{<<"a">>,[1,2,3]}]}),
+    j2m("{\"a\" : [1,2, {\"b\" : 10}]}",
+        {struct,[{<<"a">>,[1,2,{struct,[{<<"b">>,10}]}]}]}),
+    j2m("{\"a\" : [1,2, {\"b\" : 10}], \"b\" : \"string\", \"c\" : 1.22}",
+        {struct,[{<<"a">>,[1,2,{struct,[{<<"b">>,10}]}]},
+                 {<<"b">>,<<"string">>},
+                 {<<"c">>,1.22}]}),
+    j2m("{\"user\" : { \"password\" : \"password\"}}",
+        {struct, [{<<"user">>,
+                   {struct,[{<<"password">>,<<"password">>}]}}]}). 
 
-m2j_test(_Config) ->
-    check_m2j("{}"),
-    check_m2j("{\"a\": 1}"),
-    check_m2j("{\"a\": [1,2,3]}"),
-    check_m2j("{\"a\" : [1,2, {\"b\" : 10}]}"),
-    check_m2j("{\"a\" : [1,2, {\"b\" : 10}], \"b\" : \"string\", \"c\" : 1.22}"),
-    check_m2j("{\"user\" : { \"password\" : \"password\"}}").
-
-check_j2m(Json) ->
+j2m(Json, Struct) ->
     A = jsx:decode(list_to_binary(Json)),
-    ?eq(mochijson2:decode(Json), mochi2jsx:j2m(A)).
-
-check_m2j(Json) ->
-    A = mochijson2:decode(Json),
-    ?eq(jsx:decode(list_to_binary(Json)), mochi2jsx:m2j(A)).
-    
+    ?eq(Struct, mochi2jsx:j2m(A)),
+    ?eq(mochi2jsx:m2j(Struct), A). 
