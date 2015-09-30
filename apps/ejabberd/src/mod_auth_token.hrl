9f4b0893618678ed439db27c5fc03a9742847874
astachurski <astachurski@gmail.com>
2015-09-30 11:34:22 +0200

    handled return of refrsh token via sasl

--- apps/ejabberd/src/mod_auth_token.hrl
+++ apps/ejabberd/src/mod_auth_token.hrl
@@ -1,6 +1,6 @@
 -record (token, { type              :: mod_auth_token:token_type(),
                   expiry_datetime   :: calendar:datetime(),
-                  user_jid = <<>>   :: binary(),
+                  user_jid          :: ejabberd:jid(),
                   sequence_no       :: integer() | undefined,
                   mac_signature     :: binary() | undefined,
                   token_body        :: binary() | undefined }).
