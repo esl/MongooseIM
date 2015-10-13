31fc8fb5eb7f41d9ae74c702cb261253a06c78e2
Michal Piotrowski <michal.piotrowski@erlang-solutions.com>
2015-10-13 12:16:46 +0200

    script starting ldap servic on docker
    test on Mac OS X only with boot2docker

---
+++ tools/docker/start_and_prepare_slapd.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+boot2docker up
+eval "$(boot2docker shellinit)"
+
+LDAP_PORT=1389
+LDAP_HOST=`boot2docker ip`
+LDAP_ROOTPASS="mongoose"
+
+LDAP_CONT_NAME="mongooseim_ldap"
+
+MIM_LDAP_CONTAINERS=`docker ps -s -f "name=${LDAP_CONT_NAME}" | wc -l`
+
+if [ ${MIM_LDAP_CONTAINERS} -le 1 ]; then
+
+docker run --name {LDAP_CONT_NAME} \
+	-p ${LDAP_PORT}:389 \
+	-e LDAP_DOMAIN=esl.com \
+	-e LDAP_ORGANISATION="MongooseIM" \
+	-e LDAP_ROOTPASS=mongoose \
+	-d nickstenning/slapd
+
+LDIF_FILE_PATH="/tmp/init_entries.ldif"
+cat > ${LDIF_FILE_PATH} << EOL
+dn: ou=Users,dc=esl,dc=com
+objectClass: organizationalUnit
+ou: users
+EOL
+
+ldapadd -h ${LDAP_HOST} -p ${LDAP_PORT} -c -x -D cn=admin,dc=esl,dc=com -w ${LDAP_ROOTPASS} \
+	-f ${LDIF_FILE_PATH}
+else
+	echo "Container exists, starting"
+	docker start  ${LDAP_CONT_NAME}
+fi
+
