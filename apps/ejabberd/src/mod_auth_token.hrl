f1c8ad49b562e15f1df3234cabba7ff23e7f8a72
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2015-10-23 16:26:32 +0200

    Extend serialize_deserialize_property to cover provision tokens

--- apps/ejabberd/src/mod_auth_token.hrl
+++ apps/ejabberd/src/mod_auth_token.hrl
@@ -2,5 +2,6 @@
                   expiry_datetime   :: calendar:datetime(),
                   user_jid          :: ejabberd:jid(),
                   sequence_no       :: mod_auth_token:sequence_no() | undefined,
+                  vcard             :: jlib:xmlel() | undefined,
                   mac_signature     :: binary() | undefined,
                   token_body        :: binary() | undefined }).
