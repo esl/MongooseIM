9be09718c3ab5ed7e5c15738892d6188528c33c6
Pawel Pikula <pawel.pikula@erlang-solutions.com>
2015-11-12 11:52:44 +0100

    describe implementation details

--- apps/ejabberd/src/mod_last_riak.erl
+++ apps/ejabberd/src/mod_last_riak.erl
@@ -1,13 +1,44 @@
 %%%----------------------------------------------------------------------
 %%% File    : mod_last.erl
 %%% Author  : Pawel Pikula <pawel.pikula@erlang-solutions.com>
-%%% Purpose : mod_last mnesia backend (XEP-0012)
+%%% Purpose : mod_last riak backend (XEP-0012)
 %%%
 %%%
 %%% ejabberd, Copyright (C) 2002-2014   ProcessOne
 %%% MongooseIM, Copyright (C) 2014      Erlang Solutions Ltd.
 %%%
 %%%----------------------------------------------------------------------
+%%% @doc Riak backend for last activity XEP
+%%%
+%%% The backend uses the existing riak connection pool, which is "globally" defined in
+%%% the ejebberd.cfg file. Therefore, we don't need to start anything in the init
+%%% function.
+%%%
+%%% The module follows the approach taken by the other riak backends - it creates
+%%% the following bucket {<<"last">>, <<"example.com">>} for each xmpp domain.
+%%% The <<"last">> bucket type has the following props :'{"props":{"last_write_wins":true}}'
+%%%
+%%% Basically, there are 3 operations the backend need to provide set/get and count
+%%% the number of users which has been active since given date. However, the count
+%%% query can be only called using mongooseimctl.
+%%%
+%%% Username is used as a key and the value is just a user status, which is just
+%%% a binary. Moreover, we have a secondary integer index on the timestamp, which
+%%% is used in the count active users query.
+%%%
+%%% Data Layout:
+%%% KV: {Username:binary, Status:binary}
+%%% 2i: [Timestamp:integer]
+%%%
+%%% Set/Get are rather simple operations - they map to riak's put/get functions.
+%%% The count query uses mapred task to calculate the number of users satisfying
+%%% the criteria. As the input timestamp index range is used. The approach is
+%%% described here: http://docs.basho.com/riak/latest/dev/using/2i/#Querying
+%%% in the *Count Bucket Objects via $bucket Index* section. The alternative approach
+%%% could be the "riakc_pb_socket:get_index_range" function
+%%%
+%%% The pros and cons of the both approaches have been discussed here:
+%%% https://github.com/esl/MongooseIM/pull/567
 
 -module(mod_last_riak).
 
@@ -27,7 +58,7 @@
 
 -spec init(ejabberd:server(), list()) ->ok.
 init(_VHost, _Opts) ->
-    %% we are using commong riak pool
+    %% we are using common riak pool
     ok.
 
 -spec get_last(ejabberd:luser(), ejabberd:lserver()) ->
