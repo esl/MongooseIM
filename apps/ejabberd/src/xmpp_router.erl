c62d617332c15e2c8ba21b413108158aaf8a5719
Michal Piotrowski <michal.piotrowski@erlang-solutions.com>
2015-12-03 11:24:50 +0100

    add spec and improve log message

--- apps/ejabberd/src/xmpp_router.erl
+++ apps/ejabberd/src/xmpp_router.erl
@@ -1,15 +1,21 @@
 -module(xmpp_router).
 -export([route/4]).
--include_lib("ejabberd/include/ejabberd.hrl").
+
+-include("ejabberd.hrl").
 
 
--callback do_route(From :: ejabberd:jid(), To :: ejabberd:jid(), Packet :: jlib:xmlel()) -> ok.
+-callback do_route(From :: ejabberd:jid(), To :: ejabberd:jid(),
+                   Packet :: jlib:xmlel()) -> ok.
+
+-spec route(Module :: module(), From :: ejabberd:jid(), To :: ejabberd:jid(),
+            Packet :: jlib:xmlel()) -> ok.
 route(Module,From,To,Packet) ->
     case (catch Module:do_route(From,To,Packet)) of
         {'EXIT', Reason} ->
-            ?ERROR_MSG("~p when routing ~ts from ~ts to ~ts in module: ~p",
-                       [Reason, exml:to_binary(Packet),
-                        jlib:jid_to_binary(From), jlib:jid_to_binary(To),
-                        Module]);
+            ?ERROR_MSG("error when routing from=~ts to=~ts in module=~p, reason=~p, packet=~ts, stack_trace=~p",
+                       [jid:to_binary(From), jid:to_binary(To),
+                        Module, Reason, exml:to_binary(Packet),
+                        erlang:get_stacktrace()]);
         _ -> ok
     end.
+
