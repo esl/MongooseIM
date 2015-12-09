48c7a5cb090ed996839ffe0d4dd8a634c4cc4853
astachurski <astachurski@gmail.com>
2015-12-09 14:38:30 +0100

    [skip ci] changes in ldap setup scripts

--- tools/setup_ldap-docker.sh
+++ tools/setup_ldap-docker.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 LDAP_INSTANCE=mongooseim-ldap
 LDAP_HOST="http://${1}:389"
 SSH_DOCKERMACHINE_ALIAS="backendhost"
@@ -9,9 +8,6 @@ PROVISIONING_LDAP_FULLFILENAME="setup_ldap-remote.sh"
 echo "uploading script to remotely initialize ldap server ${LDAP_HOST} ..."
 rsync -avz -e ssh "${PROVISIONING_LDAP_FULLFILENAME}" ${SSH_DOCKERMACHINE_ALIAS}:/home/go
 
-
-# ssh ${SSH_DOCKERMACHINE_ALIAS} 'docker exec '${LDAP_INSTANCE}' riak-admin bucket-type create users ''\{\"props\":\{\"datatype\":\"map\"\}\}'''
-
-# ssh ${SSH_DOCKERMACHINE_ALIAS} 'docker exec '${LDAP_INSTANCE}' riak-admin bucket-type create users ''\{\"props\":\{\"datatype\":\"map\"\}\}'''
-
+echo "executing remote ldap setup..."
+ssh ${SSH_DOCKERMACHINE_ALIAS} './home/go/setup_ldap-remote.sh'
 
