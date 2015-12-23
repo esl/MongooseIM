83d166f77d45727089fec6211471bff336e4bc2e
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2015-12-23 16:59:55 +0100

    Add tests for basic operations: - subscribe/unsubscribe - publish - notify - request all items

--- test/ejabberd_tests/tests/pubsub_SUITE.erl
+++ test/ejabberd_tests/tests/pubsub_SUITE.erl
@@ -24,15 +24,20 @@ all() -> [
 
 groups() -> [{pubsub_cycle_tests, [sequence],
               [
-               create_delete_node_test
+               create_delete_node_test,
+               subscribe_unsubscribe_test,
+               publish_test,
+               notify_test,
+               request_all_items_test
               ]
              }].
 
 suite() ->
     escalus:suite().
 
--define (NODE_ADDR, <<"pubsub.localhost">>).
--define (NODE_NAME, <<"princely_musings">>).
+-define(NODE_ADDR, <<"pubsub.localhost">>).
+-define(NODE_NAME, <<"princely_musings">>).
+-define(NODE, {?NODE_ADDR, ?NODE_NAME}).
 
 %%--------------------------------------------------------------------
 %% Init & teardown
@@ -59,13 +64,94 @@ end_per_testcase(_TestName, Config) ->
     escalus:end_per_testcase(_TestName, Config).
 
 %%--------------------------------------------------------------------
-%% Test cases
+%% Test cases for XEP 0060
+%% Comments in test cases refer to sections is the XEP
 %%--------------------------------------------------------------------
 
 create_delete_node_test(Config) ->
     escalus:story(
-      Config, [{alice, 1}],
+      Config,
+      [{alice, 1}],
       fun(Alice) ->
-              {true, _RecvdStanza} = pubsub_tools:create_node(Alice, ?NODE_ADDR, ?NODE_NAME),
-              pubsub_tools:delete_node(Alice, ?NODE_ADDR, ?NODE_NAME)
+              %% Request:  8.1.2 Ex.132 create node with (default) open access model
+              %% Response:       Ex.134 success
+              %%                        Note: contains node ID although XEP does not require this
+              pubsub_tools:create_node(Alice, ?NODE),
+
+              %% Request:  8.4.1 Ex.155 owner deletes a node
+              %% Response:       Ex.157 success
+              pubsub_tools:delete_node(Alice, ?NODE)
+      end).
+
+subscribe_unsubscribe_test(Config) ->
+    escalus:story(
+      Config,
+      [{alice,1}, {bob,1}],
+      fun(Alice, Bob) ->
+              pubsub_tools:create_node(Alice, ?NODE),
+
+              %% Request:  6.1.1 Ex.32 entity subscribes to a node
+              %% Response: 6.1.2 Ex.33 success (with subscription ID)
+              pubsub_tools:subscribe(Bob, ?NODE),
+
+              %% Request:  6.2.1 Ex.51 unsubscribe from a node
+              %% Response: 6.2.2 Ex.52 success
+              pubsub_tools:unsubscribe(Bob, ?NODE),
+
+              pubsub_tools:delete_node(Alice, ?NODE)
+      end).
+
+publish_test(Config) ->
+    escalus:story(
+      Config,
+      [{alice,1}],
+      fun(Alice) ->
+              pubsub_tools:create_node(Alice, ?NODE),
+
+              %% Request:  7.1.1 Ex.99  publish an item with an ItemID
+              %% Response: 7.1.2 Ex.100 success
+              pubsub_tools:publish(Alice, <<"item1">>, ?NODE),
+
+              pubsub_tools:delete_node(Alice, ?NODE)
+      end).
+
+notify_test(Config) ->
+    escalus:story(
+      Config,
+      [{alice,1}, {bob,1}, {geralt,1}],
+      fun(Alice, Bob, Geralt) ->
+              pubsub_tools:create_node(Alice, ?NODE),
+              pubsub_tools:subscribe(Bob, ?NODE),
+              pubsub_tools:subscribe(Geralt, ?NODE),
+              pubsub_tools:publish(Alice, <<"item1">>, ?NODE),
+
+              %% 7.1.2.1 Ex.101 notification with payload
+              %%                Note: message has type 'headline' by default
+              pubsub_tools:receive_notification(Bob, <<"item1">>, ?NODE),
+              pubsub_tools:receive_notification(Geralt, <<"item1">>, ?NODE),
+
+              pubsub_tools:delete_node(Alice, ?NODE)
+      end).
+
+request_all_items_test(Config) ->
+    escalus:story(
+      Config,
+      [{alice,1}, {bob,1}],
+      fun(Alice, Bob) ->
+              pubsub_tools:create_node(Alice, ?NODE),
+              pubsub_tools:publish(Alice, <<"item1">>, ?NODE),
+              pubsub_tools:publish(Alice, <<"item2">>, ?NODE),
+
+              %% Note: when Bob subscribes, the last item (item2) is sent to him
+              %%       6.1.7 Ex.50 service sends last published item
+              %%       This is sent BEFORE the response iq stanza
+              pubsub_tools:subscribe(Bob, ?NODE, <<"item2">>),
+              %% TODO look for the delay element
+
+              %% Request:  6.5.2 Ex.78 subscriber requests all items
+              %% Response: 6.5.3 Ex.79 service returns all items
+              pubsub_tools:request_all_items(Bob, [<<"item2">>, <<"item1">>], ?NODE),
+              %% TODO check ordering (although XEP does not specify this)
+
+              pubsub_tools:delete_node(Alice, ?NODE)
       end).
--- test/ejabberd_tests/tests/pubsub_tools.erl
+++ test/ejabberd_tests/tests/pubsub_tools.erl
@@ -15,48 +15,122 @@
 -include_lib("exml/include/exml_stream.hrl").
 
 -export([
-         create_node/3,
-         delete_node/3
+         create_node/2,
+         delete_node/2,
+         subscribe/2, subscribe/3,
+         unsubscribe/2,
+         publish/3,
+         receive_notification/3,
+         request_all_items/3
         ]).
 
 %%-----------------------------------------------------------------------------
 %% API: pubsub tools
 %%-----------------------------------------------------------------------------
 
-create_node(User, NodeAddr, NodeName) ->
+create_node(User, {NodeAddr, NodeName}) ->
     Id = <<"create1">>,
     CreateNodeIq = escalus_pubsub_stanza:create_node_stanza(
                        User, Id, NodeAddr, NodeName),
     log_stanza("REQUEST create node", CreateNodeIq),
     escalus:send(User, CreateNodeIq),
-    {true, _ResultStanza} = wait_for_stanza_and_match_result_iq(User, Id, NodeAddr).
+    receive_response(User, Id).
 
-delete_node(User, NodeAddr, NodeName) ->
+delete_node(User, {NodeAddr, NodeName}) ->
     Id = <<"delete1">>,
     DeleteNode = escalus_pubsub_stanza:delete_node_stanza(NodeName),
     DeleteNodeIq = escalus_pubsub_stanza:iq_with_id(set, Id, NodeAddr, User, [DeleteNode]),
     log_stanza("REQUEST delete node", DeleteNodeIq),
     escalus:send(User, DeleteNodeIq),
-    {true, _ResultStanza} = wait_for_stanza_and_match_result_iq(User, Id, NodeAddr).
+    receive_response(User, Id).
+
+subscribe(User, {NodeAddr, NodeName}) ->
+    subscribe(User, {NodeAddr, NodeName}, none).
+
+subscribe(User, {NodeAddr, NodeName}, ExpectedItemId) ->
+    UserName = escalus_utils:get_username(User),
+    Id = <<UserName/binary, <<"binsuffix">>/binary>>,
+    SubscribeIq = escalus_pubsub_stanza:subscribe_by_user_stanza(User, Id, NodeName, NodeAddr),
+    log_stanza("REQUEST subscribe", SubscribeIq),
+    escalus:send(User, SubscribeIq),
+    case ExpectedItemId of
+        none -> ok;
+        _ -> receive_notification(User, ExpectedItemId, {NodeAddr, NodeName})
+    end,
+    ResultStanza = receive_response(User, Id),
+    check_subscription(ResultStanza, User, NodeName).
+
+unsubscribe(User, {NodeAddr, NodeName}) ->
+    UserName = escalus_utils:get_username(User),
+    Id = <<UserName/binary, <<"binsuffix">>/binary>>,
+    UnsubscribeIq = escalus_pubsub_stanza:unsubscribe_by_user_stanza(User, Id, NodeName, NodeAddr),
+    log_stanza("REQUEST unsubscribe", UnsubscribeIq),
+    escalus:send(User, UnsubscribeIq),
+    receive_response(User, Id).
+
+publish(User, ItemId, {NodeAddr, NodeName}) ->
+    UserName = escalus_utils:get_username(User),
+    Id = <<UserName/binary, <<"binsuffix">>/binary>>,
+    Publish = escalus_pubsub_stanza:create_publish_node_content_stanza(NodeName, ItemId),
+    PublishIq = escalus_pubsub_stanza:iq_with_id(set, Id, NodeAddr, User, [Publish]),
+    log_stanza("REQUEST publish", PublishIq),
+    escalus:send(User, PublishIq),
+    receive_response(User, Id).
+
+receive_notification(User, ItemId, {NodeAddr, NodeName}) ->
+    Stanza = escalus:wait_for_stanza(User),
+    log_stanza("NOTIFICATION", Stanza),
+
+    true = escalus_pred:is_message(Stanza) andalso escalus_pred:has_type(<<"headline">>, Stanza),
+    true = escalus_pred:is_stanza_from(NodeAddr, Stanza),
+
+    Items = exml_query:path(Stanza, [{element, <<"event">>},
+                                     {element, <<"items">>}]),
+    check_items(Items, [ItemId], NodeName).
+
+request_all_items(User, ItemIds, {NodeAddr, NodeName}) ->
+    Id = <<"items1">>,
+    Request = escalus_pubsub_stanza:create_request_allitems_stanza(NodeName),
+    RequestIq = escalus_pubsub_stanza:iq_with_id(get, Id, NodeAddr, User, [Request]),
+    log_stanza("REQUEST all items", RequestIq),
+    escalus:send(User, RequestIq),
+    ResultStanza = receive_response(User, Id),
+    Items = exml_query:path(ResultStanza, [{element, <<"pubsub">>},
+                                           {element, <<"items">>}]),
+    check_items(Items, ItemIds, NodeName).
 
 %%-----------------------------------------------------------------------------
 %% Internal functions
 %%-----------------------------------------------------------------------------
 
-%% Checks that:
-%% - IQ from server matches the sent id
-%% - there is "result"
-%% - sender is correct.
-wait_for_stanza_and_match_result_iq(User, Id, DestinationNode) ->
+receive_response(User, Id) ->
     ResultStanza = escalus:wait_for_stanza(User),
     log_stanza("RESPONSE", ResultStanza),
-
-    QueryStanza = escalus_stanza:iq_with_type_id_from(<<"result">>, Id, DestinationNode),
-    Result = escalus_pred:is_iq_result(QueryStanza, ResultStanza),
-
-    {Result, ResultStanza}.
+    true = escalus_pred:is_iq_result(ResultStanza),
+    Id = exml_query:attr(ResultStanza, <<"id">>),
+    ResultStanza.
 
 log_stanza(ReportString, Stanza) ->
     PrettyStanza = binary:list_to_bin(exml:to_pretty_iolist(Stanza)),
+    %ct:print("~p~n", [Stanza]),
     ct:print("~s~n~s", [ReportString, PrettyStanza]),
     ct:log("~s~n~s", [ReportString, exml:escape_attr(PrettyStanza)]).
+
+check_subscription(SubscrConfirmation, User, NodeName) ->
+    Subscr = exml_query:path(SubscrConfirmation, [{element, <<"pubsub">>},
+                                                  {element, <<"subscription">>}]),
+    Jid = exml_query:attr(Subscr, <<"jid">>),
+    Jid = escalus_utils:get_jid(User),
+    NodeName = exml_query:attr(Subscr, <<"node">>),
+    true = exml_query:attr(Subscr, <<"subid">>) =/= undefined,
+    <<"subscribed">> = exml_query:attr(Subscr, <<"subscription">>).
+
+check_items(ReceivedItemsElem, ExpectedItemIds, NodeName) ->
+    NodeName = exml_query:attr(ReceivedItemsElem, <<"node">>),
+    ReceivedItems = exml_query:subelements(ReceivedItemsElem, <<"item">>),
+    [check_item(ReceivedItem, ExpectedItemId) ||
+        {ReceivedItem, ExpectedItemId} <- lists:zip(ReceivedItems, ExpectedItemIds)].
+
+check_item(ReceivedItem, ExpectedItemId) ->
+    PublishEntry = escalus_pubsub_stanza:publish_entry([]),
+    ReceivedItem = escalus_pubsub_stanza:publish_item(ExpectedItemId, PublishEntry).
