0acf0e62c238d15c3f932ccec418c41c25f12fb4
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2016-01-08 12:19:49 +0100

    Remove unused code still referring to some jlib:jid_*

--- apps/ejabberd/src/cyrsasl.erl
+++ apps/ejabberd/src/cyrsasl.erl
@@ -110,30 +110,6 @@ register_mechanism(Mechanism, Module, PasswordType) ->
 ets_insert_mechanism(MechanismRec) ->
     ets:insert(sasl_mechanism, MechanismRec).
 
-%%% TODO: use callbacks
-%%-include("ejabberd.hrl").
-%%-include("jlib.hrl").
-%%check_authzid(_State, Props) ->
-%%    AuthzId = xml:get_attr_s(authzid, Props),
-%%    case jlib:binary_to_jid(AuthzId) of
-%%      error ->
-%%          {error, "invalid-authzid"};
-%%      JID ->
-%%          LUser = jlib:nodeprep(xml:get_attr_s(username, Props)),
-%%          {U, S, R} = jlib:jid_tolower(JID),
-%%          case R of
-%%              "" ->
-%%                  {error, "invalid-authzid"};
-%%              _ ->
-%%                  case {LUser, ?MYNAME} of
-%%                      {U, S} ->
-%%                          ok;
-%%                      _ ->
-%%                          {error, "invalid-authzid"}
-%%                  end
-%%          end
-%%    end.
-
 -spec check_credentials(sasl_state(), list()) -> 'ok' | {'error', binary()}.
 check_credentials(_State, Props) ->
     case jid:nodeprep(proplists:get_value(username, Props, <<>>)) of
