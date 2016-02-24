2d822ae19051a8254e4598dac642753551a79da3
Michal Piotrowski <michal.piotrowski@erlang-solutions.com>
2016-02-24 15:40:07 +0100

    enable travis deploy to githhub's releases

--- tools/travis-build-and-push-docker.sh
+++ tools/travis-build-and-push-docker.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+make rel
+
+export MIM_TAR=mongooseim-$TRAVIS_BRANCH.$TRAVIS_OTP_RELEASE.$(lsb_release -is | tr "A-Z" "a-z").$(lsb_release -rs).$(uname -m).tar.bz2
+
+tar -cjh --transform='s,mongooseim,mongooseim-${$TRAVIS_BRANCH},S' -f ${MIM_TAR} rel/mongooseim
+
 git clone -b demo https://github.com/studzien/mongooseim-docker.git
 cd mongooseim-docker
 
@@ -9,6 +15,12 @@ echo "tag: ${TRAVIS_TAG}"
 
 export PROJECT=${TRAVIS_BRANCH}
 export VOLUMES="`pwd`/projects/${PROJECT}"
+
+DOCKERHUB_TAG=${TRAVIS_BRANCH}
+
+if [ ${TRAVIS_BRANCH} = 'master' ]; then
+	$DOCKERHUB_TAG="latest";
+fi
 
 mkdir -p "${VOLUMES}/builds"
 
@@ -24,7 +36,7 @@ docker images
 
 docker login -e=${DOCKERHUB_EMAIL} -u ${DOCKERHUB_USER} -p ${DOCKERHUB_PASS}
 
-docker tag ${PROJECT}-mongooseim ${DOCKERHUB_USER}/mongooseim
+docker tag ${PROJECT}-mongooseim ${DOCKERHUB_USER}/mongooseim:${DOCKERHUB_TAG}
 
-docker push ${DOCKERHUB_USER}/mongooseim
+docker push ${DOCKERHUB_USER}/mongooseim:${DOCKERHUB_TAG}
 
