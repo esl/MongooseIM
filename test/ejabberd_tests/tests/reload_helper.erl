f668ad1de770fe0d34145cd3b8dfcd513d177d4d
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2016-02-24 16:18:35 +0100

    wip: Rewrite computations from macros to functions

--- test/ejabberd_tests/tests/reload_helper.erl
+++ test/ejabberd_tests/tests/reload_helper.erl
@@ -24,39 +24,26 @@
          reload_through_ctl/2,
          restart_ejabberd_node/1]).
 
--import(distributed_helper, [rpc/4]).
-
--define(CWD(Config), ?config(ejabberd_node_cwd, Config)).
--define(CURRENT_CFG_PATH(Config),
-        filename:join([?CWD(Config), "etc", "ejabberd.cfg"])).
--define(BACKUP_CFG_PATH(Config),
-        filename:join([?CWD(Config), "etc","ejabberd.cfg.bak"])).
--define(CFG_TEMPLATE_PATH(Config),
-        filename:join([?CWD(Config), "..", "..", "rel", "files",
-                       "ejabberd.cfg"])).
--define(CFG_VARS_PATH(Config),
-        filename:join([?CWD(Config), "..", "..", "rel",
-                       "vars.config"])).
--define(CTL_PATH(Config),
-        filename:join([?CWD(Config), "bin", "mongooseimctl"])).
+-import(distributed_helper, [rpc/4, rpc/5]).
 
 -define(CTL_RELOAD_OUTPUT_PREFIX,
         "# Reloaded: " ++ atom_to_list(ct:get_config(ejabberd_node))).
 
 backup_ejabberd_config_file(Node, Config) ->
-    {ok, _} = rpc(Node, file, copy, [?CURRENT_CFG_PATH(Config),
-                                     ?BACKUP_CFG_PATH(Config)]).
+    {ok, _} = rpc(Node, file, copy, [node_cfg(Node, current, Config),
+                                     node_cfg(Node, backup, Config)]).
 
 restore_ejabberd_config_file(Node, Config) ->
-    ok = rpc(Node, file, rename, [?BACKUP_CFG_PATH(Config),
-                                  ?CURRENT_CFG_PATH(Config)]).
+    ok = rpc(Node, file, rename, [node_cfg(Node, backup, Config),
+                                  node_cfg(Node, current, Config)]).
 
 restart_ejabberd_node(Node) ->
-    ok = rpc(Node, application, stop, [ejabberd]),
-    ok = rpc(Node, application, start, [ejabberd]).
+    %% Node restarts might take a long time -> long timeout.
+    ok = rpc(Node, application, stop, [ejabberd], timer:seconds(10)),
+    ok = rpc(Node, application, start, [ejabberd], timer:seconds(10)).
 
 reload_through_ctl(Node, Config) ->
-    ReloadCmd = ?CTL_PATH(Config) ++ " reload_local",
+    ReloadCmd = node_ctl(Node, Config) ++ " reload_local",
     OutputStr = rpc(Node, os, cmd, [ReloadCmd]),
     ok = verify_reload_output(OutputStr).
 
@@ -69,12 +56,11 @@ verify_reload_output(OutputStr) ->
             ct:pal("~ts", [OutputStr]),
             error(config_reload_failed, [OutputStr])
     end.
-    
 
 modify_config_file(Node, CfgVarsToChange, Config) ->
-    CurrentCfgPath = ?CURRENT_CFG_PATH(Config),
-    {ok, CfgTemplate} = rpc(Node, file, read_file, [?CFG_TEMPLATE_PATH(Config)]),
-    {ok, CfgVars} = rpc(Node, file, consult, [?CFG_VARS_PATH(Config)]),
+    CurrentCfgPath = node_cfg(Node, current, Config),
+    {ok, CfgTemplate} = rpc(Node, file, read_file, [node_cfg(Node, template, Config)]),
+    {ok, CfgVars} = rpc(Node, file, consult, [node_cfg(Node, vars, Config)]),
     UpdatedCfgVars = update_config_variables(CfgVarsToChange, CfgVars),
     CfgTemplateList = binary_to_list(CfgTemplate),
     UpdatedCfgFile = mustache:render(CfgTemplateList,
@@ -85,3 +71,17 @@ update_config_variables(CfgVarsToChange, CfgVars) ->
     lists:foldl(fun({Var, Val}, Acc) ->
                         lists:keystore(Var, 1, Acc,{Var, Val})
                 end, CfgVars, CfgVarsToChange).
+
+node_cfg(N, current, C)  -> flat([node_cwd(N, C), "etc", "ejabberd.cfg"]);
+node_cfg(N, backup, C)   -> flat([node_cwd(N, C), "etc", "ejabberd.cfg.bak"]);
+node_cfg(N, template, C) -> flat([node_cwd(N, C), "..", "..", "rel", "files", "ejabberd.cfg"]);
+node_cfg(N, vars, C)     -> flat([node_cwd(N, C), "..", "..", "rel", "vars.config"]).
+
+node_ctl(N, C) -> flat([node_cwd(N, C), "bin", "mongooseimctl"]).
+
+flat(PathComponents) -> filename:join(PathComponents).
+
+node_cwd(Node, Config) ->
+    CWD = escalus_config:get_config({ejabberd_cwd, Node}, Config),
+    CWD == undefined andalso error({{ejabberd_cwd, Node}, undefined}, [Node, Config]),
+    CWD.
