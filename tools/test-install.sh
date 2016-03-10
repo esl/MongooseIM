c17cf062072ea02a8c84a9b5fae02f6e4ed84d6e
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2016-03-10 13:18:21 +0100

    Fail test-install.sh if node is running when it shouldn't be anymore

--- tools/test-install.sh
+++ tools/test-install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -e
 
@@ -11,21 +11,16 @@ rm configure.out rel/configure.vars.config rel/vars.config
 rm -rf rel/mongooseim; RUNNER_GROUP=staff make install
 cd $TARGET
 git init
-git commit --allow-empty -m "first empty"
+git commit --allow-empty -m "1st empty"
 git add .
-git commit -m "first non-empty"
+git commit -m "1st non-empty"
 $TARGET/usr/bin/mongooseimctl start
-$TARGET/usr/bin/mongooseimctl started
+$TARGET/usr/bin/mongooseimctl started && echo node: started
 $TARGET/usr/bin/mongooseimctl status
 $TARGET/usr/bin/mongooseimctl stop
-$TARGET/usr/bin/mongooseimctl stopped
-$TARGET/usr/bin/mongooseimctl status || echo node down: ok, this is expected
-ps aux | grep 'bea[m]'
-running=$?
-if [ $running -eq 0 ];  then
-	echo node still running, but should not
-	exit 1
-fi
+$TARGET/usr/bin/mongooseimctl stopped && echo node: stopped
+$TARGET/usr/bin/mongooseimctl status || echo status failed: this might be ok
+ps aux | grep 'bea[m]' && { echo "node still running, but should not"; exit 1; }
 git status
 tree $TARGET -L 3
 echo $0: all ok
