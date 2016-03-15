439b265a320a3d4e1d6568e9a630fff8e5ec4557
Michal Piotrowski <michal.piotrowski@erlang-solutions.com>
2016-03-15 19:58:01 +0100

    add sm tests and message order check tests

--- test/ejabberd_tests/tests/xep_0352_csi_SUITE.erl
+++ test/ejabberd_tests/tests/xep_0352_csi_SUITE.erl
@@ -14,7 +14,12 @@ groups() ->
       [
        server_announces_csi,
        alice_is_inactive_and_no_stanza_arrived,
-       alice_gets_msgs_after_activate
+       alice_gets_msgs_after_activate,
+       alice_gets_msgs_after_activate_in_order,
+       bob_does_not_get_msgs_from_inactive_alice,
+       bob_gets_msgs_from_aclie_after_she_is_active_back,
+       bob_and_alice_get_msgs_from_each_other_after_alice_is_active,
+       alice_is_inactive_but_sends_sm_req_and_recives_ack_after_active
       ]}].
 
 suite() ->
@@ -26,22 +31,22 @@ init_per_suite(Config) ->
 end_per_suite(Config) ->
     escalus:end_per_suite(Config).
 
-init_per_group(_Group, Config) ->
-    Config.
+init_per_group(_, Config) ->
+    escalus_users:update_userspec(Config, alice, stream_management, true).
 
 end_per_group(_Group, Config) ->
     Config.
 
 init_per_testcase(CaseName, Config) ->
+    timer:sleep(random:uniform(2000)),
     escalus:init_per_testcase(CaseName, Config).
 
 end_per_testcase(CaseName, Config) ->
     escalus:end_per_testcase(CaseName, Config).
 
 server_announces_csi(Config) ->
-    {_, Users} = escalus_fresh:create_fresh_users(Config, [{alice, 1}]),
-    Spec = proplists:get_value(alice, Users),
-    ct:print("~p", [Users]),
+    NewConfig = escalus_fresh:create_users(Config, [{alice, 1}]),
+    Spec = escalus_users:get_userspec(NewConfig, alice),
     Steps = [start_stream,
              stream_features,
              maybe_use_ssl,
@@ -54,31 +59,109 @@ server_announces_csi(Config) ->
 
 alice_is_inactive_and_no_stanza_arrived(Config) ->
     escalus:fresh_story(Config, [{alice, 1}, {bob, 1}], fun(Alice, Bob) ->
-        given_client_is_inactive_and_message_sent(Alice, Bob),
+        given_client_is_inactive_and_messages_sent(Alice, Bob, 1),
 
         escalus_assert:has_no_stanzas(Alice)
     end).
 
 alice_gets_msgs_after_activate(Config) ->
+    alice_gets_msgs_after_activate(Config, 1).
+
+alice_gets_msgs_after_activate_in_order(Config) ->
+    alice_gets_msgs_after_activate(Config, 3).
+
+
+
+alice_gets_msgs_after_activate(Config, N) ->
     escalus:fresh_story(Config, [{alice, 1}, {bob, 1}], fun(Alice, Bob) ->
         %%Given
-        given_client_is_inactive_and_message_sent(Alice, Bob),
+        Msgs = given_client_is_inactive_and_messages_sent(Alice, Bob, N),
 
         %%When client becomes active again
         escalus:send(Alice, csi_stanza(<<"active">>)),
 
-        Msg = escalus:wait_for_stanza(Alice),
-        escalus:assert(is_chat_message, Msg)
+        then_client_receives_message(Alice, Msgs)
     end).
 
-given_client_is_inactive_and_message_sent(Alice, Bob) ->
+
+bob_does_not_get_msgs_from_inactive_alice(Config) ->
+    escalus:fresh_story(Config, [{alice, 1}, {bob, 1}], fun(Alice, Bob) ->
+        given_client_is_inactive_but_sends_messages(Alice, Bob, 1),
+
+        escalus_assert:has_no_stanzas(Bob)
+    end).
+
+bob_gets_msgs_from_aclie_after_she_is_active_back(Config) ->
+    escalus:fresh_story(Config, [{alice, 1}, {bob, 1}], fun(Alice, Bob) ->
+        Msgs = given_client_is_inactive_and_messages_sent(Alice, Bob, 1),
+
+        escalus:send(Alice, escalus_stanza:chat_to(Bob, <<"Hi, Bob">>)),
+
+        %%When client becomes active again
+        escalus:send(Alice, csi_stanza(<<"active">>)),
+
+        then_client_receives_message(Alice, Msgs),
+        then_client_receives_message(Bob, [<<"Hi, Bob">>])
+    end).
+
+bob_and_alice_get_msgs_from_each_other_after_alice_is_active(Config) ->
+    escalus:fresh_story(Config, [{alice, 1}, {bob, 1}], fun(Alice, Bob) ->
+        Msgs = given_client_is_inactive_but_sends_messages(Alice, Bob, 1),
+        {_MsgsToAlice, MsgsToBob} = Msgs,
+        %%When client becomes active again
+        escalus:send(Alice, csi_stanza(<<"active">>)),
+
+        then_client_receives_message(Bob, MsgsToBob)
+    end).
+
+alice_is_inactive_but_sends_sm_req_and_recives_ack_after_active(Config) ->
+    escalus:fresh_story(Config, [{alice,1}], fun(Alice) ->
+        given_client_is_inactive(Alice),
+
+        escalus:send(Alice, escalus_stanza:sm_request()),
+
+        timer:sleep(1000),
+        escalus_assert:has_no_stanzas(Alice),
+
+        escalus:send(Alice, csi_stanza(<<"active">>)),
+        escalus:assert(is_sm_ack, escalus:wait_for_stanza(Alice))
+
+    end).
+
+given_client_is_inactive_but_sends_messages(Alice, Bob, N) ->
+    %%Given
+    MsgsToAlice = given_client_is_inactive_and_messages_sent(Alice, Bob, N),
+
+    MsgsToBob = gen_msgs(<<"Hi, Bob">>, N),
+    send_msgs(Alice, Bob, MsgsToBob),
+    timer:sleep(1),
+    {MsgsToAlice, MsgsToBob}.
+
+
+then_client_receives_message(Alice, Msgs) ->
+    [escalus:assert(is_chat_message, [Msg], escalus:wait_for_stanza(Alice)) ||
+     Msg <- Msgs].
+
+
+given_client_is_inactive_and_messages_sent(Alice, Bob, N) ->
     %%Given
     given_client_is_inactive(Alice),
 
-    %%When
-    escalus:send(Bob, escalus_stanza:chat_to(Alice, <<"Hi, Alice">>)),
-    timer:sleep(timer:seconds(1)).
+    timer:sleep(1000),
 
+    %%When
+    Msgs = gen_msgs(<<"Hi, Alice">>, N),
+    send_msgs(Bob, Alice, Msgs),
+    timer:sleep(timer:seconds(1)),
+    Msgs.
+
+send_msgs(From, To, Msgs) ->
+    [escalus:send(From, escalus_stanza:chat_to(To, Msg)) ||
+     Msg <- Msgs].
+
+
+gen_msgs(Prefix, N) ->
+    [<<Prefix/binary, (integer_to_binary(I))/binary>> || I <- lists:seq(1, N)].
 
 given_client_is_inactive(Alice) ->
     escalus:send(Alice, csi_stanza(<<"inactive">>)).
