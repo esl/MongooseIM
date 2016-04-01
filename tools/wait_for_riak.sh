5cf4d73b6c6a6333c68e9059972396a79fcd7a88
Michal Piotrowski <michal.piotrowski@erlang-solutions.com>
2016-04-01 12:17:29 +0200

    use wait-for-it.sh to wait for Riak

--- tools/wait_for_riak.sh
+++ tools/wait_for_riak.sh
@@ -3,4 +3,5 @@
 # exposes ports before the service is ready
 IP=$(/usr/bin/docker inspect -f {{.NetworkSettings.IPAddress}} riak)
 echo "riak IP is $IP"
-erl -eval 'try lists:map(fun(N) -> case gen_tcp:connect("'"$IP"'", 8098, [], 1000) of {error,econnrefused} -> io:format("Retry, n=~p~n", [N]), timer:sleep(1000); Other -> io:format("~p", [Other]), init:stop(0) end end, lists:seq(1,60)) after init:stop(1) end.'
+
+tools/wait-for-it.sh -h $IP -p 8098
