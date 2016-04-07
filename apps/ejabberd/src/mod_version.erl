283bf9764e72a38a2325a440e7e4bd22c26f1329
zsuidakra <arkadiusz.gil95@gmail.com>
2016-04-07 18:18:20 +0200

    get system info and mongoose version

--- apps/ejabberd/src/mod_version.erl
+++ apps/ejabberd/src/mod_version.erl
@@ -13,3 +13,26 @@ start(Host, Opts) ->
 stop(Host) ->
     mod_disco:unregister_feature(Host, ?NS_VERSION),
     gen_iq_handler:remove_iq_handler(ejabberd_local, Host, ?NS_VERSION).
+
+process_local_iq(_From, _To, #iq{type = set, sub_el = SubEl} = IQ) ->
+    IQ#iq{type = error, sub_el = [SubEl, ?ERR_NOT_ALLOWED]};
+
+process_local_iq(_From, _To, #iq{type = get} = IQ) ->
+    {Name, Version} = mongoose_info(),
+    System = os_info().
+
+mongoose_info() ->
+    [{Name, Version}] =
+      [{N, V} || {mongooseim, N, V} <- application:which_applications()],
+    {Name, Version}.
+
+os_info() ->
+    {Family, Name} = os:type(),
+    {Major, Minor, Release} = os:version(),
+    list_to_binary(
+        atom_to_list(Family) ++ " " ++
+        atom_to_list(Name) ++ " " ++
+        integer_to_list(Major) ++ "." ++
+        integer_to_list(Minor) ++ "." ++
+        integer_to_list(Release)
+    ).
