1bce0109cbe1fe2a2f1b6a60efbdf97dffa640d8
zsuidakra <arkadiusz.gil95@gmail.com>
2016-04-07 17:29:38 +0200

    add soft version feature

---
+++ apps/ejabberd/src/mod_version.erl
@@ -0,0 +1,15 @@
+-module(mod_version).
+-behaviour(gen_mod).
+-export([start/2, stop/1]).
+-include("jlib.hrl").
+-include("ejabberd.hrl").
+
+start(Host, Opts) ->
+    mod_disco:register_feature(Host, ?NS_VERSION),
+    IQDisc = gen_mod:get_opt(iqdisc, Opts, one_queue),
+    gen_iq_handler:add_iq_handler(ejabberd_local, Host,
+                                  ?NS_VERSION, ?MODULE, process_local_iq, IQDisc).
+
+stop(Host) ->
+    mod_disco:unregister_feature(Host, ?NS_VERSION),
+    gen_iq_handler:remove_iq_handler(ejabberd_local, Host, ?NS_VERSION).
