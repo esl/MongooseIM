20d112c39ff8195ba2e8f8aa08c8fa4b3e2bb232
Michal Piotrowski <michal.piotrowski@erlang-solutions.com>
2016-04-18 13:05:05 +0200

    add index.html file and link to the web page in logs

--- tools/travis-upload-to-s3.sh
+++ tools/travis-upload-to-s3.sh
@@ -18,5 +18,18 @@ if [ -d test/ejabberd_tests/ct_report ]; then
 	cp -Rp test/ejabberd_tests/ct_report/* ${CT_REPORTS}/big
 fi
 
+cat > ${CT_REPORTS}/index.html << EOL
+<html>
+  <head></head>
+  <body>
+    <p><a href="small/index.html">Small tests (apps/ejabberd/test)</a></p>
+    <p><a href="big/index.html">Big tests (test/ejabberd_tests)</a></p>
+  </body>
+</html>
+EOL
+
+echo "Uploading test results to s3"
+echo "http://mongooseim-ct-results.s3-website-eu-west-1.amazonaws.com/${CT_REPORTS}/index.html"
+
 aws s3 cp --recursive --quiet ${CT_REPORTS} s3://mongooseim-ct-results/${CT_REPORTS}
 
