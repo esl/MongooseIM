afb829d9250040507854b992cb3af3754b8b9a05
Michael Uvarov <arcusfelis@gmail.com>
2016-08-12 15:14:45 +0200

    Print errors in init functions in ct_progress_hooK

--- test/ejabberd_tests/src/ct_progress_hook.erl
+++ test/ejabberd_tests/src/ct_progress_hook.erl
@@ -8,7 +8,12 @@
 %% Callbacks
 -export([id/1]).
 -export([init/2]).
--export([post_end_per_testcase/4]).
+-export([post_init_per_suite/4,
+         post_init_per_group/4,
+         post_init_per_testcase/4]).
+-export([post_end_per_suite/4,
+         post_end_per_group/4,
+         post_end_per_testcase/4]).
 -record(state, { }).
 
 %% @doc Return a unique id for this CTH.
@@ -20,14 +25,52 @@ id(_Opts) ->
 init(_Id, _Opts) ->
     {ok, #state{  }}.
 
+post_init_per_suite(_SuiteName, _Config, Return, State) ->
+    handle_init_return(Return),
+    {Return, State}.
+
+post_init_per_group(_GroupName, _Config, Return, State) ->
+    handle_init_return(Return),
+    {Return, State}.
+
+post_init_per_testcase(_TC, _Config, Return, State) ->
+    handle_init_return(Return),
+    {Return, State}.
+
+post_end_per_suite(_SuiteName, _Config, Return, State) ->
+    handle_end_return(Return),
+    {Return, State}.
+
+post_end_per_group(_GroupName, _Config, Return, State) ->
+    handle_end_return(Return),
+    {Return, State}.
+
 %% @doc Called after each test case.
-post_end_per_testcase(TC, _Config, Return, State) ->
+post_end_per_testcase(_TC, _Config, Return, State) ->
+    ct:pal("Return ~p", [Return]),
+    handle_end_return(Return),
+    {Return, State}.
+
+handle_init_return(Return) ->
     case Return of
+        {'EXIT', _} ->
+            file:write_file("/tmp/progress", "-", [append]);
+        {fail, _} ->
+            file:write_file("/tmp/progress", "-", [append]);
+        {skip, _} ->
+            file:write_file("/tmp/progress", "?", [append]);
+        _ ->
+            ok
+    end.
+
+handle_end_return(Return) ->
+    case Return of
+        {'EXIT', _} ->
+            file:write_file("/tmp/progress", "-", [append]);
         {fail, _} ->
             file:write_file("/tmp/progress", "-", [append]);
         {skip, _} ->
             file:write_file("/tmp/progress", "?", [append]);
         _ ->
             file:write_file("/tmp/progress", "+", [append])
-    end,
-    {Return, State}.
+    end.
