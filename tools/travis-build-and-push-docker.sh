e163ac827968449a6738815f606ca7604859d2c1
Michal Piotrowski <michal.piotrowski@erlang-solutions.com>
2016-09-27 14:07:26 +0200

    try Dockerfile from branch less-apt-packages

--- tools/travis-build-and-push-docker.sh
+++ tools/travis-build-and-push-docker.sh
@@ -26,7 +26,7 @@ elif [ ${TRAVIS_BRANCH} == 'master' ]; then
     DOCKERHUB_TAG="latest";
 fi
 
-git clone https://github.com/michalwski/mongooseim-docker.git
+git clone -b less-apt-packages https://github.com/michalwski/mongooseim-docker.git
 cd mongooseim-docker
 
 cp ../${MONGOOSE_TGZ} member
