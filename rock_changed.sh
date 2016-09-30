ce10bb663bb115f23b99afe7fb338cd89dc98b0d
Bartłomiej Górny <bartlomiej.gorny@erlang-solutions.com>
2016-09-30 22:32:19 +0200

    removed deprecated comment, added a todo

--- rock_changed.sh
+++ rock_changed.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 # Runs elvis on modified files (both staged and unstaged)
-# you need a patched version of elvis, the one that accepts filenames
-# on commandline (https://github.com/bartekgorny/elvis/tree/cmdline, not yet in master)
+# TODO: could run on all files modified in a branch
 
 s=$(git status | grep "new file:\|modified:" | grep "erl$")
 s=$(echo $s | sed -e 's/ //g' | cut -d ':' -f 2 | sort | uniq)
