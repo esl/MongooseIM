f718b95708f872469dfd56381b70365639d5b063
Michal Piotrowski <michal.piotrowski@erlang-solutions.com>
2016-10-20 13:08:02 +0200

    add SSE handler to MongooseIM's sessions

--- apps/ejabberd/src/mongoose_client_api_sse.erl
+++ apps/ejabberd/src/mongoose_client_api_sse.erl
@@ -2,6 +2,9 @@
 
 -behaviour(lasse_handler).
 
+-include("ejabberd.hrl").
+-include("jlib.hrl").
+
 -export([init/3]).
 -export([handle_notify/2]).
 -export([handle_info/2]).
@@ -12,8 +15,15 @@ init(_InitArgs, _LastEvtId, Req) ->
     {Authorization, Req2, State} = mongoose_client_api:is_authorized(Req, #{}),
     maybe_init(Authorization, Req2, State).
 
-maybe_init(true, Req, State) ->
-    {ok, Req, State};
+maybe_init(true, Req, #{jid := JID} = State) ->
+    SID = {os:timestamp(), self()},
+    User = JID#jid.user,
+    Server = JID#jid.server,
+    Resource = <<"sse">>,
+
+    ok = ejabberd_sm:open_session(SID, User, Server, Resource, 1, []),
+
+    {ok, Req, State#{sid => SID, jid => jid:replace_resource(JID, Resource)}};
 maybe_init({false, Value}, Req, State) ->
     Headers = [{<<"www-authenticate">>, Value}],
     {shutdown, 401, Headers, <<>>, Req, State}.
@@ -28,4 +38,11 @@ handle_error(_Msg, _Reson, State) ->
     {nosend, State}.
 
 terminate(_Reson, _Req, State) ->
+    case maps:get(sid, State, undefined) of
+        undefined ->
+            ok;
+        SID ->
+            #jid{user = U, server = S, resource = R} = maps:get(jid, State),
+            ejabberd_sm:close_session(SID, U, S, R, normal)
+    end,
     State.
