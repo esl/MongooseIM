593c05cb4511827d95891b82f81fd73e77b82626
Rafał Słota <rafal.slota@erlang-solutions.com>
2016-12-05 11:53:21 +0100

    Remove MAM archive key behaviour

--- apps/ejabberd/src/mam_archive_key.erl
+++
@@ -1,7 +0,0 @@
-%%% @doc Key behaviour
-%%%
-%%% Converts JID and archive id from mod_mam_*_user module into term,
-%%% that represents archive key. Usually used with mnesia.
--module(mam_archive_key).
-
--callback archive_key(mod_mam:archive_id(), ejabberd:jid()) -> term().
