88aa171d65a195c8462293475a7b159a63d22f22
Rafał Słota <rafal.slota@erlang-solutions.com>
2017-01-25 11:05:07 +0100

    Initial code structure for XEP-357

--- apps/ejabberd/src/mod_push_mnesia.erl
+++ apps/ejabberd/src/mod_push_mnesia.erl
@@ -15,9 +15,16 @@
 
 -export([init/2, enable/4, disable/3, get_publish_services/1]).
 
+-record(subscription, {user_jid, pubsub_jid, pubsub_node, forms}).
+
 
 -spec init(Host :: ejabberd:server(), Opts :: list()) -> ok.
-init(Host, Opts) ->
+init(_Host, _Opts) ->
+    mnesia:create_table(push_subscription,
+                        [{disc_only_copies, [node()]},
+                         {type, bag},
+                         {attributes, record_info(fields, push_subscription)}]),
+    mnesia:add_table_copy(offline_msg, node(), disc_only_copies),
     ok.
 
 
