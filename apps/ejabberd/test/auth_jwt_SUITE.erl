913b4fa38211028fcda8bd72ec1cb43ca83dd712
Michal Piotrowski <michal.piotrowski@erlang-solutions.com>
2017-03-24 10:51:22 +0100

    extend jwt tests

--- apps/ejabberd/test/auth_jwt_SUITE.erl
+++ apps/ejabberd/test/auth_jwt_SUITE.erl
@@ -23,11 +23,16 @@ all_tests() ->
      check_password_succeeds_for_correct_token,
      check_password_fails_for_wrong_token,
      check_password_fails_for_correct_token_but_wrong_username,
+     authorize,
      set_password,
      try_register,
      get_password,
      is_user_exists,
-     remove_user
+     remove_user,
+     get_vh_registered_users_number,
+     get_vh_registered_users,
+     store_type,
+     dirty_get_registered_users
     ].
 
 suite() ->
@@ -79,6 +84,15 @@ check_password_fails_for_wrong_token(_C) ->
 check_password_fails_for_correct_token_but_wrong_username(_C) ->
     false = ejabberd_auth_jwt:check_password(<<"alice">>, ?DOMAIN1, generate_token(0)).
 
+authorize(_C) ->
+    Creds0 = mongoose_credentials:new(?DOMAIN1),
+    Creds = mongoose_credentials:extend(Creds0, [{username, ?USERNAME},
+                                                 {password, generate_token(0)},
+                                                 {digest, fake},
+                                                 {digest_gen, fun(A) -> A end}]),
+    {ok, Creds2} = ejabberd_auth_jwt:authorize(Creds),
+    ejabberd_auth_jwt = mongoose_credentials:get(Creds2, auth_module).
+
 set_password(_Config) ->
     {error, not_allowed} = ejabberd_auth_jwt:set_password(<<"alice">>, ?DOMAIN1, <<"mialakota">>).
 
@@ -88,6 +102,7 @@ try_register(_Config) ->
 
 % get_password + get_password_s
 get_password(_Config) ->
+    false = ejabberd_auth_jwt:get_password(<<"anaking">>, ?DOMAIN1),
     <<>> = ejabberd_auth_jwt:get_password_s(<<"anakin">>, ?DOMAIN1).
 
 is_user_exists(_Config) ->
@@ -98,6 +113,18 @@ remove_user(_Config) ->
     ok = ejabberd_auth_jwt:remove_user(<<"toremove3">>, ?DOMAIN1),
     {error, not_allowed} = ejabberd_auth_jwt:remove_user(<<"toremove3">>,
                                                          ?DOMAIN1, <<"wrongpass">>).
+
+get_vh_registered_users_number(_C) ->
+    0 = ejabberd_auth_jwt:get_vh_registered_users_number(?DOMAIN1, []).
+
+get_vh_registered_users(_C) ->
+    [] = ejabberd_auth_jwt:get_vh_registered_users(?DOMAIN1, []).
+
+store_type(_C) ->
+    external = ejabberd_auth_jwt:store_type(?DOMAIN1).
+
+dirty_get_registered_users(_C) ->
+    [] = ejabberd_auth_jwt:dirty_get_registered_users().
 
 %%--------------------------------------------------------------------
 %% Helpers
