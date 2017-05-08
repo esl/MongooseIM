0f3cb15c9f46ad330102d7c2691bdc36c6194302
Michael Uvarov <arcusfelis@gmail.com>
2017-05-08 18:25:29 +0200

    Replace ejabberd_odbc calls with mongoose_rdbms

--- apps/ejabberd/src/mod_muc_db_odbc.erl
+++ apps/ejabberd/src/mod_muc_db_odbc.erl
@@ -21,26 +21,26 @@ init(_Host, _Opts) ->
     ok.
 
 store_room(LServer, Host, Name, Opts) ->
-    SHost = ejabberd_odbc:escape(Host),
-    SName = ejabberd_odbc:escape(Name),
+    SHost = mongoose_rdbms:escape(Host),
+    SName = mongoose_rdbms:escape(Name),
     BinOpts = jlib:term_to_expr(Opts),
-    SBinOpts = ejabberd_odbc:escape(BinOpts),
-    ejabberd_odbc:sql_transaction(
+    SBinOpts = mongoose_rdbms:escape(BinOpts),
+    mongoose_rdbms:sql_transaction(
       LServer,
       fun() ->
-          odbc_queries:update_t(<<"muc_room">>,
+          rdbms_queries:update_t(<<"muc_room">>,
                [<<"name">>, <<"host">>, <<"opts">>],
                [SName, SHost, SBinOpts],
                [<<"name='">>, SName, <<"' AND host='">>, SHost, <<"'">>])
       end).
 
 restore_room(LServer, Host, Name) ->
-    SHost = ejabberd_odbc:escape(Host),
-    SName = ejabberd_odbc:escape(Name),
+    SHost = mongoose_rdbms:escape(Host),
+    SName = mongoose_rdbms:escape(Name),
     Query = ["select opts from muc_room "
               "where name='", SName, "' "
                 "and host='", SHost, "'"],
-    case (catch ejabberd_odbc:sql_query(LServer, Query)) of
+    case (catch mongoose_rdbms:sql_query(LServer, Query)) of
     {selected, _, [{Opts}]} ->
             jlib:expr_to_term(Opts);
     _ ->
@@ -48,33 +48,33 @@ restore_room(LServer, Host, Name) ->
     end.
 
 forget_room(LServer, Host, Name) ->
-    SHost = ejabberd_odbc:escape(Host),
-    SName = ejabberd_odbc:escape(Name),
+    SHost = mongoose_rdbms:escape(Host),
+    SName = mongoose_rdbms:escape(Name),
     Query = ["delete from muc_room "
               "where name='", SName, "' "
                 "and host='", SHost, "'"],
     F = fun () ->
-        ejabberd_odbc:sql_query_t(Query)
+        mongoose_rdbms:sql_query_t(Query)
     end,
-    ejabberd_odbc:sql_transaction(LServer, F).
+    mongoose_rdbms:sql_transaction(LServer, F).
 
 can_use_nick(LServer, Host, JID, Nick) ->
     BinJID = jid:to_binary(jid:to_lower(jid:to_bare(JID))),
-    SHost = ejabberd_odbc:escape(Host),
-    SNick = ejabberd_odbc:escape(Nick),
+    SHost = mongoose_rdbms:escape(Host),
+    SNick = mongoose_rdbms:escape(Nick),
     Query = ["select jid from muc_registered "
               "where nick='", SNick, "' "
                 "and host='", SHost, "'"],
-    case catch ejabberd_odbc:sql_query(LServer, Query) of
+    case catch mongoose_rdbms:sql_query(LServer, Query) of
     {selected, _, [{DbBinJID}]} -> BinJID == DbBinJID;
     _ -> true
     end.
 
 get_rooms(LServer, Host) ->
-    SHost = ejabberd_odbc:escape(Host),
+    SHost = mongoose_rdbms:escape(Host),
     Query = ["select name, opts from muc_room"
               " where host='", SHost, "'"],
-    case catch ejabberd_odbc:sql_query(LServer, Query) of
+    case catch mongoose_rdbms:sql_query(LServer, Query) of
     {selected, _, RoomOpts} ->
         lists:map(
           fun({Room, Opts}) ->
@@ -87,12 +87,12 @@ get_rooms(LServer, Host) ->
     end.
 
 get_nick(LServer, Host, From) ->
-    SHost = ejabberd_odbc:escape(Host),
+    SHost = mongoose_rdbms:escape(Host),
     BinJID = jid:to_binary(jid:to_lower(jid:to_bare(From))),
-    SBinJID = ejabberd_odbc:escape(BinJID),
+    SBinJID = mongoose_rdbms:escape(BinJID),
     Query = ["select nick from muc_registered where"
               " jid='", SBinJID, "' and host='", SHost, "'"],
-    case catch ejabberd_odbc:sql_query(LServer, Query) of
+    case catch mongoose_rdbms:sql_query(LServer, Query) of
     {selected, _, [{Nick}]} -> Nick;
     _ -> error
     end.
@@ -100,15 +100,15 @@ get_nick(LServer, Host, From) ->
 set_nick(LServer, Host, From, <<>>) ->
     unset_nick(LServer, Host, From);
 set_nick(LServer, Host, From, Nick) ->
-    SHost = ejabberd_odbc:escape(Host),
-    SNick = ejabberd_odbc:escape(Nick),
+    SHost = mongoose_rdbms:escape(Host),
+    SNick = mongoose_rdbms:escape(Nick),
     BinJID = jid:to_binary(jid:to_lower(jid:to_bare(From))),
     JidQuery = ["select jid from muc_registered "
                    " where nick='", SNick, "'"
                      " and host='", SHost, "'"],
 
     F = fun () ->
-        case ejabberd_odbc:sql_query_t(JidQuery) of
+        case mongoose_rdbms:sql_query_t(JidQuery) of
             {selected, _, [{J}]} when J == BinJID ->
                 %% Already inserted
                 ok;
@@ -117,27 +117,27 @@ set_nick(LServer, Host, From, Nick) ->
                 false;
             {selected, _, []} ->
                 %% Available nick
-                SBinJID = ejabberd_odbc:escape(BinJID),
+                SBinJID = mongoose_rdbms:escape(BinJID),
                 InsQuery = ["insert into muc_registered (jid, host, nick) "
                              " values ('", SBinJID, "', "
                                       "'", SHost, "', "
                                       "'", SNick, "')"],
-                ejabberd_odbc:sql_query_t(InsQuery),
+                mongoose_rdbms:sql_query_t(InsQuery),
                 ok
         end
     end,
-    ejabberd_odbc:sql_transaction(LServer, F).
+    mongoose_rdbms:sql_transaction(LServer, F).
 
 
 unset_nick(LServer, Host, From) ->
-    SHost = ejabberd_odbc:escape(Host),
+    SHost = mongoose_rdbms:escape(Host),
     BinJID = jid:to_binary(jid:to_lower(jid:to_bare(From))),
-    SBinJID = ejabberd_odbc:escape(BinJID),
+    SBinJID = mongoose_rdbms:escape(BinJID),
     Query = ["delete from muc_registered "
               "where jid='", SBinJID, "' "
                 "and host='", SHost, "'"],
     F = fun () ->
-            ejabberd_odbc:sql_query_t(Query),
+            mongoose_rdbms:sql_query_t(Query),
             ok
     end,
-    ejabberd_odbc:sql_transaction(LServer, F).
+    mongoose_rdbms:sql_transaction(LServer, F).
