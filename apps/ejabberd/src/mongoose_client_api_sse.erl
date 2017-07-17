bc4b93a996a5b1c4ccbe70d304e416df82b9eaac
Bartłomiej Górny <bartlomiej.gorny@erlang-solutions.com>
2017-07-17 16:33:50 +0200

    redundant helper

--- apps/ejabberd/src/mongoose_client_api_sse.erl
+++ apps/ejabberd/src/mongoose_client_api_sse.erl
@@ -37,9 +37,10 @@ handle_notify(_Msg, State) ->
     {nosend, State}.
 
 handle_info({route, _From, _To, Acc}, State) ->
-    handle_info({route, _From, _To, Acc, mongoose_acc:get(element, Acc)}, State);
-handle_info({route, _From, _To, Acc, El}, State) ->
-    handle_msg(mongoose_acc:get(name, Acc), Acc, El, State);
+    handle_msg(mongoose_acc:get(name, Acc), Acc, mongoose_acc:get(element, Acc), State);
+%%    handle_info({route, From, To, Acc, mongoose_acc:get(element, Acc)}, State);
+%%handle_info({route, _From, _To, Acc, El}, State) ->
+%%    handle_msg(mongoose_acc:get(name, Acc), Acc, El, State);
 handle_info(_Msg, State) ->
     {nosend, State}.
 
