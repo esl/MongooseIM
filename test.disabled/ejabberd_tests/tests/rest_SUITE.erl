996d62bf5350c655be45e8bdaa1d707487b193cb
Dominik Stanaszek <dominikstanaszek@icloud.com>
2017-08-29 16:34:10 +0200

    Adjust module to a new rest_helper

--- test.disabled/ejabberd_tests/tests/rest_SUITE.erl
+++ test.disabled/ejabberd_tests/tests/rest_SUITE.erl
@@ -25,11 +25,11 @@
         [assert_inlist/2,
          assert_notinlist/2,
          decode_maplist/1,
-         gett/1,
+         gett/2,
          post/2,
          putt/2,
          delete/1,
-         gett/2,
+         gett/3,
          post/3,
          putt/3,
          delete/2]
@@ -41,6 +41,7 @@
 -define(NOCONTENT, {<<"204">>, <<"No Content">>}).
 -define(ERROR, {<<"500">>, _}).
 -define(NOT_FOUND, {<<"404">>, _}).
+-define(NOT_AUTHORIZED, {<<"401">>, _}).
 
 %%--------------------------------------------------------------------
 %% Suite configuration
@@ -52,16 +53,27 @@
 all() ->
     [
      {group, admin},
-     {group, dynamic_module}
+     {group, dynamic_module},
+     {group, auth},
+     {group, blank_auth}
     ].
 
 groups() ->
     [{admin, [parallel], test_cases()},
+     {auth, [parallel], auth_test_cases()},
+     {blank_auth, [parallel], blank_auth_testcases()},
      {roster, [parallel], [list_contacts,
                              befriend_and_alienate,
                              befriend_and_alienate_auto]},
      {dynamic_module, [], [stop_start_command_module]}
     ].
+
+auth_test_cases() ->
+    [auth_passes_correct_creds,
+     auth_fails_incorrect_creds].
+
+blank_auth_testcases() ->
+    [auth_always_passes_blank_creds].
 
 test_cases() ->
     [commands_are_listed,
@@ -87,16 +99,27 @@ host() ->
 
 init_per_suite(Config) ->
     Config1 = rest_helper:maybe_enable_mam(mam_helper:backend(), host(), Config),
-    escalus:init_per_suite(Config1).
+    Config2 = ejabberd_node_utils:init(Config1),
+    ejabberd_node_utils:backup_config_file(Config2),
+    % Set username and password for blank
+    escalus:init_per_suite(Config2).
 
 end_per_suite(Config) ->
     escalus_fresh:clean(),
     rest_helper:maybe_disable_mam(mam_helper:backend(), host()),
     escalus:end_per_suite(Config).
 
+init_per_group(auth, Config) ->
+    rest_helper:change_admin_creds({<<"ala">>, <<"makota">>}),
+    Config;
+init_per_group(blank_auth, Config) ->
+    rest_helper:change_admin_creds(any),
+    Config;
 init_per_group(_GroupName, Config) ->
     escalus:create_users(Config, escalus:get_users([alice, bob])).
 
+end_per_group(auth, Config) ->
+    rest_helper:change_admin_creds(any);
 end_per_group(_GroupName, Config) ->
     escalus:delete_users(Config, escalus:get_users([alice, bob, mike])).
 
@@ -111,48 +134,65 @@ end_per_testcase(CaseName, Config) ->
 %% Tests
 %%--------------------------------------------------------------------
 
+% Authorization
+auth_passes_correct_creds(Config) ->
+    % try to login with the same creds
+    {?OK, _Lcmds} = gett(admin, <<"/commands">>, {<<"ala">>, <<"makota">>}).
+
+auth_fails_incorrect_creds(Config) ->
+    % try to login with different creds
+    {?NOT_AUTHORIZED, _} = gett(admin, <<"/commands">>, {<<"ola">>, <<"mapsa">>}).
+
+auth_always_passes_blank_creds(Config) ->
+    % we set control creds for blank
+    rest_helper:change_admin_creds(any),
+    % try with any auth
+    {?OK, _Lcmds} = gett(admin, <<"/commands">>, {<<"aaaa">>, <<"bbbb">>}),
+    % try with no auth
+    {?OK, _Lcmds} = gett(admin, <<"/commands">>).
+
 commands_are_listed(_C) ->
-    {?OK, Lcmds} = gett(<<"/commands">>),
+    {?OK, Lcmds} = gett(admin, <<"/commands">>),
     DecCmds = decode_maplist(Lcmds),
     assert_inlist(#{name => <<"list_methods">>}, DecCmds).
 
 non_existent_command_returns404(_C) ->
-    {?NOT_FOUND, _} = gett(<<"/isitthereornot">>).
+    {?NOT_FOUND, _} = gett(admin, <<"/isitthereornot">>).
 
 user_can_be_registered_and_removed(_Config) ->
     % list users
-    {?OK, Lusers} = gett(<<"/users/localhost">>),
+    {?OK, Lusers} = gett(admin, <<"/users/localhost">>),
     Domain = domain(),
     assert_inlist(<<"alice@", Domain/binary>>, Lusers),
     % create user
     CrUser = #{username => <<"mike">>, password => <<"nicniema">>},
-    {?CREATED, _} = post(<<"/users/localhost">>, CrUser),
-    {?OK, Lusers1} = gett(<<"/users/localhost">>),
+    {?CREATED, _} = post(admin, <<"/users/localhost">>, CrUser),
+    {?OK, Lusers1} = gett(admin, <<"/users/localhost">>),
     assert_inlist(<<"mike@", Domain/binary>>, Lusers1),
     % try to create the same user
-    {?ERROR, _} = post(<<"/users/localhost">>, CrUser),
+    {?ERROR, _} = post(admin, <<"/users/localhost">>, CrUser),
     % delete user
-    {?NOCONTENT, _} = delete(<<"/users/localhost/mike">>),
-    {?OK, Lusers2} = gett(<<"/users/localhost">>),
+    {?NOCONTENT, _} = delete(admin, <<"/users/localhost/mike">>),
+    {?OK, Lusers2} = gett(admin, <<"/users/localhost">>),
     assert_notinlist(<<"mike@", Domain/binary>>, Lusers2),
     ok.
 
 sessions_are_listed(_) ->
     % no session
-    {?OK, Sessions} = gett("/sessions/localhost"),
+    {?OK, Sessions} = gett(admin, "/sessions/localhost"),
     true = is_list(Sessions).
 
 session_can_be_kicked(Config) ->
     escalus:story(Config, [{alice, 1}], fun(Alice) ->
         % Alice is connected
         Domain = domain(),
-        {?OK, Sessions1} = gett("/sessions/localhost"),
+        {?OK, Sessions1} = gett(admin, "/sessions/localhost"),
         assert_inlist(<<"alice@", Domain/binary, "/res1">>, Sessions1),
         % kick alice
-        {?NOCONTENT, _} = delete("/sessions/localhost/alice/res1"),
+        {?NOCONTENT, _} = delete(admin, "/sessions/localhost/alice/res1"),
         escalus:wait_for_stanza(Alice),
         true = escalus_connection:wait_for_close(Alice, timer:seconds(1)),
-        {?OK, Sessions2} = gett("/sessions/localhost"),
+        {?OK, Sessions2} = gett(admin, "/sessions/localhost"),
         assert_notinlist(<<"alice@", Domain/binary, "/res1">>, Sessions2)
     end).
 
@@ -169,9 +209,9 @@ send_messages(Alice, Bob) ->
         AliceJID = escalus_utils:jid_to_lower(escalus_client:short_jid(Alice)),
         BobJID = escalus_utils:jid_to_lower(escalus_client:short_jid(Bob)),
         M = #{caller => BobJID, to => AliceJID, body => <<"hello from Bob">>},
-        {?NOCONTENT, _} = post(<<"/messages">>, M),
+        {?NOCONTENT, _} = post(admin, <<"/messages">>, M),
         M1 = #{caller => AliceJID, to => BobJID, body => <<"hello from Alice">>},
-        {?NOCONTENT, _} = post(<<"/messages">>, M1),
+        {?NOCONTENT, _} = post(admin, <<"/messages">>, M1),
         {M, M1}.
 
 messages_are_archived(Config) ->
@@ -184,7 +224,7 @@ messages_are_archived(Config) ->
                                  "/", binary_to_list(BobJID),
                                  "?limit=10"]),
         mam_helper:maybe_wait_for_archive(Config),
-        {?OK, Msgs} = gett(GetPath),
+        {?OK, Msgs} = gett(admin, GetPath),
         [Last, Previous|_] = lists:reverse(decode_maplist(Msgs)),
         <<"hello from Alice">> = maps:get(body, Last),
         AliceJID = maps:get(sender, Last),
@@ -195,7 +235,7 @@ messages_are_archived(Config) ->
                                   "/", binary_to_list(AliceJID),
                                   "/", binary_to_list(BobJID)]),
         mam_helper:maybe_wait_for_archive(Config),
-        {?OK, Msgs1} = gett(GetPath1),
+        {?OK, Msgs1} = gett(admin, GetPath1),
         [Last1, Previous1|_] = lists:reverse(decode_maplist(Msgs1)),
         <<"hello from Alice">> = maps:get(body, Last1),
         AliceJID = maps:get(sender, Last1),
@@ -204,7 +244,7 @@ messages_are_archived(Config) ->
         % and we can do the same without specifying contact
         GetPath2 = lists:flatten(["/messages/", binary_to_list(AliceJID)]),
         mam_helper:maybe_wait_for_archive(Config),
-        {?OK, Msgs2} = gett(GetPath2),
+        {?OK, Msgs2} = gett(admin, GetPath2),
         [Last2, Previous2|_] = lists:reverse(decode_maplist(Msgs2)),
         <<"hello from Alice">> = maps:get(body, Last2),
         AliceJID = maps:get(sender, Last2),
@@ -243,7 +283,7 @@ password_can_be_changed(Config) ->
         skip
     end),
     % we change password
-    {?NOCONTENT, _} = putt("/users/localhost/bob",
+    {?NOCONTENT, _} = putt(admin, "/users/localhost/bob",
                            #{newpass => <<"niemakrolika">>}),
     % he logs with his alternative password
     escalus:story(Config, [{bob_altpass, 1}], fun(_Bob) ->
@@ -256,7 +296,7 @@ password_can_be_changed(Config) ->
         ok
     end,
     % we change it back
-    {?NOCONTENT, _} = putt("/users/localhost/bob",
+    {?NOCONTENT, _} = putt(admin, "/users/localhost/bob",
                            #{newpass => <<"makrolika">>}),
     % now he logs again with the regular one
     escalus:story(Config, [{bob, 1}], fun(_Bob) ->
@@ -273,7 +313,7 @@ list_contacts(Config) ->
             BobJID = escalus_utils:jid_to_lower(escalus_client:short_jid(Bob)),
             add_sample_contact(Bob, Alice),
             % list bob's contacts
-            {?OK, R} = gett(lists:flatten(["/contacts/",
+            {?OK, R} = gett(client, lists:flatten(["/contacts/",
                                            binary_to_list(BobJID)])),
             [R1] =  decode_maplist(R),
             #{jid := AliceJID, subscription := <<"none">>,
@@ -298,28 +338,28 @@ befriend_and_alienate(Config) ->
             check_roster_empty(AlicePath),
             check_roster_empty(BobPath),
             % adds them to rosters
-            {?NOCONTENT, _} = post(AlicePath, #{jid => BobJID}),
-            {?NOCONTENT, _} = post(BobPath, #{jid => AliceJID}),
+            {?NOCONTENT, _} = post(admin, AlicePath, #{jid => BobJID}),
+            {?NOCONTENT, _} = post(admin, BobPath, #{jid => AliceJID}),
             check_roster(BobPath, AliceJID, none, none),
             check_roster(AlicePath, BobJID, none, none),
             % now do the subscription sequence
             PutPathA = lists:flatten([AlicePath, "/", BobS]),
-            {?NOCONTENT, _} = putt(PutPathA, #{action => <<"subscribe">>}),
+            {?NOCONTENT, _} = putt(admin, PutPathA, #{action => <<"subscribe">>}),
             check_roster(AlicePath, BobJID, none, out),
             PutPathB = lists:flatten([BobPath, "/", AliceS]),
-            {?NOCONTENT, _} = putt(PutPathB, #{action => <<"subscribed">>}),
+            {?NOCONTENT, _} = putt(admin, PutPathB, #{action => <<"subscribed">>}),
             check_roster(AlicePath, BobJID, to, none),
             check_roster(BobPath, AliceJID, from, none),
-            {?NOCONTENT, _} = putt(PutPathB, #{action => <<"subscribe">>}),
+            {?NOCONTENT, _} = putt(admin, PutPathB, #{action => <<"subscribe">>}),
             check_roster(BobPath, AliceJID, from, out),
-            {?NOCONTENT, _} = putt(PutPathA, #{action => <<"subscribed">>}),
+            {?NOCONTENT, _} = putt(admin, PutPathA, #{action => <<"subscribed">>}),
             check_roster(AlicePath, BobJID, both, none),
             check_roster(BobPath, AliceJID, both, none),
             % now remove
-            {?NOCONTENT, _} = delete(PutPathA),
+            {?NOCONTENT, _} = delete(admin, PutPathA),
             check_roster_empty(AlicePath),
             check_roster(BobPath, AliceJID, none, none),
-            {?NOCONTENT, _} = delete(PutPathB),
+            {?NOCONTENT, _} = delete(admin, PutPathB),
             check_roster_empty(BobPath),
             APushes = lists:filter(fun escalus_pred:is_roster_set/1,
                                     escalus:wait_for_stanzas(Alice, 20)),
@@ -364,10 +404,10 @@ befriend_and_alienate_auto(Config) ->
                                      BobS,
                                      "/manage"
             ]),
-            {?NOCONTENT, _} = putt(ManagePath, #{action => <<"connect">>}),
+            {?NOCONTENT, _} = putt(admin, ManagePath, #{action => <<"connect">>}),
             check_roster(AlicePath, BobJID, both, none),
             check_roster(BobPath, AliceJID, both, none),
-            {?NOCONTENT, _} = putt(ManagePath, #{action => <<"disconnect">>}),
+            {?NOCONTENT, _} = putt(admin, ManagePath, #{action => <<"disconnect">>}),
             check_roster_empty(AlicePath),
             check_roster_empty(BobPath),
             APushes = lists:filter(fun escalus_pred:is_roster_set/1,
@@ -394,14 +434,14 @@ befriend_and_alienate_auto(Config) ->
 %%--------------------------------------------------------------------
 
 check_roster(Path, Jid, Subs, Ask) ->
-    {?OK, R} = gett(Path),
+    {?OK, R} = gett(admin, Path),
     S = atom_to_binary(Subs, latin1),
     A = atom_to_binary(Ask, latin1),
     Res = decode_maplist(R),
     [#{jid := Jid, subscription := S, ask := A}] = Res.
 
 check_roster_empty(Path) ->
-    {?OK, R} = gett(Path),
+    {?OK, R} = gett(admin, Path),
     [] = decode_maplist(R).
 
 get_messages(Me, Other, Count) ->
@@ -409,7 +449,7 @@ get_messages(Me, Other, Count) ->
                              binary_to_list(Me),
                              "/", binary_to_list(Other),
                              "?limit=", integer_to_list(Count)]),
-    {?OK, Msgs} = gett(GetPath),
+    {?OK, Msgs} = gett(admin, GetPath),
     Msgs.
 
 get_messages(Me, Other, Before, Count) ->
@@ -418,7 +458,7 @@ get_messages(Me, Other, Before, Count) ->
                              "/", binary_to_list(Other),
                              "?before=", integer_to_list(Before),
                              "&limit=", integer_to_list(Count)]),
-    {?OK, Msgs} = gett(GetPath),
+    {?OK, Msgs} = gett(admin, GetPath),
     Msgs.
 
 stop_start_command_module(_) ->
@@ -428,12 +468,12 @@ stop_start_command_module(_) ->
     %% resource then the same test will succeed. With the precondition
     %% described above we test both transition from `started' to
     %% `stopped' and from `stopped' to `started'.
-    {?OK, _} = gett(<<"/commands">>),
+    {?OK, _} = gett(admin, <<"/commands">>),
     {atomic, ok} = dynamic_modules:stop(host(), mod_commands),
-    {?NOT_FOUND, _} = gett(<<"/commands">>),
+    {?NOT_FOUND, _} = gett(admin, <<"/commands">>),
     ok = dynamic_modules:start(host(), mod_commands, []),
     timer:sleep(200), %% give the server some time to build the paths again
-    {?OK, _} = gett(<<"/commands">>).
+    {?OK, _} = gett(admin, <<"/commands">>).
 
 to_list(V) when is_binary(V) ->
     binary_to_list(V);
