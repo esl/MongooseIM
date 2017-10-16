c2f971b779cf34a57bd02dcc0817d75e0b8e78df
Dominik Stanaszek <dominikstanaszek@icloud.com>
2017-10-16 16:51:12 +0200

    Implement regression tests

--- apps/ejabberd/test/mongoose_rdbms_SUITE.erl
+++ apps/ejabberd/test/mongoose_rdbms_SUITE.erl
@@ -11,6 +11,7 @@
 
 -define(KEEPALIVE_INTERVAL, 1).
 -define(KEEPALIVE_QUERY, <<"SELECT 1;">>).
+-define(MAX_INTERVAL, 30).
 
 all() ->
     [{group, odbc},
@@ -33,6 +34,8 @@ groups() ->
 
 tests() ->
     [keepalive_interval,
+     does_backoff_exceed,
+     does_backoff_increase,
      keepalive_exit].
 
 init_per_group(odbc, Config) ->
@@ -48,12 +51,30 @@ init_per_group(Group, Config) ->
 end_per_group(_, Config) ->
     Config.
 
+init_per_testcase(does_backoff_exceed, Config) ->
+    DbType = ?config(db_type, Config),
+    meck_config(DbType, ?KEEPALIVE_INTERVAL, ?MAX_INTERVAL),
+    meck_db(DbType),
+    meck_connection_error(DbType),
+    Config;
+init_per_testcase(does_backoff_increase, Config) ->
+    DbType = ?config(db_type, Config),
+    meck_config(DbType, 2, 10),
+    meck_db(DbType),
+    meck_connection_error(DbType),
+    Config;
 init_per_testcase(_, Config) ->
     DbType = ?config(db_type, Config),
-    meck_config(DbType, ?KEEPALIVE_INTERVAL),
+    meck_config(DbType, ?KEEPALIVE_INTERVAL, ?MAX_INTERVAL),
     meck_db(DbType),
     Config.
 
+end_per_testcase(T, Config) when T =:= does_backoff_icrease,
+                                 T =:= does_backoff_exceed ->
+    Db = ?config(db_type, Config),
+    meck_connection_error_unload(Db),
+    meck_unload(Db),
+    Config;
 end_per_testcase(_, Config) ->
     meck_unload(?config(db_type, Config)),
     Config.
@@ -81,8 +102,50 @@ keepalive_exit(Config) ->
         ct:fail(no_down_message)
     end.
 
+%% 5 retries. Max retry 1 and is 1 as well.
+%% 5 times one second.
+%% Proccess should die after 5 seconds. No longer.
+does_backoff_exceed(Config) ->
+    {Time, {error, _}} = timer:tc(gen_server, start, [mongoose_rdbms, default, []]),
+    ct:log("Time: ~p~n", [Time]),
+    ?assert(Time < 5100 * 1000).
+
+%% 5 retries. Max retry 10. Iniitial retry 2.
+%% We should get a sequence: 2 -> 4 -> 10 -> 10 -> 10.
+does_backoff_increase(Config) ->
+    meck_rand(),    % move to init per testcase
+    {error, _} = gen_server:start(mongoose_rdbms, default, []),
+    % We expect to have 2 at the begininng, then values up to 10 and 10 three times in total
+    receive_backoffs(2, 10, 3),
+    meck_unload_rand(). % move to end per testcase
+
+receive_backoffs(InitialValue, MaxValue, MaxCount) ->
+    receive_backoffs(InitialValue, MaxValue, MaxCount, 0).
+
+receive_backoffs(ExpectedVal, MaxValue, MaxCountExpected, MaxCount) ->
+    receive
+        {backoff, MaxValue} when MaxCount =:= MaxCountExpected - 1 ->
+            ok;
+        {backoff, MaxValue} ->
+            receive_backoffs(MaxValue, MaxValue, MaxCountExpected, MaxCount + 1);
+        {backoff, ExpectedVal} ->
+            receive_backoffs(min(ExpectedVal * ExpectedVal, MaxValue), MaxValue, MaxCountExpected, MaxCount)
+    after 5000 -> % Lower this
+            ct:fail(no_backoff)
+    end.
+
 %% Mocks
-meck_config(Server, KeepaliveInterval) ->
+
+meck_rand() ->
+    meck:new(rand, [unstick, no_link]),
+    Self = self(),
+    Fun = fun(Val) -> ct:log("sending backoff: ~p to pid: ~p~n", [Val, Self]), Self ! {backoff, Val}, 0 end,
+    meck:expect(rand, uniform, Fun).
+
+meck_unload_rand() ->
+    meck:unload(rand).
+
+meck_config(Server, KeepaliveInterval, MaxInterval) ->
     meck:new(ejabberd_config, [no_link]),
     meck:expect(ejabberd_config, get_local_option,
                 fun(max_fsm_queue) -> 1024;
@@ -91,7 +154,7 @@ meck_config(Server, KeepaliveInterval) ->
     meck:new(mongoose_rdbms_sup, [no_link, passthrough]),
     meck:expect(mongoose_rdbms_sup, get_option,
                 fun(default, odbc_keepalive_interval) -> KeepaliveInterval;
-                   (default, odbc_start_interval) -> 30;
+                   (default, odbc_start_interval) -> MaxInterval;
                    (default, odbc_server) -> server(Server)
                 end).
 
@@ -113,6 +176,25 @@ meck_db(pgsql) ->
                 fun(_Ref, <<"SET", _/binary>>)       -> {ok, 0};
                    (_Ref, [<<"SET", _/binary>> | _]) -> {ok, 0};
                    (_Ref, [<<"SELECT", _/binary>>])  -> {ok, [], [{1}]} end).
+
+meck_connection_error(pgsql) ->
+    meck:delete(epgsql, connect, 1),
+    meck:expect(epgsql, connect, fun(_) -> connection_error end);
+meck_connection_error(odbc) ->
+    meck:delete(odbc, connect, 2),
+    meck:expect(odbc, connect, fun(_, _) -> connection_error end);
+meck_connection_error(mysql) ->
+    meck:delete(mongoose_rdbms_mysql, connect, 2),
+    meck:expect(mongoose_rdbms_mysql, connect, fun(_, _) -> {error, connection_error} end).
+
+meck_connection_error_unload(pgsql) ->
+    meck:unload(epgsql);
+meck_connection_error_unload(odbc) ->
+    meck:unload(odbc);
+meck_connection_error_unload(mysql) ->
+    meck:delete(mongoose_rdbms_msql, connect, 2),
+    meck:unload(mongoose_rdbms_mysql).
+
 
 meck_error(odbc) ->
     meck:expect(odbc, sql_query,
