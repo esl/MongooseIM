b2be27cbbc4327900230f72d39752cb20513d5b2
kanes115 <dominikstanaszek@icloud.com>
2017-11-02 15:25:30 +0100

    [skip ci] Remove unnecessary todo
    This is not valid todo anymore as timestamp is
    only used internally

--- src/auth/ejabberd_auth_external.erl
+++ src/auth/ejabberd_auth_external.erl
@@ -365,7 +365,6 @@ is_fresh_enough(TimeStampLast, CacheTime) ->
 
 %% @doc Code copied from mod_configure.erl
 %% Code copied from web/ejabberd_web_admin.erl
-%% TODO: Update time format to XEP-0202: Entity Time
 -spec get_last_access(User :: jid:user(),
                       Server :: jid:server()
                       ) -> online | never | mod_last_required | integer().
