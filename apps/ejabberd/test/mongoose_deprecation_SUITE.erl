e5aa2276578f15eb79d98239fe1008ec8f8f81e1
Dominik Stanaszek <dominikstanaszek@icloud.com>
2017-11-07 12:32:51 +0100

    Fill all() list

--- apps/ejabberd/test/mongoose_deprecation_SUITE.erl
+++ apps/ejabberd/test/mongoose_deprecation_SUITE.erl
@@ -13,23 +13,27 @@
 
 all() ->
     [
-     
+        check_too_fast_pace,
+        check_ok_pace,
+        check_changing_pace
     ].
 
 init_per_suite(C) ->
+    mongoose_deprecation:start(),
     meck:new(mongoose_depreacation, [passthrough]),
     meck:excpect(mongoose_depreacation, 
                  default_cooldown, 
                  fun() -> 6000 * 200 end),    % change 6h to 200 ms
     meck:new(lager, []),
     Self = self(),
-    meck:excpect(lager, log, fun(lager_event, _, Self, ?LOG_MSG, _) ->
+    meck:excpect(lager, log, fun(lager_event, _, _, ?LOG_MSG, _) ->
                                      Self ! logged;
                                 (_, _, _, _, _) -> ok end),
     C.
     
 end_per_suite(C) ->
-    meck:unload().
+    meck:unload(),
+    C.
 
 init_per_test(C) ->
     mongoose_deprecations:start(),
@@ -39,13 +43,13 @@ end_per_testcase(C) ->
     mongoose_deprecations:stop(),
     C.
 
-check_too_fast_pace(Config) ->
+check_too_fast_pace(_Config) ->
     mongoose_deprecations:log(some_tag, ?LOG_MSG),
     mongoose_deprecations:log(some_tag, ?LOG_MSG),
     mongoose_deprecations:log(some_tag, ?LOG_MSG),
     assert_n_logged(1).
 
-check_ok_pace(Config) ->
+check_ok_pace(_Config) ->
     mongoose_deprecations:log(some_tag, ?LOG_MSG),
     timer:sleep(?TEST_COOLDOWN + ?COOLDOWN_EPS),
     mongoose_deprecations:log(some_tag, ?LOG_MSG),
@@ -53,7 +57,7 @@ check_ok_pace(Config) ->
     mongoose_deprecations:log(some_tag, ?LOG_MSG),
     assert_n_logged(3).
 
-check_changing_pace(Config) ->
+check_changing_pace(_Config) ->
     mongoose_deprecations:log(some_tag, ?LOG_MSG),
     timer:sleep(?TEST_COOLDOWN + ?COOLDOWN_EPS),
     mongoose_deprecations:log(some_tag, ?LOG_MSG),
@@ -66,15 +70,15 @@ check_changing_pace(Config) ->
 assert_n_logged(0) ->
     receive
         logged ->
-            ct:fail(to_many_logged)
+            ct:fail(too_many_logged)
     after ?TEST_COOLDOWN * 3 ->
               ok
     end;
 assert_n_logged(N) ->
     receive
         logged ->
-            assert_n_logged(N)
+            assert_n_logged(N - 1)
     after ?TEST_COOLDOWN * 3 ->
-              ct:fail(to_little_logged)
+              ct:fail(too_little_logged)
     end.
 
