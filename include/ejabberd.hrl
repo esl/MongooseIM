2b3eed436bd228e4cb5875d81ef0b18fc8f601f0
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2017-11-16 16:09:49 +0100

    Extract logging macros to mongoose_logger.hrl

--- include/ejabberd.hrl
+++ include/ejabberd.hrl
@@ -39,21 +39,7 @@
 
 %% ---------------------------------
 %% Logging mechanism
-
--define(DEBUG(Format, Args),
-    lager:debug(Format, Args)).
-
--define(INFO_MSG(Format, Args),
-    lager:info(Format, Args)).
-
--define(WARNING_MSG(Format, Args),
-    lager:warning(Format, Args)).
-
--define(ERROR_MSG(Format, Args),
-    lager:error(Format, Args)).
-
--define(CRITICAL_MSG(Format, Args),
-    lager:critical(Format, Args)).
+-include("mongoose_logger.hrl").
 
 -record(session, {sid,
                   usr,
