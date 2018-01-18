715fb04268a20ee161049d97481fb5014daf3e1b
Michael Uvarov <arcusfelis@gmail.com>
2018-01-18 17:54:57 +0100

    Allow unicode in commit description ¯\_(ツ)_/¯ #1674
    Хорошо

--- tools/travis-build-tests.sh
+++ tools/travis-build-tests.sh
@@ -5,5 +5,8 @@ source tools/travis-common-vars.sh
 TRAVIS_DB_PASSWORD=$(cat /tmp/travis_db_password)
 ${TOOLS}/set-odbc-password test ${TRAVIS_DB_PASSWORD}
 
+# rebar doesn't like Unicode in commit messages #1674
+# So, we unset commit message env variable
+export TRAVIS_COMMIT_MESSAGE=no_message
 cd ${BASE}/test.disabled/ejabberd_tests && make prepare
 
