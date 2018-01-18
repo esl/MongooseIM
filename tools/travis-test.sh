50b6ae40dc260a5096a2fb96140266bee57ddbac
Michael Uvarov <freeakk@gmail.com>
2018-01-18 13:13:19 +0100

    Docker compose in docker (#1653)
    * Add longer description of skip flag in travis-test.sh
    
    * Run docker-compose.sh inside docker
    
    Time report docker-compose.sh command
    
    * Do not download docker-compose
    
    * Add Advice comment
    
    * Add docker-compose.sh script

--- tools/travis-test.sh
+++ tools/travis-test.sh
@@ -80,7 +80,10 @@ run_small_tests() {
   echo "############################"
   echo "Running small tests (test/)"
   echo "############################"
-  echo "Add option -s false to skip embeded common tests"
+  echo "Advice: "
+  echo "    Add option \"-s false\" to skip embeded common tests"
+  echo "Example: "
+  echo "    ./tools/travis-test.sh -s false"
   make ct
   SMALL_SUMMARIES_DIRS=${BASE}/_build/test/logs/ct_run*
   SMALL_SUMMARIES_DIR=$(summaries_dir ${SMALL_SUMMARIES_DIRS} 1)
@@ -100,7 +103,9 @@ maybe_run_small_tests() {
 start_services() {
     for env in ${BASE}/test.disabled/ejabberd_tests/services/*-compose.yml; do
         echo "Stating service" $(basename "${env}") "..."
-        docker-compose -f "${env}" up -d
+        time ${BASE}/tools/docker-compose.sh -f "${env}" up -d
+        echo "docker-compose execution time reported above"
+        echo ""
     done
 }
 
