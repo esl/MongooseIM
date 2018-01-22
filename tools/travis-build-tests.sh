544e547705fd89f82b6299023f95cfc77abc3d09
Piotr Nosek <piotr.nosek@erlang-solutions.com>
2018-01-22 15:46:28 +0100

    [skip ci] Add a TODO for TRAVIS_COMMIT_MESSAGE hack

--- tools/travis-build-tests.sh
+++ tools/travis-build-tests.sh
@@ -7,6 +7,7 @@ ${TOOLS}/set-odbc-password test ${TRAVIS_DB_PASSWORD}
 
 # rebar doesn't like Unicode in commit messages #1674
 # So, we unset commit message env variable
+# TODO: Remove it when we switch to rebar3 for big tests.
 export TRAVIS_COMMIT_MESSAGE=no_message
 cd ${BASE}/test.disabled/ejabberd_tests && make prepare
 
