88c03d051b62d1c04f95452105ab3208f0df2d4c
Michael Uvarov <freeakk@gmail.com>
2018-03-02 18:38:50 +0100

    Run riak in a docker container for CI #1747
    Add healthcheck for Docker MySQL container

--- tools/travis-setup-db.sh
+++ tools/travis-setup-db.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Environment variable DB is used by this script.
+# If DB is undefined, than this script does nothing.
+
+set -e
 
 TOOLS=`dirname $0`
-
-echo $DB
 
 source tools/travis-common-vars.sh
 
@@ -16,12 +19,11 @@ MYSQL_DIR=/etc/mysql/conf.d
 
 PGSQL_ODBC_CERT_DIR=~/.postgresql
 
-RIAK_DIR=/etc/riak
-
 SSLDIR=${BASE}/${TOOLS}/ssl
 
-if [ $DB = 'mysql' ]; then
+if [ "$DB" = 'mysql' ]; then
     echo "Configuring mysql"
+    # TODO We should not use sudo
     sudo -n service mysql stop || echo "Failed to stop mysql"
     mkdir -p ${SQL_TEMP_DIR}
     cp ${SSLDIR}/fake_cert.pem ${SQL_TEMP_DIR}/.
@@ -36,9 +38,10 @@ if [ $DB = 'mysql' ]; then
         -v ${MIM_PRIV_DIR}/mysql.sql:/docker-entrypoint-initdb.d/mysql.sql:ro \
         -v ${BASE}/${TOOLS}/docker-setup-mysql.sh:/docker-entrypoint-initdb.d/docker-setup-mysql.sh \
         -v ${SQL_TEMP_DIR}:${SQL_TEMP_DIR} \
+        --health-cmd='mysqladmin ping --silent' \
         -p 3306:3306 --name=mongooseim-mysql mysql
 
-elif [ $DB = 'pgsql' ]; then
+elif [ "$DB" = 'pgsql' ]; then
     echo "Configuring postgres with SSL"
     sudo service postgresql stop || echo "Failed to stop psql"
     mkdir ${SQL_TEMP_DIR}
@@ -68,18 +71,50 @@ Debug                = 1
 ByteaAsLongVarBinary = 1
 EOL
 
-elif [ $DB = 'riak' ]; then
+elif [ "$DB" = 'riak' ]; then
     echo "Configuring Riak with SSL"
-    sudo cp ${SSLDIR}/fake_cert.pem ${RIAK_DIR}/cert.pem
-    sudo cp ${SSLDIR}/fake_key.pem ${RIAK_DIR}/key.pem
-    sudo cp ${SSLDIR}/ca/cacert.pem ${RIAK_DIR}/cacertfile.pem
-    sudo cp ${DB_CONF_DIR}/advanced.config ${RIAK_DIR}/advanced.config
-    sudo cp ${DB_CONF_DIR}/riak.conf ${RIAK_DIR}/riak.conf
-    sudo service riak restart
-    echo "Setup Riak"
-    sudo tools/setup_riak mongooseim_secret
+    # Instead of docker run, use "docker create" + "docker start".
+    # So we can prepare our container.
+    # We use HEALTHCHECK here, check "docker ps" to get healthcheck status.
+    # We can't use volumes for riak.conf, because:
+    # - we want to change it
+    # - container starting code runs sed on it and gets IO error,
+    #   if it's a volume
+    time docker create -p 8087:8087 -p 8098:8098 \
+        -e DOCKER_RIAK_BACKEND=leveldb \
+        -e DOCKER_RIAK_CLUSTER_SIZE=1 \
+        --name=mongooseim-riak \
+        -v "${DB_CONF_DIR}/advanced.config:/etc/riak/advanced.config:ro" \
+        -v "${SSLDIR}/fake_cert.pem:/etc/riak/cert.pem:ro" \
+        -v "${SSLDIR}/fake_key.pem:/etc/riak/key.pem:ro" \
+        -v "${SSLDIR}/ca/cacert.pem:/etc/riak/ca/cacertfile.pem:ro" \
+        --health-cmd='riak-admin status' \
+        "michalwski/docker-riak:1.0.6"
+    # Use a temporary file to store config
+    TEMP_RIAK_CONF=$(mktemp)
+    # Export config from a container
+    docker cp "mongooseim-riak:/etc/riak/riak.conf" "$TEMP_RIAK_CONF"
+    # Enable search
+    sed -i "s/^search = \(.*\)/search = on/" "$TEMP_RIAK_CONF"
+    # Enable ssl by appending settings from riak.conf.ssl
+    cat "${DB_CONF_DIR}/riak.conf.ssl" >> "$TEMP_RIAK_CONF"
+    # Import config back into container
+    docker cp "$TEMP_RIAK_CONF" "mongooseim-riak:/etc/riak/riak.conf"
+    # Erase temporary config file
+    rm "$TEMP_RIAK_CONF"
+    docker start mongooseim-riak
+    echo "Waiting for docker healthcheck"
+    echo ""
+    tools/wait_for_healthcheck.sh mongooseim-riak
+    echo "Waiting for a listener to appear"
+    tools/wait_for_service.sh mongooseim-riak 8098
+    # Use riak-admin from inside the container
+    export RIAK_ADMIN="docker exec mongooseim-riak riak-admin"
+    tools/setup_riak
+    # Use this command to read Riak's logs if something goes wrong
+    # docker exec -it mongooseim-riak bash -c 'tail -f /var/log/riak/*'
 
-elif [ $DB = 'cassandra' ]; then
+elif [ "$DB" = 'cassandra' ]; then
     docker image pull cassandra:${CASSANDRA_VERSION}
 
     opts="$(docker inspect -f '{{range .Config.Entrypoint}}{{println}}{{.}}{{end}}' cassandra:${CASSANDRA_VERSION})"
@@ -108,4 +143,6 @@ elif [ $DB = 'cassandra' ]; then
                --link cassandra:cassandra                       \
                cassandra:${CASSANDRA_VERSION}                   \
                sh -c 'exec cqlsh "$CASSANDRA_PORT_9042_TCP_ADDR" --ssl -f /cassandra.cql'
+else
+    echo "Skip setting up database"
 fi
