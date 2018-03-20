ffa8447e07abad10bbfa964cdb234121dae7ebc4
Dominik Stanaszek <dominikstanaszek@icloud.com>
2018-03-20 08:55:11 +0100

    Add logging to debug

--- src/ejabberd_config.erl
+++ src/ejabberd_config.erl
@@ -1127,15 +1127,18 @@ compute_config_version(LC, LCH) ->
     crypto:hash(sha, term_to_binary(L1)).
 
 compute_config_file_version(#state{opts = Opts, hosts = Hosts}) ->
-    Opts2 = filter_out_node_specific_options(Opts),
+    Opts2 = filter_out_node_specific_options(Opts, wrapper),
     L = sort_config(Opts2 ++ Hosts),
     crypto:hash(sha, term_to_binary(L)).
 
+filter_out_node_specific_options(Opts, wrapper) ->
+    filter_out_node_specific_options(Opts).
 
 filter_out_node_specific_options([]) ->
     [];
 filter_out_node_specific_options([{local_config, {modules, Host}, Mods} | Opts]) -> % use record
-    NewMods = lists:foldl(fun(Path, Mods) -> delete_path_in_proplist(Mods, Path) end,
+    ?ERROR_MSG("[reload_cluster2] modules for host: ~p", [Host]),
+    NewMods = lists:foldl(fun(Path, Mods) -> ?ERROR_MSG("deleting ~p", [Path]), delete_path_in_proplist(Mods, Path) end,
                           Mods, node_specific_module_options()),
     [{local_config, {modules, Host}, NewMods} | filter_out_node_specific_options(Opts)];
 filter_out_node_specific_options([Opt | Opts]) ->
