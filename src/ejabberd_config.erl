b9884386d32b3a52fe83d2bc9a4371303b8c0120
Dominik Stanaszek <dominikstanaszek@icloud.com>
2018-03-20 09:37:43 +0100

    Fix typo

--- src/ejabberd_config.erl
+++ src/ejabberd_config.erl
@@ -1151,7 +1151,7 @@ filter_out_node_specific_options([Opt | Opts]) ->
 node_specific_module_options() ->
     [
      [mod_global_distrib, connections, endpoints],
-     [mod_global_ditrib, redis, server]
+     [mod_global_distrib, redis, server]
     ].
 
 delete_path_in_proplist(Plist, [Step]) ->
