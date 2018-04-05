89f9d946f088a9ad18b691e242388a1da228f440
Dominik Stanaszek <dominikstanaszek@icloud.com>
2018-04-05 11:25:46 +0200

    Move files that got lost after introducing big_tests/

--- big_tests/tests/push_helper.erl
+++ big_tests/tests/push_helper.erl
@@ -1,10 +1,9 @@
 -module(push_helper).
-
--include_lib("exml/include/exml.hrl").
 -include_lib("escalus/include/escalus.hrl").
 -include_lib("escalus/include/escalus_xmlns.hrl").
+-include_lib("exml/include/exml.hrl").
 
--define(NS_XDATA,               <<"jabber:x:data">>).
+-include("push_helper.hrl").
 
 -export([enable_stanza/2, enable_stanza/3, enable_stanza/4,
          disable_stanza/1, disable_stanza/2,
@@ -19,18 +18,18 @@ ns_push() -> <<"urn:xmpp:push:0">>.
 ns_pubsub_pub_options() -> <<"http://jabber.org/protocol/pubsub#publish-options">>.
 push_form_type()-> <<"urn:xmpp:push:summary">>.
 
-
 disable_stanza(JID, undefined) ->
     disable_stanza([
-        {<<"xmlns">>, <<"urn:xmpp:push:0">>},
-        {<<"jid">>, JID}
-    ]);
+                    {<<"xmlns">>, <<"urn:xmpp:push:0">>},
+                    {<<"jid">>, JID}
+                   ]);
 disable_stanza(JID, Node) ->
     disable_stanza([
-        {<<"xmlns">>, <<"urn:xmpp:push:0">>},
-        {<<"jid">>, JID},
-        {<<"node">>, Node}
-    ]).
+                    {<<"xmlns">>, <<"urn:xmpp:push:0">>},
+                    {<<"jid">>, JID},
+                    {<<"node">>, Node}
+                   ]).
+
 disable_stanza(JID) when is_binary(JID) ->
     disable_stanza(JID, undefined);
 disable_stanza(Attrs) when is_list(Attrs) ->
@@ -93,4 +92,3 @@ wait_for(TimeLeft, Fun) ->
             timer:sleep(Step),
             wait_for(TimeLeft - Step, Fun)
     end.
-
