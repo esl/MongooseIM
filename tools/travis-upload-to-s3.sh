f59c692391d8bffca1404ab1a0cbd1d294f7ca5e
Michael Uvarov <arcusfelis@gmail.com>
2018-05-23 14:19:20 +0200

    Use aws-s3-cp instead of aws-s3-sync

--- tools/travis-upload-to-s3.sh
+++ tools/travis-upload-to-s3.sh
@@ -8,5 +8,9 @@ CT_REPORTS=$(ct_reports_dir)
 
 echo "Uploading test results to s3"
 echo $(s3_url ${CT_REPORTS})
-sudo pip install awscli --ignore-installed six
-aws s3 sync --quiet ${CT_REPORTS} s3://${AWS_BUCKET:-mongooseim-ct-results}/${CT_REPORTS}
+
+echo "Installing awscli"
+sudo time pip install awscli --ignore-installed six
+
+echo "Uploading"
+time aws s3 cp --recursive --quiet ${CT_REPORTS} s3://${AWS_BUCKET:-mongooseim-ct-results}/${CT_REPORTS}
