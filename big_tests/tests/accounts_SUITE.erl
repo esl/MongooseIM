a76de4c049e6f2188fba0970d693d108b6211287
Ludwik Bukowski <ludwik.bukowski@gmail.com>
2018-05-29 11:26:33 +0200

    make accounts_SUITE parallel part 1

--- big_tests/tests/accounts_SUITE.erl
+++ big_tests/tests/accounts_SUITE.erl
@@ -22,6 +22,7 @@
 all() ->
     [
      {group, register},
+     {group, registration_watchers},
      {group, bad_registration},
      {group, bad_cancelation},
      {group, registration_timeout},
@@ -29,16 +30,18 @@ all() ->
     ].
 
 groups() ->
-    [{register, [sequence], [register,
+    [{register, [parallel], [register,
                              already_registered,
+                             registration_conflict,
                              check_unregistered]},
-     {bad_registration, [no_sequence], [null_password]},
-     {bad_cancelation, [no_sequence], [bad_request_registration_cancelation,
-                                       not_allowed_registration_cancelation]},
+     {registration_watchers, [sequence], [admin_notify]},
+     {bad_registration, [sequence], [null_password]},
+     {bad_cancelation, [sequence], [bad_request_registration_cancelation,
+                                    not_allowed_registration_cancelation]},
      {registration_timeout, [sequence], [registration_timeout,
                                          registration_failure_timeout]},
-     {change_account_details, [no_sequence], [change_password,
-                                              change_password_to_null]}
+     {change_account_details, [parallel], [change_password,
+                                           change_password_to_null]}
      ].
 
 suite() ->
@@ -48,19 +51,32 @@ suite() ->
 %% Init & teardown
 %%--------------------------------------------------------------------
 
-init_per_suite(Config) ->
-    escalus:init_per_suite(Config).
+init_per_suite(Config1) ->
+    Host = ct:get_config({hosts, mim, domain}),
+    ok = dynamic_modules:ensure_modules(Host, required_modules()),
+    Config2 = [{mod_register_options, mod_register_options()} | Config1],
+    escalus:init_per_suite(Config2).
 
 end_per_suite(Config) ->
     escalus_fresh:clean(),
     escalus:end_per_suite(Config).
 
+required_modules() ->
+    [{mod_register, mod_register_options()}].
+
+mod_register_options() ->
+    [{welcome_message, {""}},
+     {ip_access, [{allow, "127.0.0.0/8"},
+     {deny, "0.0.0.0/0"}]},
+     {access, register},
+     {registration_watchers, []}].
+
 init_per_group(register, Config) ->
     skip_if_mod_register_not_enabled([{escalus_user_db, xmpp} | Config]);
-init_per_group(bad_registration, Config) ->
-    [{escalus_user_db, xmpp} | Config];
-init_per_group(bad_cancelation, Config) ->
+init_per_group(registration_watchers, Config) ->
     skip_if_mod_register_not_enabled([{escalus_user_db, xmpp} | Config]);
+init_per_group(bad_cancelation, Config) ->
+    escalus:create_users(Config, escalus:get_users([alice]));
 init_per_group(registration_timeout, Config) ->
     case escalus_users:is_mod_register_enabled(Config) of
         true ->
@@ -71,69 +87,74 @@ init_per_group(registration_timeout, Config) ->
 init_per_group(change_account_details, Config) ->
     skip_if_mod_register_not_enabled(Config);
 init_per_group(_GroupName, Config) ->
-    escalus:create_users(Config, escalus:get_users([alice, bob])).
+    Config.
 
-end_per_group(register, Config) ->
-    escalus:delete_users(Config, escalus:get_users([admin, alice, bob]));
-end_per_group(change_account_details, _Config) ->
-    ok;
-end_per_group(bad_registration, _Config) ->
-    ok;
-end_per_group(bad_cancelation, _Config) ->
-    ok;
+end_per_group(bad_cancelation, Config) ->
+    escalus:delete_users(Config, escalus:get_users([alice]));
 end_per_group(registration_timeout, Config) ->
     restore_registration_timeout(Config);
 end_per_group(_GroupName, Config) ->
-    escalus:delete_users(Config, escalus:get_users([alice, bob])).
+    Config.
 
-init_per_testcase(check_unregistered, Config) ->
-    Config;
-init_per_testcase(change_password, Config0) ->
-    Config1 =  escalus:init_per_testcase(change_password, Config0),
-    escalus:create_users(Config1, escalus:get_users([alice]));
-init_per_testcase(change_password_to_null, Config0) ->
-    Config1 =  escalus:init_per_testcase(change_password_to_null, Config0),
-    escalus:create_users(Config1, escalus:get_users([alice]));
-init_per_testcase(bad_request_registration_cancelation, Config0) ->
-    Config1 =  escalus:init_per_testcase(bad_request_registration, Config0),
-    escalus:create_users(Config1, escalus:get_users([alice]));
-init_per_testcase(not_allowed_registration_cancelation, Config0) ->
-    Config1 = escalus:init_per_testcase(not_allowed_registration_cancelation, Config0),
-    Config2 = escalus:create_users(Config1, escalus:get_users([alice])),
+init_per_testcase(admin_notify, Config) ->
+    [{_, AdminSpec}] = escalus_users:get_users([admin]),
+    [AdminU, AdminS, _AdminP] = escalus_users:get_usp(Config, AdminSpec),
+    AdminJid = <<AdminU/binary, "@", AdminS/binary>>,
+    enable_watcher(Config, AdminJid),
+    escalus:init_per_testcase(admin_notify, Config);
+init_per_testcase(not_allowed_registration_cancelation, Config) ->
     %% Use a configuration that will not allow inband cancelation (and
     %% registration).
-    restart_mod_register_with_option(Config2, access, {access, none});
+    reload_mod_register_option(Config, access, {access, none}),
+    escalus:init_per_testcase(not_allowed_registration_cancelation, Config);
 init_per_testcase(registration_failure_timeout, Config) ->
     ok = deny_everyone_registration(),
     escalus:init_per_testcase(registration_failure_timeout, Config);
 init_per_testcase(CaseName, Config) ->
     escalus:init_per_testcase(CaseName, Config).
 
-end_per_testcase(change_password, Config) ->
-    [{alice, Details}] = escalus_users:get_users([alice]),
-    Alice = {alice, lists:keyreplace(password, 1, Details, {password, strong_pwd()})},
-    {ok, result, _Response} = escalus_users:delete_user(Config, Alice);
-end_per_testcase(change_password_to_null, Config) ->
-    escalus:delete_users(Config, escalus:get_users([alice]));
-end_per_testcase(message_zlib_limit, Config) ->
-    escalus:delete_users(Config, escalus:get_users([hacker]));
-end_per_testcase(check_unregistered, Config) ->
-    Config;
-end_per_testcase(bad_request_registration_cancelation, Config0) ->
-    true = user_exists(alice, Config0),
-    escalus:delete_users(Config0, escalus:get_users([alice]));
+end_per_testcase(register, Config) ->
+    escalus:end_per_testcase(register, Config);
+end_per_testcase(admin_notify, Config) ->
+    disable_watcher(Config),
+    escalus:delete_users(Config, escalus:get_users([alice, bob, admin])),
+    escalus:end_per_testcase(admin_notify, Config);
+end_per_testcase(registration_conflict, Config) ->
+    escalus_users:delete_users(Config, escalus:get_users([alice])),
+    escalus:end_per_testcase(registration_conflict, Config);
 end_per_testcase(not_allowed_registration_cancelation, Config) ->
     restore_mod_register_options(Config),
-    true = user_exists(alice, Config),
-    escalus:delete_users(Config, escalus:get_users([alice]));
+    escalus:end_per_testcase(not_allowed_registration_cancelation, Config);
 end_per_testcase(registration_timeout, Config) ->
-    escalus:delete_users(Config, escalus:get_users([alice, bob]));
-end_per_testcase(registration_failure_timeout, _Config) ->
-    ok = allow_everyone_registration();
+    escalus:delete_users(Config, escalus:get_users([alice, bob])),
+    escalus:end_per_testcase(registration_timeout, Config);
+end_per_testcase(registration_failure_timeout, Config) ->
+    ok = allow_everyone_registration(),
+    escalus:delete_users(Config, escalus:get_users([alice, bob])),
+    escalus:end_per_testcase(registration_failure_timeout, Config);
 end_per_testcase(CaseName, Config) ->
     escalus:end_per_testcase(CaseName, Config).
 
 register(Config) ->
+    [{Name1, _UserSpec1}, {Name2, _UserSpec2}] = escalus_users:get_users([alice, bob]),
+    escalus_fresh:create_users(Config, escalus:get_users([Name1, Name2])).
+
+already_registered(Config) ->
+    escalus_fresh:story(Config, [{alice, 1}], fun(Alice) ->
+        escalus:send(Alice, escalus_stanza:get_registration_fields()),
+        Stanza = escalus:wait_for_stanza(Alice),
+        escalus:assert(is_iq_result, Stanza),
+        true = has_registered_element(Stanza)
+
+                                              end).
+registration_conflict(Config) ->
+    [Alice] = escalus_users:get_users([alice]),
+    {ok, result, _Stanza} = escalus_users:create_user(Config, Alice),
+    {ok, conflict, _Raw} = escalus_users:create_user(Config, Alice).
+
+
+
+admin_notify(Config) ->
     [{Name1, UserSpec1}, {Name2, UserSpec2}] = escalus_users:get_users([alice, bob]),
     [{_, AdminSpec}] = escalus_users:get_users([admin]),
     [Username1, _Server1, _Pass1] = escalus_users:get_usp(Config, UserSpec1),
@@ -141,9 +162,8 @@ register(Config) ->
     [AdminU, AdminS, AdminP] = escalus_users:get_usp(Config, AdminSpec),
 
     rpc(mim(), ejabberd_auth, try_register, [AdminU, AdminS, AdminP]),
-
     escalus:story(Config, [{admin, 1}], fun(Admin) ->
-            escalus:create_users(Config, escalus:get_users([Name1, Name2])),
+        escalus:create_users(Config, escalus:get_users([Name1, Name2])),
 
             Predicates = [
                           fun(Stanza) ->
@@ -159,21 +179,7 @@ register(Config) ->
             escalus:assert_many(Predicates, escalus:wait_for_stanzas(Admin, 2))
         end).
 
-already_registered(Config) ->
 
-    %% This relies on Alice already being registered in test case
-    %% `register' in the same group as this test (group `register').
-
-    escalus:story(Config, [{alice, 1}], fun(Alice) ->
-
-        escalus:send(Alice, escalus_stanza:get_registration_fields()),
-
-        Stanza = escalus:wait_for_stanza(Alice),
-
-        escalus:assert(is_iq_result, Stanza),
-        true = has_registered_element(Stanza)
-
-    end).
 
 null_password(Config) ->
     [{alice, Details}] = escalus_users:get_users([alice]),
@@ -189,8 +195,7 @@ null_password(Config) ->
     false = rpc(mim(), ejabberd_auth, is_user_exists, [Name, Server]).
 
 check_unregistered(Config) ->
-    escalus:delete_users(Config, escalus:get_users([admin, alice, bob])),
-    [{_, UserSpec}| _] = escalus_users:get_users(all),
+    [{_, UserSpec}] = escalus_users:get_users([bob]),
     [Username, Server, _Pass] = escalus_users:get_usp(Config, UserSpec),
     false = rpc(mim(), ejabberd_auth, is_user_exists, [Username, Server]).
 
@@ -199,7 +204,6 @@ bad_request_registration_cancelation(Config) ->
     %% To quote XEP 0077, section 3.2, table 1 (unregister error
     %% cases): "The <remove/> element [is] not the only child element
     %% of the <query/> element."
-
     escalus:story(Config, [{alice, 1}], fun(Alice) ->
 
         %% Alice sends bad cancelation request
@@ -267,12 +271,12 @@ registration_failure_timeout(Config) ->
 
 change_password(Config) ->
 
-    escalus:story(Config, [{alice, 1}], fun(Alice) ->
-
+    escalus_fresh:story(Config, [{alice, 1}], fun(Alice) ->
+        Username = escalus_client:username(Alice),
         escalus:send(Alice,
             Q = escalus_stanza:iq_set(?NS_INBAND_REGISTER,
                 [#xmlel{name = <<"username">>,
-                        children = [#xmlcdata{content = <<"alice">>}]},
+                        children = [#xmlcdata{content = Username}]},
                  #xmlel{name = <<"password">>,
                         children = [#xmlcdata{content = strong_pwd()}]}])),
 
@@ -295,12 +299,12 @@ change_password_to_null(Config) ->
     %% this is in IQ, it must: so we choose to require a `not-allowed'
     %% response.
 
-    escalus:story(Config, [{alice, 1}], fun(Alice) ->
-
+    escalus_fresh:story(Config, [{alice, 1}], fun(Alice) ->
+        Username = escalus_client:username(Alice),
         escalus:send(Alice,
             escalus_stanza:iq_set(?NS_INBAND_REGISTER,
                 [#xmlel{name = <<"username">>,
-                        children = [#xmlcdata{content = <<"alice">>}]},
+                        children = [#xmlcdata{content = Username}]},
                  #xmlel{name = <<"password">>,
                         children = [#xmlcdata{content = <<"">>}]}])),
 
@@ -362,25 +366,31 @@ bad_cancelation_stanza() ->
                     %% <query/> element.
                     #xmlel{name = <<"foo">>}]}]).
 
-restart_mod_register_with_option(Config, Name, Value) ->
-    Domain = ct:get_config({hosts, mim, domain}),
-    ModuleOptions = rpc(mim(), gen_mod, loaded_modules_with_opts, [Domain]),
-    {mod_register, OldRegisterOptions} = lists:keyfind(mod_register, 1, ModuleOptions),
-    ok = dynamic_modules:stop(Domain, mod_register),
-    NewRegisterOptions = lists:keystore(Name, 1, OldRegisterOptions, Value),
-    {ok, _} = dynamic_modules:start(Domain, mod_register, NewRegisterOptions),
-    [{old_mod_register_opts, OldRegisterOptions}|Config].
-
-restore_mod_register_options(Config0) ->
-    Domain = ct:get_config({hosts, mim, domain}),
-    {value, {old_mod_register_opts, RegisterOpts}, Config1} =
-        lists:keytake(old_mod_register_opts, 1, Config0),
-    ok = dynamic_modules:stop(Domain, mod_register),
-    {ok, _} = dynamic_modules:start(Domain, mod_register, RegisterOpts),
-    Config1.
-
 user_exists(Name, Config) ->
     {Name, Client} = escalus_users:get_user_by_name(Name),
     [Username, Server, _Pass] = escalus_users:get_usp(Config, Client),
     rpc(mim(), ejabberd_auth, is_user_exists, [Username, Server]).
+
+reload_mod_register_option(Config, Key, Value) ->
+    Host = domain(),
+    Args = proplists:get_value(mod_register_options, Config),
+    Args1 = lists:keyreplace(Key, 1, Args, {Key, Value}),
+    dynamic_modules:restart(Host, mod_register, Args1).
+
+restore_mod_register_options(Config) ->
+    Host = domain(),
+    Args = proplists:get_value(mod_register_options, Config),
+    dynamic_modules:restart(Host, mod_register, Args).
+
+enable_watcher(Config, Watcher) ->
+    reload_mod_register_option(Config, registration_watchers, [Watcher]).
+
+disable_watcher(Config) ->
+    restore_mod_register_options(Config).
+
+watcher(Watcher) ->
+    {registration_watchers, [binary_to_list(Watcher)]}.
+
+domain() ->
+    ct:get_config({hosts, mim, domain}).
 
