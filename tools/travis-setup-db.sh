471bba694c35a3a9cff051ff4e02c8bb26763211
Michael Uvarov <freeakk@gmail.com>
2018-05-29 17:50:54 +0200

    Data on volume (#1886)
    * Store sql data on a volume
    
    * Introduce SQL_ROOT_DIR
    
    * Use innodb_flush_log_at_trx_commit=2 for mysql in docker
    
    * Add DATA_ON_VOLUME env variable
    
    Disable --rm on travis, but execute it locally
    Data volume supports for all containers

--- tools/travis-setup-db.sh
+++ tools/travis-setup-db.sh
@@ -13,13 +13,39 @@ MIM_PRIV_DIR=${BASE}/priv
 
 DB_CONF_DIR=${BASE}/${TOOLS}/db_configs/$DB
 
-SQL_TEMP_DIR=/tmp/sql
-
 MYSQL_DIR=/etc/mysql/conf.d
 
 PGSQL_ODBC_CERT_DIR=~/.postgresql
 
 SSLDIR=${BASE}/${TOOLS}/ssl
+
+# Don't need it for travis for speed up
+RM_FLAG=" --rm "
+if [ "$TRAVIS" = 'true' ]; then
+    echo "Disable --rm flag on Travis"
+    RM_FLAG=""
+fi
+
+# Linux volumes are faster than layer fs.
+# Mac volumes are actually slower than layer fs.
+case "$(uname -s)" in
+    Darwin*)    DEFAULT_DATA_ON_VOLUME=false;;
+    *)          DEFAULT_DATA_ON_VOLUME=true
+esac
+DATA_ON_VOLUME=${DATA_ON_VOLUME:-$DEFAULT_DATA_ON_VOLUME}
+
+echo "DATA_ON_VOLUME is $DATA_ON_VOLUME"
+
+# Returns its arguments if data on volume is enabled
+function data_on_volume
+{
+    if [ "$DATA_ON_VOLUME" = 'true' ]; then
+        echo "$@"
+    fi
+}
+
+# Default cassandra version
+CASSANDRA_VERSION=${CASSANDRA_VERSION:-3.9}
 
 # There is one odbc.ini for both mssql and pgsql
 # Allows to run both in parallel
@@ -73,17 +99,29 @@ ByteaAsLongVarBinary = 1
 EOL
 }
 
+# Stores all the data needed by the container
+SQL_ROOT_DIR="$(mktemp -d --suffix=mongoose_sql_root)"
+echo "SQL_ROOT_DIR is $SQL_ROOT_DIR"
+
+# A directory, that contains resources that needed to bootstrap a container
+# i.e. certificates and config files
+SQL_TEMP_DIR="$SQL_ROOT_DIR/temp"
+# A directory, that contains database server data files
+# It's good to keep it outside of a container, on a volume
+SQL_DATA_DIR="$SQL_ROOT_DIR/data"
+mkdir -p "$SQL_TEMP_DIR" "$SQL_DATA_DIR"
+
 if [ "$DB" = 'mysql' ]; then
     echo "Configuring mysql"
     # TODO We should not use sudo
     sudo -n service mysql stop || echo "Failed to stop mysql"
-    mkdir -p ${SQL_TEMP_DIR}
+    docker rm -f mongooseim-mysql || echo "Skip removing previous container"
     cp ${SSLDIR}/fake_cert.pem ${SQL_TEMP_DIR}/.
     openssl rsa -in ${SSLDIR}/fake_key.pem -out ${SQL_TEMP_DIR}/fake_key.pem
     # mysql_native_password is needed until mysql-otp implements caching-sha2-password
     # https://github.com/mysql-otp/mysql-otp/issues/83
     docker run -d \
-        -e SQL_TEMP_DIR=${SQL_TEMP_DIR} \
+        -e SQL_TEMP_DIR=/tmp/sql \
         -e MYSQL_ROOT_PASSWORD=secret \
         -e MYSQL_DATABASE=ejabberd \
         -e MYSQL_USER=ejabberd \
@@ -91,7 +129,8 @@ if [ "$DB" = 'mysql' ]; then
         -v ${DB_CONF_DIR}/mysql.cnf:${MYSQL_DIR}/mysql.cnf:ro \
         -v ${MIM_PRIV_DIR}/mysql.sql:/docker-entrypoint-initdb.d/mysql.sql:ro \
         -v ${BASE}/${TOOLS}/docker-setup-mysql.sh:/docker-entrypoint-initdb.d/docker-setup-mysql.sh \
-        -v ${SQL_TEMP_DIR}:${SQL_TEMP_DIR} \
+        -v ${SQL_TEMP_DIR}:/tmp/sql \
+        $(data_on_volume -v ${SQL_DATA_DIR}:/var/lib/mysql) \
         --health-cmd='mysqladmin ping --silent' \
         -p 3306:3306 --name=mongooseim-mysql \
         mysql --default-authentication-plugin=mysql_native_password
@@ -102,24 +141,26 @@ elif [ "$DB" = 'pgsql' ]; then
     # make clean && make
     # Than rerun the script to create a new docker container.
     echo "Configuring postgres with SSL"
-    sudo service postgresql stop || echo "Failed to stop psql"
-    mkdir -p ${SQL_TEMP_DIR}
+    sudo -n service postgresql stop || echo "Failed to stop psql"
+    docker rm -f mongooseim-pgsql || echo "Skip removing previous container"
     cp ${SSLDIR}/fake_cert.pem ${SQL_TEMP_DIR}/.
     cp ${SSLDIR}/fake_key.pem ${SQL_TEMP_DIR}/.
     cp ${DB_CONF_DIR}/postgresql.conf ${SQL_TEMP_DIR}/.
     cp ${DB_CONF_DIR}/pg_hba.conf ${SQL_TEMP_DIR}/.
     cp ${MIM_PRIV_DIR}/pg.sql ${SQL_TEMP_DIR}/.
     docker run -d \
-           -e SQL_TEMP_DIR=${SQL_TEMP_DIR} \
-           -v ${SQL_TEMP_DIR}:${SQL_TEMP_DIR} \
+           -e SQL_TEMP_DIR=/tmp/sql \
+           -v ${SQL_TEMP_DIR}:/tmp/sql \
+           $(data_on_volume -v ${SQL_DATA_DIR}:/var/lib/postgresql/data) \
            -v ${BASE}/${TOOLS}/docker-setup-postgres.sh:/docker-entrypoint-initdb.d/docker-setup-postgres.sh \
            -p 5432:5432 --name=mongooseim-pgsql postgres
-    mkdir -p ${PGSQL_ODBC_CERT_DIR} || echo "PGSQL odbc cert dir already exists"
+    mkdir -p ${PGSQL_ODBC_CERT_DIR}
     cp ${SSLDIR}/ca/cacert.pem ${PGSQL_ODBC_CERT_DIR}/root.crt
     install_odbc_ini
 
 elif [ "$DB" = 'riak' ]; then
     echo "Configuring Riak with SSL"
+    docker rm -f mongooseim-riak || echo "Skip removing previous container"
     # Instead of docker run, use "docker create" + "docker start".
     # So we can prepare our container.
     # We use HEALTHCHECK here, check "docker ps" to get healthcheck status.
@@ -135,6 +176,7 @@ elif [ "$DB" = 'riak' ]; then
         -v "${SSLDIR}/fake_cert.pem:/etc/riak/cert.pem:ro" \
         -v "${SSLDIR}/fake_key.pem:/etc/riak/key.pem:ro" \
         -v "${SSLDIR}/ca/cacert.pem:/etc/riak/ca/cacertfile.pem:ro" \
+        $(data_on_volume -v ${SQL_DATA_DIR}:/var/lib/riak) \
         --health-cmd='riak-admin status' \
         "michalwski/docker-riak:1.0.6"
     # Use a temporary file to store config
@@ -163,6 +205,7 @@ elif [ "$DB" = 'riak' ]; then
 
 elif [ "$DB" = 'cassandra' ]; then
     docker image pull cassandra:${CASSANDRA_VERSION}
+    docker rm -f mongooseim-cassandra mongooseim-cassandra-proxy || echo "Skip removing previous container"
 
     opts="$(docker inspect -f '{{range .Config.Entrypoint}}{{println}}{{.}}{{end}}' cassandra:${CASSANDRA_VERSION})"
     opts+="$(docker inspect -f '{{range .Config.Cmd}}{{println}}{{.}}{{end}}' cassandra:${CASSANDRA_VERSION})"
@@ -176,33 +219,33 @@ elif [ "$DB" = 'cassandra' ]; then
                -e HEAP_NEWSIZE=64M               \
                -v "${SSLDIR}:/ssl:ro"            \
                -v "${docker_entry}:/entry.sh:ro" \
-               --name=cassandra                  \
+               $(data_on_volume -v ${SQL_DATA_DIR}:/var/lib/cassandra) \
+               --name=mongooseim-cassandra       \
                --entrypoint "/entry.sh"          \
                cassandra:${CASSANDRA_VERSION}    \
                "${init_opts[@]}"
-    tools/wait_for_service.sh cassandra         9042 || docker logs cassandra
+    tools/wait_for_service.sh mongooseim-cassandra 9042 || docker logs mongooseim-cassandra
 
     # Start TCP proxy
-    CASSANDRA_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cassandra)
+    CASSANDRA_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mongooseim-cassandra)
     echo "Connecting TCP proxy to Cassandra on $CASSANDRA_IP..."
     $SED -i "s/\"service-hostname\": \".*\"/\"service-hostname\": \"$CASSANDRA_IP\"/g" ${DB_CONF_DIR}/proxy/zazkia-routes.json
     docker run -d                               \
                -p 9042:9042                     \
                -p 9191:9191                     \
                -v ${DB_CONF_DIR}/proxy:/data    \
-               --name=cassandra_proxy           \
+               --name=mongooseim-cassandra-proxy \
                emicklei/zazkia
-    tools/wait_for_service.sh cassandra_proxy   9042 || docker logs cassandra_proxy
-
+    tools/wait_for_service.sh mongooseim-cassandra-proxy 9042 || docker logs mongooseim-cassandra-proxy
 
     MIM_SCHEMA=$(pwd)/priv/cassandra.cql
     TEST_SCHEMA=$(pwd)/big_tests/tests/mongoose_cassandra_SUITE_data/schema.cql
     for cql_file in $MIM_SCHEMA $TEST_SCHEMA; do
-        # Deleted --rm on travis for speedup
-        docker run -it -e SSL_CERTFILE=/cacert.pem                  \
+        echo "Apply ${cql_file}"
+        docker run -it $RM_FLAG -e SSL_CERTFILE=/cacert.pem         \
                        -v "${SSLDIR}/ca/cacert.pem:/cacert.pem:ro"  \
                        -v "${cql_file}:/cassandra.cql:ro"           \
-                       --link cassandra:cassandra                   \
+                       --link mongooseim-cassandra:cassandra        \
                        cassandra:${CASSANDRA_VERSION}               \
                        sh -c 'exec cqlsh "$CASSANDRA_PORT_9042_TCP_ADDR" --ssl -f /cassandra.cql'
     done
@@ -229,6 +272,8 @@ elif [ "$DB" = 'mssql' ]; then
     # > a third party the results of any benchmark test of the software.
 
     # SCRIPTING STUFF
+    docker rm -f mongoose-mssql || echo "Skip removing previous container"
+    docker volume rm -f mongoose-mssql-data || echo "Skip removing previous volume"
     #
     # MSSQL wants secure passwords
     # i.e. just "mongooseim_secret" would not work.
@@ -237,11 +282,23 @@ elif [ "$DB" = 'mssql' ]; then
     # It has no '/docker-entrypoint-initdb.d/'-like interface.
     # So we would put schema into some random place and
     # apply it inside 'docker-exec' command.
+    #
+    # ABOUT VOLUMES
+    # Just using /var/opt/mssql volume is not enough.
+    # We need mssql-data-volume.
+    #
+    # Both on Mac and Linux
+    # https://github.com/Microsoft/mssql-docker/issues/12
+    #
+    # Otherwise we get an error in logs
+    # Error 87(The parameter is incorrect.) occurred while opening file '/var/opt/mssql/data/master.mdf'
     docker run -d -p 1433:1433                                  \
                --name=mongoose-mssql                            \
                -e "ACCEPT_EULA=Y"                               \
                -e "SA_PASSWORD=mongooseim_secret+ESL123"        \
                -v "$(pwd)/priv/mssql2012.sql:/mongoose.sql:ro"  \
+               $(data_on_volume -v ${SQL_DATA_DIR}:/var/opt/mssql) \
+               $(data_on_volume -v mongoose-mssql-data:/var/opt/mssql/data) \
                --health-cmd='/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "mongooseim_secret+ESL123" -Q "SELECT 1"' \
                microsoft/mssql-server-linux
     tools/wait_for_healthcheck.sh mongoose-mssql
