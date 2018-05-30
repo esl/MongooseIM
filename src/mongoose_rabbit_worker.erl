8a5f3c838e1294cf0b3b2c93de771b28f1daa9f5
Bartek <bszaf@users.noreply.github.com>
2018-05-30 12:16:23 +0200

    Remove undefined functions from exports in mongoose_rabbit_worker

--- src/mongoose_rabbit_worker.erl
+++ src/mongoose_rabbit_worker.erl
@@ -18,7 +18,7 @@
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3, format_status/2]).
+         terminate/2]).
 
 -record(state, {connection, channel}).
 
