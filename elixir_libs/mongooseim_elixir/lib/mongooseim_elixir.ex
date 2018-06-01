6278aabffe83db94d7ea45367343da84fbcf5895
Michael Uvarov <arcusfelis@gmail.com>
2018-06-01 23:17:02 +0200

    Add elixir project

---
+++ elixir_libs/mongooseim_elixir/lib/mongooseim_elixir.ex
@@ -0,0 +1,18 @@
+defmodule MongooseimElixir do
+  @moduledoc """
+  Documentation for MongooseimElixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MongooseimElixir.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ elixir_libs/mongooseim_elixir/test/mongooseim_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule MongooseimElixirTest do
+  use ExUnit.Case
+  doctest MongooseimElixir
+
+  test "greets the world" do
+    assert MongooseimElixir.hello() == :world
+  end
+end
