d189b330d6c881e1cb2a1e254b53c3d9a1a1f356
Michal Piotrowski <michal.piotrowski@erlang-solutions.com>
2018-06-19 16:02:34 +0200

    make group summary from all suites run

--- big_tests/src/ct_groups_summary_hook.erl
+++ big_tests/src/ct_groups_summary_hook.erl
@@ -9,7 +9,8 @@
          init/2,
          pre_init_per_suite/3,
          post_end_per_suite/4,
-         post_end_per_group/4]).
+         post_end_per_group/4,
+         terminate/1]).
 
 -include_lib("common_test/include/ct.hrl").
 
@@ -21,27 +22,35 @@ id(_Opts) ->
 %% any common state.
 init(_Id, Opts) ->
     %ct:pal("init opts: ~p", [Opts]),
-    {ok, #{}}.
+    {ok, #{total_ok => 0,
+           total_failed => 0,
+           total_eventually_ok_tests => 0}}.
 
-pre_init_per_suite(_Suite, Config, State) ->
+pre_init_per_suite(Suite, Config, State) ->
     %ct:pal("pre_init_per_suite config: ~p", [Config]),
     %ct:pal("pre_init_per_suite state: ~p", [State]),
     %% Start with an empty CT hook state for each suite.
-    {Config, #{}}.
+    {Config, State#{current_suite => Suite, Suite => #{}}}.
 
 post_end_per_suite(_SuiteName, Config, Return, State) ->
     %ct:pal("post_end_per_suite config: ~p", [Config]),
-    %ct:pal("post_end_per_suite state: ~p", [State]),
+    ct:pal("post_end_per_suite state: ~p", [State]),
     %ct:pal("post_end_per_suite return: ~p", [Return]),
-    write_groups_summary(Config, State),
-    {Return, State}.
+    NewState = write_groups_summary(Config, State),
+    {Return, NewState}.
 
 post_end_per_group(GroupName, Config, Return, State) ->
     %ct:pal("post_end_per_group config: ~p", [Config]),
     %ct:pal("post_end_per_group state: ~p", [State]),
     %ct:pal("post_end_per_group return: ~p", [Return]),
     State1 = update_group_status(GroupName, Config, State),
+    ct:log("NewState: ~p", [State1]),
     {Return, State1}.
+
+terminate(State) ->
+    io:format("~p", [State]),
+    ct:log("~p", [State]),
+    State.
 
 %% @doc If the group property repeat_until_all_ok is enabled,
 %% then update the outcome of this group in State.
@@ -74,13 +83,15 @@ update_group_status(GroupName, Config, State) ->
     end.
 
 
-do_update_group_status(Status, GroupNameWPath, NFailed, State) ->
-    case maps:get(GroupNameWPath, State, undefined) of
-        undefined ->
-            State#{GroupNameWPath => group_status(Status, NFailed)};
-        {_PrevStatus, {n_failed, NFailedAcc}} ->
-            State#{GroupNameWPath => group_status(Status, NFailed + NFailedAcc)}
-    end.
+do_update_group_status(Status, GroupNameWPath, NFailed, #{current_suite := CurrentSuite} = State) ->
+    SuiteState = maps:get(CurrentSuite, State),
+    NewSuiteState = case maps:get(GroupNameWPath, SuiteState, undefined) of
+                        undefined ->
+                            SuiteState#{GroupNameWPath => group_status(Status, NFailed)};
+                        {_PrevStatus, {n_failed, NFailedAcc}} ->
+                            SuiteState#{GroupNameWPath => group_status(Status, NFailed + NFailedAcc)}
+                    end,
+    State#{CurrentSuite := NewSuiteState}.
 
 group_status(Status, NFailed) ->
     {Status, {n_failed, NFailed}}.
@@ -95,8 +106,11 @@ group_name_with_path(GroupName, Config) ->
     end.
 
 %% @doc Write down the number successful and failing groups in a per-suite groups.summary file.
-write_groups_summary(Config, State) ->
-    {Ok, Failed, FailedTests} = maps:fold(fun acc_groups_summary/3, {0, 0, 0}, State),
+write_groups_summary(Config, #{total_ok := TOK, total_failed := TFailed,
+                               total_eventually_ok_tests := TEvOK,
+                               current_suite := CurrentSuite} = State) ->
+    GroupState = maps:get(CurrentSuite, State),
+    {Ok, Failed, FailedTests} = maps:fold(fun acc_groups_summary/3, {0, 0, 0}, GroupState),
     PrivDir = ?config(priv_dir, Config),
     case PrivDir of
         undefined ->
@@ -108,7 +122,10 @@ write_groups_summary(Config, State) ->
                                                "~p.\n",
                                                [{groups_summary, {Ok, Failed}},
                                                 {eventually_ok_tests, FailedTests}]))
-    end.
+    end,
+    State#{total_ok := TOK + Ok,
+           total_failed := TFailed + Failed,
+           total_eventually_ok_tests := TEvOK + FailedTests}.
 
 acc_groups_summary(_GroupName, {ok, {n_failed, NFailedTests}},
                    {OkGroupsAcc, FailedGroupsAcc, EventuallyOkAcc}) ->
