d198f38a15055a180aceecbfd9fb8d754f460a27
kanes115 <dominikstanaszek@icloud.com>
2018-07-09 16:55:29 +0200

    Add tests for offline and chat marker

--- big_tests/tests/inbox_SUITE.erl
+++ big_tests/tests/inbox_SUITE.erl
@@ -37,7 +37,9 @@
          no_aff_stored_and_remove_on_kicked/1,
          no_stored_and_remain_after_kicked/1,
          simple_groupchat_stored_in_all_inbox_muc/1,
-        simple_groupchat_stored_in_offline_users_inbox_muc/1
+         simple_groupchat_stored_in_offline_users_inbox_muc/1,
+         unread_count_is_the_same_after_going_online_again/1,
+         unread_count_is_reset_after_sending_chatmarker/1
         ]).
 
 -import(muc_helper, [foreach_occupant/3, foreach_recipient/2]).
@@ -54,6 +56,8 @@
 -define(ROOM_MARKERS, <<"room_markers">>).
 -define(MUC_ROOM, <<"some_muc_room">>).
 -define(MUC_ROOM2, <<"some_muc_room2">>).
+-define(MUC_ROOM3, <<"some_muc_room3">>).
+-define(MUC_ROOM4, <<"some_muc_room4">>).
 -define(MUC_DOMAIN, <<"muc.localhost">>).
 -record(conv, {unread, from, to, content = <<>>, verify = fun(C, Stanza) -> ok end}).
 -record(inbox, {total, convs = []}).
@@ -106,7 +110,9 @@ groups() ->
          {muc, [sequence],
           [
            simple_groupchat_stored_in_all_inbox_muc,
-           simple_groupchat_stored_in_offline_users_inbox_muc
+           simple_groupchat_stored_in_offline_users_inbox_muc,
+           unread_count_is_the_same_after_going_online_again,
+           unread_count_is_reset_after_sending_chatmarker
           ]}
         ].
     %ct_helper:repeat_all_until_all_ok(G).
@@ -176,7 +182,7 @@ end_per_group(muclight, Config) ->
   muc_light_helper:clear_db(),
   Config;
 end_per_group(muc, Config) ->
-    muc_helper:destroy_room(Config);
+    Config;
 end_per_group(_GroupName, Config) ->
   Config.
 
@@ -217,6 +223,17 @@ init_per_testcase(simple_groupchat_stored_in_offline_users_inbox_muc = TC, Confi
   [User | _] = ?config(escalus_users, Config), % probably change this line as it should always take Alice to create the room
   Config2 = muc_helper:start_room(Config, User, ?MUC_ROOM2, <<"some_friendly_name">>, default),
   escalus:init_per_testcase(TC, Config2);
+init_per_testcase(unread_count_is_the_same_after_going_online_again = TC, Config) ->
+  clear_inbox_all(),
+  [User | _] = ?config(escalus_users, Config), % probably change this line as it should always take Alice to create the room
+  Config2 = muc_helper:start_room(Config, User, ?MUC_ROOM3, <<"some_friendly_name">>, default),
+  escalus:init_per_testcase(TC, Config2);
+init_per_testcase(unread_count_is_reset_after_sending_chatmarker = TC, Config) ->
+  clear_inbox_all(),
+  [User | _] = ?config(escalus_users, Config), % probably change this line as it should always take Alice to create the room
+  Config2 = muc_helper:start_room(Config, User, ?MUC_ROOM4, <<"some_friendly_name">>, default),
+  escalus:init_per_testcase(TC, Config2);
+
 init_per_testcase(CaseName, Config) ->
   clear_inbox_all(),
   escalus:init_per_testcase(CaseName, Config).
@@ -249,6 +266,11 @@ end_per_testcase(msg_sent_to_not_existing_user, Config) ->
   Host = ct:get_config({hosts, mim, domain}),
   escalus_ejabberd:rpc(mod_inbox_utils, clear_inbox, [<<"not_existing_user@localhost">>,Host]),
   escalus:end_per_testcase(msg_sent_to_not_existing_user, Config);
+end_per_testcase(TC, Config) when TC =:= simple_groupchat_stored_in_all_inbox_muc,
+                                  TC =:= simple_groupchat_stored_in_offline_users_inbox_muc,
+                                  TC =:= unread_count_is_the_same_after_going_online_again,
+                                  TC =:= unread_count_is_reset_after_sending_chatmarker ->
+    muc_helper:destroy_room(Config);
 end_per_testcase(CaseName, Config) ->
   clear_inbox_all(),
   escalus:end_per_testcase(CaseName, Config).
@@ -792,7 +814,9 @@ groupchat_markers_all_reset_room_created(Config) ->
     foreach_check_inbox([Bob, Kate, Alice], 1, 0, AliceRoomJid, Msg)
                                                            end).
 
+%%--------------------------------------------------------------------
 %% legacy MUC tests
+%%--------------------------------------------------------------------
 
 simple_groupchat_stored_in_all_inbox_muc(Config) ->
   escalus:story(Config, [{alice, 1}, {bob, 1}, {kate, 1}], fun(Alice, Bob, Kate) ->
@@ -862,10 +886,94 @@ simple_groupchat_stored_in_offline_users_inbox_muc(Config) ->
     end).
 
 
+unread_count_is_the_same_after_going_online_again(Config) ->
+  escalus:story(Config, [{alice, 1}, {bob, 1}, {kate, 1}], fun(Alice, Bob, Kate) ->
+    Users = [Alice, Bob, Kate],
+    Msg = <<"Hi Room!">>,
+    Id = <<"MyID">>,
+    Room = ?config(room, Config),
+    RoomAddr = muc_room_address(Room),
+
+    enter_room(Room, Users),
+    make_members(Room, Alice, Users -- [Alice]),
+    go_offline(Kate, Room, Users),
+    Stanza = escalus_stanza:set_id(
+      escalus_stanza:groupchat_to(RoomAddr, Msg), Id),
+    escalus:send(Bob, Stanza),
+    Resps = lists:map(fun(User) -> escalus:wait_for_stanza(User) end,
+              Users -- [Kate]),
+    lists:foreach(fun(Resp) -> escalus:assert(is_groupchat_message, Resp) end,
+                  Resps),
+    enter_room(Room, Kate, Users -- [Kate], 1),
+    [AliceJid, BobJid, KateJid] = lists:map(fun to_bare_lower/1, Users),
+    BobRoomJid = muc_room_address(Room, lbin(nick(Bob))),
+    %% Bob has 0 unread messages
+    check_inbox(Bob, #inbox{
+      total = 1,
+      convs = [#conv{unread = 0, from = BobRoomJid, to = BobJid,
+                     content = Msg}]}),
+    %% Alice and Kate have one conv with 1 unread message
+    check_inbox(Alice, #inbox{
+      total = 1,
+      convs = [#conv{unread = 1, from = BobRoomJid, to = AliceJid, content = Msg}]}),
+    check_inbox(Kate, #inbox{
+      total = 1,
+      convs = [#conv{unread = 1, from = BobRoomJid, to = KateJid, content = Msg}]})
+    end).
+
+unread_count_is_reset_after_sending_chatmarker(Config) ->
+  escalus:story(Config, [{alice, 1}, {bob, 1}, {kate, 1}], fun(Alice, Bob, Kate) ->
+    Users = [Alice, Bob, Kate],
+    Msg = <<"Hi Room!">>,
+    Id = <<"MyID">>,
+    Room = ?config(room, Config),
+    RoomAddr = muc_room_address(Room),
+
+    enter_room(Room, Users),
+    make_members(Room, Alice, Users -- [Alice]),
+    Stanza = escalus_stanza:set_id(
+      escalus_stanza:groupchat_to(RoomAddr, Msg), Id),
+    escalus:send(Bob, Stanza),
+    Resps = lists:map(fun(User) -> escalus:wait_for_stanza(User) end,
+              Users -- [Kate]),
+    lists:foreach(fun(Resp) -> escalus:assert(is_groupchat_message, Resp) end,
+                  Resps),
+
+    ChatMarker = set_type(escalus_stanza:chat_marker(RoomAddr, <<"displayed">>, Id), <<"groupchat">>),
+    %% User marks last message
+    escalus:send(Kate, ChatMarker),
+
+    [AliceJid, BobJid, KateJid] = lists:map(fun to_bare_lower/1, Users),
+    BobRoomJid = muc_room_address(Room, lbin(nick(Bob))),
+    %% Bob has 0 unread messages
+    check_inbox(Bob, #inbox{
+      total = 1,
+      convs = [#conv{unread = 0, from = BobRoomJid, to = BobJid,
+                     content = Msg}]}),
+    %% Alice have one conv with 1 unread message
+    check_inbox(Alice, #inbox{
+      total = 1,
+      convs = [#conv{unread = 1, from = BobRoomJid, to = AliceJid, content = Msg}]}),
+    %% Kate has 0 unread messages
+    check_inbox(Kate, #inbox{
+      total = 1,
+      convs = [#conv{unread = 0, from = BobRoomJid, to = KateJid, content = Msg}]})
+    end).
+
+
+
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Helpers
 
 go_offline(User, Room, Occupants) ->
+    UnavailavbleStanza = escalus_stanza:presence(<<"unavailable">>),
+    Stanza = muc_helper:stanza_to_room(UnavailavbleStanza, Room, nick(User)),
+    escalus:send(User, Stanza),
+    lists:foreach(fun(User) -> A = escalus:wait_for_stanza(User), ct:pal("A: ~p", [A]) end,
+                  Occupants).
+
+go_online(User, Room, Occupants) ->
     UnavailavbleStanza = escalus_stanza:presence(<<"unavailable">>),
     Stanza = muc_helper:stanza_to_room(UnavailavbleStanza, Room, nick(User)),
     escalus:send(User, Stanza),
@@ -884,6 +992,7 @@ make_members(Room, Admin, Users) ->
     ct:pal("SuccessResp: ~p", [SuccesResp]),
     lists:foreach(fun(User) -> escalus:wait_for_stanzas(User, length(Users)) end, Users). % Everybody gets aff changes of everybody
 
+% All users enter the room
 enter_room(Room, Users) ->
     lists:foreach(fun(User) ->
                           escalus:send(User, stanza_muc_enter_room(Room, nick(User))) end,
@@ -893,6 +1002,15 @@ enter_room(Room, Users) ->
                           ct:pal("For ~p: ~p", [escalus_client:short_jid(User), A])
                   end,
                   Users).
+
+% `User` enters the room `Room` where `Users` are occupants
+enter_room(Room, User, Users, DelayedMessagesCount) ->
+    escalus:send(User, stanza_muc_enter_room(Room, nick(User))),
+    lists:foreach(fun(User) ->
+                          A = escalus:wait_for_stanza(User), ct:pal("~p: Got presence from user: ~p", [escalus_client:short_jid(User), A]) end,
+                  Users),
+    Subj = escalus:wait_for_stanzas(User, length(Users) + 1 + 1 + DelayedMessagesCount), % User gets subject message and presences from everybody including himself
+    ct:pal("Subj and presence messages: ~p", [Subj]).
 
 
 stanza_set_affiliations(Room, List) ->
