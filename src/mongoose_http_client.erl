87292e90a2df6e209f2ca92de16cb71ad42f6513
Konrad Zemek <konrad.zemek@gmail.com>
2018-08-31 14:26:58 +0200

    Use changed mongoose_wpool abstraction for mongoose_http_client

--- src/mongoose_http_client.erl
+++ src/mongoose_http_client.erl
@@ -33,7 +33,6 @@
 -module(mongoose_http_client).
 -author("bartlomiej.gorny@erlang-solutions.com").
 -include("mongoose.hrl").
--include("mongoose_wpool.hrl").
 
 %% API
 -export([start/0, stop/0, start_pool/2, stop_pool/1, get/3, post/4]).
@@ -44,6 +43,13 @@
 
 -spec start() -> ok.
 start() ->
+    case ets:info(?MODULE) of
+        undefined ->
+            Heir = case whereis(ejabberd_sup) of undefined -> []; Pid -> [{heir, Pid, undefined}] end,
+            ets:new(?MODULE, [named_table, public, {read_concurrency, true} | Heir]);
+        _ ->
+            ok
+    end,
     mongoose_wpool:ensure_started(),
     case ejabberd_config:get_local_option(http_connections) of
         undefined -> ok;
@@ -58,20 +64,20 @@ stop() ->
     end.
 
 -spec start_pool(atom(), list()) -> ok | {error, already_started}.
-start_pool(Name, Opts) ->
-    PoolName = make_pool_name(Name),
-    case whereis(PoolName) of
-        undefined ->
+start_pool(PoolName, Opts) ->
+    case ets:lookup(?MODULE, PoolName) of
+        [] ->
             mongoose_wpool:ensure_started(),
             do_start_pool(PoolName, Opts),
             ok;
-        _ -> {error, already_started}
+        _ ->
+            {error, already_started}
     end.
 
 -spec stop_pool(atom()) -> ok.
 stop_pool(Name) ->
-    wpool:stop_sup_pool(pool_name(Name)),
-    mongoose_wpool:delete_pool_settings(pool_name(Name)),
+    ets:delete(?MODULE, Name),
+    mongoose_wpool:stop(?MODULE, global, Name),
     ok.
 
 -spec get(atom(), binary(), list()) ->
@@ -98,39 +104,28 @@ do_start_pool(PoolName, Opts) ->
     PathPrefix = list_to_binary(gen_mod:get_opt(path_prefix, Opts, "/")),
     RequestTimeout = gen_mod:get_opt(request_timeout, Opts, 2000),
     PoolTimeout = gen_mod:get_opt(pool_timeout, Opts, 5000),
-    PoolSettings = #mongoose_worker_pool{selection_strategy = SelectionStrategy,
-                                         extra = PathPrefix,
-                                         request_timeout = RequestTimeout,
-                                         pool_timeout = PoolTimeout},
-    mongoose_wpool:save_pool_settings(PoolName, PoolSettings),
+    ets:insert(?MODULE, {PoolName, PathPrefix, RequestTimeout}),
     PoolSize = gen_mod:get_opt(pool_size, Opts, 20),
     Server = gen_mod:get_opt(server, Opts),
     HttpOpts = gen_mod:get_opt(http_opts, Opts, []),
-    PoolOpts = [{workers, PoolSize}, {worker, {fusco, {Server, HttpOpts}}}
-                | gen_mod:get_opt(pool_opts, Opts, [])],
-    wpool:start_sup_pool(PoolName, PoolOpts).
+    PoolSettings = [{strategy, SelectionStrategy}, {call_timeout, PoolTimeout},
+                    {workers, PoolSize}, {worker, {fusco, {Server, HttpOpts}}}
+                    | gen_mod:get_opt(pool_opts, Opts, [])],
+    mongoose_wpool:start(?MODULE, global, PoolName, PoolSettings).
 
-make_request(Pool, Path, Method, Headers, Query) ->
-    case catch pool_name(Pool) of
-        {'EXIT', badarg, _} ->
+make_request(PoolName, Path, Method, Headers, Query) ->
+    case ets:lookup(?MODULE, PoolName) of
+        [] ->
             {error, pool_not_started};
-        PoolName ->
-            case mongoose_wpool:get_pool_settings(PoolName) of
-                undefined ->
-                    {error, pool_not_started};
-                PoolOpts ->
-                    make_request(PoolName, PoolOpts, Path, Method, Headers, Query)
-            end
+        [{_, PathPrefix, RequestTimeout}] ->
+            make_request(PoolName, PathPrefix, RequestTimeout, Path,
+                         Method, Headers, Query)
     end.
 
-make_request(PoolName, PoolOpts, Path, Method, Headers, Query) ->
-    #mongoose_worker_pool{extra = PathPrefix,
-                          request_timeout = RequestTimeout,
-                          pool_timeout = PoolTimeout,
-                          selection_strategy = SelectionStrategy} = PoolOpts,
+make_request(PoolName, PathPrefix, RequestTimeout, Path, Method, Headers, Query) ->
     FullPath = <<PathPrefix/binary, Path/binary>>,
     Req = {request, FullPath, Method, Headers, Query, 2, RequestTimeout},
-    try wpool:call(PoolName, Req, SelectionStrategy, PoolTimeout) of
+    try mongoose_wpool:call(?MODULE, global, PoolName, Req) of
         {ok, {{Code, _Reason}, _RespHeaders, RespBody, _, _}} ->
             {ok, {Code, RespBody}};
         {error, timeout} ->
@@ -147,9 +142,3 @@ make_request(PoolName, PoolOpts, Path, Method, Headers, Query) ->
         Type:Error ->
             {error, {Type, Error}}
     end.
-
-pool_name(PoolName) ->
-    list_to_existing_atom("mongoose_http_client_pool_" ++ atom_to_list(PoolName)).
-
-make_pool_name(PoolName) ->
-    list_to_atom("mongoose_http_client_pool_" ++ atom_to_list(PoolName)).
--- src/mongoose_wpool.erl
+++ src/mongoose_wpool.erl
@@ -9,35 +9,95 @@
 -author("bartlomiej.gorny@erlang-solutions.com").
 -include("mongoose.hrl").
 -include("mongoose_wpool.hrl").
+
+-record(mongoose_wpool, {name :: term(), strategy :: atom(), call_timeout :: pos_integer()}).
+-dialyzer({no_match, start/4}).
+
 %% API
--export([ensure_started/0, get_pool_settings/1, delete_pool_settings/1, save_pool_settings/2]).
+-export([ensure_started/0,
+         start/4, stop/3,
+         call/4, call/5,
+         cast/4, cast/5,
+         get_pool_settings/3, get_pools/0, stats/3]).
 
 ensure_started() ->
     wpool:start(),
-    case ets:info(pool_settings_tab()) of
+    case ets:info(?MODULE) of
         undefined ->
             % we set heir here because the whole thing may be started by an ephemeral process
-            ets:new(pool_settings_tab(), [named_table, public,
+            ets:new(?MODULE, [named_table, public,
+                {read_concurrency, true},
                 {keypos, 2},
                 {heir, whereis(wpool_sup), undefined}]);
         _ ->
             ok
     end.
 
-get_pool_settings(PoolName) ->
-    case ets:lookup(pool_settings_tab(), PoolName) of
-        [PoolOpts] ->
-            PoolOpts;
-        [] ->
-            undefined
+start(Type, Host, Tag, PoolOpts) ->
+    {Opts0, WpoolOpts} = proplists:split(PoolOpts, [strategy, call_timeout]),
+    Opts = lists:append(Opts0),
+    case wpool:start_sup_pool(make_pool_name(Type, Host, Tag), WpoolOpts) of
+        {ok, Pid} ->
+            Strategy = proplists:get_value(strategy, Opts, best_worker),
+            CallTimeout = proplists:get_value(call_timeout, Opts, 5000),
+            ets:insert(?MODULE, #mongoose_wpool{name = {Type, Host, Tag},
+                                                strategy = Strategy,
+                                                call_timeout = CallTimeout}),
+            {ok, Pid};
+        Error ->
+            Error
     end.
 
-save_pool_settings(PoolName, PoolSettings) ->
-    ets:insert(pool_settings_tab(), PoolSettings#mongoose_worker_pool{name = PoolName}).
+stop(Type, Host, Tag) ->
+    ets:delete(?MODULE, {Type, Host, Tag}),
+    wpool:stop_sup_pool(make_pool_name(Type, Host, Tag)).
 
-delete_pool_settings(PoolName) ->
-    ets:delete(pool_settings_tab(), PoolName).
+call(Type, Host, Tag, Request) ->
+    case ets:lookup(?MODULE, {Type, Host, Tag}) of
+        [#mongoose_wpool{strategy = Strategy, call_timeout = CallTimeout}] ->
+            wpool:call(make_pool_name(Type, Host, Tag), Request, Strategy, CallTimeout);
+        [] ->
+            {error, pool_not_started}
+    end.
 
-pool_settings_tab() ->
-    mongoose_worker_pool_settings.
+call(Type, Host, Tag, HashKey, Request) ->
+    case ets:lookup(?MODULE, {Type, Host, Tag}) of
+        [#mongoose_wpool{call_timeout = CallTimeout}] ->
+            wpool:call(make_pool_name(Type, Host, Tag), Request, {hash_worker, HashKey}, CallTimeout);
+        [] ->
+            {error, pool_not_started}
+    end.
 
+cast(Type, Host, Tag, Request) ->
+    case ets:lookup(?MODULE, {Type, Host, Tag}) of
+        [#mongoose_wpool{strategy = Strategy}] ->
+            wpool:cast(make_pool_name(Type, Host, Tag), Request, Strategy);
+        [] ->
+            {error, pool_not_started}
+    end.
+
+cast(Type, Host, Tag, HashKey, Request) ->
+    case ets:lookup(?MODULE, {Type, Host, Tag}) of
+        [#mongoose_wpool{}] ->
+            wpool:cast(make_pool_name(Type, Host, Tag), Request, {hash_worker, HashKey});
+        [] ->
+            {error, pool_not_started}
+    end.
+
+get_pool_settings(Type, Host, Tag) ->
+    case ets:lookup(?MODULE, {Type, Host, Tag}) of
+        [PoolOpts] -> PoolOpts;
+        [] -> undefined
+    end.
+
+get_pools() ->
+    lists:map(fun(#mongoose_wpool{name = Name}) -> Name end, ets:tab2list(?MODULE)).
+
+stats(Type, Host, Tag) ->
+    wpool:stats(make_pool_name(Type, Host, Tag)).
+
+make_pool_name(Type, Host, Tag) when is_atom(Host) ->
+    make_pool_name(Type, atom_to_binary(Host, utf8), Tag);
+make_pool_name(Type, Host, Tag) when is_binary(Host) ->
+    binary_to_atom(<<"mongoose_wpool$", (atom_to_binary(Type, utf8))/binary, $$,
+                     Host/binary, $$, (atom_to_binary(Tag, utf8))/binary>>, utf8).
