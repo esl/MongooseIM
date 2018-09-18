ad415f637b4fb9a8de9cc541835447de5e573e3f
zofpolkowska <zofia.polkowska@erlang-solutions.com>
2018-09-18 14:26:19 +0200

    Refactor message processing in mod_inbox

--- src/inbox/mod_inbox.erl
+++ src/inbox/mod_inbox.erl
@@ -154,6 +154,7 @@ filter_packet({From, To, Acc, Msg = #xmlel{name = <<"message">>}}) ->
     Host = To#jid.server,
     maybe_process_message(Host, From, To, Msg, incoming),
     {From, To, Acc, Msg};
+
 filter_packet({From, To, Acc, Packet}) ->
     {From, To, Acc, Packet}.
 
@@ -161,25 +162,29 @@ filter_packet({From, To, Acc, Packet}) ->
                             From :: jid:jid(),
                             To :: jid:jid(),
                             Msg :: exml:element(),
-                            Dir :: outgoing | incoming) -> ok.
+                            Dir :: outgoing | incoming) -> ok | {ok, binary()}.
 maybe_process_message(Host, From, To, Msg, Dir) ->
     AcceptableMessage = should_be_stored_in_inbox(Msg),
-    if AcceptableMessage ->
-        Type = get_message_type(Msg),
-        Type == one2one andalso
-        process_message(Host, From, To, Msg, Dir, one2one),
-        (Type == groupchat andalso muclight_enabled(Host)) andalso % legacy MUC is handled in seperate module
-        process_message(Host, From, To, Msg, Dir, groupchat);
+    case AcceptableMessage of
         true ->
+            Type = get_message_type(Msg),
+            maybe_process_acceptable_message(Host, From, To, Msg, Dir, Type);
+        false ->
             ok
     end.
+
+maybe_process_acceptable_message(Host, From, To, Msg, Dir, one2one) ->
+            process_message(Host, From, To, Msg, Dir, one2one);
+maybe_process_acceptable_message(Host, From, To, Msg, Dir, groupchat) ->
+            muclight_enabled(Host) andalso
+            process_message(Host, From, To, Msg, Dir, groupchat).
 
 -spec process_message(Host :: host(),
                       From :: jid:jid(),
                       To :: jid:jid(),
                       Message :: exml:element(),
                       Dir :: outgoing | incoming,
-                      Type :: one2one | groupchat) -> ok.
+                      Type :: one2one | groupchat) -> ok | {ok, binary()}.
 process_message(Host, From, To, Message, outgoing, one2one) ->
     mod_inbox_one2one:handle_outgoing_message(Host, From, To, Message);
 process_message(Host, From, To, Message, incoming, one2one) ->
