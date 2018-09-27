516090fa43a78571736267a1b10ccb19d91971ad
Konrad Zemek <konrad.zemek@gmail.com>
2018-09-27 18:22:18 +0200

    Set correct rdbms_server_type for mssql tests.

--- src/backend_module.erl
+++ src/backend_module.erl
@@ -72,7 +72,7 @@ backend_code(Module, Backend, TrackedFuns) when is_atom(Backend) ->
     Code = lists:flatten(
              ["-module(", ProxyModuleName, ").\n",
               "-behaviour(backend_module).\n"
-              "-export([backend/0]).\n",
+              "-export([backend/0, backend_name/0]).\n",
               BehaviourExports,
 
 
