a0f98bdafa64e84def81e1e3d3fd46a5cd1ab545
Kacper Mentel <mentel.kk@gmail.com>
2018-11-07 09:48:13 +0100

    Improve test suite

--- test/mongoose_rabbit_worker_SUITE.erl
+++ test/mongoose_rabbit_worker_SUITE.erl
@@ -24,7 +24,7 @@
 
 -define(HOST, <<"localhost">>).
 -define(AMQP_MOCK_MODULES, [amqp_connection, amqp_channel]).
--define(MAX_QUEUE_LEN, 10).
+-define(MAX_QUEUE_LEN, 1000).
 
 %%--------------------------------------------------------------------
 %% Suite configuration
@@ -46,6 +46,14 @@ init_per_suite(Config) ->
     application:ensure_all_started(lager),
     mock_amqp(),
     mock_metrics(),
+    Config.
+
+end_per_suite(Config) ->
+    unload_amqp(),
+    meck:unload(mongoose_metrics),
+    Config.
+
+init_per_testcase(_Case, Config) ->
     WorkerOpts = [{host, ?HOST},
                   {amqp_client_opts, []},
                   {confirms, false},
@@ -53,16 +61,8 @@ init_per_suite(Config) ->
     {ok, WorkerPid} = gen_server:start(mongoose_rabbit_worker, WorkerOpts, []),
     Config ++ [{worker_pid, WorkerPid}].
 
-end_per_suite(Config) ->
-    exit(proplists:get_value(worker_pid, Config), kill),
-    unload_amqp(),
-    meck:unload(mongoose_metrics),
-    Config.
-
-init_per_testcase(_Case, Config) ->
-    Config.
-
 end_per_testcase(_Case, Config) ->
+    exit(proplists:get_value(worker_pid, Config), kill),
     Config.
 
 %%--------------------------------------------------------------------
@@ -71,6 +71,7 @@ end_per_testcase(_Case, Config) ->
 
 no_request_in_worker_queue_is_lost_when_amqp_call_fails(Config) ->
     %% given
+    Self = self(),
     Worker = proplists:get_value(worker_pid, Config),
     Ref = make_ref(),
     Lock = lock_fun(),
@@ -78,14 +79,14 @@ no_request_in_worker_queue_is_lost_when_amqp_call_fails(Config) ->
     Exception = exception_fun(),
 
     %% when
-    gen_server:cast(Worker, {amqp_publish, {Lock, Ref}, ok}),
-    gen_server:cast(Worker, {amqp_publish, {Exception, ok}, ok}),
-    gen_server:cast(Worker, {amqp_publish, {SendBack, Ref}, ok}),
+    gen_server:cast(Worker, {amqp_publish, {Lock, [Ref]}, ok}),
+    gen_server:cast(Worker, {amqp_publish, {Exception, [ok]}, ok}),
+    gen_server:cast(Worker, {amqp_publish, {SendBack, [Self, Ref]}, ok}),
     spawn(fun() ->
-                  gen_server:call(Worker, {amqp_call, {Exception, ok}})
+                  gen_server:call(Worker, {amqp_call, {Exception, [ok]}})
           end),
     spawn(fun() ->
-                  gen_server:call(Worker, {amqp_call, {SendBack, Ref}})
+                  gen_server:call(Worker, {amqp_call, {SendBack, [Self, Ref]}})
           end),
 
     %% unlock the worker
@@ -101,13 +102,13 @@ worker_creates_fresh_amqp_conection_and_channel_when_amqp_call_fails(Config) ->
     ConnectionAndChannel0 = get_worker_conn_and_chann(Worker),
 
     %% when amqp_publish fails
-    gen_server:cast(Worker, {amqp_publish, {Exception, ok}, ok}),
+    gen_server:cast(Worker, {amqp_publish, {Exception, [ok]}, ok}),
     %% then
     ConnectionAndChannel1 = get_worker_conn_and_chann(Worker),
     ?assertNotMatch(ConnectionAndChannel0, ConnectionAndChannel1),
 
     %% when amqp_call fails
-    gen_server:call(Worker, {amqp_call, {Exception, ok}}),
+    gen_server:call(Worker, {amqp_call, {Exception, [ok]}}),
 
     %% then
     ConnectionAndChannel2 = get_worker_conn_and_chann(Worker),
@@ -122,8 +123,8 @@ worker_processes_msgs_when_queue_msg_len_limit_is_not_reached(Config) ->
     SendBack = send_back_fun(),
 
     %% when
-    gen_server:cast(Worker, {amqp_publish, {Lock, Ref}, ok}),
-    gen_server:cast(Worker, {amqp_publish, {SendBack, Ref}, ok}),
+    gen_server:cast(Worker, {amqp_publish, {Lock, [Ref]}, ok}),
+    gen_server:cast(Worker, {amqp_publish, {SendBack, [self(), Ref]}, ok}),
     [gen_server:cast(Worker, {amqp_publish, ok, ok})
      || _ <- lists:seq(1, ?MAX_QUEUE_LEN-1)],
 
@@ -141,8 +142,8 @@ worker_drops_msgs_when_queue_msg_len_limit_is_reached(Config) ->
     SendBack = send_back_fun(),
 
     %% when
-    gen_server:cast(Worker, {amqp_publish, {Lock, Ref}, ok}),
-    gen_server:cast(Worker, {amqp_publish, {SendBack, Ref}, ok}),
+    gen_server:cast(Worker, {amqp_publish, {Lock, [Ref]}, ok}),
+    gen_server:cast(Worker, {amqp_publish, {SendBack, [self(), Ref]}, ok}),
     [gen_server:cast(Worker, {amqp_publish, ok, ok})
      || _ <- lists:seq(1, ?MAX_QUEUE_LEN+1)],
 
@@ -163,12 +164,14 @@ mock_amqp() ->
     meck:expect(amqp_connection, open_channel, fun(_) -> {ok, random_pid()} end),
     meck:expect(amqp_connection, close, fun(_) -> ok end),
     meck:expect(amqp_channel, close, fun(_) -> ok end),
-    meck:expect(amqp_channel, call, fun(_, {F, A}) when is_function(F) -> F(A);
-                                       (_, _) -> ok
-                                    end),
-    meck:expect(amqp_channel, call, fun(_, {F, A}, _) when is_function(F) -> F(A);
-                                       (_, _, _) -> ok
-                                    end).
+    meck:expect(amqp_channel, call,
+                fun(_, {F, A}) when is_function(F) -> apply(F, A);
+                   (_, _) -> ok
+                end),
+    meck:expect(amqp_channel, call,
+                fun(_, {F, A}, _) when is_function(F) -> apply(F, A);
+                   (_, _, _) -> ok
+                end).
 
 mock_metrics() ->
     meck:new(mongoose_metrics, [no_link, passthrough]),
@@ -181,18 +184,21 @@ random_pid() ->
     spawn(fun() -> ok end).
 
 lock_fun() ->
-    fun(R) ->
-            receive
-                R -> unlocked
-            end
+    fun lock/1.
+
+lock(R) ->
+    receive
+        R -> unlocked
     end.
 
 send_back_fun() ->
-    S = self(),
-    fun(Msg) -> S ! Msg end.
+    fun erlang:send/2.
 
 exception_fun() ->
-    fun(_) -> throw(exception) end.
+    fun exception/1.
+
+exception(_) ->
+    throw(exception).
 
 get_worker_conn_and_chann(Worker) ->
     State = sys:get_state(Worker),
