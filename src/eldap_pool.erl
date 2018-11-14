0b4e9d44d9411ae7ea17cbca02046cf5593d8e4e
Michal Piotrowski <michal.piotrowski@erlang-solutions.com>
2018-11-14 19:42:22 +0100

    dd

--- src/eldap_pool.erl
+++ src/eldap_pool.erl
@@ -93,7 +93,8 @@ parse_add_attr({N, List}) ->
 start_link(Name, Hosts, _Backups, Port, Rootdn, Passwd, _Opts) ->
   PoolName = make_id(Name),
   pg2:create(PoolName),
-  LogFun = fun(_, FormatString, FormatArgs) -> ?WARNING_MSG(FormatString, FormatArgs) end,
+  LogFun = fun(Level, FormatString, FormatArgs) ->
+               ?WARNING_MSG("Level: ~p" ++ FormatString, [Level | FormatArgs]) end,
   lists:foreach(fun (Host) ->
     case catch eldap:open([maybe_b2list(Host)], [{port, Port}, {log, LogFun}])
     of
