ffe4cf9c7e573570d3923af3ee82f57d7e70286a
Aleksander Lisiecki <sebaczechu123@gmail.com>
2018-11-27 15:54:12 +0100

    Implement last item cache for calbacks

--- src/pubsub/mod_pubsub_cache_mnesia.erl
+++ src/pubsub/mod_pubsub_cache_mnesia.erl
@@ -5,35 +5,71 @@
 
 -export([start/0, stop/0]).
 
--export([create_table/0, delete_table/0, insert_last_item/4, delete_last_item/4]).
+-export([
+    create_table/0, 
+    delete_table/0, 
+    insert_last_item/4,
+    get_last_item/1, 
+    delete_last_item/1]).
+
 %% ------------------------ Backend start/stop ------------------------
 
 -spec start() -> ok.
-start() ->
-   ok.
+start() -> ok = create_table().
 
 -spec stop() -> ok.
-stop() ->
-    ok.
+stop() -> ok.
 
 -spec create_table() -> ok | {error, Reason :: term()}.
 create_table() ->
-    ok.
+    QueryResult = mnesia:create_table(
+        pubsub_last_item,
+        [
+            {ram_copies, [node()]},
+            {attributes, record_info(fields, pubsub_last_item)}
+        ]),
+        process_query_result(QueryResult).
 
 -spec delete_table() -> ok | {error, Reason :: term()}.
 delete_table() ->
-    ok. 
+    QueryResult = mnesia:delete_table(pubsub_last_item),
+    process_query_result(QueryResult).
 
 -spec insert_last_item(Nidx :: mod_pubsub:nodeIdx(),
                  ItemID :: mod_pubsub:itemId(),
                  Publisher::{erlang:timestamp(), jid:ljid()},
                  Payload::mod_pubsub:payload()) -> ok | {error, Reason :: term()}.
-insert_last_item(_, _, _, _) ->
-    ok.
- 
--spec delete_last_item(Nidx :: mod_pubsub:nodeIdx(),
-                 ItemID :: mod_pubsub:itemId(),
-                 Publisher::{erlang:timestamp(), jid:ljid()},
-                 Payload::mod_pubsub:payload()) -> ok | {error, Reason :: term()}.
-delete_last_item(_, _, _, _) ->
-    ok.
+insert_last_item(Nidx, ItemId, Publisher, Payload) ->
+    try mnesia:dirty_write(
+        {pubsub_last_item, 
+        Nidx, 
+        ItemId, 
+        {os:timestamp(), 
+            jid:to_lower(jid:to_bare(Publisher))}, 
+        Payload}
+    ) of
+        ok -> ok
+    catch
+        exit:{aborded, Reason} -> {error, Reason}
+    end.
+
+-spec get_last_item(Nidx :: mod_pubsub:nodeIdx()) -> [mod_pubsub:pubsubLastItem()] | {error, Reason :: term()}.
+get_last_item(Nidx) -> 
+    try mnesia:dirty_read({pubsub_last_item, Nidx}) of
+        ok -> ok
+    catch
+        exit:{aborded, Reason} -> {error, Reason}
+    end.
+
+-spec delete_last_item(Nidx :: mod_pubsub:nodeIdx()) -> ok | {error, Reason :: term()}.
+delete_last_item(Nidx) ->
+    try mnesia:dirty_delete({pubsub_last_item, Nidx}) of
+        ok -> ok
+    catch
+        exit:{aborded, Reason} -> {error, Reason}
+    end.
+
+%% ------------------------ Helpers ----------------------------
+
+process_query_result({atomic, ok}) -> ok;
+process_query_result({aborted, Reason}) -> {error, Reason}.
--- src/pubsub/mod_pubsub_cache_rdbms.erl
+++ src/pubsub/mod_pubsub_cache_rdbms.erl
@@ -1,1 +1,48 @@
 -module(mod_pubsub_cache_rdbms).
+
+-include("pubsub.hrl").
+-include("jlib.hrl").
+
+-export([start/0, stop/0]).
+
+-export([
+    create_table/0, 
+    delete_table/0, 
+    insert_last_item/4,
+    get_last_item/1, 
+    delete_last_item/1]).
+
+%% ------------------------ Backend start/stop ------------------------
+
+start() -> ok.
+
+stop() -> ok.
+
+-spec create_table() -> ok | {error, Reason :: term()}.
+create_table() -> ok.
+
+-spec delete_table() -> ok | {error, Reason :: term()}.
+delete_table() -> ok.
+
+
+-spec insert_last_item(Nidx :: mod_pubsub:nodeIdx(),
+                           ItemID :: mod_pubsub:itemId(),
+                           Publisher::{erlang:timestamp(), jid:ljid()},
+                           Payload::mod_pubsub:payload()) -> ok | {error, Reason :: term()}.
+insert_last_item(Nidx, ItemID, Publisher, Payload) ->
+ReadQuerySQL = mod_pubsub_db_rdbms_sql:insert_pubsub_last_item(Nidx, ItemID, Publisher, Payload),
+    {updated, _} = mongoose_rdbms:sql_query(global, ReadQuerySQL),
+    ok.
+
+-spec  delete_last_item(Nidx :: mod_pubsub:nodeIdx()) -> ok | {error, Reason :: term()}.
+delete_last_item(Nidx) -> 
+    DeleteQuerySQL = mod_pubsub_db_rdbms_sql:delete_pubsub_last_item(Nidx),
+    {updated, _} = mongoose_rdbms:sql_query(global, DeleteQuerySQL),
+    ok.
+
+-spec get_last_item(Nidx :: mod_pubsub:nodeIdx()) -> 
+    [mod_pubsub:pubsubLastItem()] | {error, Reason :: term()}.
+get_last_item(Nidx) -> 
+    ReadQuerySQL = mod_pubsub_db_rdbms_sql:get_pubsub_last_item(Nidx),
+    {selected, LastItemRows} = mongoose_rdbms:sql_query(global, ReadQuerySQL),
+    LastItemRows.
