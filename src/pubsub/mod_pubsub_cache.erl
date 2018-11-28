b34da5cfabb81ccbfe5c32750e7ad22ce4b63e40
zofpolkowska <zofpolkowska@gmail.com>
2018-11-28 11:42:00 +0100

    Add mod_pubsub_cache backend module

---
+++ src/pubsub/mod_pubsub_cache.erl
@@ -0,0 +1,25 @@
+-module(mod_pubsub_cache).
+
+%%====================================================================
+%% Behaviour callbacks
+%%====================================================================
+
+%% ------------------------ Backend start/stop ------------------------
+
+-callback start() -> ok.
+
+-callback stop() -> ok.
+
+-callback create_table() -> ok | {error, Reason :: term()}.
+
+-callback delete_table() -> ok | {error, Reason :: term()}.
+
+-callback upsert_last_item(Nidx :: mod_pubsub:nodeIdx(),
+                           ItemID :: mod_pubsub:itemId(),
+                           Publisher::jid:ljid(),
+                           Payload::mod_pubsub:payload()) -> ok | {error, Reason :: term()}.
+
+-callback delete_last_item(Nidx :: mod_pubsub:nodeIdx()) -> ok | {error, Reason :: term()}.
+
+-callback get_last_item(Nidx :: mod_pubsub:nodeIdx()) -> [mod_pubsub:pubsubLastItem()] | {error, Reason :: term()}.
+
