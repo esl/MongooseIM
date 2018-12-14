b494d669dccf3a16f1afbeb39adb912437c717b8
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2018-12-14 12:23:48 +0100

    Support item-not-found for MAM MUC queries to Cassandra backend

--- src/mam/mod_mam_muc_cassandra_arch.erl
+++ src/mam/mod_mam_muc_cassandra_arch.erl
@@ -33,6 +33,7 @@
 -import(mod_mam_utils,
         [maybe_min/2,
          maybe_max/2,
+         maybe_last/1,
          apply_start_border/2,
          apply_end_border/2]).
 
@@ -441,8 +442,15 @@ lookup_messages_before_id(PoolName, Host, RoomJID,
                           PageSize, Filter) ->
     TotalCount = calc_count(PoolName, RoomJID, Host, Filter),
     Offset = calc_offset(PoolName, RoomJID, Host, Filter, PageSize, TotalCount, RSM),
-    MessageRows = extract_messages(PoolName, RoomJID, Host, before_id(ID, Filter), PageSize, true),
-    {ok, {TotalCount, Offset, rows_to_uniform_format(MessageRows, RoomJID)}}.
+    MessageRows = extract_messages(PoolName, RoomJID, Host, to_id(ID, Filter),
+                                   PageSize + 1, true),
+    case maybe_last(MessageRows) of
+        {ok, #{id := ID}} = _IntervalEndpoint ->
+            Page = lists:sublist(MessageRows, PageSize),
+            {ok, {TotalCount, Offset, rows_to_uniform_format(Page, RoomJID)}};
+        undefined ->
+            {error, item_not_found}
+    end.
 
 lookup_messages_after_id(PoolName, Host, RoomJID,
                          RSM = #rsm_in{direction = aft, id = ID},
@@ -450,8 +458,14 @@ lookup_messages_after_id(PoolName, Host, RoomJID,
     PoolName = pool_name(RoomJID),
     TotalCount = calc_count(PoolName, RoomJID, Host, Filter),
     Offset = calc_offset(PoolName, RoomJID, Host, Filter, PageSize, TotalCount, RSM),
-    MessageRows = extract_messages(PoolName, RoomJID, Host, after_id(ID, Filter), PageSize, false),
-    {ok, {TotalCount, Offset, rows_to_uniform_format(MessageRows, RoomJID)}}.
+    MessageRows0 = extract_messages(PoolName, RoomJID, Host, from_id(ID, Filter),
+                                    PageSize + 1, false),
+    case MessageRows0 of
+        [#{id := ID} = _IntervalEndpoint | MessageRows] ->
+            {ok, {TotalCount, Offset, rows_to_uniform_format(MessageRows, RoomJID)}};
+        _ ->
+            {error, item_not_found}
+    end.
 
 
 after_id(ID, Filter = #mam_muc_ca_filter{start_id = AfterID}) ->
