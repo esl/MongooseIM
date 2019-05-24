f681410cd638023cc2d18cb0eed2cae4d450e6f6
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2019-05-24 10:48:50 +0200

    Test LDAP with TLS on Travis
    - Change LDAP docker image to support TLS
    - Add {versions, ['tlsv1.2']} because of an OpenSSL bug on Travis
    - Reuse MongooseIM certificates for OpenLDAP to make the configuration simple

--- tools/travis-setup-ldap.sh
+++ tools/travis-setup-ldap.sh
@@ -14,10 +14,11 @@ LDAP_ROOT_DIR="$(mktempdir mongoose_ldap_root)"
 LDAP_SCHEMAS_DIR="$LDAP_ROOT_DIR/prepopulate"
 LDAP_DATA_DIR="$LDAP_ROOT_DIR/data"
 LDAP_CONFIG_DIR="$LDAP_ROOT_DIR/config"
+LDAP_CERT_DIR="$LDAP_ROOT_DIR/certs"
 
 echo "LDAP_ROOT_DIR=$LDAP_ROOT_DIR"
 
-mkdir -p "$LDAP_SCHEMAS_DIR" "$LDAP_DATA_DIR" "$LDAP_CONFIG_DIR"
+mkdir -p "$LDAP_SCHEMAS_DIR" "$LDAP_DATA_DIR" "$LDAP_CONFIG_DIR" "$LDAP_CERT_DIR"
 
 cat > "$LDAP_SCHEMAS_DIR/init_entries.ldif" << EOL
 dn: ou=Users,dc=esl,dc=com
@@ -25,16 +26,36 @@ objectClass: organizationalUnit
 ou: users
 EOL
 
+cp tools/ssl/mongooseim/{cert,key}.pem "$LDAP_CERT_DIR"
+cp tools/ssl/ca/cacert.pem "$LDAP_CERT_DIR"
+
 docker rm -f mongooseim-ldap || echo "Skip removing previous container"
-# Host on non-standard higher port 3389 to avoid problems with lower ports
-# Default LDAP port is 389
+# Host on non-standard higher ports 3389 and 3636 to avoid problems with lower ports
+# Default LDAP ports are 389 (TCP) and 636 (TLS)
+
 docker run -d \
     --name mongooseim-ldap \
     -p 3389:389 \
-    -e SLAPD_DOMAIN="$LDAP_DOMAIN" \
-    -e SLAPD_PASSWORD="$LDAP_ROOTPASS" \
-    -e SLAPD_ORGANIZATION="$LDAP_ORGANISATION" \
-    $(data_on_volume -v "$LDAP_CONFIG_DIR:/etc/ldap") \
+    -p 3636:636 \
+    -e LDAP_DOMAIN="$LDAP_DOMAIN" \
+    -e LDAP_ADMIN_PASSWORD="$LDAP_ROOTPASS" \
+    -e LDAP_ORGANISATION="$LDAP_ORGANISATION" \
+    -e LDAP_TLS_CRT_FILENAME=cert.pem \
+    -e LDAP_TLS_KEY_FILENAME=key.pem \
+    -e LDAP_TLS_CA_CRT_FILENAME=cacert.pem \
+    $(data_on_volume -v "$LDAP_CONFIG_DIR:/etc/ldap/slapd.d") \
     $(data_on_volume -v "$LDAP_DATA_DIR:/var/lib/ldap") \
-    $(mount_ro_volume "$LDAP_SCHEMAS_DIR" /etc/ldap.dist/prepopulate/) \
-    openfrontier/openldap-server
+    $(mount_ro_volume "$LDAP_SCHEMAS_DIR" /container/service/slapd/assets/config/bootstrap/ldif/custom/) \
+    $(mount_ro_volume "$LDAP_CERT_DIR" /container/service/slapd/assets/certs/) \
+    osixia/openldap:1.2.4 \
+    --copy-service
+
+n=0
+until [ $n -ge 10 ]
+do
+    echo "Q" | openssl s_client -connect localhost:3636 \
+                       -cert tools/ssl/mongooseim/cert.pem -key tools/ssl/mongooseim/key.pem \
+                       -CAfile tools/ssl/ca/cacert.pem && break
+    n=$[$n+1]
+    sleep 15
+done
