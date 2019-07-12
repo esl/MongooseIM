53192510490c0791b797c95b48c51ab766f4f2e3
Ludwik Bukowski <ludwik.bukowski@gmail.com>
2019-07-12 16:04:24 +0200

    Sebas fix

--- tools/circleci-otp-to-str.sh
+++ tools/circleci-otp-to-str.sh
@@ -6,15 +6,17 @@ ARGS=("$@")
 ARGS_LEN="${#ARGS[@]}"
 
 usage() {
-    echo "Usage: $0 <debian_version_of_package>"
+  echo "Usage: $0 <debian_version_of_package>"
 }
 
 # Require valid number of arguments
 if [[ ! $ARGS_LEN -eq 1 ]]; then
-    usage && exit 1
+  usage && exit 1
 fi
 # Require `awk`
-hash awk 2>/dev/null || { echo >&2 "No 'awk' command available"; exit 1; }
+if ! hash awk 2>/dev/null; then
+  echo "No 'awk' command available" && exit 1
+fi
 
 DEBIAN_VERSION_OF_PACKAGE=$1
 SOFTWARE_VERSION="$(echo "$DEBIAN_VERSION_OF_PACKAGE" | awk -F ':|-' '{print $2}')"
