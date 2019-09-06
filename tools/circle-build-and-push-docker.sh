9632d36dde89ab86017abdd630d6a3a9bcc0bf2b
Piotr Nosek <piotr.nosek@erlang-solutions.com>
2019-09-06 10:56:52 +0200

    Update Docker tag gen. logic and enable image upload

--- tools/circle-build-and-push-docker.sh
+++ tools/circle-build-and-push-docker.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-#MIM_TAR_FULL_NAME=mongooseim-$CIRCLE_BRANCH.OTP-$OTP_RELEASE.$(lsb_release -is | tr "A-Z" "a-z").$(lsb_release -rs).$(uname -m).tar.bz2
 MONGOOSE_TGZ=mongooseim.tar.gz
 
 BUILD_PATH=_build/prod/rel/mongooseim
 
-#tar -cjh --transform="s,${BUILD_PATH},mongooseim-${CIRCLE_BRANCH},S" -f ${MIM_TAR_FULL_NAME} ${BUILD_PATH}
 tar czh --transform="s,${BUILD_PATH},mongooseim,S" -f $MONGOOSE_TGZ ${BUILD_PATH}
 
 export BUILDS=`pwd`
 
-DOCKERHUB_TAG=${CIRCLE_BRANCH}
+# We use commit hash by default, because branch name may contain characters
+# that are illegal for Docker tags, e.g. '/'.
+DOCKERHUB_TAG=${CIRCLE_SHA1}
 VERSION=`tools/generate_vsn.sh`
 GIT_REF=`git rev-parse --short HEAD`
 
-if [ ${CIRCLE_BRANCH} == 'master' ]; then
+if [ -n "$CIRCLE_PULL_REQUEST" ]; then
+    # CircleCI doesn't provide PR number in env. var., so we need to extract it from PR URL
+    # May not work with different service than GitHub
+    # TODO: Possibly change it to something else during Tide integration
+    PR_NUMBER=${CIRCLE_PULL_REQUEST##*/}
+    DOCKERHUB_TAG="PR-${PR_NUMBER}"
+elif [ ${CIRCLE_BRANCH} == 'master' ]; then
     DOCKERHUB_TAG="latest";
 fi
+
+# TODO: Add DOCKERHUB=${VERSION} when CircleCI handles weekly builds as well
 
 echo "Tag: ${DOCKERHUB_TAG}"
 
@@ -34,6 +42,6 @@ docker build -f Dockerfile.member -t ${IMAGE_TAG} \
 	     --build-arg VERSION=${VERSION} \
 	     .
 
-#docker login -u ${DOCKERHUB_USER} -p ${DOCKERHUB_PASS}
+docker login -u ${DOCKERHUB_USER} -p ${DOCKERHUB_PASS}
 
-#docker push ${IMAGE_TAG}
+docker push ${IMAGE_TAG}
