0f6ab7093a270fdf6f5838d61cb4e84db8854bef
Michal Piotrowski <michal.piotrowski@erlang-solutions.com>
2019-10-28 09:36:58 +0100

    make sure generate_vsn runs in repo's root direcotry

--- tools/generate_vsn.sh
+++ tools/generate_vsn.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+cd `dirname "$0"`/../
+
 GIT_VSN=`git describe --always --tags 2>/dev/null`
 
 if [ $? -eq 0 ]; then
 	echo $GIT_VSN
 else
-	echo `cat ${DIR}/../VERSION`
+	cat VERSION
 fi
