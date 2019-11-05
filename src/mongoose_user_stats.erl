497d9de1640262797891040bc854794856f436a0
Aleksander Lisiecki <sebaczechu123@gmail.com>
2019-11-05 15:28:42 +0100

    Test reporting hosts_count

--- src/mongoose_user_stats.erl
+++ src/mongoose_user_stats.erl
@@ -2,51 +2,51 @@
 
 -author('aleksander.lisiecki@erlang-solutions.com').
 
-% TODO replace tid to official ESL one
--define(URL_BASE, "https://www.google-analytics.com/collect?v=1&tid=UA-151110014-1&t=event").
-
 -export([report_user_stats/0]).
 
+% TODO replace tid to official ESL one
+-define(BASE_URL, "https://www.google-analytics.com/collect?v=1&tid=UA-151110014-1&t=event").
+
 report_user_stats() ->
-    IsAllowed = ejabberd_config:get_local_option_or_default(is_report_user_stats_allowed, true),
-    report_user_stats(IsAllowed).
+    ReportUrl = ejabberd_config:get_local_option_or_default(google_analytics_url, ?BASE_URL),
+    report_user_stats(ReportUrl).
 
 % Functions are spawned and not linked, as MongooseIM should not care if they fail or not.
 % Moreover the MongooseIM's start should not be blocked.
-report_user_stats(true) ->
+report_user_stats(disable) -> ok;
+report_user_stats(ReportUrl) ->
     % Data used for more then one report
     Hosts = ejabberd_config:get_global_option(hosts),
     Reports = [
-        fun() -> report_number_of_hosts(Hosts) end,
-        fun() -> report_used_modules(Hosts) end
+        fun() -> report_number_of_hosts(Hosts, ReportUrl) end,
+        fun() -> report_used_modules(Hosts, ReportUrl) end
     ],
     lists:foreach(
         fun(Fun) ->
             spawn(Fun)
-        end, Reports);
-report_user_stats(_) -> ok.
+        end, Reports).
 
-report_number_of_hosts(Hosts) ->
+report_number_of_hosts(Hosts, ReportUrl) ->
     Len = length(Hosts),
-    report(hosts_count, Len).
+    report(ReportUrl, hosts_count, Len).
 
-report_used_modules(Hosts) ->
+report_used_modules(Hosts, ReportUrl) ->
     ModulesWithOpts = lists:flatten(
         lists:map(fun gen_mod:loaded_modules_with_opts/1, Hosts)),
     lists:foreach(
         fun({Module, Opts}) ->
             Backend = proplists:get_value(backend, Opts, none),
-            report(modules, Module, Backend)
+            report(ReportUrl, modules, Module, Backend)
         end, ModulesWithOpts).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%% HELPERS %%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%--------------------------------------------------------------------
+%% Helpers
+%%--------------------------------------------------------------------
 
-report(EventCategory, EventAction) ->
-    report(EventCategory, EventAction, empty).
+report(ReportUrl, EventCategory, EventAction) ->
+    report(ReportUrl, EventCategory, EventAction, empty).
 
-report(EventCategory, EventAction, EventLabel) ->
+report(ReportUrl, EventCategory, EventAction, EventLabel) ->
     MaybeLabel = case EventLabel of
         empty ->
             [];
@@ -58,7 +58,7 @@ report(EventCategory, EventAction, EventLabel) ->
     LstEventCategory = term_to_string(EventCategory),
     LstEventAction = term_to_string(EventAction),
     ListUrl = [
-        ?URL_BASE,
+        ReportUrl,
         "&cid=", LstClientId,
         "&ec=", LstEventCategory,
         "&ea=", LstEventAction
