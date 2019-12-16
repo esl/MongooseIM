7a57d2187f8150aec6abfc1289d6d9f3fa2791ab
Jan Ciesla <jan.ciesla@erlang-solutions.com>
2019-12-16 14:15:36 +0100

    get modules for periodic updates

--- src/system_stats/mongoose_system_stats_gatherer.erl
+++ src/system_stats/mongoose_system_stats_gatherer.erl
@@ -23,12 +23,23 @@ get_reports(Fun) ->
 -spec report_getters() -> [fun(() -> [report_struct()])].
 report_getters() ->
     [
-        fun get_hosts_count/0
+        fun get_hosts_count/0,
+        fun get_modules/0
     ].
 
 get_hosts_count() ->
     Hosts = ejabberd_config:get_global_option(hosts),
     NumberOfHosts = length(Hosts),
     [#{report_name => hosts, key => count, value => NumberOfHosts}].
+
+get_modules() ->
+    Hosts = ejabberd_config:get_global_option(hosts),
+    ModulesWithOpts = lists:flatten(
+        lists:map(fun gen_mod:loaded_modules_with_opts/1, Hosts)),
+    lists:map(
+        fun({Module, Opts}) ->
+            Backend = proplists:get_value(backend, Opts, none),
+            #{report_name => Module, key => backend, value => Backend}
+        end, ModulesWithOpts).
 
 
