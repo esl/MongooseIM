e20d0cc7cfa6baad47ab71b66155215a754b425d
Gustaw Lippa <gustaw.lippa@erlang-solutions.com>
2020-03-05 15:09:23 +0100

    Change muc_light to use mongoose_hooks

--- src/muc_light/mod_muc_light_codec_legacy.erl
+++ src/muc_light/mod_muc_light_codec_legacy.erl
@@ -63,8 +63,8 @@ encode({#msg{} = Msg, AffUsers}, Sender, {RoomU, RoomS} = RoomUS, HandleFun) ->
                  {role, mod_muc_light_utils:light_aff_to_muc_role(Aff)}
     ],
     FilteredPacket = #xmlel{ children = Children }
-        = ejabberd_hooks:run_fold(filter_room_packet, RoomS, MsgForArch, [EventData]),
-    ejabberd_hooks:run(room_send_packet, RoomS, [FilteredPacket, EventData]),
+        = mongoose_hooks:filter_room_packet(RoomS, MsgForArch, EventData),
+    mongoose_hooks:room_send_packet(RoomS, FilteredPacket, EventData),
     lists:foreach(
       fun({{U, S}, _}) ->
               send_to_aff_user(RoomJID, U, S, <<"message">>, Attrs, Children, HandleFun)
