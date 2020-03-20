69e86c851094ceca0271cd312fc9d968820b6386
Kacper Mentel <mentel.kk@gmail.com>
2020-03-20 15:43:45 +0100

    Change user and database names for cockroach

--- tools/docker-setup-cockroach.sh
+++ tools/docker-setup-cockroach.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Configuring postgres and creating schema
-./cockroach sql --insecure --execute "CREATE DATABASE mongooseim"
-./cockroach sql --insecure --execute "CREATE USER mongooseim"
-./cockroach sql --insecure --execute "GRANT ALL ON DATABASE mongooseim TO mongooseim"
+./cockroach sql --insecure --execute "CREATE DATABASE ejabberd"
+./cockroach sql --insecure --execute "CREATE USER ejabberd"
+./cockroach sql --insecure --execute "GRANT ALL ON DATABASE ejabberd TO ejabberd"
 ./cockroach sql --insecure \
-    --database "mongooseim" \
-    --user "mongooseim" \
+    --database "ejabberd" \
+    --user "ejabberd" \
     < ${SQL_TEMP_DIR}/cockroach-pg.sql
