fc52bd04568c78644284b59adf7d2c95a974941a
Nelson Vides <nelson.vides@erlang-solutions.com>
2020-07-07 18:00:40 +0200

    Swap lager calls to logger macros
    The macro LOCATION has been removed as it is already defined in the
    logger's header file in `otp/lib/kernel/include/logger.hrl` as
    ```
    -define(LOCATION,#{mfa=>{?MODULE,?FUNCTION_NAME,?FUNCTION_ARITY},
                       line=>?LINE,
                       file=>?FILE}).
    ```

--- include/mongoose_logger.hrl
+++ include/mongoose_logger.hrl
@@ -1,35 +1,37 @@
 -ifndef(MONGOOSEIM_LOGGER_HRL).
 -define(MONGOOSEIM_LOGGER_HRL, true).
 
+-include_lib("kernel/include/logger.hrl").
+
 -define(LOG_IF(Level, Condition, Format, Args),
-    (Condition) == true andalso lager:Level(Format, Args)).
+    (Condition) == true andalso ?LOG(Level, Format, Args)).
 
 -define(DEBUG(Format, Args),
-    lager:debug(Format, Args)).
+    ?LOG_DEBUG(Format, Args)).
 
 -define(DEBUG_IF(Condition, Format, Args),
     ?LOG_IF(debug, Condition, Format, Args)).
 
 -define(INFO_MSG(Format, Args),
-    lager:info(Format, Args)).
+    ?LOG_INFO(Format, Args)).
 
 -define(INFO_MSG_IF(Condition, Format, Args),
     ?LOG_IF(info, Condition, Format, Args)).
 
 -define(WARNING_MSG(Format, Args),
-    lager:warning(Format, Args)).
+    ?LOG_WARNING(Format, Args)).
 
 -define(WARNING_MSG_IF(Condition, Format, Args),
     ?LOG_IF(warning, Condition, Format, Args)).
 
 -define(ERROR_MSG(Format, Args),
-    lager:error(Format, Args)).
+    ?LOG_ERROR(Format, Args)).
 
 -define(ERROR_MSG_IF(Condition, Format, Args),
     ?LOG_IF(error, Condition, Format, Args)).
 
 -define(CRITICAL_MSG(Format, Args),
-    lager:critical(Format, Args)).
+    ?LOG_CRITICAL(Format, Args)).
 
 -define(CRITICAL_MSG_IF(Condition, Format, Args),
     ?LOG_IF(critical, Condition, Format, Args)).
