eaf44999ed6af3a66b3d451584be3939c1f762f8
Michal Piotrowski <michal.piotrowski@erlang-solutions.com>
2020-07-09 14:46:05 +0200

    use esl/fast_scram to compute salted_password

--- src/mongoose_scram.erl
+++ src/mongoose_scram.erl
@@ -60,7 +60,7 @@
 
 -spec salted_password(sha_type(), binary(), binary(), non_neg_integer()) -> binary().
 salted_password(Sha, Password, Salt, IterationCount) ->
-    hi(Sha, jid:resourceprep(Password), Salt, IterationCount).
+    fast_scram:hi(Sha, jid:resourceprep(Password), Salt, IterationCount).
 
 -spec client_key(sha_type(), binary()) -> binary().
 client_key(Sha, SaltedPassword) ->
@@ -84,18 +84,6 @@ client_proof_key(ClientProof, ClientSignature) ->
 -spec server_signature(sha_type(), binary(), binary()) -> binary().
 server_signature(Sha, ServerKey, AuthMessage) ->
     crypto:hmac(Sha, ServerKey, AuthMessage).
-
--spec hi(sha_type(), binary(), binary(), non_neg_integer()) -> binary().
-hi(Sha, Password, Salt, IterationCount) ->
-    U1 = crypto:hmac(Sha, Password, <<Salt/binary, 0, 0, 0, 1>>),
-    mask(U1, hi_round(Sha, Password, U1, IterationCount - 1)).
-
--spec hi_round(sha_type(), binary(), binary(), non_neg_integer()) -> binary().
-hi_round(Sha, Password, UPrev, 1) ->
-    crypto:hmac(Sha, Password, UPrev);
-hi_round(Sha, Password, UPrev, IterationCount) ->
-    U = crypto:hmac(Sha, Password, UPrev),
-    mask(U, hi_round(Sha, Password, U, IterationCount - 1)).
 
 -spec mask(binary(), binary()) -> binary().
 mask(Key, Data) ->
