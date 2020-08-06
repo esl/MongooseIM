74ddec75a2c669fd6ad96634db695f04b5258a25
Nelson Vides <nelson.vides@erlang-solutions.com>
2020-08-06 14:28:29 +0200

    Rename state record for gun worker

--- src/wpool/mongoose_gun_worker.erl
+++ src/wpool/mongoose_gun_worker.erl
@@ -18,21 +18,25 @@
          handle_cast/2,
          handle_info/2]).
 
--record(state, {host :: inet:hostname() | inet:ip_address(),
-                port :: inet:port_number(),
-                opts :: gun:opts(),
-                pid :: pid() | undefined,
-                monitor :: reference() | undefined,
-                protocol :: http | http2 | undefined,
-                requests = #{} :: #{reference() := {request(), response_data()}}}).
+-record(gun_worker_state, {
+          host :: inet:hostname() | inet:ip_address(),
+          port :: inet:port_number(),
+          opts :: gun:opts(),
+          pid :: pid() | undefined,
+          monitor :: reference() | undefined,
+          protocol :: http | http2 | undefined,
+          requests = #{} :: #{reference() := {request(), response_data()}}
+         }).
+-type gun_worker_state() :: #gun_worker_state{}.
 
--record(response_data, {from :: {pid(), term()},
-                        timestamp :: integer(),
-                        timeout_timer :: reference(),
-                        status :: binary() | undefined,
-                        headers :: gun:headers(),
-                        acc = <<>> :: binary()}).
-
+-record(response_data, {
+          from :: {pid(), term()},
+          timestamp :: integer(),
+          timeout_timer :: reference(),
+          status :: binary() | undefined,
+          headers :: gun:headers(),
+          acc = <<>> :: binary()
+         }).
 -type response_data() :: #response_data{}.
 
 -type request() :: {request,
@@ -57,36 +61,38 @@ start_link(Destination, Options) ->
 %%%===================================================================
 
 -spec init({{inet:hostname() | inet:ip_address(), inet:port_number()}, gun:opts()}) ->
-    {ok, State :: #state{}, {continue, init}}.
+    {ok, State :: gun_worker_state(), {continue, init}}.
 init({{Host, Port}, Opts}) ->
     {H, _P, O} = parse_uri(Host),
-    {ok, #state{host = H, port = Port, opts = maps:merge(O, Opts), requests = #{}},
+    {ok, #gun_worker_state{host = H, port = Port, opts = maps:merge(O, Opts)},
      {continue, init}};
 init({Host, Opts}) ->
     {H, P, O} = parse_uri(Host),
-    {ok, #state{host = H, port = P, opts = maps:merge(O, Opts), requests = #{}},
+    {ok, #gun_worker_state{host = H, port = P, opts = maps:merge(O, Opts)},
      {continue, init}}.
 
 handle_continue(init, State) ->
     NewState = open_connection(State),
     {noreply, NewState}.
 
--spec handle_call(Request :: request(), From :: {pid(), Tag :: term()}, State :: #state{}) ->
-    {noreply, NewState :: #state{}}.
+-spec handle_call(Request, From, State) -> Result when
+      Request :: request(),
+      From :: {pid(), term()},
+      State :: gun_worker_state(),
+      Result :: {noreply, gun_worker_state()}.
 handle_call({request, FullPath, Method, Headers, Query, _Retries, Timeout} = Request,
             From,
-            #state{requests = Requests} = State) ->
-
+            #gun_worker_state{requests = Requests} = State) ->
     Now = erlang:monotonic_time(millisecond),
     LHeaders = lowercase_headers(Headers),
 
-    {StreamRef, TRef} = queue_request(FullPath, Method, LHeaders, Query, Timeout, State#state.pid),
+    {StreamRef, TRef} = queue_request(FullPath, Method, LHeaders, Query, Timeout, State#gun_worker_state.pid),
     NewRequests = Requests#{StreamRef => {Request, #response_data{from = From,
                                                                   timestamp = Now,
                                                                   timeout_timer = TRef}}},
-    {noreply, State#state{requests = NewRequests}}.
+    {noreply, State#gun_worker_state{requests = NewRequests}}.
 
--spec handle_cast(any(), State :: #state{}) -> {noreply, State :: #state{}}.
+-spec handle_cast(any(), gun_worker_state()) -> {noreply, gun_worker_state()}.
 handle_cast(_R, State) ->
     {noreply, State}.
 
@@ -95,7 +101,7 @@ handle_cast(_R, State) ->
 %% and provides the response headers and status. If there is no body, the `fin'
 %% flag is set.
 handle_info({gun_response, ConnPid, StreamRef, fin, Status, Headers},
-            #state{pid = ConnPid, requests = Requests} = State) ->
+            #gun_worker_state{pid = ConnPid, requests = Requests} = State) ->
     case maps:get(StreamRef, Requests, undefined) of
         undefined ->
             {noreply, State};
@@ -108,31 +114,31 @@ handle_info({gun_response, ConnPid, StreamRef, fin, Status, Headers},
                        no_data,
                        0,
                        ResData#response_data.timestamp - Now}}),
-            {noreply, State#state{requests = maps:remove(StreamRef, Requests)}}
+            {noreply, State#gun_worker_state{requests = maps:remove(StreamRef, Requests)}}
     end;
 handle_info({gun_response, ConnPid, StreamRef, nofin, Status, Headers},
-            #state{pid = ConnPid, requests = Requests} = State) ->
+            #gun_worker_state{pid = ConnPid, requests = Requests} = State) ->
     case maps:get(StreamRef, Requests, undefined) of
         undefined ->
             {noreply, State};
         {Req, ResData} ->
             NewData = ResData#response_data{status = integer_to_binary(Status), headers = Headers},
-            {noreply, State#state{requests = Requests#{StreamRef := {Req, NewData}}}}
+            {noreply, State#gun_worker_state{requests = Requests#{StreamRef := {Req, NewData}}}}
     end;
 %% `gun_data' is a message that carries the response body. With the last part of
 %% the body, the `fin' flag is set.
 handle_info({gun_data, ConnPid, StreamRef, nofin, Data},
-            #state{pid = ConnPid, requests = Requests} = State) ->
+            #gun_worker_state{pid = ConnPid, requests = Requests} = State) ->
     case maps:get(StreamRef, Requests, undefined) of
         undefined ->
             {noreply, State};
         {Req, ResData} ->
             Acc = ResData#response_data.acc,
             NewData = ResData#response_data{acc = <<Acc/binary, Data/binary>>},
-            {noreply, State#state{requests = Requests#{StreamRef := {Req, NewData}}}}
+            {noreply, State#gun_worker_state{requests = Requests#{StreamRef := {Req, NewData}}}}
     end;
 handle_info({gun_data, ConnPid, StreamRef, fin, Data},
-            #state{pid = ConnPid, requests = Requests} = State) ->
+            #gun_worker_state{pid = ConnPid, requests = Requests} = State) ->
     Now = erlang:monotonic_time(millisecond),
     case maps:get(StreamRef, Requests, undefined) of
     undefined ->
@@ -141,28 +147,29 @@ handle_info({gun_data, ConnPid, StreamRef, fin, Data},
         erlang:cancel_timer(ResData#response_data.timeout_timer),
         Acc = ResData#response_data.acc,
         NewData = ResData#response_data{acc = <<Acc/binary, Data/binary>>},
+        % ?ERROR_MSG("NewDataaaaaaa ~p", [NewData]),
         gen_server:reply(NewData#response_data.from,
                         {ok, {{NewData#response_data.status, reason},
                             NewData#response_data.headers,
                             NewData#response_data.acc,
                             byte_size(NewData#response_data.acc),
                             NewData#response_data.timestamp - Now}}),
-        {noreply, State#state{requests = maps:remove(StreamRef, Requests)}}
+        {noreply, State#gun_worker_state{requests = maps:remove(StreamRef, Requests)}}
     end;
 %% `timeout' is a message responsible for terminating and restarting requests
 %% that take too long to complete. There is no such functionality in Gun, so it
 %% is sent from this module.
-handle_info({timeout, StreamRef}, #state{requests = Requests} = State) ->
+handle_info({timeout, StreamRef}, #gun_worker_state{requests = Requests} = State) ->
     case maps:get(StreamRef, Requests, undefined) of
         undefined ->
             {noreply, State}; % The request was already removed, but the timer sent a message while
                               % it was being processed. This should be a very rare case.
         {Req, Res} ->
-            New = retry_request(State#state.pid, {Req, Res}, #{}),
+            New = retry_request(State#gun_worker_state.pid, {Req, Res}, #{}),
             Requests1 = maps:remove(StreamRef, Requests),
             Requests2 = maps:merge(New, Requests1),
 
-            {noreply, State#state{requests = Requests2}}
+            {noreply, State#gun_worker_state{requests = Requests2}}
     end;
 %% `gun_down' is a message informing that Gun has lost the connection.
 %% It may try to reconnect, according to the `retry' and `retry_timeout' options,
@@ -177,17 +184,17 @@ handle_info({gun_down, PID, Protocol, Reason, KilledStreams, UnprocessedStreams}
     {noreply, State};
 %% `gun_up' is a message informing that Gun has reconnected after a `gun_down'.
 handle_info({gun_up, ConnPid, _Protocol},
-            State = #state{pid = ConnPid}) ->
+            State = #gun_worker_state{pid = ConnPid}) ->
     ?DEBUG("gun_up in mongoose_gun_worker. Connection is back up with PID: ~p", [ConnPid]),
     {noreply, State};
 %% `gun_error' is a message informing about any errors concerning connections or
 %% streams handled by Gun.
 handle_info({gun_error, ConnPid, Reason},
-            State = #state{pid = ConnPid}) ->
+            State = #gun_worker_state{pid = ConnPid}) ->
     ?WARNING_MSG("gun_error in mongoose_gun_worker. Reason: ~p.", [Reason]),
     {noreply, State};
 handle_info({gun_error, ConnPid, StreamRef, Reason},
-            State = #state{pid = ConnPid}) ->
+            State = #gun_worker_state{pid = ConnPid}) ->
     ?WARNING_MSG("gun_error in mongoose_gun_worker. Reason: ~p. Stream reference: ~p",
                  [Reason, StreamRef]),
     {noreply, State};
@@ -195,7 +202,7 @@ handle_info({gun_error, ConnPid, StreamRef, Reason},
 %% Because it is monitored, we receive a `DOWN' message and may restart the
 %% connection.
 handle_info({'DOWN', MRef, process, ConnPid, Reason},
-            #state{pid = ConnPid, monitor = MRef} = State) ->
+            #gun_worker_state{pid = ConnPid, monitor = MRef} = State) ->
     ?WARNING_MSG("Mongoose_gun_worker has lost the connection with PID ~p. Reason ~p.",
                  [ConnPid, Reason]),
     ConnectedState = open_connection(State),
@@ -219,10 +226,10 @@ parse_uri(Host) ->
     {H, maps:get(port, M, undefined), maps:merge(default_opts(), Tls)}.
 
 open_connection(State) ->
-    {ok, PID} = gun:open(State#state.host, State#state.port, State#state.opts),
+    {ok, PID} = gun:open(State#gun_worker_state.host, State#gun_worker_state.port, State#gun_worker_state.opts),
     MonitorRef = monitor(process, PID),
     {ok, Protocol} = gun:await_up(PID, 10000, MonitorRef),
-    State#state{pid = PID, protocol = Protocol, monitor = MonitorRef}.
+    State#gun_worker_state{pid = PID, protocol = Protocol, monitor = MonitorRef}.
 
 queue_request(FullPath, Method, LHeaders, Query, Timeout, PID) ->
     StreamRef = gun:request(PID, Method, FullPath, LHeaders, Query),
@@ -230,11 +237,11 @@ queue_request(FullPath, Method, LHeaders, Query, Timeout, PID) ->
     {StreamRef, TRef}.
 
 retry_all(State) ->
-    Requests = State#state.requests,
-    NewRequests = maps:fold(fun(_K, V, Acc) -> retry_request(State#state.pid, V, Acc) end,
+    Requests = State#gun_worker_state.requests,
+    NewRequests = maps:fold(fun(_K, V, Acc) -> retry_request(State#gun_worker_state.pid, V, Acc) end,
                             #{},
                             Requests),
-    State#state{requests = NewRequests}.
+    State#gun_worker_state{requests = NewRequests}.
 
 retry_request(PID, {Req, Res}, ReqAcc) ->
     {request, FullPath, Method, Headers, Query, Retries, Timeout} = Req,
