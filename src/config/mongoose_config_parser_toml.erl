401c09943bfb188ae79048c35cfe97b0eb43c79e
Krzysztof Makieła <krzysztof.makiela@erlang-solutions.com>
2020-08-28 14:11:05 +0200

    review comments

--- src/config/mongoose_config_parser_toml.erl
+++ src/config/mongoose_config_parser_toml.erl
@@ -613,7 +613,7 @@ module_opt([<<"sdp_origin">>, <<"mod_jingle_sip">>|_], V) ->
     [{sdp_origin, b2l(V)}];
 module_opt([<<"ram_key_size">>, <<"mod_keystore">>|_], V) ->
     [{ram_key_size, V}];
-module_opt([<<"key">>, <<"mod_keystore">>|_] = Path, V) ->
+module_opt([<<"keys">>, <<"mod_keystore">>|_] = Path, V) ->
     Keys = parse_list(Path, V),
     [{keys, Keys}];
 module_opt([<<"archive_chat_markers">>, <<"mod_mam_meta">>|_], V) ->
@@ -646,18 +646,18 @@ module_opt([<<"history_size">>, <<"mod_muc">>|_], V) ->
     [{history_size, V}];
 module_opt([<<"room_shaper">>, <<"mod_muc">>|_], V) ->
     [{room_shaper, b2a(V)}];
-module_opt([<<"max_room_id">>, <<"mod_muc">>|_], V) when is_integer(V) ->
-    [{max_room_id, V}];
+module_opt([<<"max_room_id">>, <<"mod_muc">>|_], <<"infinity">>) ->
+    [{max_room_id, infinite}];
 module_opt([<<"max_room_id">>, <<"mod_muc">>|_], V) ->
-    [{max_room_id, b2a(V)}];
-module_opt([<<"max_room_name">>, <<"mod_muc">>|_], V) when is_integer(V) ->
-    [{max_room_name, V}];
+    [{max_room_id, V}];
+module_opt([<<"max_room_name">>, <<"mod_muc">>|_], <<"infinity">>) ->
+    [{max_room_name, infinite}];
 module_opt([<<"max_room_name">>, <<"mod_muc">>|_], V) ->
-    [{max_room_name, b2a(V)}];
-module_opt([<<"max_room_desc">>, <<"mod_muc">>|_], V) when is_integer(V) ->
-    [{max_room_desc, V}];
+    [{max_room_name, V}];
+module_opt([<<"max_room_desc">>, <<"mod_muc">>|_], <<"infinity">>) ->
+    [{max_room_desc, infinite}];
 module_opt([<<"max_room_desc">>, <<"mod_muc">>|_], V) ->
-    [{max_room_desc, b2a(V)}];
+    [{max_room_desc, V}];
 module_opt([<<"min_message_interval">>, <<"mod_muc">>|_], V) ->
     [{min_message_interval, V}];
 module_opt([<<"min_presence_interval">>, <<"mod_muc">>|_], V) ->
@@ -700,9 +700,9 @@ module_opt([<<"dirname">>, <<"mod_muc_log">>|_], V) ->
 module_opt([<<"file_format">>, <<"mod_muc_log">>|_], V) ->
     [{file_format, b2a(V)}];
 module_opt([<<"css_file">>, <<"mod_muc_log">>|_], <<"false">>) ->
-    [{css_file, false}];
+    [{cssfile, false}];
 module_opt([<<"css_file">>, <<"mod_muc_log">>|_], V) ->
-    [{css_file, V}];
+    [{cssfile, V}];
 module_opt([<<"timezone">>, <<"mod_muc_log">>|_], V) ->
     [{timezone, b2a(V)}];
 module_opt([<<"top_link">>, <<"mod_muc_log">>|_] = Path, V) ->
@@ -786,8 +786,8 @@ module_opt([<<"access">>, <<"mod_register">>|_], V) ->
     [{access, b2a(V)}];
 module_opt([<<"welcome_message">>, <<"mod_register">>|_], V) ->
     [{welcome_message, {b2l(V)}}];
-module_opt([<<"registration_watchers">>, <<"mod_register">>|_], V) ->
-    [{registration_watchers, V}];
+module_opt([<<"registration_watchers">>, <<"mod_register">>|_] = Path, V) ->
+    [{registration_watchers, parse_list(Path, V)}];
 module_opt([<<"password_strength">>, <<"mod_register">>|_], V) ->
     [{password_strength, V}];
 module_opt([<<"ip_access">>, <<"mod_register">>|_] = Path, V) ->
@@ -830,8 +830,14 @@ module_opt([<<"ldap_gfilter">>, <<"mod_shared_roster_ldap">>|_], V) ->
     [{ldap_gfilter, b2l(V)}];
 module_opt([<<"ldap_ufilter">>, <<"mod_shared_roster_ldap">>|_], V) ->
     [{ldap_ufilter, b2l(V)}];
+module_opt([<<"buffer_max">>, <<"mod_stream_management">>|_], <<"infinity">>) ->
+    [{buffer_max, infinity}];
+module_opt([<<"buffer_max">>, <<"mod_stream_management">>|_], <<"no_buffer">>) ->
+    [{buffer_max, no_buffer}];
 module_opt([<<"buffer_max">>, <<"mod_stream_management">>|_], V) ->
     [{buffer_max, V}];
+module_opt([<<"ack_freq">>, <<"mod_stream_management">>|_], <<"never">>) ->
+    [{ack_freq, never}];
 module_opt([<<"ack_freq">>, <<"mod_stream_management">>|_], V) ->
     [{ack_freq, V}];
 module_opt([<<"resume_timeout">>, <<"mod_stream_management">>|_], V) ->
@@ -847,13 +853,6 @@ module_opt([<<"matches">>, <<"mod_vcard">>|_], <<"infinity">>) ->
     [{matches, infinity}];
 module_opt([<<"matches">>, <<"mod_vcard">>|_], V) ->
     [{matches, V}];
-module_opt([<<"allow_return_all">>, <<"mod_vcard">>|_], V) ->
-    [{allow_return_all, V}];
-module_opt([<<"search_all_hosts">>, <<"mod_vcard">>|_], V) ->
-    [{search_all_hosts, V}];
-module_opt([<<"ldap_uids">>, <<"mod_vcard">>|_] = Path, V) ->
-    Uids = parse_list(Path, V),
-    [{ldap_uids, Uids}];
 module_opt([<<"ldap_vcard_map">>, <<"mod_vcard">>|_] = Path, V) ->
     Maps = parse_list(Path, V),
     [{ldap_vcard_map, Maps}];
@@ -883,16 +882,21 @@ module_opt([<<"ldap_filter">>|_], V) ->
     [{ldap_filter, b2l(V)}];
 module_opt([<<"ldap_deref">>|_], V) ->
     [{ldap_deref, b2a(V)}];
-%% Riak-specific options
-module_opt([<<"defaults_bucket_type">>|_], V) ->
+%% Backend-specific options
+module_opt([<<"riak">>|_] = Path, V) ->
+    parse_section(Path, V).
+
+%% path: modules.*.riak.*
+-spec riak_opts(path(), toml_section()) -> [option()].
+riak_opts([<<"defaults_bucket_type">>|_], V) ->
     [{defaults_bucket_type, V}];
-module_opt([<<"names_bucket_type">>|_], V) ->
+riak_opts([<<"names_bucket_type">>|_], V) ->
     [{names_bucket_type, V}];
-module_opt([<<"version_bucket_type">>|_], V) ->
+riak_opts([<<"version_bucket_type">>|_], V) ->
     [{version_bucket_type, V}];
-module_opt([<<"bucket_type">>|_], V) ->
+riak_opts([<<"bucket_type">>|_], V) ->
     [{bucket_type, V}];
-module_opt([<<"search_index">>|_], V) ->
+riak_opts([<<"search_index">>|_], V) ->
     [{search_index, V}].
 
 -spec mod_register_ip_access_rule(path(), toml_section()) -> [option()].
@@ -905,10 +909,13 @@ mod_auth_token_validity_periods(_,
         [{{validity_period, b2a(Token)}, {Value, b2a(Unit)}}].
 
 -spec mod_disco_server_info(path(), toml_section()) -> [option()].
-mod_disco_server_info(_, #{<<"module">> := <<"all">>, <<"name">> := Name, <<"urls">> := Urls}) ->
-    [{all, b2l(Name), [b2l(Url) || Url <- Urls]}];
-mod_disco_server_info(_, #{<<"module">> := Modules, <<"name">> := Name, <<"urls">> := Urls}) ->
-    [{[b2a(Mod) || Mod <- Modules], b2l(Name), [b2l(Url) || Url <- Urls]}].
+mod_disco_server_info(Path, #{<<"module">> := <<"all">>, <<"name">> := Name, <<"urls">> := Urls}) ->
+    URLList = parse_list([<<"urls">> | Path], Urls),
+    [{all, b2l(Name), URLList}];
+mod_disco_server_info(Path, #{<<"module">> := Modules, <<"name">> := Name, <<"urls">> := Urls}) ->
+    Mods = parse_list([<<"module">> | Path], Modules),
+    URLList = parse_list([<<"urls">> | Path], Urls),
+    [{Mods, b2l(Name), URLList}].
 
 -spec mod_event_pusher_backend_sns(path(), toml_section()) -> [option()].
 mod_event_pusher_backend_sns(Path, Opts) ->
@@ -930,7 +937,7 @@ mod_event_pusher_backend_rabbit(Path, Opts) ->
     ROpts = parse_section(Path, Opts),
     [{rabbit, ROpts}].
 
--spec mod_event_pusher_backend_sns_opts(path(), toml_section()) -> [option()].
+-spec mod_event_pusher_backend_sns_opts(path(), toml_value()) -> [option()].
 mod_event_pusher_backend_sns_opts([<<"presence_updates_topic">>|_], V) ->
     [{presence_updates_topic, b2l(V)}];
 mod_event_pusher_backend_sns_opts([<<"pm_messages_topic">>|_], V) ->
@@ -958,7 +965,7 @@ mod_event_pusher_backend_sns_opts([<<"publish_retry_count">>|_], V) ->
 mod_event_pusher_backend_sns_opts([<<"publish_retry_time_ms">>|_], V) ->
     [{publish_retry_time_ms, V}].
 
--spec mod_event_pusher_backend_push_opts(path(), toml_section()) -> [option()].
+-spec mod_event_pusher_backend_push_opts(path(), toml_value()) -> [option()].
 mod_event_pusher_backend_push_opts([<<"backend">>|_], V) ->
     [{backend, b2a(V)}];
 mod_event_pusher_backend_push_opts([<<"wpool">>|_] = Path, V) ->
@@ -978,7 +985,7 @@ mod_event_pusher_backend_http_opts([<<"path">>|_], V) ->
 mod_event_pusher_backend_http_opts([<<"callback_module">>|_], V) ->
     [{callback_module, b2a(V)}].
 
--spec mod_event_pusher_backend_rabbit_opts(path(), toml_section()) -> [option()].
+-spec mod_event_pusher_backend_rabbit_opts(path(), toml_value()) -> [option()].
 mod_event_pusher_backend_rabbit_opts([<<"presence_exchange">>|_] = Path, V) ->
     [{presence_exchange, parse_section(Path, V)}];
 mod_event_pusher_backend_rabbit_opts([<<"chat_msg_exchange">>|_] = Path, V) ->
@@ -986,13 +993,13 @@ mod_event_pusher_backend_rabbit_opts([<<"chat_msg_exchange">>|_] = Path, V) ->
 mod_event_pusher_backend_rabbit_opts([<<"groupchat_msg_exchange">>|_] = Path, V) ->
     [{groupchat_msg_exchange, parse_section(Path, V)}].
 
--spec mod_event_pusher_rabbit_presence_ex(path(), toml_section()) -> [option()].
+-spec mod_event_pusher_rabbit_presence_ex(path(), toml_value()) -> [option()].
 mod_event_pusher_rabbit_presence_ex([<<"name">>|_], V) ->
     [{name, V}];
 mod_event_pusher_rabbit_presence_ex([<<"type">>|_], V) ->
     [{type, V}].
 
--spec mod_event_pusher_rabbit_msg_ex(path(), toml_section()) -> [option()].
+-spec mod_event_pusher_rabbit_msg_ex(path(), toml_value()) -> [option()].
 mod_event_pusher_rabbit_msg_ex([<<"name">>|_], V) ->
     [{name, V}];
 mod_event_pusher_rabbit_msg_ex([<<"type">>|_], V) ->
@@ -1002,7 +1009,7 @@ mod_event_pusher_rabbit_msg_ex([<<"sent_topic">>|_], V) ->
 mod_event_pusher_rabbit_msg_ex([<<"recv_topic">>|_], V) ->
     [{recv_topic, V}].
 
--spec mod_http_upload_s3(path(), toml_section()) -> [option()].
+-spec mod_http_upload_s3(path(), toml_value()) -> [option()].
 mod_http_upload_s3([<<"bucket_url">>|_], V) ->
     [{bucket_url, b2l(V)}];
 mod_http_upload_s3([<<"add_acl">>|_], V) ->
@@ -1014,7 +1021,7 @@ mod_http_upload_s3([<<"access_key_id">>|_], V) ->
 mod_http_upload_s3([<<"secret_access_key">>|_], V) ->
     [{secret_access_key, b2l(V)}].
 
--spec mod_global_distrib_connections(path(), toml_section()) -> [option()].
+-spec mod_global_distrib_connections(path(), toml_value()) -> [option()].
 mod_global_distrib_connections([<<"endpoints">>|_] = Path, V) ->
     Endpoints = parse_list(Path, V),
     [{endpoints, Endpoints}];
@@ -1037,7 +1044,7 @@ mod_global_distrib_connections([<<"tls">>|_] = Path, V) ->
     TLSOpts = parse_section(Path, V),
     [{tls_opts, TLSOpts}].
 
--spec mod_global_distrib_cache(path(), toml_section()) -> [option()].
+-spec mod_global_distrib_cache(path(), toml_value()) -> [option()].
 mod_global_distrib_cache([<<"cache_missed">>|_], V) ->
     [{cache_missed, V}];
 mod_global_distrib_cache([<<"domain_lifetime_seconds">>|_], V) ->
@@ -1047,7 +1054,7 @@ mod_global_distrib_cache([<<"jid_lifetime_seconds">>|_], V) ->
 mod_global_distrib_cache([<<"max_jids">>|_], V) ->
     [{max_jids, V}].
 
--spec mod_global_distrib_redis(path(), toml_section()) -> [option()].
+-spec mod_global_distrib_redis(path(), toml_value()) -> [option()].
 mod_global_distrib_redis([<<"pool">>|_], V) ->
     [{pool, b2a(V)}];
 mod_global_distrib_redis([<<"expire_after">>|_], V) ->
@@ -1055,7 +1062,7 @@ mod_global_distrib_redis([<<"expire_after">>|_], V) ->
 mod_global_distrib_redis([<<"refresh_after">>|_], V) ->
     [{refresh_after, V}].
 
--spec mod_global_distrib_bounce(path(), toml_section()) -> [option()].
+-spec mod_global_distrib_bounce(path(), toml_value()) -> [option()].
 mod_global_distrib_bounce([<<"resend_after_ms">>|_], V) ->
     [{resend_after_ms, V}];
 mod_global_distrib_bounce([<<"max_retries">>|_], V) ->
@@ -1071,7 +1078,7 @@ mod_keystore_keys(_, #{<<"name">> := Name, <<"type">> := <<"ram">>}) ->
 mod_keystore_keys(_, #{<<"name">> := Name, <<"type">> := <<"file">>, <<"path">> := Path}) ->
     [{b2a(Name), {file, b2l(Path)}}].
 
--spec mod_mam_opts(path(), toml_section()) -> [option()].
+-spec mod_mam_opts(path(), toml_value()) -> [option()].
 mod_mam_opts([<<"backend">>|_], V) ->
     [{backend, b2a(V)}];
 mod_mam_opts([<<"no_stanzaid_element">>|_], V) ->
@@ -1101,7 +1108,7 @@ mod_mam_opts([<<"archive_groupchats">>, <<"pm">>|_], V) ->
 mod_mam_opts([<<"host">>, <<"muc">>|_], V) ->
     [{host, b2l(V)}].
 
--spec mod_muc_default_room(path(), toml_section()) -> [option()].
+-spec mod_muc_default_room(path(), toml_value()) -> [option()].
 mod_muc_default_room([<<"title">>|_], V) ->
     [{title, V}];
 mod_muc_default_room([<<"description">>|_], V) ->
@@ -1186,7 +1193,7 @@ mod_revproxy_routes(_, #{<<"host">> := Host, <<"path">> := Path, <<"method">> :=
 mod_revproxy_routes(_, #{<<"host">> := Host, <<"path">> := Path, <<"upstream">> := Upstream}) ->
         [{b2l(Host), b2l(Path), b2l(Upstream)}].
 
--spec mod_stream_management_stale_h(path(), toml_section()) -> [option()].
+-spec mod_stream_management_stale_h(path(), toml_value()) -> [option()].
 mod_stream_management_stale_h([<<"enabled">>|_], V) ->
     [{enabled, V}];
 mod_stream_management_stale_h([<<"repeat_after">>|_], V) ->
@@ -1477,14 +1484,22 @@ handler([_, _, <<"services">>]) -> fun service_opt/2;
 %% modules
 handler([_, <<"modules">>]) -> fun process_module/2;
 handler([_, _, <<"modules">>]) -> fun module_opt/2;
+handler([_, <<"riak">>, _, <<"modules">>]) ->
+    fun riak_opts/2;
 handler([_, <<"ip_access">>, <<"mod_register">>, <<"modules">>]) ->
     fun mod_register_ip_access_rule/2;
+handler([_, <<"registration_watchers">>, <<"mod_register">>, <<"modules">>]) ->
+    fun(_, V) -> [V] end;
 handler([_, <<"validity_period">>, <<"mod_auth_token">>, <<"modules">>]) ->
     fun mod_auth_token_validity_periods/2;
 handler([_, <<"extra_domains">>, <<"mod_disco">>, <<"modules">>]) ->
     fun(_, V) -> [b2l(V)] end;
 handler([_, <<"server_info">>, <<"mod_disco">>, <<"modules">>]) ->
     fun mod_disco_server_info/2;
+handler([_, <<"urls">>, _, <<"server_info">>, <<"mod_disco">>, <<"modules">>]) ->
+    fun(_, V) -> [b2l(V)] end;
+handler([_, <<"module">>, _, <<"server_info">>, <<"mod_disco">>, <<"modules">>]) ->
+    fun(_, V) -> [b2a(V)] end;
 handler([<<"sns">>, <<"backend">>, <<"mod_event_pusher">>, <<"modules">>]) ->
     fun mod_event_pusher_backend_sns/2;
 handler([<<"push">>, <<"backend">>, <<"mod_event_pusher">>, <<"modules">>]) ->
@@ -1529,7 +1544,7 @@ handler([_,<<"endpoints">>, <<"connections">>, <<"mod_global_distrib">>, <<"modu
     fun mod_global_distrib_connections_endpoints/2;
 handler([_,<<"tls">>, <<"connections">>, <<"mod_global_distrib">>, <<"modules">>]) ->
     fun fast_tls_option/2;
-handler([_, <<"key">>, <<"mod_keystore">>, <<"modules">>]) ->
+handler([_, <<"keys">>, <<"mod_keystore">>, <<"modules">>]) ->
     fun mod_keystore_keys/2;
 handler([_, _, <<"mod_mam_meta">>, <<"modules">>]) ->
     fun mod_mam_opts/2;
@@ -1561,6 +1576,7 @@ handler([_, <<"ldap_search_fields">>, <<"mod_vcard">>, <<"modules">>]) ->
     fun mod_vcard_ldap_search_fields/2;
 handler([_, <<"ldap_search_reported">>, <<"mod_vcard">>, <<"modules">>]) ->
     fun mod_vcard_ldap_search_reported/2;
+
 %% shaper, acl, access
 handler([_, <<"shaper">>]) -> fun process_shaper/2;
 handler([_, <<"acl">>]) -> fun process_acl/2;
--- test/config_parser_SUITE.erl
+++ test/config_parser_SUITE.erl
@@ -81,62 +81,18 @@ end_per_suite(_Config) ->
     ok.
 
 sample_pgsql(Config) ->
-    CfgPath = ejabberd_helper:data(Config, "mongooseim-pgsql.cfg"),
-    State1 = mongoose_config_parser_cfg:parse_file(CfgPath),
-    Hosts1 = mongoose_config_parser:state_to_host_opts(State1),
-    Opts1 = mongoose_config_parser:state_to_opts(State1),
-
-    TOMLPath = ejabberd_helper:data(Config, "mongooseim-pgsql.toml"),
-    State2 = mongoose_config_parser_toml:parse_file(TOMLPath),
-    Hosts2 = mongoose_config_parser:state_to_host_opts(State2),
-    Opts2 = mongoose_config_parser:state_to_opts(State2),
-    ?eq(Hosts1, Hosts2),
-    compare_unordered_lists(lists:filter(fun filter_config/1, Opts1), Opts2,
-                            fun handle_config_option/2).
+    test_equivalence_between_files(Config,  "mongooseim-pgsql.cfg",  "mongooseim-pgsql.toml").
 
 miscellaneous(Config) ->
-    CfgPath = ejabberd_helper:data(Config, "miscellaneous.cfg"),
-    State1 = mongoose_config_parser_cfg:parse_file(CfgPath),
-    Hosts1 = mongoose_config_parser:state_to_host_opts(State1),
-    Opts1 = mongoose_config_parser:state_to_opts(State1),
-
-    TOMLPath = ejabberd_helper:data(Config, "miscellaneous.toml"),
-    State2 = mongoose_config_parser_toml:parse_file(TOMLPath),
-    Hosts2 = mongoose_config_parser:state_to_host_opts(State2),
-    Opts2 = mongoose_config_parser:state_to_opts(State2),
-
-    ?eq(Hosts1, Hosts2),
-    compare_unordered_lists(lists:filter(fun filter_config/1, Opts1), Opts2,
-                        fun handle_config_option/2).
+    test_equivalence_between_files(Config,  "miscellaneous.cfg",  "miscellaneous.toml").
 
 s2s(Config) ->
-    Cfg_Path = ejabberd_helper:data(Config, "s2s_only.cfg"),
-    State1 = mongoose_config_parser_cfg:parse_file(Cfg_Path),
-    Opts1 = mongoose_config_parser:state_to_opts(State1),
-
-    TOML_path = ejabberd_helper:data(Config, "s2s_only.toml"),
-    State2 = mongoose_config_parser_toml:parse_file(TOML_path),
-    Opts2 = mongoose_config_parser:state_to_opts(State2),
-
-    compare_unordered_lists(lists:filter(fun filter_config/1, Opts1), Opts2,
-                            fun handle_config_option/2).
+    test_equivalence_between_files(Config,  "s2s_only.cfg",  "s2s_only.toml").
 
 modules(Config) ->
-    CfgPath = ejabberd_helper:data(Config, "modules.cfg"),
-    State1 = mongoose_config_parser_cfg:parse_file(CfgPath),
-    Hosts1 = mongoose_config_parser:state_to_host_opts(State1),
-    Opts1 = mongoose_config_parser:state_to_opts(State1),
+    test_equivalence_between_files(Config,  "modules.cfg",  "modules.toml").
 
-    TOMLPath = ejabberd_helper:data(Config, "modules.toml"),
-    State2 = mongoose_config_parser_toml:parse_file(TOMLPath),
-    Hosts2 = mongoose_config_parser:state_to_host_opts(State2),
-    Opts2 = mongoose_config_parser:state_to_opts(State2),
-
-    ?eq(Hosts1, Hosts2),
-    compare_unordered_lists(lists:filter(fun filter_config/1, Opts1), Opts2,
-                        fun handle_config_option/2).
 %% tests: general
-
 loglevel(_Config) ->
     ?eq([#local_config{key = loglevel, value = debug}],
         parse(#{<<"general">> => #{<<"loglevel">> => <<"debug">>}})),
@@ -682,3 +638,17 @@ compare_ordered_lists([H1|T1], [H2|T2], F) ->
     compare_ordered_lists(T1, T2, F);
 compare_ordered_lists([], [], _) ->
     ok.
+
+test_equivalence_between_files(Config, File1, File2) ->
+    CfgPath = ejabberd_helper:data(Config, File1),
+    State1 = mongoose_config_parser_cfg:parse_file(CfgPath),
+    Hosts1 = mongoose_config_parser:state_to_host_opts(State1),
+    Opts1 = mongoose_config_parser:state_to_opts(State1),
+
+    TOMLPath = ejabberd_helper:data(Config, File2),
+    State2 = mongoose_config_parser_toml:parse_file(TOMLPath),
+    Hosts2 = mongoose_config_parser:state_to_host_opts(State2),
+    Opts2 = mongoose_config_parser:state_to_opts(State2),
+    ?eq(Hosts1, Hosts2),
+    compare_unordered_lists(lists:filter(fun filter_config/1, Opts1), Opts2,
+                            fun handle_config_option/2).
