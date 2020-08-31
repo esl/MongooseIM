4a9450f1fadac5ab2cae4cdd797ca16114066b1e
Mikhail Uvarov <arcusfelis@gmail.com>
2020-08-31 10:45:56 +0200

    Do not print what field twice [skip ci]

--- src/mongoose_log_filter.erl
+++ src/mongoose_log_filter.erl
@@ -6,4 +6,6 @@
 %% from msg to metadata.
 fill_metadata(Event=#{msg := {report, Msg}, meta := Meta}, Fields) ->
     FieldMap = maps:with(Fields, Msg),
-    Event#{meta => maps:merge(FieldMap, Meta)}.
+    %% Remove the fields to not print them twice
+    Msg2 = maps:without(Fields, Msg),
+    Event#{meta => maps:merge(FieldMap, Meta), msg => {report, Msg2}}.
