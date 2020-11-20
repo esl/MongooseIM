5f8b2324f6644c68b71ae185f297d2be9fc29e31
Nelson Vides <nelson.vides@erlang-solutions.com>
2020-11-20 14:27:23 +0100

    Apply code review, simplifying code

--- big_tests/tests/jingle_SUITE.erl
+++ big_tests/tests/jingle_SUITE.erl
@@ -70,44 +70,33 @@ init_per_suite(Config) ->
     end.
 
 start_nksip_in_mim_nodes() ->
-    Port = 12345,
-    Host = ct:get_config({hosts, mim, domain}),
-    RPCSpec1 = mim(),
-    RPCSpec2 = mim2(),
-    Timeout = timer:seconds(60),
+    Pid1 = start_nskip_in_parallel(mim(), []),
+    Pid2 = start_nskip_in_parallel(mim2(), [{listen_port, 12346}]),
+    wait_for_process_to_stop(Pid1),
+    wait_for_process_to_stop(Pid2).
 
-    Pid1 = proc_lib:spawn_link(
-             fun() ->
-                     {ok, _} = rpc(RPCSpec1#{timeout => Timeout}, gen_mod, start_module,
-                                   [
-                                    Host, mod_jingle_sip,
-                                    [{proxy_host, "localhost"},
-                                     {proxy_port, Port},
-                                     {username_to_phone,[{<<"2000006168">>, <<"+919177074440">>}]}]
-                                   ])
-             end),
-    Pid2 = proc_lib:spawn_link(
-             fun() ->
-                     {ok, _} = rpc(RPCSpec2#{timeout => Timeout}, gen_mod, start_module,
-                                   [
-                                    Host, mod_jingle_sip,
-                                    [{proxy_host, "localhost"},
-                                     {proxy_port, Port},
-                                     {listen_port, 12346},
-                                     {username_to_phone,[{<<"2000006168">>, <<"+919177074440">>}]}]
-                                   ])
-             end),
-    %% then
-    wait_for_process_to_stop(Pid1, Timeout),
-    wait_for_process_to_stop(Pid2, Timeout).
-
-wait_for_process_to_stop(Pid, Timeout) ->
+wait_for_process_to_stop(Pid) ->
     erlang:monitor(process, Pid),
     receive
         {'DOWN', _, process, Pid, _} -> ok
-    after Timeout ->
+    after timer:seconds(60) ->
               ct:fail(wait_for_process_to_stop_timeout)
     end.
+
+start_nskip_in_parallel(RPCSpec, ExtraOpts) ->
+    Host = ct:get_config({hosts, mim, domain}),
+    proc_lib:spawn_link(
+      fun() ->
+              {ok, _} = rpc(RPCSpec#{timeout => timer:seconds(60)}, gen_mod, start_module,
+                            [
+                             Host, mod_jingle_sip,
+                             [{proxy_host, "localhost"},
+                              {proxy_port, 12345},
+                              {username_to_phone,[{<<"2000006168">>, <<"+919177074440">>}]}
+                              | ExtraOpts]
+                            ])
+      end).
+
 
 end_per_suite(Config) ->
     escalus_fresh:clean(),
