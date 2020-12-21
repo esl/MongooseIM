d755e15362a92b56167ebd02bc384994a96abc73
DenysGonchar <denys.gonchar@erlang-solutions.com>
2020-12-21 15:52:23 +0100

    adding all the big_tests presents

---
+++ tools/gh-actions-configure-preset.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+## GITHUB_ENV=/dev/tty ./gh-actions-configure-preset.sh internal-mnesia
+
+DB_ARRAY=( $(./tools/test_runner/presets_to_dbs.sh "$1" ) )
+[ "${#DB_ARRAY[@]}" -gt 0 ] && export DB="${DB_ARRAY[@]}"
+
+case "$1" in
+  internal_mnesia)
+    export REL_CONFIG="with-all" TLS_DIST=true ;;
+  odbc_mssql_mnesia)
+    export REL_CONFIG="with-odbc" ;;
+  mysql_redis)
+    export REL_CONFIG="with-mysql with-redis with-amqp_client" ;;
+  riak_mnesia)
+    export REL_CONFIG="with-riak" ;;
+  ldap_mnesia)
+    export REL_CONFIG="with-none" ;;
+  elasticsearch_and_cassandra_mnesia)
+    export REL_CONFIG="with-elasticsearch with-cassandra" TESTSPEC=mam.spec
+esac
+
+if [ ! -z "$GITHUB_ENV" ]; then
+  env | egrep "^(DB|REL_CONFIG|TLS_DIST)=" >> $GITHUB_ENV
+fi
