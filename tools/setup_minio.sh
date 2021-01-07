fc16b80a5cf4acf8904d7878dda8153cdda346d8
Nelson Vides <nelson.vides@erlang-solutions.com>
2021-01-07 14:49:17 +0100

    Remove whitespace in tools/setup_minio.sh

--- tools/setup_minio.sh
+++ tools/setup_minio.sh
@@ -4,7 +4,7 @@
 cd "$(dirname "$0")/../"
 
 ## see https://github.com/minio/minio/issues/4769 for more examples
-  
+
 minio_docker_name="mongooseim-minio"
 minio_access_key="AKIAIAOAONIULXQGMOUA"
 minio_secret_key="CG5fGqG0/n6NCPJ10FylpdgRnuV52j8IZvU7BSj8"
@@ -24,7 +24,7 @@ docker pull minio/mc &
 tools/wait_for_service.sh "${minio_docker_name}" 9000
 
 mc_cmd="$(cat <<-EOF
-  mc config host add myminio http://minio:9000 "${minio_access_key}" "${minio_secret_key}" && 
+  mc config host add myminio http://minio:9000 "${minio_access_key}" "${minio_secret_key}" &&
   mc mb "myminio/${minio_bucket}" &&
   mc policy set download "myminio/${minio_bucket}"
   exit 0
