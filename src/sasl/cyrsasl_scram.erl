93ce6d29427fc4ec936bdbafd9b18b4062562f06
Nelson Vides <nelson.vides@erlang-solutions.com>
2021-01-12 18:33:40 +0100

    [skip ci] Update copyright as the file has been fully rewritten

--- src/sasl/cyrsasl_scram.erl
+++ src/sasl/cyrsasl_scram.erl
@@ -1,31 +1,9 @@
-%%%----------------------------------------------------------------------
-%%% File    : cyrsasl_scram.erl
-%%% Author  : Stephen Röttger <stephen.roettger@googlemail.com>
-%%% Purpose : SASL SCRAM authentication
-%%% Created : 7 Aug 2011 by Stephen Röttger <stephen.roettger@googlemail.com>
-%%%
-%%%
-%%% ejabberd, Copyright (C) 2002-2013   ProcessOne
-%%%
-%%% This program is free software; you can redistribute it and/or
-%%% modify it under the terms of the GNU General Public License as
-%%% published by the Free Software Foundation; either version 2 of the
-%%% License, or (at your option) any later version.
-%%%
-%%% This program is distributed in the hope that it will be useful,
-%%% but WITHOUT ANY WARRANTY; without even the implied warranty of
-%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-%%% General Public License for more details.
-%%%
-%%% You should have received a copy of the GNU General Public License
-%%% along with this program; if not, write to the Free Software
-%%% Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
-%%%
-%%%----------------------------------------------------------------------
-
+%%%=============================================================================
+%%% @copyright (C) 1999-2021, Erlang Solutions Ltd
+%%% @doc SASL SCRAM implementation
+%%% @end
+%%%=============================================================================
 -module(cyrsasl_scram).
-
--author('stephen.roettger@googlemail.com').
 
 -export([mech_new/3, mech_step/2]).
 
