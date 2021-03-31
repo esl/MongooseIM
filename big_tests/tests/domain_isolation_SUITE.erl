a6edafcc219e9d0a65b5c13ca66f8fc5502eb612
Mikhail Uvarov <arcusfelis@gmail.com>
2021-03-31 11:11:08 +0200

    Add mod_domain_isolation to filter stanzas sent between domains

---
+++ big_tests/tests/domain_isolation_SUITE.erl
@@ -0,0 +1,83 @@
+-module(domain_isolation_SUITE).
+
+-include_lib("common_test/include/ct.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
+-compile(export_all).
+-import(distributed_helper, [mim/0, require_rpc_nodes/1, rpc/4]).
+
+suite() ->
+    require_rpc_nodes([mim]).
+
+all() ->
+    [
+     isolation_works_for_one2one,
+     isolation_works_for_subdomains
+    ].
+
+domain() -> ct:get_config({hosts, mim, domain}).
+
+%%--------------------------------------------------------------------
+%% Init & teardown
+%%--------------------------------------------------------------------
+
+init_per_suite(Config) ->
+    Config2 = dynamic_modules:save_modules(domain(), Config),
+    escalus:init_per_suite(Config2).
+
+end_per_suite(Config) ->
+    dynamic_modules:restore_modules(domain(), Config),
+    escalus_fresh:clean(),
+    escalus:end_per_suite(Config).
+
+init_per_testcase(TestcaseName, Config) ->
+    Host = domain(),
+    MucHost = binary_to_list(<<"muclight.", Host/binary>>),
+    dynamic_modules:start(Host, mod_domain_isolation, [{extra_domains, [MucHost]}]),
+    dynamic_modules:start(Host, mod_muc_light, [{host, MucHost}]),
+    escalus:init_per_testcase(TestcaseName, Config).
+
+end_per_testcase(TestcaseName, Config) ->
+    Host = domain(),
+    dynamic_modules:stop(Host, mod_domain_isolation),
+    dynamic_modules:stop(Host, mod_muc_light),
+    escalus:end_per_testcase(TestcaseName, Config).
+
+%%--------------------------------------------------------------------
+%% Tests
+%%--------------------------------------------------------------------
+
+isolation_works_for_one2one(Config) ->
+    F = fun(Alice, Bob, Bis) ->
+          %% Ignored from another domain
+          escalus_client:send(Bis, escalus_stanza:chat_to(Alice, <<"Hi!">>)),
+          %% Routed from the local domain
+          escalus_client:send(Bob, escalus_stanza:chat_to(Alice, <<"Hello">>)),
+          Stanza = escalus:wait_for_stanza(Alice),
+          escalus:assert(is_chat_message, [<<"Hello">>], Stanza),
+          %% Sender receives an error about the drop
+          Err = escalus:wait_for_stanza(Bis),
+          escalus:assert(is_error, [<<"cancel">>, <<"service-unavailable">>], Err),
+          <<"Filtered by the domain isolation">> = get_error_text(Err)
+        end,
+    escalus:fresh_story(Config, [{alice, 1}, {bob, 1}, {alice_bis, 1}], F).
+
+isolation_works_for_subdomains(Config) ->
+    F = fun(Bis) ->
+          Domain = domain(),
+          To = <<"muclight.", Domain/binary>>,
+          %% Ignored from another domain
+          escalus_client:send(Bis, escalus_stanza:chat_to(To, <<"Hi muc!">>)),
+          %% Sender receives an error about the drop
+          Err = escalus:wait_for_stanza(Bis),
+          escalus:assert(is_error, [<<"cancel">>, <<"service-unavailable">>], Err),
+          <<"Filtered by the domain isolation">> = get_error_text(Err)
+        end,
+    escalus:fresh_story(Config, [{alice_bis, 1}], F).
+
+%%--------------------------------------------------------------------
+%% Helpers
+%%--------------------------------------------------------------------
+
+get_error_text(Err) ->
+    exml_query:path(Err, [{element, <<"error">>}, {element, <<"text">>}, cdata]).
---
+++ src/mod_domain_isolation.erl
@@ -0,0 +1,62 @@
+-module(mod_domain_isolation).
+-behaviour(gen_mod).
+
+-include_lib("exml/include/exml.hrl").
+-include_lib("jid/include/jid.hrl").
+-include("mongoose_config_spec.hrl").
+
+%% gen_mod handlers
+-export([start/2, stop/1]).
+-export([config_spec/0]).
+
+%% hooks
+-export([filter_local_packet/1]).
+
+-type fpacket() :: {From :: jid:jid(),
+                    To :: jid:jid(),
+                    Acc :: mongoose_acc:t(),
+                    Packet :: exml:element()}.
+
+-spec config_spec() -> mongoose_config_spec:config_section().
+config_spec() ->
+    #section{
+       items = #{<<"extra_domains">> => extra_domains()}
+      }.
+
+extra_domains() ->
+    #list{items = #option{type = string, validate = domain_template}}.
+
+start(Host, Opts) ->
+    ExtraDomains = proplists:get_value(extra_domains, Opts, []),
+    SubHosts = [gen_mod:make_subhost(SubHost, Host) || SubHost <- ExtraDomains],
+    AllHosts = [Host|SubHosts],
+    [ejabberd_hooks:add(filter_local_packet, H, ?MODULE, filter_local_packet, 10)
+     || H <- AllHosts],
+    ok.
+
+stop(Host) ->
+    ExtraDomains = gen_mod:get_module_opt(Host, ?MODULE, extra_domains, []),
+    SubHosts = [gen_mod:make_subhost(SubHost, Host) || SubHost <- ExtraDomains],
+    AllHosts = [Host|SubHosts],
+    [ejabberd_hooks:delete(filter_local_packet, H, ?MODULE, filter_local_packet, 10)
+     || H <- AllHosts],
+    ok.
+
+-spec filter_local_packet(Value :: fpacket() | drop) -> fpacket() | drop.
+filter_local_packet(drop) ->
+    drop;
+filter_local_packet({From = #jid{lserver = FromServer},
+                     To = #jid{lserver = ToServer}, Acc, Packet}) ->
+    if
+        FromServer =:= ToServer ->
+            {From, To, Acc, Packet};
+        true ->
+            send_back_error(mongoose_xmpp_errors:service_unavailable(
+                                <<"en">>, <<"Filtered by the domain isolation">>),
+                            From, To, Acc),
+            drop
+     end.
+
+send_back_error(Etype, From, To, Acc) ->
+    {Acc1, Err} = jlib:make_error_reply(Acc, Etype),
+    ejabberd_router:route(To, From, Acc1, Err).
