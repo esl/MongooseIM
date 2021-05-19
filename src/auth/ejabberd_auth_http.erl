cf08f08e25516732bc88676733d90a9eac809fd2
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2021-05-19 14:38:02 +0200

    Use mongoose_gen_auth in ejabberd_auth_http
    Group user-related arguments in 'params'.
    This approach could be used for backend callbacks as well.

--- src/auth/ejabberd_auth_http.erl
+++ src/auth/ejabberd_auth_http.erl
@@ -8,24 +8,20 @@
 -module(ejabberd_auth_http).
 -author('piotr.nosek@erlang-solutions.com').
 
--behaviour(ejabberd_gen_auth).
+-behaviour(mongoose_gen_auth).
 
 %% External exports
 -export([start/1,
+         stop/1,
+         supports_sasl_module/2,
          set_password/4,
          authorize/1,
          try_register/4,
-         dirty_get_registered_users/0,
-         get_vh_registered_users/1,
-         get_vh_registered_users/2,
-         get_vh_registered_users_number/1,
-         get_vh_registered_users_number/2,
-         get_password/2,
-         get_password_s/2,
-         does_user_exist/2,
-         remove_user/2,
-         supports_sasl_module/2,
-         stop/1]).
+         get_password/3,
+         get_password_s/3,
+         does_user_exist/3,
+         remove_user/3,
+         supported_features/0]).
 
 %% Pre-mongoose_credentials API
 -export([check_password/4,
@@ -35,16 +31,24 @@
 -include("scram.hrl").
 
 -type http_error_atom() :: conflict | not_found | not_authorized | not_allowed.
+-type params() :: #{luser := jid:luser(),
+                    lserver := jid:lserver(),
+                    host_type := mongooseim:host_type(),
+                    password => binary()}.
 
 %%%----------------------------------------------------------------------
 %%% API
 %%%----------------------------------------------------------------------
 
--spec start(binary()) -> ok.
+-spec start(moongooseim:host_type()) -> ok.
 start(_HostType) ->
     ok.
 
--spec supports_sasl_module(binary(), cyrsasl:sasl_module()) -> boolean().
+-spec stop(moongooseim:host_type()) -> ok.
+stop(_HostType) ->
+    ok.
+
+-spec supports_sasl_module(mongooseim:host_type(), cyrsasl:sasl_module()) -> boolean().
 supports_sasl_module(_HostType, cyrsasl_plain) -> true;
 supports_sasl_module(HostType, cyrsasl_digest) ->  not mongoose_scram:enabled(HostType);
 supports_sasl_module(HostType, Mechanism) -> mongoose_scram:enabled(HostType, Mechanism).
@@ -55,8 +59,7 @@ authorize(Creds) ->
     case mongoose_credentials:get(Creds, der_cert, undefined) of
         undefined -> ejabberd_auth:authorize_with_check_password(?MODULE, Creds);
         DerCert ->
-            {LServer, LUser} = get_server_and_user_name(Creds),
-            case verify_cert(LUser, LServer, DerCert) of
+            case verify_cert(Creds, DerCert) of
                 true -> {ok, mongoose_credentials:set(Creds, auth_module, ?MODULE)};
                 false -> {error, not_authorized}
             end
@@ -65,20 +68,23 @@ authorize(Creds) ->
 
 -spec check_password(mongooseim:host_type(), jid:luser(), jid:lserver(), binary()) -> boolean().
 check_password(HostType, LUser, LServer, Password) ->
+    Params = #{luser => LUser, lserver => LServer, host_type => HostType, password => Password},
     case mongoose_scram:enabled(HostType) of
         false ->
-            case make_req(get, <<"check_password">>, LUser, LServer, Password) of
+            case make_req(get, <<"check_password">>, Params) of
                 {ok, <<"true">>} -> true;
                 _ -> false
             end;
         true ->
-            {ok, true} =:= verify_scram_password(LUser, LServer, Password)
+            {ok, true} =:= verify_scram_password(Params)
     end.
 
 -spec check_password(mongooseim:host_type(), jid:luser(), jid:lserver(),
                      binary(), binary(), fun()) -> boolean().
 check_password(HostType, LUser, LServer, Password, Digest, DigestGen) ->
-    case make_req(get, <<"get_password">>, LUser, LServer, <<"">>) of
+    case make_req(get, <<"get_password">>, #{luser => LUser,
+                                             lserver => LServer,
+                                             host_type => HostType}) of
         {error, _} ->
             false;
         {ok, GotPasswd} ->
@@ -98,7 +104,10 @@ set_password(HostType, LUser, LServer, Password) ->
                                                   Password, mongoose_scram:iterations(HostType)));
                         false -> Password
                     end,
-    case make_req(post, <<"set_password">>, LUser, LServer, PasswordFinal) of
+    case make_req(post, <<"set_password">>, #{luser => LUser,
+                                             lserver => LServer,
+                                             host_type => HostType,
+                                             password => PasswordFinal}) of
         {ok, _} -> ok;
         {error, invalid_jid} = Error -> Error;
         {error, _} -> {error, not_allowed}
@@ -112,39 +121,25 @@ try_register(HostType, LUser, LServer, Password) ->
                                                   Password, mongoose_scram:iterations(HostType)));
                         false -> Password
                     end,
-    case make_req(post, <<"register">>, LUser, LServer, PasswordFinal) of
+    case make_req(post, <<"register">>, #{luser => LUser,
+                                          lserver => LServer,
+                                          host_type => HostType,
+                                          password => PasswordFinal}) of
         {ok, created} -> ok;
         {error, conflict} -> {error, exists};
         _Error -> {error, not_allowed}
     end.
 
--spec dirty_get_registered_users() -> [].
-dirty_get_registered_users() ->
-    [].
-
--spec get_vh_registered_users(jid:lserver()) -> [].
-get_vh_registered_users(_Server) ->
-    [].
-
--spec get_vh_registered_users(jid:lserver(), list()) -> [].
-get_vh_registered_users(_Server, _Opts) ->
-    [].
-
--spec get_vh_registered_users_number(binary()) -> 0.
-get_vh_registered_users_number(_Server) ->
-    0.
-
--spec get_vh_registered_users_number(jid:lserver(), list()) -> 0.
-get_vh_registered_users_number(_Server, _Opts) ->
-    0.
-
--spec get_password(jid:luser(), jid:lserver()) -> ejabberd_auth:passterm() | false.
-get_password(LUser, LServer) ->
-    case make_req(get, <<"get_password">>, LUser, LServer, <<"">>) of
+-spec get_password(mongooseim:host_type(), jid:luser(), jid:lserver()) ->
+          ejabberd_auth:passterm() | false.
+get_password(HostType, LUser, LServer) ->
+    case make_req(get, <<"get_password">>, #{luser => LUser,
+                                             lserver => LServer,
+                                             host_type => HostType}) of
         {error, _} ->
             false;
         {ok, Password} ->
-            case mongoose_scram:enabled(LServer) of
+            case mongoose_scram:enabled(HostType) of
                 true ->
                     convert_scram_to_tuple(Password);
                 false ->
@@ -152,67 +147,66 @@ get_password(LUser, LServer) ->
             end
     end.
 
--spec get_password_s(jid:luser(), jid:lserver()) -> binary().
-get_password_s(User, Server) ->
-    case get_password(User, Server) of
+-spec get_password_s(mongooseim:host_type(), jid:luser(), jid:lserver()) -> binary().
+get_password_s(HostType, User, Server) ->
+    case get_password(HostType, User, Server) of
         Pass when is_binary(Pass) -> Pass;
         _ -> <<>>
     end.
 
--spec does_user_exist(jid:luser(), jid:lserver()) -> boolean().
-does_user_exist(LUser, LServer) ->
-    case make_req(get, <<"user_exists">>, LUser, LServer, <<"">>) of
+-spec does_user_exist(mongooseim:host_type(), jid:luser(), jid:lserver()) -> boolean().
+does_user_exist(HostType, LUser, LServer) ->
+    case make_req(get, <<"user_exists">>, #{luser => LUser,
+                                            lserver => LServer,
+                                            host_type => HostType}) of
         {ok, <<"true">>} -> true;
         _ -> false
     end.
 
--spec remove_user(jid:luser(), jid:lserver()) ->
+-spec remove_user(mongooseim:host_type(), jid:luser(), jid:lserver()) ->
     ok | {error, not_allowed}.
-remove_user(LUser, LServer) ->
-    case remove_user_req(LUser, LServer, <<"">>, <<"remove_user">>) of
-        ok -> ok;
-        _ -> {error, not_allowed}
+remove_user(HostType, LUser, LServer) ->
+    case make_req(post, <<"remove_user">>,  #{luser => LUser,
+                                              lserver => LServer,
+                                              host_type => HostType}) of
+        {ok, _} -> ok;
+        {error, _} -> {error, not_allowed}
     end.
 
--spec remove_user_req(binary(), binary(), binary(), binary()) ->
-    ok | {error, not_exists | not_allowed | bad_request}.
-remove_user_req(LUser, LServer, Password, Method) ->
-    case make_req(post, Method, LUser, LServer, Password) of
-        {error, not_allowed} -> {error, not_allowed};
-        {error, not_found} -> {error, not_exists};
-        {error, _} -> {error, bad_request};
-        {ok, _} -> ok
-    end.
+-spec supported_features() -> [atom()].
+supported_features() -> [dynamic_domains].
 
 %%%----------------------------------------------------------------------
 %%% Request maker
 %%%----------------------------------------------------------------------
 
--spec make_req(post | get, binary(), binary(), binary(), binary()) ->
-    {ok, BodyOrCreated :: binary() | created} | {error, invalid_jid | http_error_atom() | binary()}.
-make_req(_, _, LUser, LServer, _) when LUser == error orelse LServer == error ->
+-spec make_req(post | get, binary(), params()) ->
+    {ok, BodyOrCreated :: binary() | created} | {error, invalid_jid
+        | http_error_atom() | binary()}.
+make_req(_, _, #{luser := LUser, lserver := LServer}) when LUser == error orelse LServer == error ->
     {error, invalid_jid};
-make_req(Method, Path, LUser, LServer, Password) ->
+make_req(Method, Path, Params = #{luser := LUser, lserver := LServer, host_type := HostType}) ->
+    Password = maps:get(password, Params, <<>>),
     Query = uri_string:compose_query([{<<"user">>, LUser},
                                       {<<"server">>, LServer},
                                       {<<"pass">>, Password}]),
-    Header =  case ejabberd_auth:get_opt(LServer, basic_auth) of
-                  undefined ->
-                      [];
-                  BasicAuth ->
-                      BasicAuth64 = base64:encode(BasicAuth),
-                      [{<<"Authorization">>, <<"Basic ", BasicAuth64/binary>>}]
-              end,
+    Header = case ejabberd_auth:get_opt(HostType, basic_auth) of
+                 undefined ->
+                     [];
+                 BasicAuth ->
+                     BasicAuth64 = base64:encode(BasicAuth),
+                     [{<<"Authorization">>, <<"Basic ", BasicAuth64/binary>>}]
+             end,
 
     Result = case Method of
-        get -> mongoose_http_client:get(LServer, auth, <<Path/binary, "?", Query/binary>>, Header);
-        post -> mongoose_http_client:post(LServer, auth, Path, Header, Query)
+        get -> mongoose_http_client:get(HostType, auth, <<Path/binary, "?", Query/binary>>, Header);
+        post -> mongoose_http_client:post(HostType, auth, Path, Header, Query)
     end,
-    handle_result(Result, Method, Path, LUser, LServer).
+    handle_result(Result, Method, Path, LUser, HostType).
 
-handle_result({ok, {Code, RespBody}}, Method, Path, LUser, LServer) ->
+handle_result({ok, {Code, RespBody}}, Method, Path, LUser, HostType) ->
     ?LOG_DEBUG(#{what => http_auth_request, text => <<"Received HTTP request result">>,
-                 path => Path, method => Method, user => LUser, server => LServer,
+                 path => Path, method => Method, user => LUser, host_type => HostType,
                  code => Code, result => RespBody}),
     case Code of
         <<"409">> -> {error, conflict};
@@ -224,9 +218,9 @@ handle_result({ok, {Code, RespBody}}, Method, Path, LUser, LServer) ->
         <<"201">> -> {ok, created};
         <<"200">> -> {ok, RespBody}
     end;
-handle_result({error, Reason}, Method, Path, LUser, LServer) ->
+handle_result({error, Reason}, Method, Path, LUser, HostType) ->
     ?LOG_DEBUG(#{what => http_auth_request_failed, text => <<"HTTP request failed">>,
-                 path => Path, method => Method, user => LUser, server => LServer,
+                 path => Path, method => Method, user => LUser, host_type => HostType,
                  reason => Reason}),
     {error, bad_request}.
 
@@ -234,40 +228,39 @@ handle_result({error, Reason}, Method, Path, LUser, LServer) ->
 %%% Other internal functions
 %%%----------------------------------------------------------------------
 
--spec verify_scram_password(binary(), binary(), binary()) ->
-    {ok, boolean()} | {error, bad_request | not_exists}.
-verify_scram_password(LUser, LServer, Password) ->
-    case make_req(get, <<"get_password">>, LUser, LServer, <<"">>) of
+-spec verify_scram_password(params()) -> {ok, boolean()} | {error, bad_request | not_exists}.
+verify_scram_password(Params) ->
+    {Password, ParamsWithoutPass} = maps:take(password, Params),
+    case make_req(get, <<"get_password">>, ParamsWithoutPass) of
         {ok, RawPassword} ->
             case mongoose_scram:deserialize(RawPassword) of
                 {ok, DeserializedScramMap} ->
                     {ok, mongoose_scram:check_password(Password, DeserializedScramMap)};
                 {error, Reason} ->
                     ?LOG_WARNING(#{what => scram_serialisation_incorrect, reason => Reason,
-                                   user => LUser, server => LServer}),
+                                   params => Params}),
                     {error, bad_request}
             end;
         _ ->
             {error, not_exists}
     end.
 
-verify_cert(LUser, LServer, DerCert) ->
-    case make_req(get, <<"get_certs">>, LUser, LServer, <<"">>) of
+verify_cert(Creds, DerCert) ->
+    Params = creds_to_params(Creds),
+    case make_req(get, <<"get_certs">>, Params) of
         {ok, PemCert} ->
             UserCert = {'Certificate', DerCert, not_encrypted},
             [] =/= [Cert || Cert <- public_key:pem_decode(PemCert), Cert =:= UserCert];
         _ -> false
     end.
 
-get_server_and_user_name(Creds) ->
+creds_to_params(Creds) ->
     User = mongoose_credentials:get(Creds, username),
     LUser = jid:nodeprep(User),
     LUser == error andalso error({nodeprep_error, User}),
+    HostType = mongoose_credentials:host_type(Creds),
     LServer = mongoose_credentials:lserver(Creds),
-    {LServer, LUser}.
-
-stop(_HostType) ->
-    ok.
+    #{host_type => HostType, luser => LUser, lserver => LServer}.
 
 -spec check_scram_password(binary(), binary(), binary(), fun()) -> boolean().
 check_scram_password(OriginalPassword, GotPassword, Digest, DigestGen) ->
