9bc27161dde626a0442981bc4bbb511ab70eb32d
Nelson Vides <nelson.vides@erlang-solutions.com>
2021-05-27 17:00:19 +0200

    Remove_user hook takes a host_type
    This hook makes no sense without a host_type, it logically only triggers
    for a user that belongs to MongooseIM.

--- src/auth/ejabberd_auth.erl
+++ src/auth/ejabberd_auth.erl
@@ -363,7 +363,7 @@ remove_user(#jid{luser = LUser, lserver = LServer}) ->
                                        host_type => HostType,
                                        lserver => LServer,
                                        element => undefined }),
-            mongoose_hooks:remove_user(LServer, Acc, LUser),
+            mongoose_hooks:remove_user(HostType, Acc, LServer, LUser),
             ok;
         Error ->
             ?LOG_ERROR(#{what => backend_disallows_user_removal,
