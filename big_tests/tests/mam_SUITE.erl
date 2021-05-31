3f8b05beff5c547d22c21f04cdb57d2405d030cb
Mikhail Uvarov <arcusfelis@gmail.com>
2021-05-31 16:37:16 +0200

    Handle already started mod_muc_light in mam_SUITE

--- big_tests/tests/mam_SUITE.erl
+++ big_tests/tests/mam_SUITE.erl
@@ -855,7 +855,7 @@ rdbms_simple_opts() ->
     [{db_jid_format, mam_jid_rfc}, {db_message_format, mam_message_xml}].
 
 init_modules_for_muc_light(BackendType, Config) ->
-    dynamic_modules:start(host_type(), mod_muc_light, [{host, subhost_pattern(muc_light_helper:muc_host_pattern())}]),
+    dynamic_modules:restart(host_type(), mod_muc_light, [{host, subhost_pattern(muc_light_helper:muc_host_pattern())}]),
     Config1 = init_modules(BackendType, muc_all, [{muc_domain, muc_helper:muc_host_pattern()} | Config]),
     init_modules(BackendType, pm, [{archive_groupchats, false} | Config1]).
 
