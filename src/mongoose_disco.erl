612fa3d803e4558d5adedc3673d6a2067820a3ea
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2021-05-31 08:11:51 +0200

    Make item_acc a list to be more consistent with feature_acc

--- src/mongoose_disco.erl
+++ src/mongoose_disco.erl
@@ -9,15 +9,17 @@
 -include("mongoose.hrl").
 -include("jlib.hrl").
 
--type item_acc() :: empty | {result, map()}.
 -type feature_acc() :: empty | {result, [feature()]}.
--type item() :: #{jid := jid:lserver(),
-                  name => binary(),
-                  node => binary()}.
 -type feature() :: binary().
+
+-type item_acc() :: empty | {result, [item()]}.
+-type item() :: #{jid := jid:lserver(), name => binary(), node => binary()}.
 
 -export_type([item_acc/0, feature_acc/0, item/0, feature/0]).
 
+%% @doc Run the 'disco_local_features' hook and unpack the results.
+%% Used by extension modules which support their own subdomains
+%% and add their own features to the end result.
 -spec get_local_features(jid:lserver(), jid:jid(), jid:jid(), binary(), ejabberd:lang()) ->
           [feature()].
 get_local_features(Host, From, To, Node, Lang) ->
@@ -29,13 +31,10 @@ get_local_features(Host, From, To, Node, Lang) ->
     end.
 
 -spec add_features([feature()], feature_acc())  -> feature_acc().
-add_features(Features, Acc) ->
-    case Acc of
-        empty ->
-            {result, Features};
-        {result, InitialFeatures} ->
-            {result, Features ++ InitialFeatures}
-    end.
+add_features(Features, empty) ->
+    {result, Features};
+add_features(Features, {result, InitialFeatures}) ->
+    {result, Features ++ InitialFeatures}.
 
 -spec features_to_xml([feature()]) -> [exml:element()].
 features_to_xml(Features) ->
@@ -47,17 +46,16 @@ feature_to_xml(Feature) when is_binary(Feature) ->
 
 -spec add_items([item()], item_acc())  -> item_acc().
 add_items(Items, empty) ->
-    {result, add_items_to_map(Items, #{})};
-add_items(Items, {result, InitialItemMap}) ->
-    {result, add_items_to_map(Items, InitialItemMap)}.
+    {result, Items};
+add_items(Items, {result, InitialItems}) ->
+    {result, Items ++ InitialItems}.
 
-add_items_to_map(Items, InitialItemMap) ->
-    ToAdd = maps:from_list([{JID, Item} || Item = #{jid := JID} <- Items]),
-    maps:merge(ToAdd, InitialItemMap).
-
--spec items_to_xml(map()) -> [exml:element()].
+-spec items_to_xml([item()]) -> [exml:element()].
 items_to_xml(Items) ->
-    [item_to_xml(Item) || Item <- maps:values(Items)].
+    %% For each JID, leave only the rightmost item with that JID (the one which was added first).
+    %% This is needed as extension modules might add more detailed information about an item
+    %% than the default which is obtained from the registered routes and contains only the JID.
+    maps:values(maps:from_list([{JID, item_to_xml(Item)} || #{jid := JID} = Item <- Items])).
 
 item_to_xml(Item) ->
     #xmlel{name = <<"item">>,
