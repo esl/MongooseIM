2aa31b766440ab627006baf4cac5c0fc949d8405
DenysGonchar <denys.gonchar@erlang-solutions.com>
2021-06-01 16:08:23 +0200

    updating mod_stream_management module

--- src/mod_stream_management.erl
+++ src/mod_stream_management.erl
@@ -49,15 +49,15 @@
          sid :: ejabberd_sm:sid()
         }).
 
+-type buffer_max() :: pos_integer() | infinity | no_buffer.
+-type ack_freq() :: pos_integer() | never.
 %%
 %% `gen_mod' callbacks
 %%
 
-start(Host, Opts) ->
+start(HostType, Opts) ->
     ?LOG_INFO(#{what => stream_management_starting}),
-    ejabberd_hooks:add(c2s_stream_features, Host, ?MODULE, add_sm_feature, 50),
-    ejabberd_hooks:add(sm_remove_connection_hook, Host, ?MODULE, remove_smid, 50),
-    ejabberd_hooks:add(session_cleanup, Host, ?MODULE, session_cleanup, 50),
+    ejabberd_hooks:add(hooks(HostType)),
     mnesia:create_table(sm_session, [{ram_copies, [node()]},
                                      {attributes, record_info(fields, sm_session)}]),
     mnesia:add_table_index(sm_session, sid),
@@ -65,12 +65,15 @@ start(Host, Opts) ->
     stream_management_stale_h:maybe_start(Opts),
     ok.
 
-stop(Host) ->
+stop(HostType) ->
     ?LOG_INFO(#{what => stream_management_stopping}),
-    ejabberd_hooks:delete(sm_remove_connection_hook, Host, ?MODULE, remove_smid, 50),
-    ejabberd_hooks:delete(c2s_stream_features, Host, ?MODULE, add_sm_feature, 50),
-    ejabberd_hooks:delete(session_cleanup, Host, ?MODULE, session_cleanup, 50),
+    ejabberd_hooks:delete(hooks(HostType)),
     ok.
+
+hooks(HostType) ->
+    [{sm_remove_connection_hook, HostType, ?MODULE, remove_smid, 50},
+     {c2s_stream_features, HostType, ?MODULE, add_sm_feature, 50},
+     {session_cleanup, HostType, ?MODULE, session_cleanup, 50}].
 
 -spec config_spec() -> mongoose_config_spec:config_section().
 config_spec() ->
@@ -133,16 +136,19 @@ sm() ->
       Info :: undefined | [any()],
       Reason :: undefined | ejabberd_sm:close_reason(),
       Acc1 :: mongoose_acc:t().
-remove_smid(Acc, SID, #jid{lserver = LServer}, _Info, _Reason) ->
-    do_remove_smid(Acc, LServer, SID).
+remove_smid(Acc, SID, _JID, _Info, _Reason) ->
+    HostType = mongoose_acc:host_type(Acc),
+    do_remove_smid(Acc, HostType, SID).
 
 -spec session_cleanup(Acc :: map(), LUser :: jid:luser(), LServer :: jid:lserver(),
                       LResource :: jid:lresource(), SID :: ejabberd_sm:sid()) -> any().
-session_cleanup(Acc, _LUser, LServer, _LResource, SID) ->
-    do_remove_smid(Acc, LServer, SID).
+session_cleanup(Acc, _LUser, _LServer, _LResource, SID) ->
+    HostType = mongoose_acc:host_type(Acc),
+    do_remove_smid(Acc, HostType, SID).
 
--spec do_remove_smid(mongoose_acc:t(), jid:lserver(), ejabberd_sm:sid()) -> mongoose_acc:t().
-do_remove_smid(Acc, LServer, SID) ->
+-spec do_remove_smid(mongoose_acc:t(), mongooseim:host_type(), ejabberd_sm:sid()) ->
+    mongoose_acc:t().
+do_remove_smid(Acc, HostType, SID) ->
     H = mongoose_acc:get(stream_mgmt, h, undefined, Acc),
     MaybeSMID = case mnesia:dirty_index_read(sm_session, SID, #sm_session.sid) of
         [] -> {error, smid_not_found};
@@ -150,7 +156,7 @@ do_remove_smid(Acc, LServer, SID) ->
             mnesia:dirty_delete(sm_session, SMID),
             case H of
                 undefined -> ok;
-                _ -> register_stale_smid_h(LServer, SMID, H)
+                _ -> register_stale_smid_h(HostType, SMID, H)
             end,
             {ok, SMID}
     end,
@@ -160,75 +166,73 @@ do_remove_smid(Acc, LServer, SID) ->
 %% `mongooseim.toml' options (don't use outside of tests)
 %%
 
--spec get_buffer_max(jid:lserver(), pos_integer() | infinity | no_buffer)
-    -> pos_integer() | infinity | no_buffer.
-get_buffer_max(LServer, Default) ->
-    gen_mod:get_module_opt(LServer, ?MODULE, buffer_max, Default).
+-spec get_buffer_max(mongooseim:host_type(), buffer_max()) -> buffer_max().
+get_buffer_max(HostType, Default) ->
+    gen_mod:get_module_opt(HostType, ?MODULE, buffer_max, Default).
 
 %% Return true if succeeded, false otherwise.
--spec set_buffer_max(jid:lserver(), pos_integer() | infinity | no_buffer | undefined)
-    -> boolean().
-set_buffer_max(LServer, undefined) ->
-    del_module_opt(LServer, ?MODULE, buffer_max);
-set_buffer_max(LServer, infinity) ->
-    set_module_opt(LServer, ?MODULE, buffer_max, infinity);
-set_buffer_max(LServer, no_buffer) ->
-    set_module_opt(LServer, ?MODULE, buffer_max, no_buffer);
-set_buffer_max(LServer, Seconds) when is_integer(Seconds), Seconds > 0 ->
-    set_module_opt(LServer, ?MODULE, buffer_max, Seconds).
+-spec set_buffer_max(mongooseim:host_type(), buffer_max() | undefined) -> boolean().
+set_buffer_max(HostType, undefined) ->
+    del_module_opt(HostType, ?MODULE, buffer_max);
+set_buffer_max(HostType, infinity) ->
+    set_module_opt(HostType, ?MODULE, buffer_max, infinity);
+set_buffer_max(HostType, no_buffer) ->
+    set_module_opt(HostType, ?MODULE, buffer_max, no_buffer);
+set_buffer_max(HostType, Seconds) when is_integer(Seconds), Seconds > 0 ->
+    set_module_opt(HostType, ?MODULE, buffer_max, Seconds).
 
--spec get_ack_freq(jid:lserver(), pos_integer() | never) -> pos_integer() | never.
-get_ack_freq(LServer, Default) ->
-    gen_mod:get_module_opt(LServer, ?MODULE, ack_freq, Default).
+-spec get_ack_freq(mongooseim:host_type(), ack_freq()) -> ack_freq().
+get_ack_freq(HostType, Default) ->
+    gen_mod:get_module_opt(HostType, ?MODULE, ack_freq, Default).
 
 %% Return true if succeeded, false otherwise.
--spec set_ack_freq(jid:lserver(), pos_integer() | never | undefined) -> boolean().
-set_ack_freq(LServer, undefined) ->
-    del_module_opt(LServer, ?MODULE, ack_freq);
-set_ack_freq(LServer, never) ->
-    set_module_opt(LServer, ?MODULE, ack_freq, never);
-set_ack_freq(LServer, Freq) when is_integer(Freq), Freq > 0 ->
-    set_module_opt(LServer, ?MODULE, ack_freq, Freq).
+-spec set_ack_freq(mongooseim:host_type(), ack_freq() | undefined) -> boolean().
+set_ack_freq(HostType, undefined) ->
+    del_module_opt(HostType, ?MODULE, ack_freq);
+set_ack_freq(HostType, never) ->
+    set_module_opt(HostType, ?MODULE, ack_freq, never);
+set_ack_freq(HostType, Freq) when is_integer(Freq), Freq > 0 ->
+    set_module_opt(HostType, ?MODULE, ack_freq, Freq).
 
--spec get_resume_timeout(jid:lserver(), pos_integer()) -> pos_integer().
-get_resume_timeout(LServer, Default) ->
-    gen_mod:get_module_opt(LServer, ?MODULE, resume_timeout, Default).
+-spec get_resume_timeout(mongooseim:host_type(), pos_integer()) -> pos_integer().
+get_resume_timeout(HostType, Default) ->
+    gen_mod:get_module_opt(HostType, ?MODULE, resume_timeout, Default).
 
--spec set_resume_timeout(jid:lserver(), pos_integer()) -> boolean().
-set_resume_timeout(LServer, ResumeTimeout) ->
-    set_module_opt(LServer, ?MODULE, resume_timeout, ResumeTimeout).
+-spec set_resume_timeout(mongooseim:host_type(), pos_integer()) -> boolean().
+set_resume_timeout(HostType, ResumeTimeout) ->
+    set_module_opt(HostType, ?MODULE, resume_timeout, ResumeTimeout).
 
 
--spec get_stale_h_opt(LServer :: jid:lserver(), Opt :: atom(), Def :: pos_integer()) -> pos_integer().
-get_stale_h_opt(LServer, Option, Default) ->
-    MaybeModOpts = gen_mod:get_module_opt(LServer, ?MODULE, stale_h, []),
+-spec get_stale_h_opt(mongooseim:host_type(), atom(), pos_integer()) -> pos_integer().
+get_stale_h_opt(HostType, Option, Default) ->
+    MaybeModOpts = gen_mod:get_module_opt(HostType, ?MODULE, stale_h, []),
     proplists:get_value(Option, MaybeModOpts, Default).
 
--spec get_stale_h_repeat_after(jid:lserver(), pos_integer()) -> pos_integer().
-get_stale_h_repeat_after(LServer, Default) ->
-    get_stale_h_opt(LServer, stale_h_repeat_after, Default).
+-spec get_stale_h_repeat_after(mongooseim:host_type(), pos_integer()) -> pos_integer().
+get_stale_h_repeat_after(HostType, Default) ->
+    get_stale_h_opt(HostType, stale_h_repeat_after, Default).
 
--spec get_stale_h_geriatric(jid:lserver(), pos_integer()) -> pos_integer().
-get_stale_h_geriatric(LServer, Default) ->
-    get_stale_h_opt(LServer, stale_h_geriatric, Default).
+-spec get_stale_h_geriatric(mongooseim:host_type(), pos_integer()) -> pos_integer().
+get_stale_h_geriatric(HostType, Default) ->
+    get_stale_h_opt(HostType, stale_h_geriatric, Default).
 
--spec set_stale_h_opt(LServer :: jid:lserver(), Option :: atom(), Value :: pos_integer()) -> boolean().
-set_stale_h_opt(LServer, Option, Value) ->
-    MaybeModOpts = gen_mod:get_module_opt(LServer, ?MODULE, stale_h, []),
+-spec set_stale_h_opt(mongooseim:host_type(), atom(), pos_integer()) -> boolean().
+set_stale_h_opt(HostType, Option, Value) ->
+    MaybeModOpts = gen_mod:get_module_opt(HostType, ?MODULE, stale_h, []),
     case MaybeModOpts of
         [] -> false;
         GCOpts ->
             NewGCOpts = lists:keystore(Option, 1, GCOpts, {Option, Value}),
-            set_module_opt(LServer, ?MODULE, stale_h, NewGCOpts)
+            set_module_opt(HostType, ?MODULE, stale_h, NewGCOpts)
     end.
 
--spec set_stale_h_repeat_after(jid:lserver(), pos_integer()) -> boolean().
-set_stale_h_repeat_after(LServer, ResumeTimeout) ->
-    set_stale_h_opt(LServer, stale_h_repeat_after, ResumeTimeout).
+-spec set_stale_h_repeat_after(mongooseim:host_type(), pos_integer()) -> boolean().
+set_stale_h_repeat_after(HostType, ResumeTimeout) ->
+    set_stale_h_opt(HostType, stale_h_repeat_after, ResumeTimeout).
 
--spec set_stale_h_geriatric(jid:lserver(), pos_integer()) -> boolean().
-set_stale_h_geriatric(LServer, GeriatricAge) ->
-    set_stale_h_opt(LServer, stale_h_geriatric, GeriatricAge).
+-spec set_stale_h_geriatric(mongooseim:host_type(), pos_integer()) -> boolean().
+set_stale_h_geriatric(HostType, GeriatricAge) ->
+    set_stale_h_opt(HostType, stale_h_geriatric, GeriatricAge).
 
 %%
 %% API for `ejabberd_c2s'
@@ -239,12 +243,12 @@ make_smid() ->
     base64:encode(crypto:strong_rand_bytes(21)).
 
 %% Getters
--spec get_session_from_smid(jid:lserver(), smid()) ->
+-spec get_session_from_smid(mongooseim:host_type(), smid()) ->
     {sid, ejabberd_sm:sid()} | {stale_h, non_neg_integer()} | {error, smid_not_found}.
-get_session_from_smid(LServer, SMID) ->
+get_session_from_smid(HostType, SMID) ->
     case get_sid(SMID) of
         {sid, SID} -> {sid, SID};
-        {error, smid_not_found} -> get_stale_h(LServer, SMID)
+        {error, smid_not_found} -> get_stale_h(HostType, SMID)
     end.
 
 -spec get_sid(smid()) ->
@@ -255,10 +259,10 @@ get_sid(SMID) ->
         [] -> {error, smid_not_found}
     end.
 
--spec get_stale_h(LServer :: jid:lserver(), SMID :: smid()) ->
+-spec get_stale_h(mongooseim:host_type(), SMID :: smid()) ->
     {stale_h, non_neg_integer()} | {error, smid_not_found}.
-get_stale_h(LServer, SMID) ->
-    MaybeModOpts = gen_mod:get_module_opt(LServer, ?MODULE, stale_h, []),
+get_stale_h(HostType, SMID) ->
+    MaybeModOpts = gen_mod:get_module_opt(HostType, ?MODULE, stale_h, []),
     case proplists:get_value(enabled, MaybeModOpts, false) of
         false -> {error, smid_not_found};
         true -> stream_management_stale_h:read_stale_h(SMID)
@@ -274,15 +278,15 @@ register_smid(SMID, SID) ->
               {error, Reason}
     end.
 
-register_stale_smid_h(LServer, SMID, H) ->
-    MaybeModOpts = gen_mod:get_module_opt(LServer, ?MODULE, stale_h, []),
+register_stale_smid_h(HostType, SMID, H) ->
+    MaybeModOpts = gen_mod:get_module_opt(HostType, ?MODULE, stale_h, []),
     case proplists:get_value(enabled, MaybeModOpts, false) of
         false -> ok;
         true -> stream_management_stale_h:write_stale_h(SMID, H)
     end.
 
-remove_stale_smid_h(LServer, SMID) ->
-    MaybeModOpts = gen_mod:get_module_opt(LServer, ?MODULE, stale_h, []),
+remove_stale_smid_h(HostType, SMID) ->
+    MaybeModOpts = gen_mod:get_module_opt(HostType, ?MODULE, stale_h, []),
     case proplists:get_value(enabled, MaybeModOpts, false) of
         false -> ok;
         true -> stream_management_stale_h:delete_stale_h(SMID)
@@ -294,12 +298,11 @@ remove_stale_smid_h(LServer, SMID) ->
 set_module_opt(Host, Module, Opt, Value) ->
     mod_module_opt(Host, Module, Opt, Value, fun set_opt/3).
 
-del_module_opt(Host, Module, Opt) ->
-    mod_module_opt(Host, Module, Opt, undefined, fun del_opt/3).
+del_module_opt(HostType, Module, Opt) ->
+    mod_module_opt(HostType, Module, Opt, undefined, fun del_opt/3).
 
--spec mod_module_opt(_Host, _Module, _Opt, _Value, _Modify) -> boolean().
-mod_module_opt(Host, Module, Opt, Value, Modify) ->
-    Key = {Module, Host},
+mod_module_opt(HostType, Module, Opt, Value, Modify) ->
+    Key = {Module, HostType},
     OptsList = ets:lookup(ejabberd_modules, Key),
     case OptsList of
         [] ->
