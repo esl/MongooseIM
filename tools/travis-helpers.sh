2885c9fe2f8bbf09c9caf6b88bf47ac1786f1d36
vkatsuba <v.katsuba.dev@gmail.com>
2021-07-08 16:24:25 +0300

    Remove travis from tools/travis-helpers.sh
    * TRAVIS_BUILD_NUMBER -> GITHUB_RUN_NUMBER
    * TRAVIS_OTP_RELEASE  -> CI_OTP_RELEASE
    * TRAVIS_BRANCH       -> CI_BRANCH
    * TRAVIS_PULL_REQUEST -> CI_PULL_REQUEST

--- tools/travis-helpers.sh
+++ tools/travis-helpers.sh
@@ -1,10 +1,16 @@
 ct_reports_dir() {
-  local BUILD_NO=${TRAVIS_BUILD_NUMBER:-ct_reports}
+  local BUILD_NO=${GITHUB_RUN_NUMBER:-ct_reports}
   local PRESET_NAME=${PRESET:-default}
-  local ERLANG=${TRAVIS_OTP_RELEASE:-default}
+  # @TODO CI:
+  # CI_OTP_RELEASE: ${{ matrix.otp }}
+  local ERLANG=${CI_OTP_RELEASE:-default}
   local CT_REPORTS="${BUILD_NO}/${PRESET_NAME}.${ERLANG}"
-  local BRANCH=${TRAVIS_BRANCH:-master}
-  local PR=${TRAVIS_PULL_REQUEST:-false}
+  # @TODO CI:
+  # CI_BRANCH: $(echo ${GITHUB_REF##*/})
+  local BRANCH=${CI_BRANCH:-master}
+  # @TODO CI:
+  # CI_PULL_REQUEST: ${{ github.event.pull_request.number }}
+  local PR=${CI_PULL_REQUEST:-false}
 
   if [ ${PR} == false ]; then
   	echo "branch/${BRANCH}/${CT_REPORTS}"
