dd638b69306a8a9639124f32a94cd1ae5aaeea42
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2021-07-26 15:54:38 +0200

    Update callbacks in mod_last_rdbms
    Use the new 'server' column as well.

--- src/mod_last_rdbms.erl
+++ src/mod_last_rdbms.erl
@@ -14,69 +14,79 @@
 
 -behaviour(mod_last).
 
--include("mod_last.hrl").
 -include("mongoose.hrl").
 
 %% API
 -export([init/2,
-         get_last/2,
-         count_active_users/2,
-         set_last_info/4,
-         remove_user/2]).
+         get_last/3,
+         count_active_users/3,
+         set_last_info/5,
+         remove_user/3]).
 
--spec init(jid:server(), list()) -> ok.
-init(Host, _Opts) ->
-    prepare_queries(Host),
+-type host_type() :: mongooseim:host_type().
+
+-spec init(host_type(), gen_mod:module_opts()) -> ok.
+init(HostType, _Opts) ->
+    prepare_queries(HostType),
     ok.
 
 %% Prepared query functions
-prepare_queries(Host) ->
-    mongoose_rdbms:prepare(last_select, last, [username],
-                           <<"SELECT seconds, state FROM last WHERE username=?">>),
-    mongoose_rdbms:prepare(last_count_active, last, [seconds],
-                           <<"SELECT COUNT(*) FROM last WHERE seconds > ?">>),
-    mongoose_rdbms:prepare(last_delete, last, [username],
-                           <<"delete from last where username=?">>),
-    rdbms_queries:prepare_upsert(Host, last_upsert, last,
-                                 [<<"username">>, <<"seconds">>, <<"state">>],
+prepare_queries(HostType) ->
+    mongoose_rdbms:prepare(last_select, last, [server, username],
+                           <<"SELECT seconds, state FROM last WHERE server = ? AND username = ?">>),
+    mongoose_rdbms:prepare(last_count_active, last, [server, seconds],
+                           <<"SELECT COUNT(*) FROM last WHERE server = ? AND seconds > ?">>),
+    mongoose_rdbms:prepare(last_delete, last, [server, username],
+                           <<"DELETE FROM last WHERE server = ? AND username = ?">>),
+    rdbms_queries:prepare_upsert(HostType, last_upsert, last,
+                                 [<<"server">>, <<"username">>, <<"seconds">>, <<"state">>],
                                  [<<"seconds">>, <<"state">>],
-                                 [<<"username">>]).
+                                 [<<"server">>, <<"username">>]).
 
-execute_get_last(LServer, LUser) ->
-    mongoose_rdbms:execute_successfully(LServer, last_select, [LUser]).
+-spec execute_get_last(host_type(), jid:lserver(), jid:luser()) -> mongoose_rdbms:query_result().
+execute_get_last(HostType, LServer, LUser) ->
+    mongoose_rdbms:execute_successfully(HostType, last_select, [LServer, LUser]).
 
-execute_count_active_users(LServer, Seconds) ->
-    mongoose_rdbms:execute_successfully(LServer, last_count_active, [Seconds]).
+-spec execute_count_active_users(host_type(), jid:lserver(), mod_last:timestamp()) ->
+          mongoose_rdbms:query_result().
+execute_count_active_users(HostType, LServer, Seconds) ->
+    mongoose_rdbms:execute_successfully(HostType, last_count_active, [LServer, Seconds]).
 
-execute_remove_user(LServer, LUser) ->
-    mongoose_rdbms:execute_successfully(LServer, last_delete, [LUser]).
+-spec execute_remove_user(host_type(), jid:lserver(), jid:luser()) -> mongoose_rdbms:query_result().
+execute_remove_user(HostType, LServer, LUser) ->
+    mongoose_rdbms:execute_successfully(HostType, last_delete, [LServer, LUser]).
 
-execute_upsert_last(Host, LUser, Seconds, State) ->
-    InsertParams = [LUser, Seconds, State],
+-spec execute_upsert_last(host_type(), jid:lserver(), jid:luser(),
+                          mod_last:timestamp(), mod_last:status()) ->
+          mongoose_rdbms:query_result().
+execute_upsert_last(HostType, LServer, LUser, Seconds, State) ->
+    InsertParams = [LServer, LUser, Seconds, State],
     UpdateParams = [Seconds, State],
-    UniqueKeyValues = [LUser],
-    rdbms_queries:execute_upsert(Host, last_upsert, InsertParams, UpdateParams, UniqueKeyValues).
+    UniqueKeyValues = [LServer, LUser],
+    rdbms_queries:execute_upsert(HostType, last_upsert,
+                                 InsertParams, UpdateParams, UniqueKeyValues).
 
 %% API functions
--spec get_last(jid:luser(), jid:lserver()) ->
-    {ok, non_neg_integer(), binary()} | not_found.
-get_last(LUser, LServer) ->
-    Result = execute_get_last(LServer, LUser),
+-spec get_last(host_type(), jid:luser(), jid:lserver()) ->
+    {ok, mod_last:timestamp(), mod_last:status()} | not_found.
+get_last(HostType, LUser, LServer) ->
+    Result = execute_get_last(HostType, LServer, LUser),
     decode_last_result(Result).
 
--spec count_active_users(jid:lserver(), non_neg_integer()) -> non_neg_integer().
-count_active_users(LServer, Seconds) ->
-    Result = execute_count_active_users(LServer, Seconds),
+-spec count_active_users(host_type(), jid:lserver(), mod_last:timestamp()) -> non_neg_integer().
+count_active_users(HostType, LServer, Seconds) ->
+    Result = execute_count_active_users(HostType, LServer, Seconds),
     mongoose_rdbms:selected_to_integer(Result).
 
--spec set_last_info(jid:luser(), jid:lserver(),
-                    non_neg_integer(), binary()) -> ok | {error, term()}.
-set_last_info(LUser, LServer, Seconds, State) ->
-    wrap_rdbms_result(execute_upsert_last(LServer, LUser, Seconds, State)).
+-spec set_last_info(host_type(), jid:luser(), jid:lserver(),
+                    mod_last:timestamp(), mod_last:status()) ->
+          ok | {error, term()}.
+set_last_info(HostType, LUser, LServer, Seconds, State) ->
+    wrap_rdbms_result(execute_upsert_last(HostType, LServer, LUser, Seconds, State)).
 
--spec remove_user(jid:luser(), jid:lserver()) -> ok | {error, term()}.
-remove_user(LUser, LServer) ->
-    wrap_rdbms_result(execute_remove_user(LServer, LUser)).
+-spec remove_user(host_type(), jid:luser(), jid:lserver()) -> ok | {error, term()}.
+remove_user(HostType, LUser, LServer) ->
+    wrap_rdbms_result(execute_remove_user(HostType, LServer, LUser)).
 
 %% Helper functions
 decode_last_result({selected, []}) ->
