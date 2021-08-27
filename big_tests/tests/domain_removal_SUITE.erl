d13628235185ffb16129efb782aa1159f7dba78c
Mikhail Uvarov <arcusfelis@gmail.com>
2021-08-27 16:18:51 +0200

    Import host_type into domain_removal_SUITE

--- big_tests/tests/domain_removal_SUITE.erl
+++ big_tests/tests/domain_removal_SUITE.erl
@@ -18,6 +18,7 @@
          private_removal/1]).
 
 -import(distributed_helper, [mim/0, rpc/4, subhost_pattern/1]).
+-import(domain_helper, [host_type/0]).
 
 -include("mam_helper.hrl").
 -include_lib("escalus/include/escalus.hrl").
@@ -43,9 +44,6 @@ groups() ->
 
 domain() ->
     ct:get_config({hosts, mim, domain}).
-
-host_type() ->
-    ct:get_config({hosts, mim, host_type}).
 
 %%%===================================================================
 %%% Overall setup/teardown
