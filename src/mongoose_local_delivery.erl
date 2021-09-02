79dcea61f4a4e7f0d1c25ed2df6b72091b887220
Nelson Vides <nelson.vides@erlang-solutions.com>
2021-09-02 15:36:46 +0200

    ensure host_type and server for the sender are preserved

--- src/mongoose_local_delivery.erl
+++ src/mongoose_local_delivery.erl
@@ -7,7 +7,6 @@
 -module(mongoose_local_delivery).
 -author('bartlomiej.gorny@erlang-solutions.com').
 
--include("mongoose.hrl").
 -include("jlib.hrl").
 
 %% API
@@ -27,10 +26,11 @@ do_route(OrigFrom, OrigTo, OrigAcc, OrigPacket, Handler) ->
             %% This can happen e.g. for external components
             %% No handlers can be registered for filter_local_packet in this case
             Acc = mongoose_acc:strip(#{lserver => LDstDomain,
-                                        from_jid => OrigFrom,
-                                        to_jid => OrigTo,
-                                        element => OrigPacket}, OrigAcc),
-            mongoose_packet_handler:process(Handler, Acc, OrigFrom, OrigTo, OrigPacket);
+                                       from_jid => OrigFrom,
+                                       to_jid => OrigTo,
+                                       element => OrigPacket}, OrigAcc),
+            Acc1 = mongoose_packet_handler:process(Handler, Acc, OrigFrom, OrigTo, OrigPacket),
+            restore_acc_fields(OrigAcc, Acc1);
         {ok, HostType} ->
             Acc0 = mongoose_acc:strip(#{lserver => LDstDomain,
                                         host_type => HostType,
@@ -42,10 +42,16 @@ do_route(OrigFrom, OrigTo, OrigAcc, OrigPacket, Handler) ->
                     Acc1 = mongoose_acc:update_stanza(#{from_jid => From,
                                                         to_jid => To,
                                                         element => Packet}, Acc),
-                    mongoose_packet_handler:process(Handler, Acc1, From, To, Packet);
+                    Acc2 = mongoose_packet_handler:process(Handler, Acc1, From, To, Packet),
+                    restore_acc_fields(OrigAcc, Acc2);
                 drop ->
                     mongoose_hooks:xmpp_stanza_dropped(Acc0, OrigFrom,
                                                        OrigTo, OrigPacket),
                     Acc0
             end
     end.
+
+restore_acc_fields(OrigAcc, Acc) ->
+    HostType = mongoose_acc:host_type(OrigAcc),
+    LServer = mongoose_acc:lserver(OrigAcc),
+    Acc#{host_type := HostType, lserver := LServer}.
