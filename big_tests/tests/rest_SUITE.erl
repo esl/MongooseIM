078bcf0b9e5d8dd08bbc101a3ca80f9726a47bca
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2021-09-03 10:15:55 +0200

    Prepare rest_SUITE for dynamic domains

--- big_tests/tests/rest_SUITE.erl
+++ big_tests/tests/rest_SUITE.erl
@@ -27,14 +27,12 @@
          assert_notinlist/2,
          decode_maplist/1,
          gett/2,
-         post/2,
-         putt/2,
-         delete/1,
          gett/3,
          post/3,
          putt/3,
          delete/2]
     ).
+-import(domain_helper, [host_type/0]).
 
 -define(PRT(X, Y), ct:pal("~p: ~p", [X, Y])).
 -define(OK, {<<"200">>, <<"OK">>}).
@@ -101,17 +99,14 @@ suite() ->
 %% Init & teardown
 %%--------------------------------------------------------------------
 
-host() ->
-    ct:get_config({hosts, mim, domain}).
-
 init_per_suite(Config) ->
-    Config1 = rest_helper:maybe_enable_mam(mam_helper:backend(), host(), Config),
+    Config1 = rest_helper:maybe_enable_mam(mam_helper:backend(), host_type(), Config),
     Config2 = ejabberd_node_utils:init(Config1),
     escalus:init_per_suite(Config2).
 
 end_per_suite(Config) ->
     escalus_fresh:clean(),
-    rest_helper:maybe_disable_mam(mam_helper:backend(), host()),
+    rest_helper:maybe_disable_mam(mam_helper:backend(), host_type()),
     escalus:end_per_suite(Config).
 
 init_per_group(auth, Config) ->
@@ -131,14 +126,14 @@ end_per_group(_GroupName, Config) ->
 init_per_testcase(types_are_checked_separately_for_args_and_return = CaseName, Config) ->
     {Mod, Code} = rpc(dynamic_compile, from_string, [custom_module_code()]),
     rpc(code, load_binary, [Mod, "mod_commands_test.erl", Code]),
-    rpc(gen_mod, start_module, [<<"localhost">>, mod_commands_test, []]),
+    rpc(gen_mod, start_module, [host_type(), mod_commands_test, []]),
     escalus:init_per_testcase(CaseName, Config);
 init_per_testcase(CaseName, Config) ->
     MAMTestCases = [messages_are_archived, messages_can_be_paginated],
     rest_helper:maybe_skip_mam_test_cases(CaseName, MAMTestCases, Config).
 
 end_per_testcase(types_are_checked_separately_for_args_and_return = CaseName, Config) ->
-    rpc(gen_mod, stop_module, [<<"localhost">>, mod_commands_test]),
+    rpc(gen_mod, stop_module, [host_type(), mod_commands_test]),
     escalus:end_per_testcase(CaseName, Config);
 end_per_testcase(CaseName, Config) ->
     escalus:end_per_testcase(CaseName, Config).
@@ -148,11 +143,12 @@ rpc(M, F, A) ->
 
 custom_module_code() ->
     "-module(mod_commands_test).
-     -export([start/0, stop/0, start/2, stop/1, test_arg/1, test_return/1]).
+     -export([start/0, stop/0, start/2, stop/1, test_arg/1, test_return/1, supported_features/0]).
      start() -> mongoose_commands:register(commands()).
      stop() -> mongoose_commands:unregister(commands()).
      start(_,_) -> start().
      stop(_) -> stop().
+     supported_features() -> [dynamic_domains].
      commands() ->
          [
           [
@@ -212,31 +208,31 @@ non_existent_command_returns404(_C) ->
 
 user_can_be_registered_and_removed(_Config) ->
     % list users
-    {?OK, Lusers} = gett(admin, <<"/users/localhost">>),
+    {?OK, Lusers} = gett(admin, path("users")),
     Domain = domain(),
     assert_inlist(<<"alice@", Domain/binary>>, Lusers),
     % create user
     CrUser = #{username => <<"mike">>, password => <<"nicniema">>},
-    {?CREATED, _} = post(admin, <<"/users/localhost">>, CrUser),
-    {?OK, Lusers1} = gett(admin, <<"/users/localhost">>),
+    {?CREATED, _} = post(admin, path("users"), CrUser),
+    {?OK, Lusers1} = gett(admin, path("users")),
     assert_inlist(<<"mike@", Domain/binary>>, Lusers1),
     % try to create the same user
-    {?FORBIDDEN, _} = post(admin, <<"/users/localhost">>, CrUser),
+    {?FORBIDDEN, _} = post(admin, path("users"), CrUser),
     % delete user
-    {?NOCONTENT, _} = delete(admin, <<"/users/localhost/mike">>),
-    {?OK, Lusers2} = gett(admin, <<"/users/localhost">>),
+    {?NOCONTENT, _} = delete(admin, path("users", ["mike"])),
+    {?OK, Lusers2} = gett(admin, path("users")),
     assert_notinlist(<<"mike@", Domain/binary>>, Lusers2),
     % invalid jid
     CrBadUser = #{username => <<"m@ke">>, password => <<"nicniema">>},
-    {?BAD_REQUEST, <<"Invalid jid", _/binary>>} = post(admin, <<"/users/localhost">>, CrBadUser),
-    {?BAD_REQUEST, <<"Invalid jid", _/binary>>} = delete(admin, <<"/users/localhost/@mike">>),
-%%    {?FORBIDDEN, _} = delete(admin, <<"/users/localhost/mike">>), % he's already gone, but we
+    {?BAD_REQUEST, <<"Invalid jid", _/binary>>} = post(admin, path("users"), CrBadUser),
+    {?BAD_REQUEST, <<"Invalid jid", _/binary>>} = delete(admin, path("users", ["@mike"])),
+%%    {?FORBIDDEN, _} = delete(admin, path("users", ["mike"])), % he's already gone, but we
 %%    can't test it because ejabberd_auth_internal:remove_user/2 always returns ok, grrrr
     ok.
 
 sessions_are_listed(_) ->
     % no session
-    {?OK, Sessions} = gett(admin, "/sessions/localhost"),
+    {?OK, Sessions} = gett(admin, path("sessions")),
     true = is_list(Sessions).
 
 session_can_be_kicked(Config) ->
@@ -246,13 +242,13 @@ session_can_be_kicked(Config) ->
         AliceSessionPath = <<"/sessions/", (escalus_client:server(Alice))/binary,
                              "/", (escalus_client:username(Alice))/binary,
                              "/", (escalus_client:resource(Alice))/binary>>,
-        {?OK, Sessions1} = gett(admin, "/sessions/localhost"),
+        {?OK, Sessions1} = gett(admin, path("sessions")),
         assert_inlist(AliceJid, Sessions1),
         % kick alice
         {?NOCONTENT, _} = delete(admin, AliceSessionPath),
         escalus:wait_for_stanza(Alice),
         true = escalus_connection:wait_for_close(Alice, timer:seconds(1)),
-        {?OK, Sessions2} = gett(admin, "/sessions/localhost"),
+        {?OK, Sessions2} = gett(admin, path("sessions")),
         assert_notinlist(AliceJid, Sessions2),
         {?NOT_FOUND, <<"no active session">>} = delete(admin, AliceSessionPath),
         ok
@@ -360,7 +356,7 @@ password_can_be_changed(Config) ->
     end),
     % we change password
     NewPass = <<"niemakrolika">>,
-    {?NOCONTENT, _} = putt(admin, "/users/localhost/bob",
+    {?NOCONTENT, _} = putt(admin, path("users", ["bob"]),
                            #{newpass => NewPass}),
     % he logs with his alternative password
     ConfigWithBobsAltPass = escalus_users:update_userspec(Config, bob, password, NewPass),
@@ -374,17 +370,17 @@ password_can_be_changed(Config) ->
         ok
     end,
     % we change it back
-    {?NOCONTENT, _} = putt(admin, "/users/localhost/bob",
+    {?NOCONTENT, _} = putt(admin, path("users", ["bob"]),
                            #{newpass => <<"makrolika">>}),
     % now he logs again with the regular one
     escalus:story(Config, [{bob, 1}], fun(#client{} = _Bob) ->
         just_dont_do_anything
     end),
     % test invalid calls
-    Res1 = putt(admin, "/users/localhost/bob",
+    Res1 = putt(admin, path("users", ["bob"]),
                            #{newpass => <<>>}),
     {?BAD_REQUEST, <<"empty password">>} = Res1,
-    Res2 = putt(admin, "/users/localhost/b@b",
+    Res2 = putt(admin, path("users", ["b@b"]),
                 #{newpass => NewPass}),
     {?BAD_REQUEST, <<"invalid jid">>} = Res2,
     ok.
@@ -658,9 +654,9 @@ stop_start_command_module(_) ->
     %% described above we test both transition from `started' to
     %% `stopped' and from `stopped' to `started'.
     {?OK, _} = gett(admin, <<"/commands">>),
-    {ok, _Opts} = dynamic_modules:stop(host(), mod_commands),
+    {ok, _Opts} = dynamic_modules:stop(host_type(), mod_commands),
     {?NOT_FOUND, _} = gett(admin, <<"/commands">>),
-    {ok, _} = dynamic_modules:start(host(), mod_commands, []),
+    {ok, _} = dynamic_modules:start(host_type(), mod_commands, []),
     timer:sleep(200), %% give the server some time to build the paths again
     {?OK, _} = gett(admin, <<"/commands">>).
 
@@ -668,9 +664,6 @@ to_list(V) when is_binary(V) ->
     binary_to_list(V);
 to_list(V) when is_list(V) ->
     V.
-
-domain() ->
-    ct:get_config({hosts, mim, domain}).
 
 add_sample_contact(Bob, Alice) ->
     escalus:send(Bob, escalus_stanza:roster_add_contact(Alice,
@@ -709,3 +702,13 @@ does_push_match(Iq, ExpectedSub, ExpectedAsk) ->
     ESub = atom_to_binary(ExpectedSub, latin1),
     EAsk = atom_to_binary(ExpectedAsk, latin1),
     {Subs, Ask} == {ESub, EAsk}.
+
+path(Category) ->
+    path(Category, []).
+
+path(Category, Items) ->
+    DomainStr = binary_to_list(domain()),
+    string:join(["", Category, DomainStr | Items], "/").
+
+domain() ->
+    ct:get_config({hosts, mim, domain}).
