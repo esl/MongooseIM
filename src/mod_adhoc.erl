93283e12f968c16fc1f4e974e463990c37bf718f
Przemysław Wojtasik <przemyslaw.wojtasik@erlang-solutions.com>
2021-09-09 13:52:05 +0200

    Add support for dynamic domains in mod_adhoc

--- src/mod_adhoc.erl
+++ src/mod_adhoc.erl
@@ -31,11 +31,15 @@
 -type command_hook_acc() :: {error, exml:element()} | exml:element() | ignore | empty.
 -export_type([command_hook_acc/0]).
 
+%% Gen_mod callbacks
 -export([start/2,
          stop/1,
          config_spec/0,
-         process_local_iq/4,
-         process_sm_iq/4,
+         supported_features/0]).
+
+%% IQ and hook handlers
+-export([process_local_iq/5,
+         process_sm_iq/5,
          disco_local_items/1,
          disco_local_identity/1,
          disco_local_features/1,
@@ -46,36 +50,41 @@
 
 -ignore_xref([disco_local_features/1, disco_local_identity/1, disco_local_items/1,
               disco_sm_features/1, disco_sm_identity/1, disco_sm_items/1,
-              ping_command/4, process_local_iq/4, process_sm_iq/4]).
+              ping_command/4, process_local_iq/5, process_sm_iq/5]).
 
 -include("mongoose.hrl").
 -include("jlib.hrl").
 -include("adhoc.hrl").
 -include("mongoose_config_spec.hrl").
 
-start(Host, Opts) ->
+start(HostType, Opts) ->
     IQDisc = gen_mod:get_opt(iqdisc, Opts, one_queue),
 
-    gen_iq_handler:add_iq_handler(ejabberd_local, Host, ?NS_COMMANDS,
-                                  ?MODULE, process_local_iq, IQDisc),
-    gen_iq_handler:add_iq_handler(ejabberd_sm, Host, ?NS_COMMANDS,
-                                  ?MODULE, process_sm_iq, IQDisc),
-    ejabberd_hooks:add(hooks(Host)).
+    [gen_iq_handler:add_iq_handler_for_domain(HostType, ?NS_COMMANDS, Component, Fn, #{}, IQDisc) ||
+        {Component, Fn} <- iq_handlers()],
+    ejabberd_hooks:add(hooks(HostType)).
 
-stop(Host) ->
-    ejabberd_hooks:delete(hooks(Host)),
+stop(HostType) ->
+    ejabberd_hooks:delete(hooks(HostType)),
+    [gen_iq_handler:remove_iq_handler_for_domain(HostType, ?NS_COMMANDS, Component) ||
+        {Component, _Fn} <- iq_handlers()].
 
-    gen_iq_handler:remove_iq_handler(ejabberd_sm, Host, ?NS_COMMANDS),
-    gen_iq_handler:remove_iq_handler(ejabberd_local, Host, ?NS_COMMANDS).
+iq_handlers() ->
+    [{ejabberd_local, fun ?MODULE:process_local_iq/5},
+     {ejabberd_sm, fun ?MODULE:process_sm_iq/5}].
 
-hooks(Host) ->
-    [{disco_local_identity, Host, ?MODULE, disco_local_identity, 99},
-     {disco_local_features, Host, ?MODULE, disco_local_features, 99},
-     {disco_local_items, Host, ?MODULE, disco_local_items, 99},
-     {disco_sm_identity, Host, ?MODULE, disco_sm_identity, 99},
-     {disco_sm_features, Host, ?MODULE, disco_sm_features, 99},
-     {disco_sm_items, Host, ?MODULE, disco_sm_items, 99},
-     {adhoc_local_commands, Host, ?MODULE, ping_command, 100}].
+hooks(HostType) ->
+    [{disco_local_identity, HostType, ?MODULE, disco_local_identity, 99},
+     {disco_local_features, HostType, ?MODULE, disco_local_features, 99},
+     {disco_local_items, HostType, ?MODULE, disco_local_items, 99},
+     {disco_sm_identity, HostType, ?MODULE, disco_sm_identity, 99},
+     {disco_sm_features, HostType, ?MODULE, disco_sm_features, 99},
+     {disco_sm_items, HostType, ?MODULE, disco_sm_items, 99},
+     {adhoc_local_commands, HostType, ?MODULE, ping_command, 100}].
+
+%%%
+%%% config_spec
+%%%
 
 -spec config_spec() -> mongoose_config_spec:config_section().
 config_spec() ->
@@ -84,11 +93,56 @@ config_spec() ->
                  <<"iqdisc">> => mongoose_config_spec:iqdisc()}
       }.
 
-%%-------------------------------------------------------------------------
+-spec supported_features() -> [atom()].
+supported_features() -> [dynamic_domains].
+
+%%%
+%%% IQ handlers
+%%%
+
+-spec process_local_iq(mongoose_acc:t(), jid:jid(), jid:jid(), jlib:iq(), map())
+        -> {mongoose_acc:t(), ignore | jlib:iq()}.
+process_local_iq(Acc, From, To, IQ, _Extra) ->
+    {Acc, process_adhoc_request(Acc, From, To, IQ, adhoc_local_commands)}.
+
+-spec process_sm_iq(mongoose_acc:t(), jid:jid(), jid:jid(), jlib:iq(), map()) ->
+    {mongoose_acc:t(), ignore | jlib:iq()}.
+process_sm_iq(Acc, From, To, IQ, _Extra) ->
+    {Acc, process_adhoc_request(Acc, From, To, IQ, adhoc_sm_commands)}.
+
+-spec process_adhoc_request(mongoose_acc:t(), jid:jid(), jid:jid(), jlib:iq(),
+        Hook :: atom()) -> ignore | jlib:iq().
+process_adhoc_request(Acc, From, To, #iq{sub_el = SubEl} = IQ, Hook) ->
+    ?LOG_DEBUG(#{what => adhoc_parse_request, iq => IQ, hook => Hook}),
+    case adhoc:parse_request(IQ) of
+        {error, Error} ->
+            IQ#iq{type = error, sub_el = [SubEl, Error]};
+        #adhoc_request{} = AdhocRequest ->
+            HostType = mongoose_acc:host_type(Acc),
+            case run_request_hook(Hook, HostType, From, To, AdhocRequest) of
+                ignore ->
+                    ignore;
+                empty ->
+                    IQ#iq{type = error, sub_el = [SubEl, mongoose_xmpp_errors:item_not_found()]};
+                {error, Error} ->
+                    IQ#iq{type = error, sub_el = [SubEl, Error]};
+                Command ->
+                    IQ#iq{type = result, sub_el = [Command]}
+            end
+    end.
+
+run_request_hook(adhoc_local_commands, HostType, From, To, AdhocRequest) ->
+    mongoose_hooks:adhoc_local_commands(HostType, From, To, AdhocRequest);
+run_request_hook(adhoc_sm_commands, HostType, From, To, AdhocRequest) ->
+    mongoose_hooks:adhoc_sm_commands(HostType, From, To, AdhocRequest).
+
+%%%
+%%% Hooks handlers
+%%%
 
 -spec disco_local_items(mongoose_disco:item_acc()) -> mongoose_disco:item_acc().
-disco_local_items(Acc = #{to_jid := #jid{lserver = LServer}, node := <<>>, lang := Lang}) ->
-    Items = case are_commands_visible(LServer) of
+disco_local_items(Acc = #{host_type := HostType, to_jid := #jid{lserver = LServer}, node := <<>>, lang := Lang}) ->
+    Items = case are_commands_visible(HostType) of
                 false ->
                     [];
                 _ ->
@@ -106,8 +160,8 @@ disco_local_items(Acc) ->
 %%-------------------------------------------------------------------------
 
 -spec disco_sm_items(mongoose_disco:item_acc()) -> mongoose_disco:item_acc().
-disco_sm_items(Acc = #{to_jid := #jid{lserver = LServer} = To, node := <<>>, lang := Lang}) ->
-    Items = case are_commands_visible(LServer) of
+disco_sm_items(Acc = #{host_type := HostType, to_jid := To, node := <<>>, lang := Lang}) ->
+    Items = case are_commands_visible(HostType) of
                 false ->
                     [];
                 _ ->
@@ -117,8 +171,8 @@ disco_sm_items(Acc = #{to_jid := #jid{lserver = LServer} = To, node := <<>>, lan
 disco_sm_items(Acc) ->
     Acc.
 
-are_commands_visible(LServer) ->
-    gen_mod:get_module_opt(LServer, ?MODULE, report_commands_node, false).
+are_commands_visible(HostType) ->
+    gen_mod:get_module_opt(HostType, ?MODULE, report_commands_node, false).
 
 item(LServer, Node, Name, Lang) ->
     #{jid => LServer, node => Node, name => translate:translate(Lang, Name)}.
@@ -179,43 +233,6 @@ disco_sm_features(Acc) ->
     Acc.
 
 %%-------------------------------------------------------------------------
-
--spec process_local_iq(jid:jid(), jid:jid(), mongoose_acc:t(), jlib:iq()) ->
-    {mongoose_acc:t(), ignore | jlib:iq()}.
-process_local_iq(From, To, Acc, IQ) ->
-    {Acc, process_adhoc_request(From, To, IQ, adhoc_local_commands)}.
-
--spec process_sm_iq(jid:jid(), jid:jid(), mongoose_acc:t(), jlib:iq()) ->
-    {mongoose_acc:t(), ignore | jlib:iq()}.
-process_sm_iq(From, To, Acc, IQ) ->
-    {Acc, process_adhoc_request(From, To, IQ, adhoc_sm_commands)}.
-
--spec process_adhoc_request(jid:jid(), jid:jid(), jlib:iq(),
-        Hook :: atom()) -> ignore | jlib:iq().
-process_adhoc_request(From, To, #iq{sub_el = SubEl} = IQ, Hook) ->
-    ?LOG_DEBUG(#{what => adhoc_parse_request, iq => IQ, hook => Hook}),
-    case adhoc:parse_request(IQ) of
-        {error, Error} ->
-            IQ#iq{type = error, sub_el = [SubEl, Error]};
-        #adhoc_request{} = AdhocRequest ->
-            Host = To#jid.lserver,
-            case run_request_hook(Hook, Host, From, To, AdhocRequest) of
-                ignore ->
-                    ignore;
-                empty ->
-                    IQ#iq{type = error, sub_el = [SubEl, mongoose_xmpp_errors:item_not_found()]};
-                {error, Error} ->
-                    IQ#iq{type = error, sub_el = [SubEl, Error]};
-                Command ->
-                    IQ#iq{type = result, sub_el = [Command]}
-            end
-    end.
-
-run_request_hook(adhoc_local_commands, Host, From, To, AdhocRequest) ->
-    mongoose_hooks:adhoc_local_commands(Host, From, To, AdhocRequest);
-run_request_hook(adhoc_sm_commands, Host, From, To, AdhocRequest) ->
-    mongoose_hooks:adhoc_sm_commands(Host, From, To, AdhocRequest).
-
 
 -spec ping_command(Acc :: command_hook_acc(),
                    From :: jid:jid(),
