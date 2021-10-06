a30caa3e54def59cd7e2166570405cb6e5ed9dc0
Mikhail Uvarov <arcusfelis@gmail.com>
2021-10-06 11:42:36 +0200

    Add remove_domain callback

--- src/mod_private_backend.erl
+++ src/mod_private_backend.erl
@@ -3,7 +3,8 @@
          multi_set_data/4,
          multi_get_data/4,
          get_all_nss/3,
-         remove_user/3]).
+         remove_user/3,
+         remove_domain/2]).
 
 -author('arcusfelis@gmail.com').
 -behaviour(mod_private).
@@ -32,4 +33,8 @@ get_all_nss(HostType, LUser, LServer) ->
 
 remove_user(HostType, LUser, LServer) ->
     Args = [HostType, LUser, LServer],
+    backend_module:call(HostType, ?MAIN_MODULE, ?FUNCTION_NAME, Args).
+
+remove_domain(HostType, LServer) ->
+    Args = [HostType, LServer],
     backend_module:call(HostType, ?MAIN_MODULE, ?FUNCTION_NAME, Args).
