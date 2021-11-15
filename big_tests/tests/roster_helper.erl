baf08cdf3f32d54b1ce7a39311459f1d2ee0d8c5
Przemysław Wojtasik <przemyslaw.wojtasik@erlang-solutions.com>
2021-11-15 14:43:56 +0100

    Do not use gen_mod:set_module_opt(s) in roster_helper

--- big_tests/tests/roster_helper.erl
+++ big_tests/tests/roster_helper.erl
@@ -1,23 +1,12 @@
 -module(roster_helper).
--export([set_versioning/3, restore_versioning/1]).
+-export([set_versioning/3]).
 
 -import(distributed_helper, [mim/0, rpc/4]).
 -import(domain_helper, [host_type/0]).
 
 -spec set_versioning(boolean(), boolean(), escalus_config:config()) -> escalus_config:config(). 
 set_versioning(Versioning, VersionStore, Config) ->
-    RosterVersioning = rpc(mim(), gen_mod, get_module_opt,
-                           [host_type(), mod_roster, versioning, false]),
-    RosterVersionOnDb = rpc(mim(), gen_mod, get_module_opt,
-                            [host_type(), mod_roster, store_current_id, false]),
-    rpc(mim(), gen_mod, set_module_opt, [host_type(), mod_roster, versioning, Versioning]),
-    rpc(mim(), gen_mod, set_module_opt, [host_type(), mod_roster, store_current_id, VersionStore]),
-    [{versioning, RosterVersioning},
-     {store_current_id, RosterVersionOnDb} | Config].
-
--spec restore_versioning(escalus_config:config()) -> escalus_config:config().
-restore_versioning(Config) ->
-    RosterVersioning = proplists:get_value(versioning, Config),
-    RosterVersionOnDb = proplists:get_value(store_current_id, Config),
-    rpc(mim(), gen_mod, get_module_opt, [host_type(), mod_roster, versioning, RosterVersioning]),
-    rpc(mim(), gen_mod, get_module_opt, [host_type(), mod_roster, store_current_id, RosterVersionOnDb]).
+    Opts = dynamic_modules:get_saved_config(host_type(), mod_roster, Config),
+    dynamic_modules:ensure_modules(host_type(), [{mod_roster, [{versioning, Versioning},
+                                                               {store_current_id, VersionStore} | Opts]}]),
+    Config.
