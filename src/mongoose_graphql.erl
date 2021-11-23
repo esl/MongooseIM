e8f0d7bb4c9a3318f8c51a52fef76520ac8261be
Przemysław Wojtasik <przemyslaw.wojtasik@erlang-solutions.com>
2021-11-23 12:09:05 +0100

    Add support for multi file schema

--- src/mongoose_graphql.erl
+++ src/mongoose_graphql.erl
@@ -4,13 +4,15 @@
 
 -ignore_xref([execute/1, execute/3]).
 
--define(SCHEMA_PATH, "graphql/api_schema.gql").
+-define(SCHEMA_PATH_PATTERN, "graphql/*.gql").
 
 -spec init() -> ok.
 init() ->
     graphql_schema:reset(),
-    PrivDir = code:priv_dir(mongooseim),
-    {ok, SchemaData} = file:read_file( filename:join(PrivDir, ?SCHEMA_PATH)), 
+    Pattern =
+        filename:join(
+            code:priv_dir(mongooseim), ?SCHEMA_PATH_PATTERN),
+    {ok, SchemaData} = load_multiple_file_schema(Pattern),
     Mapping = mapping_rules(),
     ok = graphql:load_schema(Mapping, SchemaData),
     ok = setup_root(),
@@ -21,15 +23,17 @@ init() ->
 execute(Doc) ->
     execute(<<>>, admin, Doc).
 
--spec execute(binary(), mongoose_graphql_permission:role(), binary()) -> {ok, map()} | {error, term()}.
+-spec execute(binary(), mongoose_graphql_permission:role(), binary()) ->
+                 {ok, map()} | {error, term()}.
 execute(OpName, Role, Doc) ->
     case graphql:parse(Doc) of
         {ok, Ast} ->
             try
-                {ok, #{ast := Ast2 }} = graphql:type_check(Ast),
+                {ok, #{ast := Ast2}} = graphql:type_check(Ast),
                 ok = graphql:validate(Ast2),
-                Ctx = #{params => #{}, operation_name => OpName,
-                                    role => Role},
+                Ctx = #{params => #{},
+                        operation_name => OpName,
+                        role => Role},
                 {ok, graphql:execute(Ctx, Ast2)}
             catch
                 throw:{error, Err} ->
@@ -50,14 +54,25 @@ mapping_rules() ->
         'UserQuery' => mongoose_graphql_user_query,
         'UserMutation' => mongoose_graphql_user_mutation,
         'Domain' => mongoose_graphql_domain,
-        'default' => mongoose_graphql_default
-        }
-    }.
+        default => mongoose_graphql_default
+       }
+     }.
 
 setup_root() ->
-    Root = {root,
-            #{ query => 'Query',
-               mutation => 'Mutation'
-            }},
+    Root = {root, #{query => 'Query', mutation => 'Mutation'}},
     ok = graphql:insert_root(Root),
     ok.
+
+load_multiple_file_schema(Pattern) ->
+    Paths = filelib:wildcard(Pattern),
+    try
+        SchemaData = [read_schema_file(P) || P <- Paths],
+        {ok, lists:flatten(SchemaData)}
+    catch
+        _:_ ->
+            {error, cannot_load}
+    end.
+
+read_schema_file(Path) ->
+    {ok, Data} = file:read_file(Path),
+    binary_to_list(Data).
