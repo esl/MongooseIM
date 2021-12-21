e079148763524b6d963c7cfdf8e734cd1efc4705
Mikhail Uvarov <arcusfelis@gmail.com>
2021-12-21 15:04:46 +0100

    Log dropped messages
    Use unique name for each BatchSize

--- src/mam/mod_mam_rdbms_arch_async.erl
+++ src/mam/mod_mam_rdbms_arch_async.erl
@@ -64,17 +64,25 @@ supported_features() ->
 -spec start_pool(mongooseim:host_type(), {writer_type(), gen_mod:module_opts()}) ->
     supervisor:startchild_ret().
 start_pool(HostType, {Type, Opts}) ->
-    Opts1 = extend_opts(Type, Opts),
-    prepare_insert_queries(Type, Opts1),
+    {Opts1, Extra} = extend_opts(Type, Opts),
+    prepare_insert_queries(Type, Extra),
     ensure_metrics(Type, HostType),
     register_hooks(Type, HostType),
     start_pool(Type, HostType, Opts1).
 
 extend_opts(Type, Opts) ->
     Merge = maps:merge(defaults(), maps:from_list(Opts)),
-    Opts1 = maps:to_list(Merge),
-    Opts2 = gen_mod:set_opt(flush_extra, Opts1, Merge),
-    add_callback(Type, Opts2).
+    Extra = add_batch_name(Type, Merge),
+    Opts1 = maps:to_list(Extra),
+    Opts2 = gen_mod:set_opt(flush_extra, Opts1, Extra),
+    {add_callback(Type, Opts2), Extra}.
+
+%% Put batch_size into a statement name, so we could survive the module restarts
+%% with different batch sizes
+add_batch_name(pm, #{batch_size := MaxSize} = Opts) ->
+    Opts#{batch_name => multi_name(insert_mam_messages, MaxSize)};
+add_batch_name(muc, #{batch_size := MaxSize} = Opts) ->
+    Opts#{batch_name => multi_name(insert_mam_muc_messages, MaxSize)}.
 
 add_callback(pm, Opts) ->
     gen_mod:set_opt(flush_callback, Opts, fun ?MODULE:flush_pm/2);
@@ -86,14 +94,15 @@ defaults() ->
       batch_size => 30,
       pool_size => 4 * erlang:system_info(schedulers_online)}.
 
-prepare_insert_queries(pm, Opts) ->
-    MaxSize = gen_mod:get_opt(batch_size, Opts, 30),
+prepare_insert_queries(pm, #{batch_size := MaxSize, batch_name := BatchName}) ->
     mod_mam_rdbms_arch:prepare_insert(insert_mam_message, 1),
-    mod_mam_rdbms_arch:prepare_insert(insert_mam_messages, MaxSize);
-prepare_insert_queries(muc, Opts) ->
-    MaxSize = gen_mod:get_opt(batch_size, Opts, 30),
+    mod_mam_rdbms_arch:prepare_insert(BatchName, MaxSize);
+prepare_insert_queries(muc, #{batch_size := MaxSize, batch_name := BatchName}) ->
     mod_mam_muc_rdbms_arch:prepare_insert(insert_mam_muc_message, 1),
-    mod_mam_muc_rdbms_arch:prepare_insert(insert_mam_muc_messages, MaxSize).
+    mod_mam_muc_rdbms_arch:prepare_insert(BatchName, MaxSize).
+
+multi_name(Name, Times) ->
+    list_to_atom(atom_to_list(Name) ++ integer_to_list(Times)).
 
 ensure_metrics(pm, HostType) ->
     mongoose_metrics:ensure_metric(HostType, ?PM_PER_MESSAGE_FLUSH_TIME, histogram),
@@ -140,13 +149,13 @@ flush_muc(Acc, Extra = #{host_type := HostType}) ->
     Result.
 
 %% mam workers callbacks
-do_flush_pm(Acc, #{host_type := HostType, batch_size := MaxSize}) ->
+do_flush_pm(Acc, #{host_type := HostType, batch_size := MaxSize, batch_name := BatchName}) ->
     MessageCount = length(Acc),
     Rows = [mod_mam_rdbms_arch:prepare_message(HostType, Params) || Params <- Acc],
     InsertResult =
         case MessageCount of
             MaxSize ->
-                mongoose_rdbms:execute(HostType, insert_mam_messages, lists:append(Rows));
+                mongoose_rdbms:execute(HostType, BatchName, lists:append(Rows));
             OtherSize ->
                 Results = [mongoose_rdbms:execute(HostType, insert_mam_message, Row) || Row <- Rows],
                 case lists:keyfind(error, 1, Results) of
@@ -158,6 +167,9 @@ do_flush_pm(Acc, #{host_type := HostType, batch_size := MaxSize}) ->
         {updated, _Count} -> ok;
         {error, Reason} ->
             mongoose_metrics:update(HostType, modMamDropped, MessageCount),
+            [?LOG_ERROR(#{what => archive_message_dropped,
+                          sensitive => true, params => Params})
+             || Params <- Acc],
             ?LOG_ERROR(#{what => archive_message_failed,
                          text => <<"archive_message query failed">>,
                          message_count => MessageCount, reason => Reason}),
@@ -167,13 +179,13 @@ do_flush_pm(Acc, #{host_type := HostType, batch_size := MaxSize}) ->
     mongoose_hooks:mam_flush_messages(HostType, MessageCount),
     ok.
 
-do_flush_muc(Acc, #{host_type := HostType, batch_size := MaxSize}) ->
+do_flush_muc(Acc, #{host_type := HostType, batch_size := MaxSize, batch_name := BatchName}) ->
     MessageCount = length(Acc),
     Rows = [mod_mam_muc_rdbms_arch:prepare_message(HostType, Params) || Params <- Acc],
     InsertResult =
         case MessageCount of
             MaxSize ->
-                mongoose_rdbms:execute(HostType, insert_mam_muc_messages, lists:append(Rows));
+                mongoose_rdbms:execute(HostType, BatchName, lists:append(Rows));
             OtherSize ->
                 Results = [mongoose_rdbms:execute(HostType, insert_mam_muc_message, Row) || Row <- Rows],
                 case lists:keyfind(error, 1, Results) of
@@ -185,6 +197,9 @@ do_flush_muc(Acc, #{host_type := HostType, batch_size := MaxSize}) ->
         {updated, _Count} -> ok;
         {error, Reason} ->
             mongoose_metrics:update(HostType, modMucMamDropped, MessageCount),
+            [?LOG_ERROR(#{what => archive_muc_message_dropped,
+                          sensitive => true, params => Params})
+             || Params <- Acc],
             ?LOG_ERROR(#{what => archive_message_query_failed,
                          text => <<"archive_message query failed, modMucMamDropped metric updated">>,
                          message_count => MessageCount, reason => Reason}),
