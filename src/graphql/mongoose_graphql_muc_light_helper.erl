28ac94cd7146da10c35bee4c163c62f52d9dbb41
Przemysław Wojtasik <przemyslaw.wojtasik@erlang-solutions.com>
2022-02-24 20:08:56 +0100

    Add graphql resolvers for muc light user queries

--- src/graphql/mongoose_graphql_muc_light_helper.erl
+++ src/graphql/mongoose_graphql_muc_light_helper.erl
@@ -1,6 +1,13 @@
 -module(mongoose_graphql_muc_light_helper).
 
--export([make_room/1, make_ok_user/1]).
+-export([make_room/1, make_ok_user/1, page_size_or_max_limit/2]).
+
+page_size_or_max_limit(null, MaxLimit) ->
+    MaxLimit;
+page_size_or_max_limit(PageSize, MaxLimit) when PageSize > MaxLimit ->
+    MaxLimit;
+page_size_or_max_limit(PageSize, _MaxLimit) ->
+    PageSize.
 
 -spec make_room(mod_muc_light_api:room()) -> map().
 make_room(#{jid := JID, name := Name, subject := Subject, aff_users := Users}) ->
