528a4bd865706fa266cc4b602b42e32b2939223e
Mikhail Uvarov <arcusfelis@gmail.com>
2022-02-28 15:53:15 +0100

    Move some code into mongoose_graphql_mam_helper

--- src/graphql/admin/mongoose_graphql_stanza_admin_query.erl
+++ src/graphql/admin/mongoose_graphql_stanza_admin_query.erl
@@ -19,18 +19,7 @@ get_last_messages(#{<<"caller">> := Caller, <<"limit">> := Limit,
         when is_integer(Limit) ->
     case mongoose_graphql_helper:check_user(Caller) of
         {ok, _HostType} ->
-            get_last_messages2(Caller, Limit, With, Before);
+            mongoose_graphql_mam_helper:get_last_messages(Caller, Limit, With, Before);
         Error ->
             Error
     end.
-
-get_last_messages2(Caller, Limit, With, Before) ->
-    With2 = null_as_undefined(With),
-    Before2 = null_as_undefined(Before), %% Before is in microseconds
-    Limit2 = min(500, Limit),
-    Rows = mongoose_stanza_api:lookup_recent_messages(Caller, With2, Before2, Limit2),
-    Maps = lists:map(fun mongoose_graphql_stanza_helper:row_to_map/1, Rows),
-    {ok, #{<<"stanzas">> => Maps, <<"limit">> => Limit2}}.
-
-null_as_undefined(null) -> undefined;
-null_as_undefined(Value) -> Value.
--- src/graphql/user/mongoose_graphql_stanza_user_query.erl
+++ src/graphql/user/mongoose_graphql_stanza_user_query.erl
@@ -17,28 +17,4 @@ execute(#{user := User}, _Obj, <<"getLastMessages">>, Opts) ->
 get_last_messages(#{<<"limit">> := Limit,
                     <<"with">> := With, <<"before">> := Before}, Caller)
         when is_integer(Limit) ->
-    case mongoose_graphql_helper:check_user(Caller) of
-        {ok, _HostType} ->
-            get_last_messages2(Caller, Limit, With, Before);
-        Error ->
-            Error
-    end.
-
-get_last_messages2(Caller, Limit, With, Before) ->
-    With2 = null_as_undefined(With),
-    Before2 = null_as_undefined(Before), %% Before is in microseconds
-    Limit2 = min(500, Limit),
-    Rows = mongoose_stanza_api:lookup_recent_messages(Caller, With2, Before2, Limit2),
-    Maps = lists:map(fun row_to_map/1, Rows),
-    {ok, #{<<"stanzas">> => Maps, <<"limit">> => Limit2}}.
-
-null_as_undefined(null) -> undefined;
-null_as_undefined(Value) -> Value.
-
--spec row_to_map(mod_mam:message_row()) -> {ok, map()}.
-row_to_map(#{id := Id, jid := From, packet := Msg}) ->
-    {Microseconds, _} = mod_mam_utils:decode_compact_uuid(Id),
-    StanzaID = mod_mam_utils:mess_id_to_external_binary(Id),
-    Map = #{<<"sender">> => From, <<"timestamp">> => Microseconds,
-            <<"stanza_id">> => StanzaID, <<"stanza">> => Msg},
-    {ok, Map}.
+    mongoose_graphql_mam_helper:get_last_messages(Caller, Limit, With, Before).
