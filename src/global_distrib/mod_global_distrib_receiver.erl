ca0d2de25d429c64469627fe82c195098a2b3dde
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2022-03-17 12:04:52 +0100

    Update mod_global_distrib_receiver
    - Use opts from the provided map
    - Eliminate the custom callbacks from the utils module

--- src/global_distrib/mod_global_distrib_receiver.erl
+++ src/global_distrib/mod_global_distrib_receiver.erl
@@ -26,8 +26,8 @@
 -include("jlib.hrl").
 -include("global_distrib_metrics.hrl").
 
--export([endpoints/0, start_link/4]).
--export([start/2, stop/1]).
+-export([start_link/4]).
+-export([start/2, stop/1, deps/2]).
 -export([init/1, handle_info/2, handle_cast/2, handle_call/3, code_change/3, terminate/2]).
 
 -define(LISTEN_RETRIES, 5). %% Number of retries in case of eaddrinuse
@@ -58,14 +58,25 @@ start_link(Ref, Socket, ranch_tcp, Opts) ->
 %% gen_mod API
 %%--------------------------------------------------------------------
 
--spec start(Host :: jid:lserver(), Opts :: proplists:proplist()) -> any().
-start(Host, Opts0) ->
-    Opts = prepare_opts(Opts0),
-    mod_global_distrib_utils:start(?MODULE, Host, Opts, fun start/0).
+-spec start(mongooseim:host_type(), gen_mod:module_opts()) -> any().
+start(_HostType, _Opts) ->
+    mongoose_metrics:ensure_metric(global, ?GLOBAL_DISTRIB_RECV_QUEUE_TIME, histogram),
+    mongoose_metrics:ensure_metric(global, ?GLOBAL_DISTRIB_INCOMING_ESTABLISHED, spiral),
+    mod_global_distrib_utils:ensure_metric(?GLOBAL_DISTRIB_INCOMING_ERRORED(undefined), spiral),
+    mod_global_distrib_utils:ensure_metric(?GLOBAL_DISTRIB_INCOMING_CLOSED(undefined), spiral),
+    ChildMod = mod_global_distrib_worker_sup,
+    Child = {ChildMod, {ChildMod, start_link, []}, permanent, 10000, supervisor, [ChildMod]},
+    ejabberd_sup:start_child(Child),
+    start_listeners().
 
--spec stop(Host :: jid:lserver()) -> any().
-stop(Host) ->
-    mod_global_distrib_utils:stop(?MODULE, Host, fun stop/0).
+-spec stop(mongooseim:host_type()) -> any().
+stop(_HostType) ->
+    stop_listeners(),
+    ejabberd_sup:stop_child(mod_global_distrib_worker_sup).
+
+-spec deps(mongooseim:host_type(), gen_mod:module_opts()) -> gen_mod_deps:deps().
+deps(_HostType, Opts) ->
+    [{mod_global_distrib_utils, Opts, hard}].
 
 %%--------------------------------------------------------------------
 %% ranch_protocol API
@@ -74,7 +85,8 @@ stop(Host) ->
 init({Ref, RawSocket, _Opts}) ->
     process_flag(trap_exit, true),
     ok = ranch:accept_ack(Ref),
-    {ok, Socket} = mod_global_distrib_transport:wrap(RawSocket, opt(tls_opts)),
+    ConnOpts = opt(connections),
+    {ok, Socket} = mod_global_distrib_transport:wrap(RawSocket, ConnOpts),
     ok = mod_global_distrib_transport:setopts(Socket, [{active, once}]),
     mongoose_metrics:update(global, ?GLOBAL_DISTRIB_INCOMING_ESTABLISHED, 1),
     State = #state{socket = Socket, waiting_for = header,
@@ -120,34 +132,7 @@ terminate(Reason, State) ->
 %% Helpers
 %%--------------------------------------------------------------------
 
-prepare_opts(Opts0) ->
-    case lists:keyfind(local_host, 1, Opts0) of
-        {local_host, LocalHost} ->
-            [{endpoints, [{LocalHost, 5555}]} | Opts0];
-        _ ->
-            error({missing_option, local_host}, [Opts0])
-    end.
-
--spec start() -> any().
-start() ->
-    opt(tls_opts), %% Check for required tls_opts
-    mongoose_metrics:ensure_metric(global, ?GLOBAL_DISTRIB_RECV_QUEUE_TIME, histogram),
-    mongoose_metrics:ensure_metric(global, ?GLOBAL_DISTRIB_INCOMING_ESTABLISHED, spiral),
-    mod_global_distrib_utils:ensure_metric(?GLOBAL_DISTRIB_INCOMING_ERRORED(undefined), spiral),
-    mod_global_distrib_utils:ensure_metric(?GLOBAL_DISTRIB_INCOMING_CLOSED(undefined), spiral),
-    ChildMod = mod_global_distrib_worker_sup,
-    Child = {ChildMod, {ChildMod, start_link, []}, permanent, 10000, supervisor, [ChildMod]},
-    ejabberd_sup:start_child(Child),
-    Endpoints = mod_global_distrib_utils:resolve_endpoints(opt(endpoints)),
-    ets:insert(?MODULE, {endpoints, Endpoints}),
-    start_listeners().
-
--spec stop() -> any().
-stop() ->
-    stop_listeners(),
-    ejabberd_sup:stop_child(mod_global_distrib_worker_sup).
-
--spec opt(Key :: atom()) -> term().
+-spec opt(gen_mod:opt_key() | gen_mod:key_path()) -> gen_mod:opt_value().
 opt(Key) ->
     mod_global_distrib_utils:opt(?MODULE, Key).
 
@@ -210,10 +195,6 @@ handle_buffered(#state{waiting_for = Size, buffer = Buffer} = State)
 handle_buffered(State) ->
     State.
 
--spec endpoints() -> [mod_global_distrib_utils:endpoint()].
-endpoints() ->
-    opt(endpoints).
-
 -spec start_listeners() -> any().
 start_listeners() ->
     [start_listener(Endpoint, ?LISTEN_RETRIES) || Endpoint <- endpoints()],
@@ -235,3 +216,7 @@ start_listener({Addr, Port} = Ref, RetriesLeft) ->
 -spec stop_listeners() -> any().
 stop_listeners() ->
     lists:foreach(fun ranch:stop_listener/1, endpoints()).
+
+-spec endpoints() -> [mod_global_distrib_utils:endpoint()].
+endpoints() ->
+    opt([connections, resolved_endpoints]).
