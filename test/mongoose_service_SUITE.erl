6245641a7bc2045116675716d0d568cf3d83f7f1
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2022-04-05 11:53:32 +0200

    Test replace_services
    The tests are very similar to the ones for replace_modules,
    but it seems that there are enough differences to keep them separate.

--- test/mongoose_service_SUITE.erl
+++ test/mongoose_service_SUITE.erl
@@ -8,7 +8,11 @@ all() ->
     [starts_and_stops_services,
      ensures_service,
      reverts_config_when_service_fails_to_start,
-     does_not_change_config_when_service_fails_to_stop].
+     does_not_change_config_when_service_fails_to_stop,
+     replaces_services,
+     replaces_services_with_new_deps,
+     replaces_services_with_old_deps,
+     replaces_services_with_same_deps].
 
 init_per_suite(C) ->
     C.
@@ -64,6 +68,68 @@ does_not_change_config_when_service_fails_to_stop(_Config) ->
     ?assertError(something_awful, mongoose_service:ensure_stopped(service_a)),
     ?assertEqual(#{service_a => #{}}, get_services()).
 
+replaces_services(_Config) ->
+    set_services(Services = #{service_a => #{}, service_b => #{opt => val}, service_c => #{}}),
+    ok = mongoose_service:start(),
+    check_started(maps:to_list(Services)),
+
+    %% Stop service_a, change opts for service_b, do not change service_c, start service_d
+    NewServices = #{service_b => #{new_opt => new_val}, service_c => #{}, service_d => #{}},
+    ok = mongoose_service:replace_services([service_a], NewServices),
+    check_stopped([service_a, service_b]),
+    check_not_stopped([service_c]),
+    check_started([{service_b, #{new_opt => new_val}}, {service_d, #{}}]),
+    ?assertEqual(NewServices, get_services()),
+
+    ok = mongoose_service:stop(),
+    check_stopped([service_b, service_c, service_d]).
+
+replaces_services_with_new_deps(_Config) ->
+    set_deps(#{service_b => [service_c]}),
+    set_services(Services = #{service_a => #{}}),
+    ok = mongoose_service:start(),
+    check_started(maps:to_list(Services)),
+
+    %% Start service_b, which depends on service_c
+    ok = mongoose_service:replace_services([], #{service_b => #{}}),
+    check_not_stopped([service_a]),
+    check_started([{service_b, #{}}, {service_c, #{}}]),
+    ?assertEqual(Services#{service_b => #{}, service_c => #{}}, get_services()),
+
+    ok = mongoose_service:stop(),
+    check_stopped([service_a, service_b, service_c]).
+
+replaces_services_with_old_deps(_Config) ->
+    set_deps(#{service_a => [service_c]}),
+    set_services(Services = #{service_a => #{}, service_c => #{}}),
+    ok = mongoose_service:start(),
+    check_started(maps:to_list(Services)),
+
+    %% Stop service_a, which depends on service_c, and start service_b
+    ok = mongoose_service:replace_services([service_a], #{service_b => #{}}),
+    check_stopped([service_a, service_c]),
+    check_started([{service_b, #{}}]),
+    ?assertEqual(#{service_b => #{}}, get_services()),
+
+    ok = mongoose_service:stop(),
+    check_stopped([service_b]).
+
+replaces_services_with_same_deps(_Config) ->
+    set_deps(#{service_a => [service_c], service_b => [service_c]}),
+    set_services(Services = #{service_a => #{}, service_c => #{}}),
+    ok = mongoose_service:start(),
+    check_started(maps:to_list(Services)),
+
+    %% Stop service_a, and start service_b, both depending on service_c
+    ok = mongoose_service:replace_services([service_a], #{service_b => #{}}),
+    check_stopped([service_a]),
+    check_not_stopped([service_c]),
+    check_started([{service_b, #{}}]),
+    ?assertEqual(#{service_b => #{}, service_c => #{}}, get_services()),
+
+    ok = mongoose_service:stop(),
+    check_stopped([service_b]).
+
 %% Helpers
 
 set_services(Services) ->
@@ -82,10 +148,19 @@ check_stopped(Services) ->
                           ?assert(meck:called(Service, stop, []))
                   end, Services).
 
+check_not_stopped(Services) ->
+    lists:foreach(fun(Service) ->
+                          ?assertNot(meck:called(Service, stop, []))
+                  end, Services).
+
 mock_service(Service) ->
     meck:new(Service, [non_strict]),
     meck:expect(Service, start, fun(_) -> start_result end),
     meck:expect(Service, stop, fun() -> ok end).
+
+set_deps(DepsMap) ->
+    maps:fold(fun(Service, Deps, _) -> meck:expect(Service, deps, fun() -> Deps end) end,
+              undefined, DepsMap).
 
 test_services() ->
     [service_a, service_b, service_c, service_d].
