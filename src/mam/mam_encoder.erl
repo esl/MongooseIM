579538f35d9516ff3ac1ecdee7f3605f80395af4
Nelson Vides <nelson.vides@erlang-solutions.com>
2022-06-13 13:25:53 +0200

    Remove unused headers

--- src/mam/mam_encoder.erl
+++ src/mam/mam_encoder.erl
@@ -5,9 +5,7 @@
 -export([encode_packet/2]).
 -export([extend_lookup_params/2]).
 
--include("mongoose.hrl").
 -include("jlib.hrl").
--include_lib("exml/include/exml.hrl").
 -include("mongoose_mam.hrl").
 
 -type value_type() :: int | maybe_string | direction | bare_jid | jid | jid_resource | xml | search.
