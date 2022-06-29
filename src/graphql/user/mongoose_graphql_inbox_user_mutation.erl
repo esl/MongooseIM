07f7dd3d12ee88bc67d322214b947de763056855
Przemysław Wojtasik <przemyslaw.wojtasik@erlang-solutions.com>
2022-06-29 18:15:19 +0200

    Apply review - Add PosInt scalar type and use it in inbox schemas
    Now the user needs to give a positive integer value to remove older than `n` days or null to flush all.

--- src/graphql/user/mongoose_graphql_inbox_user_mutation.erl
+++ src/graphql/user/mongoose_graphql_inbox_user_mutation.erl
@@ -4,10 +4,10 @@
 
 -export([execute/4]).
 
--import(mongoose_graphql_helper, [format_result/2]).
+-import(mongoose_graphql_helper, [format_result/2, null_to_default/2]).
 
 -ignore_xref([execute/4]).
 
 execute(#{user := UserJID}, inbox, <<"flushBin">>, #{<<"days">> := Days}) ->
-    Res = mod_inbox_api:flush_user_bin(UserJID, Days),
+    Res = mod_inbox_api:flush_user_bin(UserJID, null_to_default(Days, 0)),
     format_result(Res, #{user => jid:to_binary(UserJID)}).
