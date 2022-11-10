c7bf7f1e697f86e348e6acc8a9b781e003748c6a
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2022-11-10 15:57:30 +0100

    Update the resolver for the Domain type
    It is a bit easier (and more consistent with the remaining API) to use maps.

--- src/graphql/mongoose_graphql_domain.erl
+++ src/graphql/mongoose_graphql_domain.erl
@@ -4,11 +4,11 @@
 
 -ignore_xref([execute/4]).
 
--include("mongoose_graphql_types.hrl").
-
-execute(_Ctx, #domain{host_type = HostType}, <<"hostType">>, _Args) ->
+execute(_Ctx, #{host_type := HostType}, <<"hostType">>, _Args) ->
     {ok, HostType};
-execute(_Ctx, #domain{status = Status}, <<"status">>, _Args) ->
+execute(_Ctx, #{status := Status}, <<"status">>, _Args) ->
     {ok, Status};
-execute(_Ctx, #domain{domain = Name}, <<"domain">>, _Args) ->
-    {ok, Name}.
+execute(_Ctx, #{domain := Name}, <<"domain">>, _Args) ->
+    {ok, Name};
+execute(_Ctx, #{}, _, _Args) ->
+    {ok, null}.
