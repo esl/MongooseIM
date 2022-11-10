74d2aa863f1b937750ba47eb492c86b1ec3bec99
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2022-11-10 15:53:57 +0100

    Update the legacy domain CLI
    - Check if the service is running
    - Use the error messages

--- src/domain/service_admin_extra_domain.erl
+++ src/domain/service_admin_extra_domain.erl
@@ -10,9 +10,7 @@
     commands/0, insert_domain/2, delete_domain/2, enable_domain/1, disable_domain/1
 ]).
 
--include("mongoose.hrl").
 -include("ejabberd_commands.hrl").
--include("jlib.hrl").
 
 -type cmd_result() :: {ok, string()} | {error, string()}.
 
@@ -44,59 +42,30 @@ commands() ->
 -spec insert_domain(binary(), binary()) -> cmd_result().
 insert_domain(Domain, HostType) ->
     SDomain = jid:nameprep(Domain),
-    case mongoose_domain_api:insert_domain(SDomain, HostType) of
-        ok -> {ok, "Added"};
-        {error, duplicate} ->
-            {error, "Domain already exists"};
-        {error, static} ->
-            {error, "Domain is static"};
-        {error, {db_error, _}} ->
-            {error, "Database error"};
-        {error, service_disabled} ->
-            {error, "Service disabled"};
-        {error, unknown_host_type} ->
-            {error, "Unknown host type"}
-    end.
+    call_api(fun() -> mongoose_domain_api:insert_domain(SDomain, HostType) end, "Added").
 
 -spec delete_domain(binary(), binary()) -> cmd_result().
 delete_domain(Domain, HostType) ->
     SDomain = jid:nameprep(Domain),
-    case mongoose_domain_api:delete_domain(SDomain, HostType) of
-        ok -> {ok, "Deleted"};
-        {error, {db_error, _}} ->
-            {error, "Database error"};
-        {error, static} ->
-            {error, "Domain is static"};
-        {error, service_disabled} ->
-            {error, "Service disabled"};
-        {error, wrong_host_type} ->
-            {error, "Wrong host type"};
-        {error, unknown_host_type} ->
-            {error, "Unknown host type"}
-    end.
+    call_api(fun() -> mongoose_domain_api:delete_domain(SDomain, HostType) end, "Deleted").
 
 -spec enable_domain(binary()) -> cmd_result().
 enable_domain(Domain) ->
     SDomain = jid:nameprep(Domain),
-    Res = mongoose_domain_api:enable_domain(SDomain),
-    handle_enabled_result(Res, "enabled").
+    call_api(fun() -> mongoose_domain_api:enable_domain(SDomain) end, "Enabled").
 
 -spec disable_domain(binary()) -> cmd_result().
 disable_domain(Domain) ->
     SDomain = jid:nameprep(Domain),
-    Res = mongoose_domain_api:disable_domain(SDomain),
-    handle_enabled_result(Res, "disabled").
+    call_api(fun() -> mongoose_domain_api:disable_domain(SDomain) end, "Disabled").
 
-handle_enabled_result(Res, OkText) ->
-    case Res of
-        ok ->
-            {ok, OkText};
-        {error, not_found} ->
-            {error, "Domain not found"};
-        {error, static} ->
-            {error, "Domain is static"};
-        {error, service_disabled} ->
-            {error, "Service disabled"};
-        {error, {db_error, _}} ->
-            {error, "Database error"}
+call_api(F, OkMsg) ->
+    case service_domain_db:enabled() of
+        true ->
+            case F() of
+                {ok, _} -> {ok, OkMsg};
+                {_Reason, ErrMsg} -> {error, ErrMsg}
+            end;
+        false ->
+            {error, <<"Dynamic domains service is disabled">>}
     end.
