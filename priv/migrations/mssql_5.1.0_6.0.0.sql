cceb536dab994554eaaa5eddc7ec541c68079493
Janusz Jakubiec <januszjakubiec@MacBook-Pro-administrator-3.local>
2022-12-15 11:31:36 +0100

    Updating migration guide

--- priv/migrations/mssql_5.1.0_6.0.0.sql
+++ priv/migrations/mssql_5.1.0_6.0.0.sql
@@ -1,2 +1,9 @@
 -- DOMAINS
-sp_rename 'domains_settings.enabled', 'status', 'COLUMN';
+sp_rename 'domain_settings.enabled', 'status', 'COLUMN';
+
+-- Table containing domain_admins
+-- They are being used by graphql_admin API
+CREATE TABLE domain_admins(
+     domain VARCHAR(250) NOT NULL PRIMARY KEY,
+     pass_details NVARCHAR(max) NOT NULL
+);
