c82583f9c006b0a6e02ebd714c10251ca71c9ff1
Denys Gonchar <denys.gonchar@erlang-solutions.com>
2023-02-15 17:13:12 +0100

    renaming global.data.xmpp.sent.message.processing_time metric into global.data.xmpp.message.processing_time

--- src/metrics/mongoose_metrics_definitions.hrl
+++ src/metrics/mongoose_metrics_definitions.hrl
@@ -70,7 +70,7 @@
                             [data, xmpp, received, xml_stanza_size],
                             [data, xmpp, sent, encrypted_size],
                             [data, xmpp, sent, xml_stanza_size],
-                            [data, xmpp, sent, message, processing_time]
+                            [data, xmpp, message, processing_time]
                            ]).
 
 -define(DATA_FUN_METRICS,
