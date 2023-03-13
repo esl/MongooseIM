5fe1413885ae7d8769dee66e1c6ed505f8865b22
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2023-03-13 16:08:58 +0100

    Build and push docker images for amd64 and arm64 architectures

--- tools/circle-build-and-push-docker.sh
+++ tools/circle-build-and-push-docker.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
 set -e
-# From https://github.com/esl/mongooseim-docker/pull/47
-MIM_DOCKER_VERSION=fe6d9d31a720a254ea0150c44138e5f3955bf77b
-
-MONGOOSE_TGZ=mongooseim.tar.gz
-
-BUILD_PATH=_build/prod/rel/mongooseim
-
-tar czh --transform="s,${BUILD_PATH},mongooseim,S" -f $MONGOOSE_TGZ ${BUILD_PATH}
-
-export BUILDS=`pwd`
+# From https://github.com/esl/mongooseim-docker/pull/49
+MIM_DOCKER_VERSION=6b62e9cf26c523e35fe78c57cbae2e8832f555d2
 
 # We use output of generate_vsn, because it does not contain illegal characters, returns
 # git tag when building from tag itself, and is unique in any other case
@@ -29,8 +21,6 @@ elif [ ${CIRCLE_BRANCH} == 'master' ]; then
     DOCKERHUB_TAG="latest";
 fi
 
-# TODO: Add DOCKERHUB=${VERSION} when CircleCI handles weekly builds as well
-
 echo "Tag: ${DOCKERHUB_TAG}"
 
 IMAGE_TAG=${DOCKERHUB_REPO}/mongooseim:${DOCKERHUB_TAG}
@@ -39,15 +29,18 @@ git clone https://github.com/esl/mongooseim-docker.git
 cd mongooseim-docker
 git checkout $MIM_DOCKER_VERSION
 
-cp ../${MONGOOSE_TGZ} member
+cp ../mongooseim-*.tar.gz member
 
-docker build -f Dockerfile.member -t mongooseim -t ${IMAGE_TAG} \
+docker run --privileged --rm tonistiigi/binfmt --install all
+docker buildx create --name builder --driver docker-container --bootstrap --use
+docker login -u ${DOCKERHUB_USER} -p ${DOCKERHUB_PASS}
+
+docker buildx build --platform linux/amd64,linux/arm64 \
+             -f Dockerfile.member -t ${IMAGE_TAG} --push --progress=plain \
              --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
 	     --build-arg VCS_REF=${GIT_REF} \
 	     --build-arg VCS_REF_DESC="${GIT_COMMIT_MSG}" \
 	     --build-arg VERSION=${VERSION} \
 	     .
 
-docker login -u ${DOCKERHUB_USER} -p ${DOCKERHUB_PASS}
-
-docker push ${IMAGE_TAG}
+echo $IMAGE_TAG > ../image_tag
