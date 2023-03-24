566d06796b6559318190343d225d2c2c3c2089d1
Denys Gonchar <denys.gonchar@erlang-solutions.com>
2023-03-24 01:50:36 +0100

    adding test-spec optional input parameter for big-tests action

--- tools/gh-actions-configure-preset.sh
+++ tools/gh-actions-configure-preset.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
-## GITHUB_ENV=/dev/tty ./gh-actions-configure-preset.sh internal-mnesia
+##########################################################################################################
+## for testing purposes run:
+##    GITHUB_ENV=/dev/tty tools/gh-actions-configure-preset.sh internal_mnesia TESTSPEC=default.spec
+##    GITHUB_ENV=/dev/tty tools/gh-actions-configure-preset.sh mysql_redis TESTSPEC=default.spec
+##
+## this script can be also sourced with parameters to set env variables in the current shell:
+##    GITHUB_ENV=/dev/null . tools/gh-actions-configure-preset.sh internal_mnesia TESTSPEC=default.spec
+##    GITHUB_ENV=/dev/tty source tools/gh-actions-configure-preset.sh mysql_redis TESTSPEC=default.spec
+##########################################################################################################
 
 export PRESET="$1"
+shift 1
+
+function export_env(){
+  [ -z "$GITHUB_ENV" ] && { return; }
+  # echo "exporting: $@"
+  local env_var
+  for env_var in "$@"; do
+     export "$env_var"
+     echo "$env_var" >> $GITHUB_ENV
+  done
+}
+
+export_env "$@"
 
 DB_ARRAY=( $(./tools/test_runner/presets_to_dbs.sh "$PRESET" ) )
 [ "${#DB_ARRAY[@]}" -gt 0 ] && export DB="${DB_ARRAY[@]}"
