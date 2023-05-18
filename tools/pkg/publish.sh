ecea23497ec1ad5e477cd364888af66e10f688b6
Paweł Długosz <pawel.dlugosz@erlang-solutions.com>
2023-05-18 11:58:18 +0200

    added publish package job

--- tools/pkg/publish.sh
+++ tools/pkg/publish.sh
@@ -1,41 +1,21 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -e
 
-# Params - override for your env
-SHA256SUM="shasum -a 256"
-REMOTE_SHA256SUM=sha256sum
-PACKAGES_LOCAL=packages
-. env/publish
+cd tools/pkg/packages
+PACKAGE_NAME=$(ls)
+PKG_PROFILE=packages
+prefix=${pkg_PLATFORM/_//}
 
-# CLI arguments
-VERSION=${1:-"(missing)"}
-REVISION=${2:-1}
-
-if [ x"$VERSION" = x"(missing)" ]; then
-    echo "usage: $0 VERSION [REVISION]"
-    echo
-    echo Look for package \$VERSION-\$REVISION and publish to $PACKAGES_HOST.
-    exit 1
+if which aws ; then
+    echo "aws tool ready"
+else
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip -q awscliv2.zip
+    sudo ./aws/install
 fi
 
-set +e
-PKG=$(ls -1 ${PACKAGES_LOCAL} | grep "$VERSION-$REVISION" | head -1)
-if [ -z "$PKG" ]; then
-    echo "$0: can't find package version $VERSION-$REVISION"
-    exit 2
-fi
-set -e
+aws configure set aws_access_key_id $PKG_ACCESS_KEY_ID --profile $PKG_PROFILE
+aws configure set aws_secret_access_key $PKG_SECRET_ACCESS_KEY --profile $PKG_PROFILE
 
-rsync $PACKAGES_LOCAL/$PKG $PACKAGES_HOST:$PACKAGES_PATH
-CHKSUM=$($SHA256SUM $PACKAGES_LOCAL/$PKG)
-REMOTE_CHKSUM=$(ssh $PACKAGES_HOST "cd $PACKAGES_PATH && $REMOTE_SHA256SUM $PKG")
-
-if [ x"$(echo $CHKSUM | cut -d" " -f1)" != x"$(echo $REMOTE_CHKSUM | cut -d" " -f1)" ]; then
-    echo "$0: checksum mismatch - try again or check your connection"
-    exit 3
-fi
-ssh $PACKAGES_HOST "sed -i /$VERSION-$REVISION/d $PACKAGES_PATH/sha256sum " \
-                   "&& echo $REMOTE_CHKSUM >> $PACKAGES_PATH/sha256sum"
-echo Package $PKG is now public at:
-echo https://$PACKAGES_HOST/$PACKAGES_PROJECT/$PKG
+aws s3 cp ${PACKAGE_NAME} s3://arn:aws:s3:${PKG_AWS_REGION}:767271724337:accesspoint/mim-packages/$prefix/${PACKAGE_NAME} --acl public-read --profile $PKG_PROFILE --region $PKG_AWS_REGION
