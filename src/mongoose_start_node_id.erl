372b8fd721dab05f667b2e492f356ba736f2401e
Mikhail Uvarov <arcusfelis@gmail.com>
2023-06-21 22:04:52 +0200

    Fix xref for mongoose_start_node_id

--- src/mongoose_start_node_id.erl
+++ src/mongoose_start_node_id.erl
@@ -12,6 +12,8 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
+-ignore_xref([start_link/0]).
+
 -include("mongoose.hrl").
 
 -type id() :: binary().
