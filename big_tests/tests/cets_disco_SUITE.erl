2ba064e7d757ab40bb8da7e820bca058e1602bfd
Mikhail Uvarov <arcusfelis@gmail.com>
2023-06-29 09:38:37 +0200

    Cleaning discovery_nodes from inactive nodes

--- big_tests/tests/cets_disco_SUITE.erl
+++ big_tests/tests/cets_disco_SUITE.erl
@@ -1,7 +1,7 @@
 -module(cets_disco_SUITE).
 -compile([export_all, nowarn_export_all]).
 
--import(distributed_helper, [mim/0, rpc/4]).
+-import(distributed_helper, [mim/0, mim2/0, rpc/4]).
 -include_lib("common_test/include/ct.hrl").
 
 %%--------------------------------------------------------------------
@@ -19,10 +19,11 @@ file_cases() ->
     [file_backend].
 
 rdbms_cases() ->
-    [rdbms_backend].
+    [rdbms_backend,
+     rdbms_backend_supports_auto_cleaning].
 
 suite() ->
-    escalus:suite().
+    distributed_helper:require_rpc_nodes([mim, mim2]) ++ escalus:suite().
 
 %%--------------------------------------------------------------------
 %% Init & teardown
@@ -63,14 +64,47 @@ file_backend(Config) ->
     ['node1@localhost', 'node2@otherhost'] = lists:sort(Nodes).
 
 rdbms_backend(_Config) ->
-    Opts1 = #{cluster_name => <<"big_test">>, node_name_to_insert => <<"test1">>},
-    Opts2 = #{cluster_name => <<"big_test">>, node_name_to_insert => <<"test2">>},
-    State1 = rpc(mim(), mongoose_cets_discovery_rdbms, init, [Opts1]),
-    rpc(mim(), mongoose_cets_discovery_rdbms, get_nodes, [State1]),
-    State2 = rpc(mim(), mongoose_cets_discovery_rdbms, init, [Opts2]),
-    {{ok, Nodes}, State2_2} = rpc(mim(), mongoose_cets_discovery_rdbms, get_nodes, [State2]),
+    CN = <<"big_test">>,
+    Opts1 = #{cluster_name => CN, node_name_to_insert => <<"test1">>},
+    Opts2 = #{cluster_name => CN, node_name_to_insert => <<"test2">>},
+    State1 = disco_init(Opts1),
+    disco_get_nodes(State1),
+    State2 = disco_init(Opts2),
+    {{ok, Nodes}, State2_2} = disco_get_nodes(State2),
     %% "test2" node can see "test1"
     true = lists:member(test1, Nodes),
-    {{ok, _}, State2_3} = rpc(mim(), mongoose_cets_discovery_rdbms, get_nodes, [State2_2]),
+    {{ok, _}, State2_3} = disco_get_nodes(State2_2),
     %% Check that we follow the right code branch
     #{last_query_info := #{already_registered := true}} = State2_3.
+
+rdbms_backend_supports_auto_cleaning(_Config) ->
+    CN = <<"big_test2">>,
+    Opts1 = #{cluster_name => CN, node_name_to_insert => <<"test1">>, override_timestamp => month_ago()},
+    Opts2 = #{cluster_name => CN, node_name_to_insert => <<"test2">>, min_node_count_to_expire => 1},
+    %% test1 row is written
+    State1 = disco_init(Opts1),
+    {_, State1_2} = disco_get_nodes(State1),
+    {{ok, Nodes1}, _} = disco_get_nodes(State1_2),
+    %% It is in DB
+    true = lists:member(test1, Nodes1),
+    %% test2 would clean test1 registration
+    State2 = disco_init(Opts2),
+    {{ok, Nodes2}, State2_2} = disco_get_nodes(State2),
+    false = lists:member(test1, Nodes2),
+    #{last_query_info := #{run_cleaning_result := {removed, [test1]}}} = State2_2.
+
+%%--------------------------------------------------------------------
+%% Helpers
+%%--------------------------------------------------------------------
+
+disco_init(Opts) ->
+    rpc(mim(), mongoose_cets_discovery_rdbms, init, [Opts]).
+
+disco_get_nodes(State) ->
+    rpc(mim(), mongoose_cets_discovery_rdbms, get_nodes, [State]).
+
+timestamp() ->
+    os:system_time(second).
+
+month_ago() ->
+    timestamp() - timer:hours(24 * 30) div 1000.
