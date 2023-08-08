fa0c589e9d4a16b001de4bb2a54416377b5a1cbe
Mikhail Uvarov <arcusfelis@gmail.com>
2023-08-08 16:04:49 +0200

    Clean mongoose_node_num.erl

--- src/mongoose_node_num.erl
+++ src/mongoose_node_num.erl
@@ -1,23 +1,16 @@
 %% Returns a numeric id from 0 to 255 for the current node.
 %% Used to generate MAM IDs.
 -module(mongoose_node_num).
--export([set_node_num/1]).
--export([node_num/0]).
-
--include("mongoose.hrl").
--include("jlib.hrl").
--include("mongoose_config_spec.hrl").
--include("mongoose_logger.hrl").
+-export([node_num/0, set_node_num/1]).
 
 -type node_num() :: 0..255.
--define(KEY, ?MODULE).
 -export_type([node_num/0]).
 
 %% @doc Return an integer node ID.
 -spec node_num() -> node_num().
 node_num() ->
-    %% We just return 0 if service is not running.
-    persistent_term:get(?KEY, 0).
+    %% We just return 0 if set_node_num/1 is not called.
+    persistent_term:get(?MODULE, 0).
 
 -spec set_node_num(node_num()) -> ignore | updated | same.
 set_node_num(Num) ->
@@ -25,6 +18,6 @@ set_node_num(Num) ->
         true ->
             same;
         false ->
-            persistent_term:put(?KEY, Num),
+            persistent_term:put(?MODULE, Num),
             updated
     end.
