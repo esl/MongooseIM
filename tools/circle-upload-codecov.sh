514bd83597fa0ab991eef80aa219fddbb73acfba
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2023-10-12 08:39:29 +0200

    Download uploaders for aws and codecov for the correct platform
    The machine arm executor was able to run the x86_64 code,
    while the docker one is not.

--- tools/circle-upload-codecov.sh
+++ tools/circle-upload-codecov.sh
@@ -2,11 +2,15 @@
 
 set -eo pipefail
 
+PLATFORM=`uname -m`
+[ $PLATFORM == "x86_64" ] && PLATFORM=linux
+BASE_URL="https://uploader.codecov.io/latest/$PLATFORM"
+
 # Source: https://docs.codecov.com/docs/codecov-uploader#integrity-checking-the-uploader
 curl https://keybase.io/codecovsecurity/pgp_keys.asc | gpg --no-default-keyring --keyring trustedkeys.gpg --import
-curl -Os https://uploader.codecov.io/latest/linux/codecov
-curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM
-curl -Os https://uploader.codecov.io/latest/linux/codecov.SHA256SUM.sig
+curl -Os "$BASE_URL/codecov"
+curl -Os "$BASE_URL/codecov.SHA256SUM"
+curl -Os "$BASE_URL/codecov.SHA256SUM.sig"
 gpgv codecov.SHA256SUM.sig codecov.SHA256SUM
 shasum -a 256 -c codecov.SHA256SUM
 
