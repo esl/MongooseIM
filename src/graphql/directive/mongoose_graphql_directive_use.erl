32f0c23810d140ede7a939065b24a6d62c82f131
jacekwegr <jacek.wegrzynowski@erlang-solutions.com>
2023-12-15 09:39:42 +0100

    Add `internal_databases` to the `@use` directive

--- src/graphql/directive/mongoose_graphql_directive_use.erl
+++ src/graphql/directive/mongoose_graphql_directive_use.erl
@@ -42,6 +42,7 @@
 -type use_ctx() ::
     #{modules := [binary()],
       services := [binary()],
+      internal_databases := [binary()],
       arg => binary(),
       atom => term()}.
 
@@ -49,14 +50,16 @@
 %% is not loaded. The new field resolver returns the error that some modules or services
 %% are not loaded.
 handle_directive(#directive{id = <<"use">>, args = Args}, #schema_field{} = Field, Ctx) ->
-    #{modules := Modules, services := Services} = UseCtx = aggregate_use_ctx(Args, Ctx),
+    #{modules := Modules, services := Services, internal_databases := DB} =
+        UseCtx = aggregate_use_ctx(Args, Ctx),
     UnloadedServices = filter_unloaded_services(Services),
     UnloadedModules = filter_unloaded_modules(UseCtx, Ctx, Modules),
-    case {UnloadedModules, UnloadedServices} of
-        {[], []} ->
+    UnloadedDBs = filter_unloaded_db(DB),
+    case {UnloadedModules, UnloadedServices, UnloadedDBs} of
+        {[], [], []} ->
             Field;
-        {_, _} ->
-            Fun = resolve_not_loaded_fun(UnloadedModules, UnloadedServices),
+        {_, _, _} ->
+            Fun = resolve_not_loaded_fun(UnloadedModules, UnloadedServices, UnloadedDBs),
             Field#schema_field{resolve = Fun}
     end.
 
@@ -76,15 +79,21 @@ get_arg_value(_UseCtx, #{admin := #jid{lserver = Domain}}) ->
     Domain.
 
 -spec aggregate_use_ctx(list(), ctx()) -> use_ctx().
-aggregate_use_ctx(Args, #{use_dir := #{modules := Modules0, services := Services0}}) ->
-    #{modules := Modules, services := Services} = UseCtx = prepare_use_dir_args(Args),
-    UseCtx#{modules => Modules0 ++ Modules, services => Services0 ++ Services};
+aggregate_use_ctx(Args, #{use_dir := #{modules := Modules0, services := Services0,
+                                       internal_databases := DB0}}) ->
+    #{modules := Modules, services := Services, internal_databases := DB} =
+        UseCtx = prepare_use_dir_args(Args),
+    UpdatedModules = Modules0 ++ Modules,
+    UpdatedServices = Services0 ++ Services,
+    UpdatedDB = DB0 ++ DB,
+    UseCtx#{modules => UpdatedModules, services => UpdatedServices,
+            internal_databases => UpdatedDB};
 aggregate_use_ctx(Args, _Ctx) ->
     prepare_use_dir_args(Args).
 
 -spec prepare_use_dir_args([{graphql:name(), term()}]) -> use_ctx().
 prepare_use_dir_args(Args) ->
-    Default = #{modules => [], services => []},
+    Default = #{modules => [], services => [], internal_databases => []},
     RdyArgs = maps:from_list([{binary_to_existing_atom(name(N)), V} || {N, V} <- Args]),
     maps:merge(Default, RdyArgs).
 
@@ -128,8 +137,19 @@ filter_unloaded_services(Services) ->
     lists:filter(fun(S) -> not mongoose_service:is_loaded(binary_to_existing_atom(S)) end,
                  Services).
 
--spec resolve_not_loaded_fun([binary()], [binary()]) -> resolver().
-resolve_not_loaded_fun(Modules, Services) ->
+-spec filter_unloaded_db([binary()]) -> [binary()].
+filter_unloaded_db(DBs) ->
+    lists:filter(fun(DB) ->
+                    mongoose_config:get_opt([internal_databases,
+                                             binary_to_existing_atom(DB)], undefined) == undefined
+                 end, DBs).
+
+-spec resolve_not_loaded_fun([binary()], [binary()], [binary()]) -> resolver().
+resolve_not_loaded_fun(Modules, Services, []) ->
     Msg = <<"Some of required modules or services are not loaded">>,
     Extra = #{not_loaded_modules => Modules, not_loaded_services => Services},
+    fun(_, _, _, _) -> mongoose_graphql_helper:make_error(deps_not_loaded, Msg, Extra) end;
+resolve_not_loaded_fun(_Modules, _Services, Databases) ->
+    Msg = <<"The required internal databases is not configured">>,
+    Extra = #{not_loaded_databases => Databases},
     fun(_, _, _, _) -> mongoose_graphql_helper:make_error(deps_not_loaded, Msg, Extra) end.
