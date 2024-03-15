2768c2895b2bdd0953d56bc186e2ac509a674c5a
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2024-03-15 18:03:38 +0100

    Add helpers for testing instrumentation events in big tests
    Testing procedure:
    1. In init_per_suite, call instrument_helper:start(DeclaredEvents).
       The helper installs instrument_event_table on mim(),
       which will collect events with matching EventName and Labels.
    2. In the tests, call instrument_helper:assert/3.
       The function fails if there is no matching event.
    3. In end_per_suite, call instrument_helper:stop/0,
       which deletes the collected events, and logs a summary
       of event status (tested/untested).
    
    Don't delete

---
+++ big_tests/tests/instrument_helper.erl
@@ -0,0 +1,85 @@
+%% @doc A helper module for checking instrumentation events in tests.
+%% For now it always uses mim(). We can extend it to other nodes when needed.
+
+-module(instrument_helper).
+
+-export([declared_events/1, start/1, stop/0, assert/3]).
+
+-import(distributed_helper, [rpc/4, mim/0]).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-define(STATUS_TABLE, instrument_event_status_table).
+
+-type event_name() :: atom().
+-type labels() :: #{atom() => term()}.
+-type measurements() :: #{atom() => term()}.
+
+%% API
+
+%% @doc Helper to get `DeclaredEvents' needed by `start/1'
+-spec declared_events([module()] | module()) -> [{event_name(), labels()}].
+declared_events(Modules) when is_list(Modules) ->
+    lists:flatmap(fun declared_events/1, Modules);
+declared_events(Module) ->
+    Specs = rpc(mim(), Module, instrumentation, [domain_helper:host_type()]),
+    [{Event, Labels} || {Event, Labels, _Config} <- Specs].
+
+%% @doc Only `DeclaredEvents' will be logged, and can be tested with `assert/3'
+-spec start([{event_name(), labels()} | module()]) -> ok.
+start(DeclaredEvents) ->
+    mongoose_helper:inject_module(ets_helper),
+    mongoose_helper:inject_module(instrument_event_table),
+    ets_helper:new(?STATUS_TABLE),
+    [ets:insert(?STATUS_TABLE, {Event, untested}) || Event <- DeclaredEvents],
+    ok = rpc(mim(), instrument_event_table, set_up, [DeclaredEvents]).
+
+-spec stop() -> ok.
+stop() ->
+    #{tested := Tested, untested := Untested} = classify_events(),
+    ets_helper:delete(?STATUS_TABLE),
+    {ok, Logged} = rpc(mim(), instrument_event_table, tear_down, []),
+    ct:log("Tested instrumentation events:~n ~p", [lists:sort(Tested)]),
+    verify_unlogged(Untested -- Logged),
+    verify_logged_but_untested(Logged -- Tested).
+
+%% @doc `CheckF' can return a boolean or fail with `function_clause', which means `false'.
+%% This is for convenience - you only have to code one clause.
+-spec assert(event_name(), labels(), fun((measurements()) -> boolean())) -> ok.
+assert(EventName, Labels, CheckF) ->
+    Events = rpc(mim(), instrument_event_table, get_events, [EventName, Labels]),
+    case lists:filter(fun({_, Measurements}) ->
+                              try CheckF(Measurements) catch error:function_clause -> false end
+                      end, Events) of
+        [] ->
+            ct:log("All ~p events with labels ~p:~n~p", [EventName, Labels, Events]),
+            ct:fail("No instrumentation events matched");
+        Filtered ->
+            ct:log("Matching events: ~p", [Filtered]),
+            event_tested(EventName, Labels)
+    end.
+
+%% Internal functions
+
+%% Don't fail if some events are unlogged, because we don't have full test coverage (yet)
+verify_unlogged([]) -> ok;
+verify_unlogged(Events) ->
+    ct:log("Instrumentation events that were not logged - functionality not covered by tests:~n~p",
+           [lists:sort(Events)]).
+
+%% Fail if any events were logged, but there were no assertions for them
+verify_logged_but_untested([]) -> ok;
+verify_logged_but_untested(Events) ->
+    ct:fail("Instrumentation events that were logged, but not tested:~n~p~n"
+            "You need to test them with ~p:assert/3",
+            [lists:sort(Events), ?MODULE]).
+
+event_tested(EventName, Labels) ->
+    ets:insert(?STATUS_TABLE, {{EventName, Labels}, tested}),
+    ok.
+
+classify_events() ->
+    ets:foldl(fun classify_event/2, #{tested => [], untested => []}, ?STATUS_TABLE).
+
+classify_event({Event, Status}, M) ->
+    M#{Status => [Event | maps:get(Status, M)]}.
