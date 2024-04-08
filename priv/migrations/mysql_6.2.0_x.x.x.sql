cf0e7f441e1266fa57052ba7c970230869123631
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2024-04-08 15:54:01 +0200

    Update mysql migration
    - Add missing 'caps' table
    - Update primary key for 'discovery_nodes'

--- priv/migrations/mysql_6.2.0_x.x.x.sql
+++ priv/migrations/mysql_6.2.0_x.x.x.sql
@@ -10,3 +10,15 @@ ALTER TABLE rostergroups MODIFY COLUMN grp VARCHAR(255), ADD PRIMARY KEY(server,
 -- Store information whether the message is of type "groupchat" in the user's archive
 ALTER TABLE mam_message
 ADD COLUMN is_groupchat boolean NOT NULL DEFAULT false;
+
+-- Create table for mod_caps
+CREATE TABLE caps (
+    node varchar(250) NOT NULL,
+    sub_node varchar(250) NOT NULL,
+    features text NOT NULL,
+    PRIMARY KEY (node, sub_node)
+);
+
+-- In case of duplicates, you need to remove stale rows manually or wait for cleanup
+ALTER TABLE discovery_nodes
+DROP PRIMARY KEY, ADD PRIMARY KEY (node_name);
