8f737af14d44d2c0802dfee630d69dd7f0fbe194
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2024-04-08 15:53:15 +0200

    Update pgsql migration
    - Add missing 'caps' table
    - Update primary key for 'discovery_nodes'

--- priv/migrations/pgsql_6.2.0_x.x.x.sql
+++ priv/migrations/pgsql_6.2.0_x.x.x.sql
@@ -10,3 +10,15 @@ ALTER TABLE rostergroups ADD PRIMARY KEY (server, username, jid, grp);
 -- Store information whether the message is of type "groupchat" in the user's archive
 ALTER TABLE mam_message
 ADD COLUMN is_groupchat boolean NOT NULL DEFAULT false;
+
+-- Create table for mod_caps
+CREATE TABLE caps (
+    node varchar(250) NOT NULL,
+    sub_node varchar(250) NOT NULL,
+    features text NOT NULL,
+    PRIMARY KEY (node, sub_node)
+);
+
+-- In case of duplicates, you need to remove stale rows manually or wait for cleanup
+ALTER TABLE discovery_nodes
+DROP CONSTRAINT discovery_nodes_pkey, ADD PRIMARY KEY (node_name);
