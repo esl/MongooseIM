de6434377a1a928986f82f54c32b8dbd9c9bda2b
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2024-04-24 09:52:42 +0200

    Test MUC instrumentation events

--- big_tests/tests/muc_helper.erl
+++ big_tests/tests/muc_helper.erl
@@ -338,3 +338,22 @@ change_nick_form_iq(Nick) ->
 
 set_nick(User, Nick) ->
     escalus:send_iq_and_wait_for_result(User, change_nick_form_iq(Nick)).
+
+%% Instrumentation utilities
+
+wait_for_room_count(ExpectedCounts) ->
+    [Measurements | _] = instrument_helper:wait_for_new(mod_muc_rooms, labels()),
+    F = fun(Counts) -> Counts =:= ExpectedCounts end,
+    instrument_helper:assert(mod_muc_rooms, labels(), [Measurements], F).
+
+assert_room_event(EventName, RoomJid) ->
+    assert_event(EventName, fun(#{count := 1, jid := Jid}) -> Jid =:= RoomJid end).
+
+assert_event(EventName, F) ->
+    instrument_helper:assert(EventName, labels(), F).
+
+count_rooms() ->
+    rpc(mim(), mod_muc, probe, [mod_muc_rooms, labels()]).
+
+labels() ->
+    #{host_type => domain_helper:host_type()}.
