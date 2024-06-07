52d9abaa6039282f2c985add0405b732b76ef0d2
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2024-06-07 16:37:34 +0200

    Test roster instrumentation events
    By replacing metrics with instrumentation in assertions,
    tests can be parallelized.

--- big_tests/tests/roster_helper.erl
+++ big_tests/tests/roster_helper.erl
@@ -1,5 +1,5 @@
 -module(roster_helper).
--export([set_versioning/3]).
+-compile([export_all, nowarn_export_all]).
 
 -import(distributed_helper, [mim/0, rpc/4]).
 -import(domain_helper, [host_type/0]).
@@ -10,3 +10,20 @@ set_versioning(Versioning, VersionStore, Config) ->
     dynamic_modules:ensure_modules(host_type(), [{mod_roster, Opts#{versioning => Versioning,
                                                                     store_current_id => VersionStore}}]),
     Config.
+
+%% Intrumentation events
+
+assert_roster_event(Client, Event) ->
+    ClientJid = jid:from_binary(escalus_utils:get_jid(Client)),
+    instrument_helper:assert(
+      Event, #{host_type => host_type()},
+      fun(#{count := 1, jid := Jid}) -> jid:are_bare_equal(ClientJid, Jid) end).
+
+assert_subscription_event(FromClient, ToClient, CheckF) ->
+    FromClientJid = jid:from_binary(escalus_utils:get_short_jid(FromClient)),
+    ToClientJid = jid:from_binary(escalus_utils:get_short_jid(ToClient)),
+    instrument_helper:assert(
+      sm_presence_subscription, #{},
+      fun(#{from_jid := FromJid, to_jid := ToJid} = M) ->
+              FromClientJid =:= FromJid andalso ToClientJid =:= ToJid andalso CheckF(M)
+      end).
