f145184ac69bbcc42eee4c8d38454104f5f26e4b
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2024-06-18 16:31:35 +0200

    Update session metrics tests
    - Use instrumentation events
    - Parallelize tests

--- big_tests/tests/metrics_session_SUITE.erl
+++ big_tests/tests/metrics_session_SUITE.erl
@@ -17,16 +17,15 @@
 -module(metrics_session_SUITE).
 -compile([export_all, nowarn_export_all]).
 
--include_lib("escalus/include/escalus.hrl").
--include_lib("common_test/include/ct.hrl").
-
--define(RT_WINDOW, 3).  % seconds
+-include_lib("stdlib/include/assert.hrl").
 
 -import(metrics_helper, [assert_counter/2,
                          assert_counter/3,
                          get_counter_value/1,
                          wait_for_counter/2,
                          wait_for_counter/3]).
+-import(domain_helper, [host_type/0]).
+
 %%--------------------------------------------------------------------
 %% Suite configuration
 %%--------------------------------------------------------------------
@@ -36,7 +35,7 @@ all() ->
      {group, session_global}].
 
 groups() ->
-    [{session, [sequence], [login_one,
+    [{session, [parallel], [login_one,
                             login_many,
                             auth_failed]},
      {session_global, [sequence], [session_global,
@@ -50,10 +49,14 @@ suite() ->
 %%--------------------------------------------------------------------
 
 init_per_suite(Config) ->
+    instrument_helper:start([{sm_session, #{host_type => host_type()}},
+                             {c2s_auth_failed, #{host_type => host_type()}}]),
     escalus:init_per_suite(Config).
 
 end_per_suite(Config) ->
-    escalus:end_per_suite(Config).
+    escalus_fresh:clean(),
+    escalus:end_per_suite(Config),
+    instrument_helper:stop().
 
 init_per_group(_GroupName, Config) ->
     escalus:create_users(Config, escalus:get_users([alice, bob])).
@@ -71,39 +74,31 @@ end_per_testcase(CaseName, Config) ->
 %% Tests
 %%--------------------------------------------------------------------
 
-
 login_one(Config) ->
-    {value, Logins} = get_counter_value(sessionSuccessfulLogins),
-    escalus:story(Config, [{alice, 1}], fun(Alice) ->
+    escalus:fresh_story(Config, [{alice, 1}], fun login_one_story/1).
 
-        assert_counter(1, sessionCount),
-        assert_counter(Logins + 1, sessionSuccessfulLogins),
-
-        {value, Logouts} = get_counter_value(sessionLogouts),
-        escalus_client:stop(Config, Alice),
-        wait_for_counter(0, sessionCount),
-        wait_for_counter(Logouts + 1, sessionLogouts)
-
-    end).
+login_one_story(Alice) ->
+    assert_sm_login_event(Alice),
+    sm_helper:stop_client_and_wait_for_termination(Alice),
+    assert_sm_logout_event(Alice).
 
 login_many(Config) ->
-    {value, Logins} = get_counter_value(sessionSuccessfulLogins),
-    escalus:story(Config, [{alice, 1}, {bob, 1}], fun(_Alice, _Bob) ->
+    escalus:fresh_story(Config, [{alice, 1}, {bob, 1}], fun login_many_story/2).
 
-        assert_counter(2, sessionCount),
-        assert_counter(Logins + 2, sessionSuccessfulLogins)
-
-        end).
+login_many_story(Alice, Bob) ->
+    assert_sm_login_event(Alice),
+    assert_sm_login_event(Bob),
+    sm_helper:stop_client_and_wait_for_termination(Alice),
+    assert_sm_logout_event(Alice),
+    sm_helper:stop_client_and_wait_for_termination(Bob),
+    assert_sm_logout_event(Bob).
 
 auth_failed(Config) ->
-    {value, AuthFails} = get_counter_value(sessionAuthFails),
-
-    [{_, UserSpec} | _] = escalus_config:get_config(escalus_users, Config),
+    UserSpec = escalus_fresh:create_fresh_user(Config, alice),
     UserSpecM = proplists:delete(password, UserSpec) ++ [{password, <<"mazabe">>}],
-
     {error, _} = escalus_client:start(Config, UserSpecM, <<"res1">>),
-    assert_counter(0, sessionCount),
-    assert_counter(AuthFails + 1, sessionAuthFails).
+    assert_no_sm_login_event(UserSpec),
+    assert_c2s_auth_failed(UserSpec).
 
 %% Global
 
@@ -120,3 +115,27 @@ session_unique(Config) ->
         wait_for_counter(global, 1, uniqueSessionCount),
         wait_for_counter(global, 2, totalSessionCount)
         end).
+
+%% Instrumentation events
+
+assert_sm_login_event(Client) ->
+    JID = jid:from_binary(escalus_client:full_jid(Client)),
+    F = fun(M) -> M =:= #{logins => 1, count => 1, jid => JID} end,
+    instrument_helper:assert(sm_session, #{host_type => host_type()}, F).
+
+assert_no_sm_login_event(UserSpec) ->
+    LUser = jid:nodeprep(proplists:get_value(username, UserSpec)),
+    LoginEvents = instrument_helper:lookup(sm_session, #{host_type => host_type()}),
+    F = fun(#{jid := JID}) -> jid:luser(JID) =:= LUser end,
+    ?assertEqual([], instrument_helper:filter(F, LoginEvents)).
+
+assert_sm_logout_event(Client) ->
+    JID = jid:from_binary(escalus_client:full_jid(Client)),
+    F = fun(M) -> M =:= #{logouts => 1, count => -1, jid => JID} end,
+    instrument_helper:assert(sm_session, #{host_type => host_type()}, F).
+
+assert_c2s_auth_failed(UserSpec) ->
+    Server = proplists:get_value(server, UserSpec),
+    UserName = proplists:get_value(username, UserSpec),
+    F = fun(M) -> M =:= #{count => 1, server => Server, username => UserName} end,
+    instrument_helper:assert(c2s_auth_failed, #{host_type => host_type()}, F).
