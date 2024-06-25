1dfd59cc3d4e37a632752784bd2c1966df23e92b
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2024-06-25 09:24:46 +0200

    Add 'drop_stanza' utility with instrumentation

--- src/mongoose_router.erl
+++ src/mongoose_router.erl
@@ -2,10 +2,15 @@
 
 -define(TABLE, ?MODULE).
 
--export([start/0, routing_modules_list/0, default_routing_modules/0]).
+-export([start/0, stop/0, routing_modules_list/0, default_routing_modules/0]).
 
 -export([get_all_domains/0, lookup_route/1, is_registered_route/1,
          register_route/2, unregister_route/1]).
+
+-include("mongoose.hrl").
+
+%% Instrumentation
+-export([drop_stanza/1]).
 
 -spec get_all_domains() -> [jid:lserver()].
 get_all_domains() ->
@@ -44,7 +49,11 @@ is_registered_route(LDomain) ->
 %% start/stop
 start() ->
     ets:new(?TABLE, [named_table, public, set, {read_concurrency, true}]),
-    mongoose_metrics:ensure_metric(global, routingErrors, spiral).
+    mongoose_metrics:ensure_metric(global, routingErrors, spiral),
+    mongoose_instrument:set_up(instrumentation()).
+
+stop() ->
+    mongoose_instrument:tear_down(instrumentation()).
 
 default_routing_modules() ->
     List = [mongoose_router_global,
@@ -57,3 +66,23 @@ default_routing_modules() ->
 
 routing_modules_list() ->
     mongoose_config:get_opt(routing_modules).
+
+%% Instrumentation
+
+-spec drop_stanza(mongoose_acc:t()) -> ok.
+drop_stanza(Acc) ->
+    HostType = mongoose_acc:host_type(Acc),
+    From = mongoose_acc:from_jid(Acc),
+    To = mongoose_acc:to_jid(Acc),
+    Stanza = mongoose_acc:element(Acc),
+    mongoose_instrument:execute(router_stanza_dropped, #{host_type => HostType},
+                                #{count => 1, from_jid => From, to_jid => To, stanza => Stanza}).
+
+-spec instrumentation() -> [mongoose_instrument:spec()].
+instrumentation() ->
+    lists:flatmap(fun instrumentation/1, ?ALL_HOST_TYPES).
+
+-spec instrumentation(mongooseim:host_type()) -> [mongoose_instrument:spec()].
+instrumentation(HostType) ->
+    [{router_stanza_dropped, #{host_type => HostType},
+      #{metrics => #{count => spiral}}}].
