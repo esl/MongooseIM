fcfcf85e628722a7dbddc95822f324b3fc84e236
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2024-07-23 16:35:39 +0200

    Make carbon_cache_server easier to use
    - Add API: start, stop, subscribe, get_metric
    - Unsubscription is automatic when the subscriber goes down

--- test/carbon_cache_server.erl
+++ test/carbon_cache_server.erl
@@ -1,8 +1,41 @@
 -module(carbon_cache_server).
 
--export([start/0]).
+%% API for use in tests
+-export([start/1, stop/1, subscribe/1, get_metric/1]).
+
+%% Internal API
 -export([server/2]).
--export([wait_for_accepting/0]).
+
+-include_lib("common_test/include/ct.hrl").
+
+%% API
+
+start(Config) ->
+    {Port, Socket} = start(),
+    PortServer = wait_for_accepting(),
+    gen_tcp:controlling_process(Socket, PortServer),
+    [{carbon_port, Port}, {carbon_server, PortServer}, {carbon_socket, Socket} | Config].
+
+stop(Config) ->
+    CarbonServer = ?config(carbon_server, Config),
+    erlang:exit(CarbonServer, kill),
+    CarbonSocket = ?config(carbon_socket, Config),
+    gen_tcp:close(CarbonSocket).
+
+subscribe(Config) ->
+    CarbonServer = ?config(carbon_server, Config),
+    CarbonServer ! {subscribe, self()}.
+
+get_metric(Metric) ->
+    receive
+        {packet, Metric, Value, TS} ->
+            ct:log("Received metric ~p with value ~p and timestamp ~p", [Metric, Value, TS]),
+            {Value, TS}
+    after 0 ->
+            no_metric
+    end.
+
+%% Internal functions
 
 start() ->
     case gen_tcp:listen(0, [{active, false}, {packet, line}]) of
@@ -20,20 +53,27 @@ server(LS, Parent) ->
 
 server(LS) ->
     case gen_tcp:accept(LS) of
-        {ok,S} ->
-            loop(S),
+        {ok, S} ->
+            loop(S, []),
             server(LS);
         Other ->
-            ct:log("accept returned ~w - goodbye!~n",[Other])
+            ct:log("accept returned ~w - goodbye!~n", [Other])
     end.
 
-loop(S) ->
+loop(S, Pids) ->
     inet:setopts(S, [{active, once}]),
     receive
+        {subscribe, Pid} ->
+            monitor(process, Pid),
+            loop(S, [Pid | Pids]);
+        {'DOWN', _Ref, process, Pid, _Reason} ->
+            loop(S, Pids -- [Pid]);
         {tcp, S, Data} ->
             ct:log("Carbon cache server received packet: ~p", [Data]),
-            exometer:update([carbon, packets], 1),
-            loop(S);
+            [Metric, ValueStr, TimeStamp] = string:tokens(Data, " \n"),
+            Msg = {packet, Metric, list_to_integer(ValueStr), list_to_integer(TimeStamp)},
+            [Pid ! Msg || Pid <- Pids],
+            loop(S, Pids);
         {tcp_closed, S} ->
             ct:log("Socket ~w closed [~w]~n", [S, self()])
     end.
