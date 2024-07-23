aef513d36eb9c1dd2fa2c9b78da819e2294c5abf
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2024-07-23 15:58:21 +0200

    Remove reporters from mongoose_metrics
    They are handled by mongoose_instrument now

--- src/metrics/mongoose_metrics.erl
+++ src/metrics/mongoose_metrics.erl
@@ -19,7 +19,6 @@
 
 %% API
 -export([init/0,
-         init_mongooseim_metrics/0,
          update/3,
          ensure_metric/3,
          get_metric_value/1,
@@ -30,15 +29,12 @@
          get_global_metric_names/0,
          get_aggregated_values/1,
          remove_host_type_metrics/1,
-         remove_all_metrics/0,
-         get_report_interval/0
+         remove_all_metrics/0
         ]).
 
--ignore_xref([remove_host_type_metrics/1, get_report_interval/0,
-              sample_metric/1, get_metric_value/1]).
+-ignore_xref([remove_host_type_metrics/1, sample_metric/1, get_metric_value/1]).
 
 -define(PREFIXES, mongoose_metrics_prefixes).
--define(DEFAULT_REPORT_INTERVAL, 60000). %%60s
 
 -type metric_name() :: atom() | list(atom() | binary()).
 -type short_metric_type() :: spiral | histogram | counter | gauge.
@@ -51,18 +47,6 @@
 -spec init() -> ok.
 init() ->
     prepare_prefixes().
-
--spec init_mongooseim_metrics() -> ok.
-init_mongooseim_metrics() ->
-    init_subscriptions().
-
-init_subscriptions() ->
-    Reporters = exometer_report:list_reporters(),
-    lists:foreach(
-        fun({Name, _ReporterPid}) ->
-                Interval = get_report_interval(),
-                subscribe_to_all(Name, Interval)
-        end, Reporters).
 
 -spec update(HostType :: mongooseim:host_type_or_global(), Name :: term() | list(),
              Change :: term()) -> any().
@@ -151,20 +135,10 @@ pick_prefix_by_all_metrics_are_global(HostType) ->
         false -> get_host_type_prefix(HostType)
     end.
 
-pick_by_all_metrics_are_global(WhenGlobal, WhenNot) ->
-    case all_metrics_are_global() of
-        true -> WhenGlobal;
-        false -> WhenNot
-    end.
-
 -spec name_by_all_metrics_are_global(HostType :: mongooseim:host_type_or_global(),
                                      Name :: list()) -> FinalName :: list().
 name_by_all_metrics_are_global(HostType, Name) ->
     [pick_prefix_by_all_metrics_are_global(HostType) | Name].
-
-get_report_interval() ->
-    application:get_env(exometer_core, mongooseim_report_interval,
-                        ?DEFAULT_REPORT_INTERVAL).
 
 remove_metric({Name, _, _}) ->
     exometer_admin:delete_entry(Name).
@@ -188,32 +162,3 @@ do_create_metric(PrefixedMetric, ExometerType, ExometerOpts) ->
         ok -> ok;
         {'EXIT', Error} -> {error, Error}
     end.
-
-start_metrics_subscriptions(Reporter, MetricPrefix, Interval) ->
-    [subscribe_metric(Reporter, Metric, Interval)
-     || Metric <- exometer:find_entries(MetricPrefix)].
-
-subscribe_metric(Reporter, {Name, counter, _}, Interval) ->
-    subscribe_verbose(Reporter, Name, [value], Interval);
-subscribe_metric(Reporter, {Name, histogram, _}, Interval) ->
-    subscribe_verbose(Reporter, Name, [min, mean, max, median, 95, 99, 999], Interval);
-subscribe_metric(Reporter, {Name, _, _}, Interval) ->
-    subscribe_verbose(Reporter, Name, default, Interval).
-
-subscribe_verbose(Reporter, Name, Types, Interval) ->
-    case exometer_report:subscribe(Reporter, Name, Types, Interval) of
-        ok -> ok;
-        Other ->
-            ?LOG_ERROR(#{what => metrics_subscribe_failed,
-                         reporter => Reporter, metric_name => Name,
-                         reason => Other}),
-            Other
-    end.
-
-subscribe_to_all(Reporter, Interval) ->
-    HostTypePrefixes = pick_by_all_metrics_are_global([], ?ALL_HOST_TYPES),
-    lists:foreach(
-      fun(Prefix) ->
-              UnspacedPrefix = get_host_type_prefix(Prefix),
-              start_metrics_subscriptions(Reporter, [UnspacedPrefix], Interval)
-      end, [global | HostTypePrefixes]).
