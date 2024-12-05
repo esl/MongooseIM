139d1d7d98b5f9c3c183ff0cc5d8ddb5ff9be65e
Nelson Vides <nelson.vides@erlang-solutions.com>
2024-12-05 12:28:37 +0100

    Migrate components listener to a ranch supervision

--- src/component/mongoose_component_listener.erl
+++ src/component/mongoose_component_listener.erl
@@ -1,6 +1,10 @@
 -module(mongoose_component_listener).
 
 -behaviour(mongoose_listener).
+-export([start_listener/1, instrumentation/1]).
+
+-behaviour(ranch_protocol).
+-export([start_link/3]).
 
 -type conflict_behaviour() :: disconnect | kick_old.
 
@@ -14,12 +18,7 @@
 
 -export_type([conflict_behaviour/0, options/0]).
 
--export([start_listener/1, instrumentation/1]).
-
--spec start_listener(options()) -> ok.
-start_listener(Opts) ->
-    mongoose_tcp_listener:start_listener(Opts).
-
+%% mongoose_listener
 -spec instrumentation(options()) -> [mongoose_instrument:spec()].
 instrumentation(#{connection_type := component} = _Opts) ->
     [{component_xmpp_element_size_in, #{}, #{metrics => #{byte_size => histogram}}},
@@ -28,3 +27,14 @@ instrumentation(#{connection_type := component} = _Opts) ->
      {component_tls_data_in, #{}, #{metrics => #{byte_size => spiral}}},
      {component_tcp_data_out, #{}, #{metrics => #{byte_size => spiral}}},
      {component_tls_data_out, #{}, #{metrics => #{byte_size => spiral}}}].
+
+-spec start_listener(options()) -> ok.
+start_listener(#{module := Module} = Opts) when is_atom(Module) ->
+    TransportOpts = mongoose_listener:prepare_socket_opts(Opts),
+    ListenerId = mongoose_listener_config:listener_id(Opts),
+    ChildSpec = ranch:child_spec(ListenerId, ranch_tcp, TransportOpts, Module, Opts),
+    mongoose_listener_sup:start_child(ChildSpec).
+
+%% ranch_protocol
+start_link(Ref, Transport, Opts) ->
+    ejabberd_service:start_link({Transport, Ref, Opts}, Opts).
