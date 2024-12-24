c90c65b3fff351e3a413a2485ee3cd7feff0da8d
jacekwegr <jacek.wegrzynowski@erlang-solutions.com>
2024-12-24 13:56:15 +0100

    Publishing packages on GitHub

--- tools/pkg/publish.sh
+++ tools/pkg/publish.sh
@@ -18,8 +18,20 @@ aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 aws configure set default.region $AWS_DEFAULT_REGION
 
+if which gh > /dev/null ; then
+    echo "GitHub CLI ready"
+else
+    echo "Installing GitHub CLI"
+    sudo apt update
+    sudo apt install gh -y
+fi
+
+echo "$GH_RELEASE_TOKEN" | gh auth login --with-token
+
 if [ -n "$CIRCLE_TAG" ]; then
     aws s3 cp "${PACKAGE_NAME}" "s3://mim-packages/tags/${CIRCLE_TAG}/${PACKAGE_NAME}" --acl public-read --quiet
+
+    gh release upload "$CIRCLE_TAG" "${PACKAGE_NAME}" --repo "$GITHUB_REPOSITORY"
 else
     aws s3 cp "${PACKAGE_NAME}" "s3://mim-packages/branches/${CIRCLE_BRANCH}/${prefix}/${PACKAGE_NAME}" --acl public-read --quiet
 fi
