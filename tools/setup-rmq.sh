6f19fe07889817ac60b1179205f9a50465fd68c0
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2025-11-27 18:01:32 +0100

    Update RabbitMQ version to 4.2.1 and enable HTTP API
    The API is needed to get detailed information e.g. about exchanges.

--- tools/setup-rmq.sh
+++ tools/setup-rmq.sh
@@ -4,11 +4,13 @@ source tools/common-vars.sh
 source tools/db-versions.sh
 NAME=$(db_name rmq)
 RMQ_PORT=5672
+RMQ_HTTP_PORT=15672
 
 $DOCKER rm -v -f $NAME || echo "Skip removing the previous container"
 $DOCKER run -d \
        --name $NAME \
        -p $RMQ_PORT:5672 \
+       -p $RMQ_HTTP_PORT:15672 \
        rabbitmq:$RMQ_VERSION
 
 tools/wait_for_service.sh $NAME $RMQ_PORT
