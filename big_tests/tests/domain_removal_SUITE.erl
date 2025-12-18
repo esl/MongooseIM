13563c8f3ec35f5c3df8c0fdd40ea5b98817e259
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2025-12-18 10:05:52 +0100

    Remove mssql, odbc and rdbms_server_type from big tests

--- big_tests/tests/domain_removal_SUITE.erl
+++ big_tests/tests/domain_removal_SUITE.erl
@@ -66,16 +66,8 @@ end_per_suite(Config) ->
 %%%===================================================================
 %%% Group specific setup/teardown
 %%%===================================================================
-init_per_group(Group, Config)
-  when Group =:= mam_removal_incremental; Group =:= inbox_removal_incremental ->
-    case rpc(mim(), mongoose_rdbms, db_engine, [host_type()]) of
-        odbc -> {skip, mssql_not_supported_for_incremental_removals};
-        _ -> do_init_per_group(Group, Config)
-    end;
-init_per_group(Group, Config) ->
-    do_init_per_group(Group, Config).
 
-do_init_per_group(auth_removal = Group, Config) ->
+init_per_group(auth_removal = Group, Config) ->
     case is_internal_or_rdbms() of
         true ->
             HostTypes = domain_helper:host_types(),
@@ -86,7 +78,7 @@ do_init_per_group(auth_removal = Group, Config) ->
         false ->
             {skip, require_internal_or_rdbms}
     end;
-do_init_per_group(Group, Config) ->
+init_per_group(Group, Config) ->
     case mongoose_helper:is_rdbms_enabled(host_type()) of
         true ->
             HostTypes = domain_helper:host_types(),
