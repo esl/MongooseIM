ad4a2df0f460c4a174badf0362b915c194424033
tomasz-wojcikowski <tomasz.wojcikowski@erlang-solutions.com>
2026-01-29 14:23:24 +0100

    Add RDBMS backend tests for domain removal in mod_event_pusher_push

--- test/mod_event_pusher_push_SUITE.erl
+++ test/mod_event_pusher_push_SUITE.erl
@@ -24,15 +24,20 @@
 
 all() ->
     [{group, mnesia_backend},
+     {group, rdbms_backend},
      supported_features_tests].
 
 groups() ->
-    [{mnesia_backend, [sequence], mnesia_tests()}].
+    [{mnesia_backend, [sequence], mnesia_tests()},
+     {rdbms_backend, [sequence], rdbms_tests()}].
 
 mnesia_tests() ->
     [mnesia_remove_domain_deletes_subscriptions,
      mnesia_remove_domain_keeps_other_domain_subscriptions,
      mnesia_remove_domain_empty_table].
+
+rdbms_tests() ->
+    [rdbms_remove_domain_calls_execute_successfully].
 
 %%--------------------------------------------------------------------
 %% Init/End per suite
@@ -58,9 +63,14 @@ end_per_suite(Config) ->
 
 init_per_group(mnesia_backend, Config) ->
     [{backend, mnesia} | Config];
+init_per_group(rdbms_backend, Config) ->
+    meck:new(mongoose_rdbms, [passthrough]),
+    [{backend, rdbms} | Config];
 init_per_group(_, Config) ->
     Config.
 
+end_per_group(rdbms_backend, _Config) ->
+    meck:unload(mongoose_rdbms);
 end_per_group(_, Config) ->
     Config.
 
@@ -73,6 +83,8 @@ init_per_testcase(TestCase, Config) when TestCase =:= mnesia_remove_domain_delet
                                          TestCase =:= mnesia_remove_domain_empty_table ->
     ok = mod_event_pusher_push_mnesia:init(?HOST_TYPE, #{}),
     Config;
+init_per_testcase(rdbms_remove_domain_calls_execute_successfully, Config) ->
+    Config;
 init_per_testcase(_, Config) ->
     Config.
 
@@ -80,6 +92,9 @@ end_per_testcase(TestCase, _Config) when TestCase =:= mnesia_remove_domain_delet
                                          TestCase =:= mnesia_remove_domain_keeps_other_domain_subscriptions;
                                          TestCase =:= mnesia_remove_domain_empty_table ->
     mnesia:clear_table(push_subscription),
+    ok;
+end_per_testcase(rdbms_remove_domain_calls_execute_successfully, _Config) ->
+    meck:reset(mongoose_rdbms),
     ok;
 end_per_testcase(_, _Config) ->
     ok.
@@ -136,6 +151,26 @@ mnesia_remove_domain_empty_table(_Config) ->
     ok = mod_event_pusher_push_mnesia:remove_domain(?HOST_TYPE, ?DOMAIN),
     %% THEN no error occurs
     ok.
+
+%%--------------------------------------------------------------------
+%% Test cases - RDBMS backend
+%%--------------------------------------------------------------------
+
+rdbms_remove_domain_calls_execute_successfully(_Config) ->
+    %% GIVEN expected LIKE pattern for the domain
+    LikePattern = <<"%@", ?DOMAIN/binary>>,
+    meck:expect(mongoose_rdbms, execute_successfully,
+                fun(?HOST_TYPE, event_pusher_push_remove_domain, [ActualPattern]) ->
+                        ?assertEqual(LikePattern, ActualPattern),
+                        ok
+                end),
+
+    %% WHEN remove_domain is called
+    ok = mod_event_pusher_push_rdbms:remove_domain(?HOST_TYPE, ?DOMAIN),
+
+    %% THEN mongoose_rdbms:execute_successfully was called exactly once
+    ?assertEqual(1, meck:num_calls(mongoose_rdbms, execute_successfully,
+                                    [?HOST_TYPE, event_pusher_push_remove_domain, [LikePattern]])).
 
 %%--------------------------------------------------------------------
 %% Test cases - supported_features
