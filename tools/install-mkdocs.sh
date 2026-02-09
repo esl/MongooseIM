63beb79167b9f241ffe0d10a56d9084ff87666bf
Denys Gonchar <denys.gonchar@erlang-solutions.com>
2026-02-09 22:51:08 +0100

    adding docs make target
    The docker image for the docs_build_deploy job has been updated as ARM architecture is supported starting with that version.

---
+++ tools/install-mkdocs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+set -e
+
+## Install spectaql for building GraphQL static docs
+npm install --global cheerio@1.0.0-rc.12 spectaql
+
+## Install mkdocs and plugins
+pip3 install mkdocs
+pip3 install mkdocs-material
+pip3 install mkdocs-include-markdown-plugin
