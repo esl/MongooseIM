997d8886668d68ea98fb1254754f844446ebc9b6
Piotr Nosek <piotr.nosek@erlang-solutions.com>
2026-02-12 12:20:43 +0100

    MIM-2500 Add extra logging to broadcast manager to debug CI fail

--- src/broadcast/broadcast_manager.erl
+++ src/broadcast/broadcast_manager.erl
@@ -182,7 +182,12 @@ handle_info({{'DOWN', JobId}, _Ref, process, Pid, Reason}, State) ->
 handle_info(_Info, State) ->
     {noreply, State}.
 
-terminate(_Reason, _State) ->
+terminate(normal, _State) ->
+    ok;
+terminate(Reason, State) ->
+    ?LOG_WARNING(#{what => broadcast_manager_terminated_abnormally,
+                   reason => Reason,
+                   state => State}),
     ok.
 
 code_change(_OldVsn, State, _Extra) ->
