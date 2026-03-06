7c074b9be8071095243b4545e7bd427f54f18a35
Denys Gonchar <denys.gonchar@erlang-solutions.com>
2026-03-06 16:27:05 +0100

    adding modules.mod_extdisco.service.secret configuration option

--- src/mod_extdisco.erl
+++ src/mod_extdisco.erl
@@ -22,7 +22,9 @@
 %% gen_mod callbacks.
 -export([start/2, stop/1, supported_features/0, config_spec/0]).
 
--export([process_iq/5]).
+-export([process_iq/5, validate_service_config/1]).
+
+-define(DEFAULT_TTL, 3600). %% in seconds
 
 -ignore_xref([process_iq/5]).
 
@@ -57,28 +59,48 @@ service_config_spec() ->
                                              validate = port},
                        <<"transport">> => #option{type = binary,
                                                   validate = {enum, [<<"udp">>, <<"tcp">>]}},
+                       <<"secret">> => #option{type = binary,
+                                               validate = non_empty},
                        <<"username">> => #option{type = binary,
-                                           validate = non_empty},
+                                                 validate = non_empty},
                        <<"password">> => #option{type = binary,
                                                  validate = non_empty}
                       },
+             process = fun ?MODULE:validate_service_config/1,
              required = [<<"type">>, <<"host">>]}.
+
+-spec validate_service_config(mongoose_config_parser_toml:config_part()) ->
+    mongoose_config_parser_toml:config_part().
+validate_service_config(ConfigPart) when is_map_key(secret, ConfigPart) andalso
+                                         is_map_key(username, ConfigPart) ->
+    error(#{what => invalid_service_config,
+            text => <<"Cannot specify both 'secret' and 'username' in service config">>});
+validate_service_config(ConfigPart) when is_map_key(secret, ConfigPart) andalso
+                                         is_map_key(password, ConfigPart) ->
+    error(#{what => invalid_service_config,
+            text => <<"Cannot specify both 'secret' and 'password' in service config">>});
+validate_service_config(ConfigPart) when is_map_key(secret, ConfigPart)  ->
+    %% make TTL configurable if needed.
+    maps:merge(#{ttl => ?DEFAULT_TTL}, ConfigPart);
+validate_service_config(ConfigPart) ->
+    ConfigPart.
 
 -spec process_iq(mongoose_acc:t(), jid:jid(), jid:jid(), jlib:iq(), map()) ->
     {mongoose_acc:t(), jlib:iq()}.
-process_iq(Acc, _From, _To, #iq{type = get, sub_el = SubEl} = IQ, _Extra) ->
+process_iq(Acc, From, _To, #iq{type = get, sub_el = SubEl} = IQ, _Extra) ->
     HostType = mongoose_acc:host_type(Acc),
+    User = jid:to_bare_binary(From),
     {ResponseType, Response} = case request_type(SubEl) of
         all_services ->
             RequestedServices = get_external_services(HostType),
-            {result, create_iq_response(RequestedServices)};
+            {result, create_iq_response(RequestedServices, User)};
         {credentials, {Type, Host}} ->
             Services = get_external_services(HostType, Type),
             RequestedServices = lists:filter(fun(#{host := H}) -> H =:= Host end, Services),
-            {result, create_iq_response_credentials(RequestedServices)};
+            {result, create_iq_response_credentials(RequestedServices, User)};
         {selected_services, Type} ->
             RequestedServices = get_external_services(HostType, Type),
-            {result, create_iq_response_services(RequestedServices, Type)};
+            {result, create_iq_response_services(RequestedServices, User, Type)};
         _ ->
             {error, [mongoose_xmpp_errors:bad_request()]}
     end,
@@ -106,21 +128,21 @@ request_type(#xmlel{name = <<"credentials">>, children = [Children]}) ->
 request_type(_) ->
     {error, bad_request}.
 
-create_iq_response(Services) ->
+create_iq_response(Services, User) ->
     #xmlel{name = <<"services">>,
            attrs = #{<<"xmlns">> => ?NS_EXTDISCO},
-           children = prepare_services_element(Services)}.
+           children = prepare_services_element(Services, User)}.
 
-create_iq_response_services(Services, Type) ->
+create_iq_response_services(Services, User, Type) ->
     #xmlel{name = <<"services">>,
            attrs = #{<<"xmlns">> => ?NS_EXTDISCO,
                      <<"type">> => atom_to_binary(Type, utf8)},
-           children = prepare_services_element(Services)}.
+           children = prepare_services_element(Services, User)}.
 
-create_iq_response_credentials(Services) ->
+create_iq_response_credentials(Services, User) ->
     #xmlel{name = <<"credentials">>,
            attrs = #{<<"xmlns">> => ?NS_EXTDISCO},
-           children = prepare_services_element(Services)}.
+           children = prepare_services_element(Services, User)}.
 
 get_external_services(HostType) ->
     gen_mod:get_module_opt(HostType, ?MODULE, service).
@@ -128,12 +150,72 @@ get_external_services(HostType) ->
 get_external_services(HostType, Type) ->
     [Service || Service = #{type := T} <- get_external_services(HostType), T =:= Type].
 
-prepare_services_element(Services) ->
-    [#xmlel{name = <<"service">>, attrs = make_attrs(Service)} || Service <- Services].
+prepare_services_element(Services, User) ->
+    [#xmlel{name = <<"service">>, attrs = make_attrs(Service, User)} || Service <- Services].
 
-make_attrs(Service) ->
+make_attrs(#{secret := Secret, ttl := TTL} = Service, User) ->
+    ExpirationTime = erlang:system_time(second) + TTL,
+    {TempUsername, TempPassword} = generate_credentials(User, Secret, ExpirationTime),
+    NewService = maps:merge(
+        maps:without([secret, ttl], Service),
+        #{expires => ExpirationTime, username => TempUsername, password => TempPassword}),
+    make_attrs(NewService, User);
+make_attrs(Service, _) ->
     #{atom_to_binary(Key) => format_value(Key, Value) || Key := Value <- Service}.
 
 format_value(port, Port) -> integer_to_binary(Port);
 format_value(type, Type) -> atom_to_binary(Type);
+format_value(expires, UnixSeconds) ->
+    {{Y,M,D},{H,Min,S}} = calendar:system_time_to_universal_time(UnixSeconds, second),
+    Chars = io_lib:format("~4..0B-~2..0B-~2..0BT~2..0B:~2..0B:~2..0BZ", [Y,M,D,H,Min,S]),
+    list_to_binary(lists:flatten(Chars));
 format_value(_, Value) when is_binary(Value) -> Value.
+
+%% see https://datatracker.ietf.org/doc/html/draft-uberti-behave-turn-rest-00#section-2.2
+generate_credentials(Username, Secret, ExpirationTime) ->
+    Timestamp = integer_to_binary(ExpirationTime),
+    %% temporary-username = "timestamp:username"
+    TempUsername = <<Timestamp/binary, ":", Username/binary>>,
+    %% HMAC-SHA1(secret, temporary-username)
+    Hmac = crypto:mac(hmac, sha, Secret, TempUsername),
+    %% Base64 encode
+    TempPassword = base64:encode(Hmac),
+    {TempUsername, TempPassword}.
+
+%%--------------------------------------------------------------------
+%% Tests
+%%--------------------------------------------------------------------
+
+-ifdef(TEST).
+
+-include_lib("eunit/include/eunit.hrl").
+
+generate_credentials_test_() ->
+    %% Reference values are generated using this bash function:
+    %%
+    %%     function get_turn_credentials()
+    %%     {
+    %%       ## $1 - basic username
+    %%       ## $2 - secret
+    %%       ## $3 - timestamp
+    %%       local username="${3}:${1}"
+    %%       local secret="$2"
+    %%       local password="$(echo -n "$username" | openssl dgst -hmac "$secret" -sha1 -binary | openssl base64)"
+    %%       echo -e "username: '${username}'\npassword: '${password}'"
+    %%     }
+    %%
+    %%     get_turn_credentials "dummy_user" "my shared secret" 100
+    %%     get_turn_credentials "dummy_user@example.org" "secret123" 10000
+    %%     get_turn_credentials "example.org" "super#secret" "$(($(date "+%s") + 3600))"
+    %%     get_turn_credentials "example.org" "super#secret" "$(($(date "+%s") * 3))"
+
+    [?_assertEqual(generate_credentials(~"dummy_user", ~"my shared secret", 100),
+                   {~"100:dummy_user", ~"aBGRg1t/TGlc9+gkWObW2Li97Ew="}),
+     ?_assertEqual(generate_credentials(~"dummy_user@example.org", ~"secret123", 10000),
+                   {~"10000:dummy_user@example.org", ~"CKDrp61KWUnSXKusLAMKmL0qcQo="}),
+     ?_assertEqual(generate_credentials(~"example.org", ~"super#secret", 1772853308),
+                   {~"1772853308:example.org", ~"+THIMe4fdg70xzgyfQe1sXwl8ys="}),
+     ?_assertEqual(generate_credentials(~"example.org", ~"super#secret", 5318549598),
+                   {~"5318549598:example.org", ~"D8j9h+xPoWi8AL7Q5AfGywcRy10="})].
+
+-endif.
