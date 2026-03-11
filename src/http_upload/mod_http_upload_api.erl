0b595010a2dcf605e0d1ab5f7f8986bae86af5fd
Denys Gonchar <denys.gonchar@erlang-solutions.com>
2026-03-11 23:19:19 +0100

    removing dead code from mod_http_upload_api
    this validation is done with '@use(modules: ["mod_http_upload"])' directive

--- src/http_upload/mod_http_upload_api.erl
+++ src/http_upload/mod_http_upload_api.erl
@@ -5,13 +5,12 @@
 -spec get_urls(Domain :: jid:lserver(), Filename :: nonempty_binary(), Size :: pos_integer(),
                ContentType :: binary() | null | undefined, Timeout :: pos_integer()) ->
         {ok, #{binary() => term()}}
-        | {size_error | timeout_error | module_not_loaded_error | domain_not_found |
-           file_too_large_error, string()}.
+        | {size_error | timeout_error | domain_not_found | file_too_large_error, string()}.
 get_urls(Domain, Filename, Size, ContentType, Timeout) ->
     ContentType1 = content_type(ContentType),
     case mongoose_domain_api:get_domain_host_type(Domain) of
         {ok, HostType} ->
-            check_module_and_get_urls(HostType, Filename, Size, ContentType1, Timeout);
+            do_get_urls(HostType, Filename, Size, ContentType1, Timeout);
         _ ->
             {domain_not_found, "domain does not exist"}
     end.
@@ -20,19 +19,12 @@ content_type(null) -> undefined;
 content_type(<<>>) -> undefined;
 content_type(Binary) -> Binary.
 
-check_module_and_get_urls(HostType, Filename, Size, ContentType, Timeout) ->
-    case gen_mod:is_loaded(HostType, mod_http_upload) of
-        true ->
-            case mod_http_upload:get_urls(HostType, Filename, Size, ContentType, Timeout) of
-                {PutURL, GetURL, Headers} ->
-                    Headers1 = lists:map(fun({Name, Value}) -> {ok, #{<<"name">> => Name, <<"value">> => Value}} end,
-                                         maps:to_list(Headers)),
-                    {ok, #{<<"putUrl">> => PutURL, <<"getUrl">> => GetURL,
-                           <<"headers">> => Headers1}};
-                file_too_large_error ->
-                    {file_too_large_error,
-                     "Declared file size exceeds the host's maximum file size."}
-            end;
-        false ->
-            {module_not_loaded_error, "mod_http_upload is not loaded for this host"}
+do_get_urls(HostType, Filename, Size, ContentType, Timeout) ->
+    case mod_http_upload:get_urls(HostType, Filename, Size, ContentType, Timeout) of
+        {PutURL, GetURL, Headers} ->
+            Headers1 =
+                [{ok, #{~"name" => Name, ~"value" => Value}} || Name := Value <- Headers],
+            {ok, #{~"putUrl" => PutURL, ~"getUrl" => GetURL, ~"headers" => Headers1}};
+        file_too_large_error ->
+            {file_too_large_error, "Declared file size exceeds the host's maximum file size."}
     end.
