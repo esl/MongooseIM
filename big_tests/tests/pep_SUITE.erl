c5141cdca3858839ebca7de10346e8d1904d0fd2
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2026-04-15 16:25:34 +0200

    Improve tests for the presence access model

--- big_tests/tests/pep_SUITE.erl
+++ big_tests/tests/pep_SUITE.erl
@@ -44,8 +44,9 @@ groups() ->
 
 %% Tests for old and new PEP
 pep_tests() ->
-    [publish_and_notify,
-     create_and_publish_and_notify,
+    [auto_create_and_publish_implicit_sub,
+     create_presence_and_publish_implicit_sub,
+     create_presence_and_publish_explicit_sub,
      create_open_and_publish_explicit_sub,
      create_open_and_publish_implicit_sub,
      create_open_and_publish_both_subs,
@@ -232,14 +233,19 @@ bookmark_element() ->
     #xmlel{name = <<"conference">>,
            attrs = #{<<"xmlns">> => ?NS_PEP_BOOKMARKS}}.
 
-publish_and_notify(Config) ->
-    Config1 = set_caps(Config),
-    escalus:fresh_story_with_config(Config1, [{alice, 1}, {bob, 1}], fun publish_and_notify_story/3).
-
-create_and_publish_and_notify(Config) ->
+auto_create_and_publish_implicit_sub(Config) ->
     Config1 = set_caps(Config),
     escalus:fresh_story_with_config(Config1, [{alice, 1}, {bob, 1}],
-                                    fun create_and_publish_and_notify_story/3).
+                                    fun auto_create_and_publish_implicit_sub_story/3).
+
+create_presence_and_publish_implicit_sub(Config) ->
+    Config1 = set_caps(Config),
+    escalus:fresh_story_with_config(Config1, [{alice, 1}, {bob, 1}],
+                                    fun create_presence_and_publish_implicit_sub_story/3).
+
+create_presence_and_publish_explicit_sub(Config) ->
+    escalus:fresh_story_with_config(Config, [{alice, 1}, {bob, 1}],
+                                    fun create_presence_and_publish_explicit_sub_story/3).
 
 create_open_and_publish_explicit_sub(Config) ->
     escalus:fresh_story_with_config(Config, [{alice, 1}, {bob, 1}],
@@ -260,18 +266,26 @@ create_open_and_publish_no_sub(Config) ->
     escalus:fresh_story_with_config(Config1, [{alice, 1}, {bob, 1}, {mike, 1}],
                                     fun create_open_and_publish_no_sub_story/4).
 
-publish_and_notify_story(Config, Alice, Bob) ->
+auto_create_and_publish_implicit_sub_story(Config, Alice, Bob) ->
     NodeNS = ?config(node_ns, Config),
     make_friends(Bob, Alice, 0),
     pubsub_tools:publish(Alice, <<"item1">>, {pep, NodeNS}, []),
     pubsub_tools:receive_item_notification(
       Bob, <<"item1">>, {escalus_utils:get_short_jid(Alice), NodeNS}, []).
 
-create_and_publish_and_notify_story(Config, Alice, Bob) ->
+create_presence_and_publish_implicit_sub_story(Config, Alice, Bob) ->
     NodeNS = ?config(node_ns, Config),
     PepNode = make_pep_node_info(Alice, NodeNS),
     pubsub_tools:create_node(Alice, PepNode, []),
-    publish_and_notify_story(Config, Alice, Bob).
+    auto_create_and_publish_implicit_sub_story(Config, Alice, Bob).
+
+create_presence_and_publish_explicit_sub_story(_Config, Alice, Bob) ->
+    NodeNS = random_node_ns(),
+    PepNode = make_pep_node_info(Alice, NodeNS),
+    pubsub_tools:create_node(Alice, PepNode, []),
+    pubsub_tools:subscribe(Bob, PepNode, []),
+    pubsub_tools:publish(Alice, <<"item1">>, {pep, NodeNS}, []),
+    [] = escalus:wait_for_stanzas(Bob, 1, 500).
 
 create_open_and_publish_explicit_sub_story(_Config, Alice, Bob) ->
     NodeNS = random_node_ns(),
