930708f70802375fba94c685fb07e0f05849b61c
Stefan Strigler <stefan@strigler.de>
2026-09-22 09:27:40 +0200

    add js deps

---
+++ tools/dl_invites_page_deps.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+
+bootstrap_checksum='3258c873cbcb1e2d81f4374afea2ea6437d9eee9077041073fd81dd579c5ba6b'
+
+check() {
+  echo "$1 $2" | sha256sum -c - || (echo "checksum failed: $2 (does not match $1)"; exit 1)
+}
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <install_dir>"
+    exit 1
+fi
+install_dir="$1"
+
+bootstrap="$(mktemp /tmp/bootstrap.XXXXXXXXX)"
+curl -L -s -o "$bootstrap" https://github.com/twbs/bootstrap/releases/download/v5.3.8/bootstrap-5.3.8-dist.zip
+check "$bootstrap_checksum" "$bootstrap"
+
+rm -rf "$install_dir/bootstrap"
+unzip -q -d "$install_dir" "$bootstrap"
+mv "$install_dir/bootstrap-5.3.8-dist" "$install_dir/bootstrap"
+rm "$bootstrap"
+echo "landing page dependencies for mod_invites installed to $install_dir"
